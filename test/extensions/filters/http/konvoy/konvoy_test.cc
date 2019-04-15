104018131f5c0d7e9f94377229dce7817efa55a1
Yaroslav Skopets <y.skopets@gmail.com>
2019-04-15 12:49:40 +0100

    Test coverage: HttpFilters::Konvoy::Filter

--- test/extensions/filters/http/konvoy/konvoy_test.cc
+++ test/extensions/filters/http/konvoy/konvoy_test.cc
@@ -73,6 +73,56 @@ TEST_F(KonvoyHttpFilterTest, KonvoyServiceIsDown) {
   EXPECT_EQ(0U, config_->stats().rq_cancel_.value());
 }
 
+TEST_F(KonvoyHttpFilterTest, MutateGetRequest) {
+  // given : simple GET request
+  headers_.addCopy(":authority", "example.org");
+  // expect : an attempt to open a new stream to Http Konvoy Service to succeed
+  EXPECT_CALL(*async_client_, start(_, _))
+          .WillOnce(Invoke([this](const Protobuf::MethodDescriptor&, Grpc::AsyncStreamCallbacks&) {
+              return &this->async_stream_;
+          }));
+  // and expect : request headers to be forwarded to Http Konvoy Service
+  ProxyHttpRequestClientMessage client_request_headers_message{};
+  auto* original_authority_header = client_request_headers_message.mutable_request_headers()->mutable_headers()->add_headers();
+  original_authority_header->set_key(":authority");
+  original_authority_header->set_value("example.org");
+  EXPECT_CALL(async_stream_, sendMessage(WhenDynamicCastTo<const ProxyHttpRequestClientMessage&>(ProtoEq(client_request_headers_message)), false));
+  // and expect : pseudo request trailers to be forwarded to Http Konvoy Service
+  ProxyHttpRequestClientMessage client_request_trailers_message{};
+  client_request_trailers_message.mutable_request_trailers();
+  EXPECT_CALL(async_stream_, sendMessage(WhenDynamicCastTo<const ProxyHttpRequestClientMessage&>(ProtoEq(client_request_trailers_message)), true));
+  // when : HTTP request headers received
+  EXPECT_EQ(Http::FilterHeadersStatus::StopIteration, filter_->decodeHeaders(headers_, true));
+  // and then : metrics get updated
+  EXPECT_EQ(1U, config_->stats().rq_active_.value());
+  EXPECT_EQ(1U, config_->stats().rq_total_.value());
+  EXPECT_EQ(0U, config_->stats().rq_error_.value());
+  EXPECT_EQ(0U, config_->stats().rq_cancel_.value());
+
+  // given : Http Konvoy Service mutates request headers
+  auto server_message = std::make_unique<ProxyHttpRequestServerMessage>();
+  auto* modified_authority_header = server_message->mutable_request_headers()->mutable_headers()->add_headers();;
+  modified_authority_header->set_key(":authority");
+  modified_authority_header->set_value("httpbin.org");
+  // when : a message with mutated request headers is received back from Http Konvoy Service
+  EXPECT_NO_THROW(filter_->onReceiveMessage(std::move(server_message)));
+  // then : metrics don't change
+  EXPECT_EQ(1U, config_->stats().rq_active_.value());
+  EXPECT_EQ(1U, config_->stats().rq_total_.value());
+  EXPECT_EQ(0U, config_->stats().rq_error_.value());
+  EXPECT_EQ(0U, config_->stats().rq_cancel_.value());
+
+  // expect : filter chain to proceed
+  EXPECT_CALL(callbacks_, continueDecoding());
+  // when : a stream is closed by Http Konvoy Service
+  EXPECT_NO_THROW(filter_->onRemoteClose(Grpc::Status::GrpcStatus::Ok, "OK"));
+  // then : metrics get updated
+  EXPECT_EQ(0U, config_->stats().rq_active_.value());
+  EXPECT_EQ(1U, config_->stats().rq_total_.value());
+  EXPECT_EQ(0U, config_->stats().rq_error_.value());
+  EXPECT_EQ(0U, config_->stats().rq_cancel_.value());
+}
+
 } // namespace Konvoy
 } // namespace HttpFilters
 } // namespace Extensions
