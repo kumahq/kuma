cd5a7da2a46918a8c05292d7c869ac82214785aa
Yaroslav Skopets <y.skopets@gmail.com>
2019-06-24 22:37:51 +0200

    control-plane: implement ResourceStore abstraction for k8s

---
+++ components/konvoy-control-plane/pkg/plugins/resources/k8s/k8s_suite_test.go
@@ -0,0 +1,81 @@
+/*
+Copyright 2019 Konvoy authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package k8s_test
+
+import (
+	"path/filepath"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	// +kubebuilder:scaffold:imports
+	sample_v1alpha1 "github.com/Kong/konvoy/components/konvoy-control-plane/pkg/plugins/resources/k8s/native/test/api/sample/v1alpha1"
+)
+
+// These tests use Ginkgo (BDD-style Go testing framework). Refer to
+// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
+
+var cfg *rest.Config
+var k8sClient client.Client
+var testEnv *envtest.Environment
+var k8sClientScheme = runtime.NewScheme()
+
+func TestKubernetes(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	RunSpecsWithDefaultAndCustomReporters(t,
+		"Kubernetes Resources Suite",
+		[]Reporter{envtest.NewlineReporter{}})
+}
+
+var _ = BeforeSuite(func(done Done) {
+	logf.SetLogger(zap.LoggerTo(GinkgoWriter, true))
+
+	By("bootstrapping test environment")
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths: []string{filepath.Join("native", "test", "config", "crd", "bases")},
+	}
+
+	cfg, err := testEnv.Start()
+	Expect(err).ToNot(HaveOccurred())
+	Expect(cfg).ToNot(BeNil())
+
+	err = sample_v1alpha1.AddToScheme(k8sClientScheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	// +kubebuilder:scaffold:scheme
+
+	k8sClient, err = client.New(cfg, client.Options{Scheme: k8sClientScheme})
+	Expect(err).ToNot(HaveOccurred())
+	Expect(k8sClient).ToNot(BeNil())
+
+	close(done)
+}, 60)
+
+var _ = AfterSuite(func() {
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).ToNot(HaveOccurred())
+})
---
+++ components/konvoy-control-plane/pkg/plugins/resources/k8s/store.go
@@ -0,0 +1,200 @@
+package k8s
+
+import (
+	"context"
+	"fmt"
+
+	core_model "github.com/Kong/konvoy/components/konvoy-control-plane/pkg/core/resources/model"
+	"github.com/Kong/konvoy/components/konvoy-control-plane/pkg/core/resources/store"
+	k8s_model "github.com/Kong/konvoy/components/konvoy-control-plane/pkg/plugins/resources/k8s/native/pkg/model"
+	sample_k8s "github.com/Kong/konvoy/components/konvoy-control-plane/pkg/plugins/resources/k8s/native/test/api/sample/v1alpha1"
+	sample_core "github.com/Kong/konvoy/components/konvoy-control-plane/pkg/test/resources/apis/sample"
+	util_proto "github.com/Kong/konvoy/components/konvoy-control-plane/pkg/util/proto"
+	"github.com/pkg/errors"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var _ store.ResourceStore = &KubernetesStore{}
+
+type KubernetesStore struct {
+	Client    client.Client
+	Converter Converter
+}
+
+func (s *KubernetesStore) Create(ctx context.Context, r core_model.Resource, fs ...store.CreateOptionsFunc) error {
+	opts := store.NewCreateOptions(fs...)
+	obj, err := s.Converter.ToKubernetesObject(r)
+	if err != nil {
+		return errors.Wrap(err, "failed to convert core model into k8s counterpart")
+	}
+	obj.GetObjectMeta().SetNamespace(opts.Namespace)
+	obj.GetObjectMeta().SetName(opts.Name)
+	if err := s.Client.Create(ctx, obj); err != nil {
+		if apierrs.IsAlreadyExists(err) {
+			return store.ErrorResourceAlreadyExists(r.GetType(), opts.Namespace, opts.Name)
+		}
+		return errors.Wrap(err, "failed to create k8s resource")
+	}
+	err = s.Converter.ToCoreResource(obj, r)
+	if err != nil {
+		return errors.Wrap(err, "failed to convert k8s model into core counterpart")
+	}
+	return nil
+}
+func (s *KubernetesStore) Update(ctx context.Context, r core_model.Resource, fs ...store.UpdateOptionsFunc) error {
+	obj, err := s.Converter.ToKubernetesObject(r)
+	if err != nil {
+		return errors.Wrap(err, "failed to convert core model into k8s counterpart")
+	}
+	if err := s.Client.Update(ctx, obj); err != nil {
+		if apierrs.IsConflict(err) {
+			return store.ErrorResourceConflict(r.GetType(), r.GetMeta().GetNamespace(), r.GetMeta().GetName())
+		}
+		return errors.Wrap(err, "failed to update k8s resource")
+	}
+	err = s.Converter.ToCoreResource(obj, r)
+	if err != nil {
+		return errors.Wrap(err, "failed to convert k8s model into core counterpart")
+	}
+	return nil
+}
+func (s *KubernetesStore) Delete(ctx context.Context, r core_model.Resource, fs ...store.DeleteOptionsFunc) error {
+	opts := store.NewDeleteOptions(fs...)
+	obj, err := s.Converter.ToKubernetesObject(r)
+	if err != nil {
+		return errors.Wrap(err, "failed to convert core model into k8s counterpart")
+	}
+	obj.GetObjectMeta().SetNamespace(opts.Namespace)
+	obj.GetObjectMeta().SetName(opts.Name)
+	if err := s.Client.Delete(ctx, obj); err != nil {
+		if apierrs.IsNotFound(err) {
+			return nil
+		}
+		return errors.Wrap(err, "failed to delete k8s resource")
+	}
+	return nil
+}
+func (s *KubernetesStore) Get(ctx context.Context, r core_model.Resource, fs ...store.GetOptionsFunc) error {
+	opts := store.NewGetOptions(fs...)
+	obj, err := s.Converter.ToKubernetesObject(r)
+	if err != nil {
+		return errors.Wrap(err, "failed to convert core model into k8s counterpart")
+	}
+	if err := s.Client.Get(ctx, client.ObjectKey{Namespace: opts.Namespace, Name: opts.Name}, obj); err != nil {
+		if apierrs.IsNotFound(err) {
+			return store.ErrorResourceNotFound(r.GetType(), opts.Namespace, opts.Name)
+		}
+		return errors.Wrap(err, "failed to get k8s resource")
+	}
+	if err := s.Converter.ToCoreResource(obj, r); err != nil {
+		return errors.Wrap(err, "failed to convert k8s model into core counterpart")
+	}
+	return nil
+}
+func (s *KubernetesStore) List(ctx context.Context, rs core_model.ResourceList, fs ...store.ListOptionsFunc) error {
+	opts := store.NewListOptions(fs...)
+	obj, err := s.Converter.ToKubernetesList(rs)
+	if err != nil {
+		return errors.Wrap(err, "failed to convert core model into k8s counterpart")
+	}
+	if err := s.Client.List(ctx, obj, client.InNamespace(opts.Namespace)); err != nil {
+		return errors.Wrap(err, "failed to list k8s resources")
+	}
+	if err := s.Converter.ToCoreList(obj, rs); err != nil {
+		return errors.Wrap(err, "failed to convert k8s model into core counterpart")
+	}
+	return nil
+}
+
+var _ core_model.ResourceMeta = &KubernetesMetaAdapter{}
+
+type KubernetesMetaAdapter struct {
+	metav1.ObjectMeta
+}
+
+func (m *KubernetesMetaAdapter) GetVersion() string {
+	return m.ObjectMeta.GetResourceVersion()
+}
+
+type KubeFactory interface {
+	NewObject(r core_model.Resource) (k8s_model.KubernetesObject, error)
+	NewList(rl core_model.ResourceList) (k8s_model.KubernetesList, error)
+}
+
+var _ KubeFactory = &SimpleKubeFactory{}
+
+type SimpleKubeFactory struct {
+}
+
+func (f *SimpleKubeFactory) NewObject(r core_model.Resource) (k8s_model.KubernetesObject, error) {
+	switch r.GetType() {
+	case sample_core.TrafficRouteType:
+		return &sample_k8s.TrafficRoute{}, nil
+	default:
+		return nil, fmt.Errorf("unknown type: %q", r.GetType())
+	}
+}
+
+func (f *SimpleKubeFactory) NewList(rl core_model.ResourceList) (k8s_model.KubernetesList, error) {
+	switch rl.GetItemType() {
+	case sample_core.TrafficRouteType:
+		return &sample_k8s.TrafficRouteList{}, nil
+	default:
+		return nil, fmt.Errorf("unknown type: %q", rl.GetItemType())
+	}
+}
+
+type Converter interface {
+	ToKubernetesObject(core_model.Resource) (k8s_model.KubernetesObject, error)
+	ToKubernetesList(core_model.ResourceList) (k8s_model.KubernetesList, error)
+	ToCoreResource(obj k8s_model.KubernetesObject, out core_model.Resource) error
+	ToCoreList(obj k8s_model.KubernetesList, out core_model.ResourceList) error
+}
+
+var _ Converter = &SimpleConverter{}
+
+type SimpleConverter struct {
+	KubeFactory KubeFactory
+}
+
+func (c *SimpleConverter) ToKubernetesObject(r core_model.Resource) (k8s_model.KubernetesObject, error) {
+	obj, err := c.KubeFactory.NewObject(r)
+	if err != nil {
+		return nil, err
+	}
+	spec, err := util_proto.ToMap(r.GetSpec())
+	if err != nil {
+		return nil, err
+	}
+	obj.SetSpec(spec)
+	if r.GetMeta() != nil {
+		if adapter, ok := r.GetMeta().(*KubernetesMetaAdapter); ok {
+			obj.SetObjectMeta(&adapter.ObjectMeta)
+		} else {
+			return nil, fmt.Errorf("meta has unexpected type: %#v", r.GetMeta())
+		}
+	}
+	return obj, nil
+}
+
+func (c *SimpleConverter) ToKubernetesList(rl core_model.ResourceList) (k8s_model.KubernetesList, error) {
+	return c.KubeFactory.NewList(rl)
+}
+
+func (c *SimpleConverter) ToCoreResource(obj k8s_model.KubernetesObject, out core_model.Resource) error {
+	out.SetMeta(&KubernetesMetaAdapter{*obj.GetObjectMeta()})
+	return util_proto.FromMap(obj.GetSpec(), out.GetSpec())
+}
+
+func (c *SimpleConverter) ToCoreList(in k8s_model.KubernetesList, out core_model.ResourceList) error {
+	for _, o := range in.GetItems() {
+		r := out.NewItem()
+		if err := c.ToCoreResource(o, r); err != nil {
+			return err
+		}
+		_ = out.AddItem(r)
+	}
+	return nil
+}
