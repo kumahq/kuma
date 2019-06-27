154a9e489f552c3cc22e97430d8aa287ab566026
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2019-06-27 20:02:12 +0200

    control-plane: run integration tests without dir manipulation

--- components/konvoy-control-plane/test/run-integration-tests.sh
+++ components/konvoy-control-plane/test/run-integration-tests.sh
@@ -16,21 +16,17 @@ export KONVOY_STORE_POSTGRES_USER=konvoy
 export KONVOY_STORE_POSTGRES_PASSWORD=konvoy
 export KONVOY_STORE_POSTGRES_DB_NAME=konvoy
 
-rootDir=`pwd`
-cd `dirname "$0"`
-dockerComposeDir=`pwd`
+dockerCompose="$(dirname "$0")/docker-compose.yaml"
 
-docker-compose up --build --no-start
-docker-compose up -d
-trap "cd ${dockerComposeDir} && docker-compose down" EXIT
+docker-compose -f ${dockerCompose} up --build --no-start
+docker-compose -f ${dockerCompose} up -d
+trap "docker-compose -f ${dockerCompose} down" EXIT
 
 # wait for postgres
 while ! nc -z localhost ${KONVOY_STORE_POSTGRES_PORT}; do sleep 1; done;
 sleep 5;
 
 # run tests
-cd ${rootDir}
 eval $1
 
-cd ${dockerComposeDir}
-docker-compose down
+docker-compose -f ${dockerCompose} down
