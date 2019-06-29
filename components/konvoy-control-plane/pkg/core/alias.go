b96e2d772722917c3ec984addc76e27684dd9603
Yaroslav Skopets <y.skopets@gmail.com>
2019-06-29 13:04:27 +0200

    control-plane: add DiscoverySource API

--- components/konvoy-control-plane/pkg/core/alias.go
+++ components/konvoy-control-plane/pkg/core/alias.go
@@ -1,10 +1,13 @@
 package core
 
 import (
+	kube_types "k8s.io/apimachinery/pkg/types"
 	kube_ctrl "sigs.k8s.io/controller-runtime"
 	kube_log "sigs.k8s.io/controller-runtime/pkg/log"
 	kube_log_zap "sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
+
+type NamespacedName = kube_types.NamespacedName
 
 var (
 	Log       = kube_ctrl.Log
---
+++ components/konvoy-control-plane/pkg/core/discovery/interfaces.go
@@ -0,0 +1,42 @@
+package discovery
+
+import (
+	discovery_proto "github.com/Kong/konvoy/components/konvoy-control-plane/api/discovery/v1alpha1"
+	"github.com/Kong/konvoy/components/konvoy-control-plane/pkg/core"
+)
+
+// DiscoverySource is a source of discovery information, i.e. Services and Workloads.
+type DiscoverySource interface {
+	AddConsumer(DiscoveryConsumer)
+}
+
+// ServiceInfo combines original proto object with auxiliary information.
+type ServiceInfo struct {
+	Service *discovery_proto.Service
+}
+
+// WorkloadInfo combines original proto object with auxiliary information.
+type WorkloadInfo struct {
+	Workload *discovery_proto.Workload
+	Desc     *WorkloadDescription
+}
+
+// WorkloadDescription represents auxiliary information about a Workload.
+type WorkloadDescription struct {
+	Version   string
+	Endpoints []WorkloadEndpoint
+}
+
+type WorkloadEndpoint struct {
+	Address string
+	Port    uint32
+}
+
+// DiscoveryConsumer is a consumer of discovery information, i.e. Services and Workloads.
+type DiscoveryConsumer interface {
+	OnServiceUpdate(*ServiceInfo) error
+	OnServiceDelete(core.NamespacedName) error
+
+	OnWorkloadUpdate(*WorkloadInfo) error
+	OnWorkloadDelete(core.NamespacedName) error
+}
