836100271c4928f9eca6749733888924b0e76e2d
Yaroslav Skopets <y.skopets@gmail.com>
2019-08-09 11:37:06 +0200

    control-plane-api: define Dataplane resource

--- components/konvoy-control-plane/api/internal/util/proto/proto.go
+++ components/konvoy-control-plane/api/internal/util/proto/proto.go
@@ -1,6 +1,8 @@
 package proto
 
 import (
+	"bytes"
+
 	"github.com/ghodss/yaml"
 	"github.com/gogo/protobuf/jsonpb"
 	"github.com/gogo/protobuf/proto"
@@ -13,4 +15,17 @@ func ToYAML(pb proto.Message) ([]byte, error) {
 		return nil, err
 	}
 	return yaml.JSONToYAML([]byte(json))
+}
+
+func FromYAML(content []byte, pb proto.Message) error {
+	json, err := yaml.YAMLToJSON(content)
+	if err != nil {
+		return err
+	}
+	return FromJSON(json, pb)
+}
+
+func FromJSON(content []byte, out proto.Message) error {
+	unmarshaler := &jsonpb.Unmarshaler{AllowUnknownFields: true}
+	return unmarshaler.Unmarshal(bytes.NewReader(content), out)
 }
