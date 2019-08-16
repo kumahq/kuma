82cbfc81f6ed3e12d54443cfddfbe5a66a529d1a
Yaroslav Skopets <y.skopets@gmail.com>
2019-08-16 15:03:39 +0200

    control-plane: regenerate dataplane config periodically

--- components/konvoy-control-plane/pkg/xds/server/server.go
+++ components/konvoy-control-plane/pkg/xds/server/server.go
@@ -2,11 +2,8 @@ package server
 
 import (
 	"github.com/Kong/konvoy/components/konvoy-control-plane/pkg/core"
-	core_discovery "github.com/Kong/konvoy/components/konvoy-control-plane/pkg/core/discovery"
-	core_store "github.com/Kong/konvoy/components/konvoy-control-plane/pkg/core/resources/store"
 	core_runtime "github.com/Kong/konvoy/components/konvoy-control-plane/pkg/core/runtime"
-	core_xds "github.com/Kong/konvoy/components/konvoy-control-plane/pkg/core/xds"
-	"github.com/Kong/konvoy/components/konvoy-control-plane/pkg/xds/template"
+	util_xds "github.com/Kong/konvoy/components/konvoy-control-plane/pkg/util/xds"
 	envoy_xds "github.com/envoyproxy/go-control-plane/pkg/server"
 )
 
@@ -15,12 +12,17 @@ var (
 )
 
 func SetupServer(rt core_runtime.Runtime) error {
-	r := newReconciler(rt.XDS(), rt.ResourceStore())
+	reconciler := DefaultReconciler(rt)
 	for _, ds := range rt.DiscoverySources() {
-		ds.AddConsumer(r)
+		ds.AddConsumer(reconciler)
 	}
 
-	srv := envoy_xds.NewServer(rt.XDS().Cache(), DefaultDataplaneStatusTracker(rt))
+	callbacks := util_xds.CallbacksChain{
+		DefaultDataplaneSyncTracker(rt, reconciler),
+		DefaultDataplaneStatusTracker(rt),
+	}
+
+	srv := envoy_xds.NewServer(rt.XDS().Cache(), callbacks)
 	return core_runtime.Add(
 		rt,
 		// xDS gRPC API
@@ -29,18 +31,4 @@ func SetupServer(rt core_runtime.Runtime) error {
 		&httpGateway{srv, rt.Config().XdsServer.HttpPort},
 		// diagnostics server
 		&diagnosticsServer{rt.Config().XdsServer.DiagnosticsPort})
-}
-
-func newReconciler(xds core_xds.XdsContext, rs core_store.ResourceStore) core_discovery.DiscoveryConsumer {
-	return &core_discovery.DiscoverySink{
-		DataplaneConsumer: &reconciler{
-			&templateSnapshotGenerator{
-				ProxyTemplateResolver: &simpleProxyTemplateResolver{
-					ResourceStore:        rs,
-					DefaultProxyTemplate: template.DefaultProxyTemplate,
-				},
-			},
-			&simpleSnapshotCacher{xds.Hasher(), xds.Cache()},
-		},
-	}
 }
