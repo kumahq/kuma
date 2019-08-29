8bf8bcbb5f72357837ecf7af67b6da18f772adc5
Yaroslav Skopets <y.skopets@gmail.com>
2019-08-29 18:28:59 +0200

    control-plane: sds: add debug logging to SDS server

--- components/konvoy-control-plane/pkg/sds/server/server.go
+++ components/konvoy-control-plane/pkg/sds/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"github.com/Kong/konvoy/components/konvoy-control-plane/pkg/core"
 	core_runtime "github.com/Kong/konvoy/components/konvoy-control-plane/pkg/core/runtime"
+	util_xds "github.com/Kong/konvoy/components/konvoy-control-plane/pkg/util/xds"
 )
 
 var (
@@ -10,6 +11,9 @@ var (
 )
 
 func SetupServer(rt core_runtime.Runtime) error {
-	srv := NewServer(DefaultSecretDiscoveryHandler(), nil, sdsServerLog)
+	callbacks := util_xds.CallbacksChain{
+		util_xds.LoggingCallbacks{Log: sdsServerLog},
+	}
+	srv := NewServer(DefaultSecretDiscoveryHandler(), callbacks, sdsServerLog)
 	return core_runtime.Add(rt, &grpcServer{srv, rt.Config().SdsServer.GrpcPort})
 }
