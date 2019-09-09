631d9b85834b1d2f18fbe877783b04783764196c
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2019-09-09 23:11:00 +0200

    control-plane: access logs

--- pkg/xds/server/components.go
+++ pkg/xds/server/components.go
@@ -64,7 +64,9 @@ func DefaultDataplaneSyncTracker(rt core_runtime.Runtime, reconciler SnapshotRec
 				envoyCtx := xds_context.Context{
 					ControlPlane: envoyCpCtx,
 					Mesh: xds_context.MeshContext{
-						TlsEnabled: meshList.Items[0].Spec.Mtls.Enabled,
+						TlsEnabled:     meshList.Items[0].Spec.GetMtls().Enabled,
+						LoggingEnabled: meshList.Items[0].Spec.Logging.GetAccessLogs().GetEnabled(),
+						LoggingPath:    meshList.Items[0].Spec.Logging.GetAccessLogs().GetFilePath(),
 					},
 				}
 
