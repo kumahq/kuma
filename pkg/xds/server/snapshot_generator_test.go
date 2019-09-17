5ebccf75e34289acc191552a8ceb016951f0eb8c
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2019-09-17 09:59:24 +0200

    feat(kuma-cp) cleanup test

--- pkg/xds/server/snapshot_generator_test.go
+++ pkg/xds/server/snapshot_generator_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/Kong/kuma/pkg/core/resources/manager"
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
 	mesh_proto "github.com/Kong/kuma/api/mesh/v1alpha1"
@@ -32,63 +31,56 @@ var _ = Describe("Reconcile", func() {
 			},
 		}
 
-		type testCase struct {
-			dataplaneFile   string
-			envoyConfigFile string
-		}
+		It("Generate Snapshot per Envoy Node", func() {
+			// given
+			ctx := xds_context.Context{
+				ControlPlane: &xds_context.ControlPlaneContext{
+					SdsLocation: "kuma-system:5677",
+					SdsTlsCert:  []byte("12345"),
+				},
+				Mesh: xds_context.MeshContext{
+					TlsEnabled: true,
+				},
+			}
 
-		DescribeTable("Generate Snapshot per Envoy Node",
-			func(given testCase) {
-				// given
-				ctx := xds_context.Context{
-					ControlPlane: &xds_context.ControlPlaneContext{
-						SdsLocation: "kuma-system:5677",
-						SdsTlsCert:  []byte("12345"),
-					},
-					Mesh: xds_context.MeshContext{
-						TlsEnabled: true,
-					},
-				}
+			dataplane := mesh_proto.Dataplane{}
+			dpBytes, err := ioutil.ReadFile(filepath.Join("testdata", "dataplane.input.yaml"))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(util_proto.FromYAML(dpBytes, &dataplane)).To(Succeed())
 
-				dataplane := mesh_proto.Dataplane{}
-				dpBytes, err := ioutil.ReadFile(filepath.Join("testdata", given.dataplaneFile))
-				Expect(err).ToNot(HaveOccurred())
-				Expect(util_proto.FromYAML(dpBytes, &dataplane)).To(Succeed())
-
-				proxy := &model.Proxy{
-					Id: model.ProxyId{Name: "side-car", Namespace: "default"},
-					Dataplane: &mesh_core.DataplaneResource{
-						Meta: &test_model.ResourceMeta{
-							Version: "1",
-						},
-						Spec: dataplane,
+			proxy := &model.Proxy{
+				Id: model.ProxyId{Name: "side-car", Namespace: "default"},
+				Dataplane: &mesh_core.DataplaneResource{
+					Meta: &test_model.ResourceMeta{
+						Version: "1",
 					},
-					TrafficPermissions: permissions.MatchedPermissions{
-						"192.168.0.1:80:8080": &mesh_core.TrafficPermissionResourceList{
-							Items: []*mesh_core.TrafficPermissionResource{
-								&mesh_core.TrafficPermissionResource{
-									Meta: &test_model.ResourceMeta{
-										Name:      "tp-1",
-										Mesh:      "default",
-										Namespace: "default",
-									},
-									Spec: mesh_proto.TrafficPermission{
-										Rules: []*mesh_proto.TrafficPermission_Rule{
-											{
-												Sources: []*mesh_proto.TrafficPermission_Rule_Selector{
-													{
-														Match: map[string]string{
-															"service": "web1",
-															"version": "1.0",
-														},
+					Spec: dataplane,
+				},
+				TrafficPermissions: permissions.MatchedPermissions{
+					"192.168.0.1:80:8080": &mesh_core.TrafficPermissionResourceList{
+						Items: []*mesh_core.TrafficPermissionResource{
+							&mesh_core.TrafficPermissionResource{
+								Meta: &test_model.ResourceMeta{
+									Name:      "tp-1",
+									Mesh:      "default",
+									Namespace: "default",
+								},
+								Spec: mesh_proto.TrafficPermission{
+									Rules: []*mesh_proto.TrafficPermission_Rule{
+										{
+											Sources: []*mesh_proto.TrafficPermission_Rule_Selector{
+												{
+													Match: map[string]string{
+														"service": "web1",
+														"version": "1.0",
 													},
 												},
-												Destinations: []*mesh_proto.TrafficPermission_Rule_Selector{
-													{
-														Match: map[string]string{
-															"service": "backend1",
-															"env":     "dev",
-														},
+											},
+											Destinations: []*mesh_proto.TrafficPermission_Rule_Selector{
+												{
+													Match: map[string]string{
+														"service": "backend1",
+														"env":     "dev",
 													},
 												},
 											},
@@ -98,55 +90,23 @@ var _ = Describe("Reconcile", func() {
 							},
 						},
 					},
-				}
+				},
+			}
 
-				// when
-				s, err := gen.GenerateSnapshot(ctx, proxy)
+			// when
+			s, err := gen.GenerateSnapshot(ctx, proxy)
 
-				// then
-				Expect(err).ToNot(HaveOccurred())
+			// then
+			Expect(err).ToNot(HaveOccurred())
 
-				// then
-				resp := util_cache.ToDeltaDiscoveryResponse(s)
-				actual, err := util_proto.ToYAML(resp)
-				Expect(err).ToNot(HaveOccurred())
+			// then
+			resp := util_cache.ToDeltaDiscoveryResponse(s)
+			actual, err := util_proto.ToYAML(resp)
+			Expect(err).ToNot(HaveOccurred())
 
-				expected, err := ioutil.ReadFile(filepath.Join("testdata", given.envoyConfigFile))
-				Expect(err).ToNot(HaveOccurred())
-				Expect(actual).To(MatchYAML(expected))
-			},
-			//Entry("01. transparent_proxying=false, ip_addresses=0, ports=0", testCase{
-			//	dataplaneFile:   "1-dataplane.input.yaml",
-			//	envoyConfigFile: "1-envoy-config.golden.yaml",
-			//}),
-			//Entry("02. transparent_proxying=true, ip_addresses=0, ports=0", testCase{
-			//	dataplaneFile:   "2-dataplane.input.yaml",
-			//	envoyConfigFile: "2-envoy-config.golden.yaml",
-			//}),
-			//Entry("03. transparent_proxying=false, ip_addresses=1, ports=1", testCase{
-			//	dataplaneFile:   "3-dataplane.input.yaml",
-			//	envoyConfigFile: "3-envoy-config.golden.yaml",
-			//}),
-			//Entry("04. transparent_proxying=true, ip_addresses=1, ports=1", testCase{
-			//	dataplaneFile:   "4-dataplane.input.yaml",
-			//	envoyConfigFile: "4-envoy-config.golden.yaml",
-			//}),
-			//Entry("05. transparent_proxying=false, ip_addresses=1, ports=2", testCase{
-			//	dataplaneFile:   "5-dataplane.input.yaml",
-			//	envoyConfigFile: "5-envoy-config.golden.yaml",
-			//}),
-			//Entry("06. transparent_proxying=true, ip_addresses=1, ports=2", testCase{
-			//	dataplaneFile:   "6-dataplane.input.yaml",
-			//	envoyConfigFile: "6-envoy-config.golden.yaml",
-			//}),
-			//Entry("07. transparent_proxying=false, ip_addresses=2, ports=2", testCase{
-			//	dataplaneFile:   "7-dataplane.input.yaml",
-			//	envoyConfigFile: "7-envoy-config.golden.yaml",
-			//}),
-			Entry("08. 	transparent_proxying=true, ip_addresses=2, ports=2", testCase{
-				dataplaneFile:   "8-dataplane.input.yaml",
-				envoyConfigFile: "8-envoy-config.golden.yaml",
-			}),
-		)
+			expected, err := ioutil.ReadFile(filepath.Join("testdata", "envoy-config.golden.yaml"))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(actual).To(MatchYAML(expected))
+		})
 	})
 })
