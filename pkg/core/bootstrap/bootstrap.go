5c0ef9fb47701cf7cdbf2750958dc4ee58a5e8d0
Yaroslav Skopets <y.skopets@gmail.com>
2019-09-27 13:32:14 +0200

    kuma-cp: refactor default Mesh creation

--- pkg/core/bootstrap/bootstrap.go
+++ pkg/core/bootstrap/bootstrap.go
@@ -1,19 +1,15 @@
 package bootstrap
 
 import (
-	"context"
-
 	kuma_cp "github.com/Kong/kuma/pkg/config/app/kuma-cp"
 	config_core "github.com/Kong/kuma/pkg/config/core"
 	"github.com/Kong/kuma/pkg/config/core/resources/store"
-	"github.com/Kong/kuma/pkg/core"
 	builtin_ca "github.com/Kong/kuma/pkg/core/ca/builtin"
 	mesh_managers "github.com/Kong/kuma/pkg/core/managers/apis/mesh"
 	core_plugins "github.com/Kong/kuma/pkg/core/plugins"
 	"github.com/Kong/kuma/pkg/core/resources/apis/mesh"
 	core_manager "github.com/Kong/kuma/pkg/core/resources/manager"
 	core_model "github.com/Kong/kuma/pkg/core/resources/model"
-	core_store "github.com/Kong/kuma/pkg/core/resources/store"
 	core_runtime "github.com/Kong/kuma/pkg/core/runtime"
 	runtime_reports "github.com/Kong/kuma/pkg/core/runtime/reports"
 	secret_cipher "github.com/Kong/kuma/pkg/core/secrets/cipher"
@@ -54,70 +50,51 @@ func buildRuntime(cfg kuma_cp.Config) (core_runtime.Runtime, error) {
 	return rt, nil
 }
 
-func createDefaultMesh(runtime core_runtime.Runtime) error {
-	resManager := runtime.ResourceManager()
-	defaultMesh := mesh.MeshResource{}
-	cfg := runtime.Config()
-
-	namespace := core_model.DefaultNamespace
-	if runtime.Config().Environment == config_core.KubernetesEnvironment {
-		namespace = runtime.Config().Store.Kubernetes.SystemNamespace
-	}
-
-	key := core_model.ResourceKey{Namespace: namespace, Mesh: core_model.DefaultMesh, Name: core_model.DefaultMesh}
-
-	if err := resManager.Get(context.Background(), &defaultMesh, core_store.GetBy(key)); err != nil {
-		if core_store.IsResourceNotFound(err) {
-			meshProto, err := cfg.Defaults.MeshProto()
-			if err != nil {
-				return err
-			}
-			defaultMesh.Spec = meshProto
-			core.Log.Info("Creating default mesh from the settings", "mesh", defaultMesh.Spec)
-
-			if err := resManager.Create(context.Background(), &defaultMesh, core_store.CreateBy(key)); err != nil {
-				return errors.Wrapf(err, "Failed to create `default` Mesh resource in a given resource store")
-			}
-		} else {
-			return err
-		}
-	}
-
-	return nil
-}
-
 func Bootstrap(cfg kuma_cp.Config) (core_runtime.Runtime, error) {
 	runtime, err := buildRuntime(cfg)
 	if err != nil {
 		return nil, err
 	}
 
-	if err = onStartup(runtime, cfg); err != nil {
+	if err = onStartup(runtime); err != nil {
 		return nil, err
 	}
 
 	return runtime, nil
 }
 
-func onStartup(runtime core_runtime.Runtime, cfg kuma_cp.Config) error {
-	err := runtime.Add(core_runtime.ComponentFunc(func(stop <-chan struct{}) error {
-		if err := createDefaultMesh(runtime); err != nil {
+func onStartup(runtime core_runtime.Runtime) error {
+	if err := scheduleCreateDefaultMesh(runtime); err != nil {
+		return err
+	}
+	return scheduleStartReporter(runtime)
+}
+
+func scheduleCreateDefaultMesh(runtime core_runtime.Runtime) error {
+	ns := core_model.DefaultNamespace
+	switch env := runtime.Config().Environment; env {
+	case config_core.KubernetesEnvironment:
+		ns = runtime.Config().Store.Kubernetes.SystemNamespace
+	case config_core.UniversalEnvironment:
+	default:
+		return errors.Errorf("unknown environment type %s", env)
+	}
+	// schedule a one-off create default Mesh operation
+	return runtime.Add(core_runtime.ComponentFunc(func(stop <-chan struct{}) error {
+		if err := mesh_managers.CreateDefaultMesh(runtime.ResourceManager(), runtime.Config().Defaults.MeshProto(), ns); err != nil {
 			return err
 		}
 		<-stop // it has to block, otherwise the k8s component manager stops all other components
 		return nil
 	}))
-	if err != nil {
-		return err
-	}
+}
 
-	err = runtime.Add(core_runtime.ComponentFunc(func(stop <-chan struct{}) error {
-		runtime_reports.Init(runtime, cfg)
+func scheduleStartReporter(runtime core_runtime.Runtime) error {
+	return runtime.Add(core_runtime.ComponentFunc(func(stop <-chan struct{}) error {
+		runtime_reports.Init(runtime, runtime.Config())
 		<-stop
 		return nil
 	}))
-
-	return err
 }
 
 func initializeBootstrap(cfg kuma_cp.Config, builder *core_runtime.Builder) error {
