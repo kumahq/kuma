bba841eac6e5b0cdf81b1c5c274d091eb360ff84
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2019-11-29 12:31:23 +0100

    feat(kuma-cp) provided manager

---
+++ pkg/core/ca/provided/manager.go
@@ -0,0 +1,196 @@
+package provided
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/Kong/kuma/pkg/tls"
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+
+	builtin_issuer "github.com/Kong/kuma/pkg/core/ca/builtin/issuer"
+	core_system "github.com/Kong/kuma/pkg/core/resources/apis/system"
+	core_model "github.com/Kong/kuma/pkg/core/resources/model"
+	core_store "github.com/Kong/kuma/pkg/core/resources/store"
+	secret_manager "github.com/Kong/kuma/pkg/core/secrets/manager"
+)
+
+type CaRootCert struct {
+	Id   string `json:"id"`
+	Cert []byte `json:"cert"`
+}
+
+type CaRoot struct {
+	CaRootCert
+	Key  []byte `json:"key"`
+}
+
+type ProvidedCa struct {
+	Roots []CaRoot `json:"roots"`
+}
+
+type ProvidedCaManager interface {
+	AddCaRoot(ctx context.Context, mesh string, root tls.KeyPair) error
+	DeleteCaRoot(ctx context.Context, mesh string, id string) error
+
+	DeleteCa(ctx context.Context, mesh string) error
+
+	GetRootCerts(ctx context.Context, mesh string) ([]CaRootCert, error)
+	GenerateWorkloadCert(ctx context.Context, mesh string, workload string) (*tls.KeyPair, error)
+}
+
+type providedCaManager struct {
+	secretManager secret_manager.SecretManager
+}
+
+var _ ProvidedCaManager = &providedCaManager{}
+
+func NewCaManager(secretManager secret_manager.SecretManager) ProvidedCaManager {
+	return &providedCaManager{secretManager}
+}
+
+func (p *providedCaManager) AddCaRoot(ctx context.Context, mesh string, root tls.KeyPair) error {
+	providedCaSecret := &core_system.SecretResource{}
+	if err := p.secretManager.Get(ctx, providedCaSecret, core_store.GetBy(providedCaSecretKey(mesh))); err != nil {
+		if core_store.IsResourceNotFound(err) {
+			if err := p.secretManager.Create(ctx, providedCaSecret, core_store.CreateBy(providedCaSecretKey(mesh))); err != nil {
+				return errors.Wrapf(err, "could not create CA for mesh %q", mesh)
+			}
+		} else {
+			return errors.Wrapf(err, "failed to load CA for mesh %q", mesh)
+		}
+	}
+
+	providedCa := ProvidedCa{}
+	if len(providedCaSecret.Spec.Value) > 0 {
+		if err := json.Unmarshal(providedCaSecret.Spec.Value, &providedCa); err != nil {
+			return errors.Wrapf(err, "failed to deserialize a Root CA cert for Mesh %q", mesh)
+		}
+	}
+
+	if len(providedCa.Roots) > 0 {
+		return errors.New("cannot add more than 1 CA root to provided CA")
+	}
+
+	caRoot := CaRoot{
+		Key: root.KeyPEM,
+		CaRootCert: CaRootCert{
+			Id:   uuid.New().String(),
+			Cert: root.CertPEM,
+		},
+	}
+	providedCa.Roots = append(providedCa.Roots, caRoot)
+
+	caBytes, err := json.Marshal(providedCa)
+	if err != nil {
+		return errors.Wrap(err, "failed to marshal CA")
+	}
+	providedCaSecret.Spec.Value = caBytes
+	if err := p.secretManager.Update(ctx, providedCaSecret); err != nil {
+		return errors.Wrapf(err, "failed to update CA for mesh %q", mesh)
+	}
+	return nil
+}
+
+func (p *providedCaManager) DeleteCaRoot(ctx context.Context, mesh string, id string) error {
+	meshCa, err := p.getMeshCa(ctx, mesh)
+	if err != nil {
+		return errors.Wrapf(err, "failed to load CA key pair for Mesh %q", mesh)
+	}
+
+	var retainedCaRoots []CaRoot
+	for _, root := range meshCa.Roots {
+		if root.Id != id {
+			retainedCaRoots = append(retainedCaRoots, root)
+		}
+	}
+
+	if len(retainedCaRoots) == len(meshCa.Roots) {
+		return errors.Errorf("could not find CA Root of id %q for mesh %q", id, mesh)
+	}
+
+	meshCa.Roots = retainedCaRoots
+	newBytes, err := json.Marshal(meshCa)
+	if err != nil {
+		return err
+	}
+
+	providedCaSecret := &core_system.SecretResource{}
+	if err := p.secretManager.Get(ctx, providedCaSecret, core_store.GetBy(providedCaSecretKey(mesh))); err != nil {
+		return errors.Wrapf(err, "failed to load CA for mesh %q", mesh)
+	}
+	// todo(jakubdyszkiewicz) should we delete CA when there are 0 certs?
+	providedCaSecret.Spec.Value = newBytes
+	if err := p.secretManager.Update(ctx, providedCaSecret); err != nil {
+		return errors.Wrapf(err, "failed to update CA for mesh %q", mesh)
+	}
+	return nil
+}
+
+func (p *providedCaManager) DeleteCa(ctx context.Context, mesh string) error {
+	secretKey := providedCaSecretKey(mesh)
+	builtinCaSecret := &core_system.SecretResource{}
+	if err := p.secretManager.Delete(ctx, builtinCaSecret, core_store.DeleteBy(secretKey)); err != nil {
+		if core_store.IsResourceNotFound(err) {
+			return nil
+		}
+		return errors.Wrapf(err, "failed to delete Builtin CA for Mesh %q", mesh)
+	}
+	return nil
+}
+
+func (p *providedCaManager) GetRootCerts(ctx context.Context, mesh string) ([]CaRootCert, error) {
+	meshCa, err := p.getMeshCa(ctx, mesh)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to load CA key pair for Mesh %q", mesh)
+	}
+	caRootCerts := make([]CaRootCert, len(meshCa.Roots))
+	for i, root := range meshCa.Roots {
+		caRootCerts[i] = CaRootCert{
+			Id: root.Id,
+			Cert: root.Cert,
+		}
+	}
+	return caRootCerts, nil
+}
+
+func (p *providedCaManager) GenerateWorkloadCert(ctx context.Context, mesh string, workload string) (*tls.KeyPair, error) {
+	meshCa, err := p.getMeshCa(ctx, mesh)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to load CA key pair for Mesh %q", mesh)
+	}
+	if len(meshCa.Roots) < 1 {
+		return nil, errors.Wrapf(err, "CA for Mesh %q has no key pair", mesh)
+	}
+	active := meshCa.Roots[0]
+	signer := tls.KeyPair{CertPEM: active.Cert, KeyPEM: active.Key}
+	keyPair, err := builtin_issuer.NewWorkloadCert(signer, mesh, workload)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to generate a Workload Identity cert for workload %q in Mesh %q", workload, mesh)
+	}
+	return keyPair, nil
+}
+
+func (p *providedCaManager) getMeshCa(ctx context.Context, mesh string) (*ProvidedCa, error) {
+	secretKey := providedCaSecretKey(mesh)
+	providedCaSecret := &core_system.SecretResource{}
+	if err := p.secretManager.Get(ctx, providedCaSecret, core_store.GetBy(secretKey)); err != nil {
+		return nil, err
+	}
+	providedCa := ProvidedCa{}
+	if err := json.Unmarshal(providedCaSecret.Spec.Value, &providedCa); err != nil {
+		return nil, errors.Wrapf(err, "failed to deserialize a Root CA cert for Mesh %q", mesh)
+	}
+	return &providedCa, nil
+}
+
+func providedCaSecretKey(mesh string) core_model.ResourceKey {
+	return core_model.ResourceKey{
+		Mesh: mesh,
+		Name: providedCaSecretName(mesh),
+	}
+}
+
+func providedCaSecretName(mesh string) string {
+	return fmt.Sprintf("providedca.%s", mesh)
+}
---
+++ pkg/core/ca/provided/manager_test.go
@@ -0,0 +1,192 @@
+package provided_test
+
+import (
+	"context"
+	"github.com/Kong/kuma/pkg/core/ca/provided"
+	"github.com/Kong/kuma/pkg/core/secrets/cipher"
+	"github.com/Kong/kuma/pkg/core/secrets/manager"
+	"github.com/Kong/kuma/pkg/core/secrets/store"
+	"github.com/Kong/kuma/pkg/plugins/resources/memory"
+	"github.com/Kong/kuma/pkg/tls"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("CA Provided Manager", func() {
+
+	var caManager provided.ProvidedCaManager
+	const meshName = "demo"
+
+	BeforeEach(func() {
+		caManager = provided.NewCaManager(manager.NewSecretManager(store.NewSecretStore(memory.NewStore()), cipher.None()))
+	})
+
+	Describe("AddCaRoot", func() {
+		It("should create CA when adding new CA Root to it", func() {
+			// when
+			_, err := caManager.GetRootCerts(context.Background(), meshName)
+
+			// then
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(HavePrefix(`failed to load CA key pair for Mesh "demo"`))
+
+			// when
+			pair := tls.KeyPair{
+				CertPEM: []byte("CERT"),
+				KeyPEM:  []byte("KEY"),
+			}
+			err = caManager.AddCaRoot(context.Background(), meshName, pair)
+
+			// then
+			Expect(err).ToNot(HaveOccurred())
+
+			// and
+			rootCerts, err := caManager.GetRootCerts(context.Background(), meshName)
+
+			// then
+			Expect(err).ToNot(HaveOccurred())
+			Expect(rootCerts).To(HaveLen(1))
+			Expect(rootCerts[0].Id).ToNot(BeEmpty())
+			Expect(rootCerts[0].Cert).To(Equal([]byte("CERT")))
+		})
+
+		It("should not allow to add another CA Root to existing CA", func() {
+			// setup CA with CA Root
+			caRoot := tls.KeyPair{
+				CertPEM: []byte("CERT"),
+				KeyPEM:  []byte("KEY"),
+			}
+			err := caManager.AddCaRoot(context.Background(), meshName, caRoot)
+			Expect(err).ToNot(HaveOccurred())
+
+			// given
+			newRoot := tls.KeyPair{
+				CertPEM: []byte("CERT2"),
+				KeyPEM:  []byte("KEY2"),
+			}
+
+			// when
+			err = caManager.AddCaRoot(context.Background(), meshName, newRoot)
+
+			// then
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError("cannot add more than 1 CA root to provided CA"))
+		})
+	})
+
+	Describe("DeleteCaRoot", func() {
+		BeforeEach(func() {
+			// setup CA with CA Root
+			caRoot := tls.KeyPair{
+				CertPEM: []byte("CERT"),
+				KeyPEM:  []byte("KEY"),
+			}
+			err := caManager.AddCaRoot(context.Background(), meshName, caRoot)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("should delete CA root", func() {
+			// when
+			caRootCerts, err := caManager.GetRootCerts(context.Background(), meshName)
+
+			// then
+			Expect(err).ToNot(HaveOccurred())
+			Expect(caRootCerts).To(HaveLen(1))
+
+			// when
+			err = caManager.DeleteCaRoot(context.Background(), meshName, caRootCerts[0].Id)
+
+			// then
+			Expect(err).ToNot(HaveOccurred())
+
+			// when
+			caRootCerts, err = caManager.GetRootCerts(context.Background(), meshName)
+
+			// then
+			Expect(err).ToNot(HaveOccurred())
+			Expect(caRootCerts).To(HaveLen(0))
+		})
+
+		It("should throw an error for invalid mesh", func() {
+			// when
+			err := caManager.DeleteCaRoot(context.Background(), "unknown-mesh", "id-1")
+
+			// then
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError(`failed to load CA key pair for Mesh "unknown-mesh": Resource not found: type="Secret" name="providedca.unknown-mesh" mesh="unknown-mesh"`))
+		})
+
+		It("should throw an error for unknown CA root", func() {
+			// when
+			err := caManager.DeleteCaRoot(context.Background(), meshName, "unknown-id")
+
+			// then
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError(`could not find CA Root of id "unknown-id" for mesh "demo"`))
+		})
+	})
+
+	Describe("DeleteCa", func() {
+		It("should delete CA", func() {
+			// setup CA with CA Root
+			caRoot := tls.KeyPair{
+				CertPEM: []byte("CERT"),
+				KeyPEM:  []byte("KEY"),
+			}
+			err := caManager.AddCaRoot(context.Background(), meshName, caRoot)
+			Expect(err).ToNot(HaveOccurred())
+
+			// when
+			err = caManager.DeleteCa(context.Background(), meshName)
+
+			// then
+			Expect(err).ToNot(HaveOccurred())
+
+			// when
+			_, err = caManager.GetRootCerts(context.Background(), meshName)
+
+			// then
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError(`failed to load CA key pair for Mesh "demo": Resource not found: type="Secret" name="providedca.demo" mesh="demo"`))
+		})
+
+		It("should not throw an error when deleting non existing CA", func() {
+			// when
+			err := caManager.DeleteCa(context.Background(), "unknown-mesh")
+
+			// then
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
+
+	Describe("GenerateWorkloadCert", func() {
+		BeforeEach(func() {
+			// setup CA with CA Root
+			pair, err := tls.NewSelfSignedCert("kuma", tls.ServerCertType)
+			Expect(err).ToNot(HaveOccurred())
+			err = caManager.AddCaRoot(context.Background(), meshName, pair)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("should generate workload cert", func() {
+			// when
+			pair, err := caManager.GenerateWorkloadCert(context.Background(), meshName, "backend")
+
+			// then
+			Expect(err).ToNot(HaveOccurred())
+
+			// and
+			Expect(pair.CertPEM).ToNot(HaveLen(0))
+			Expect(pair.KeyPEM).ToNot(HaveLen(0))
+		})
+
+		It("should throw an error for mesh without CA", func() {
+			// when
+			_, err := caManager.GenerateWorkloadCert(context.Background(), "mesh-without-ca", "backend")
+
+			// then
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError(`failed to load CA key pair for Mesh "mesh-without-ca": Resource not found: type="Secret" name="providedca.mesh-without-ca" mesh="mesh-without-ca"`))
+		})
+	})
+})
