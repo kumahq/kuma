9cc0fd951d5d59cfd9ce63946f0ce9b3d14f6755
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2019-12-20 16:12:07 +0100

    chore(*) remove legacy discovery api

--- pkg/core/discovery/interfaces.go
+++ pkg/core/discovery/interfaces.go
@@ -1,7 +1,6 @@
 package discovery
 
 import (
-	discovery_proto "github.com/Kong/kuma/api/discovery/v1alpha1"
 	mesh_core "github.com/Kong/kuma/pkg/core/resources/apis/mesh"
 	"github.com/Kong/kuma/pkg/core/resources/model"
 )
@@ -9,28 +8,6 @@ import (
 // DiscoverySource is a source of discovery information, i.e. Services and Workloads.
 type DiscoverySource interface {
 	AddConsumer(DiscoveryConsumer)
-}
-
-// ServiceInfo combines original proto object with auxiliary information.
-type ServiceInfo struct {
-	Service *discovery_proto.Service
-}
-
-// WorkloadInfo combines original proto object with auxiliary information.
-type WorkloadInfo struct {
-	Workload *discovery_proto.Workload
-	Desc     *WorkloadDescription
-}
-
-// WorkloadDescription represents auxiliary information about a Workload.
-type WorkloadDescription struct {
-	Version   string
-	Endpoints []WorkloadEndpoint
-}
-
-type WorkloadEndpoint struct {
-	Address string
-	Port    uint32
 }
 
 // DiscoveryConsumer is a consumer of discovery information, i.e. Services and Workloads.
