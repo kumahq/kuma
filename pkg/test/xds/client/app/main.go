0ec477dba11a67d4cc26542f00135eda3a0bfa01
Yaroslav Skopets <y.skopets@gmail.com>
2020-01-16 23:41:37 +0100

    test: create a test client for Kuma xDS server to simulate a fleet of Envoys

---
+++ pkg/test/xds/client/app/main.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"go.uber.org/multierr"
+
+	"github.com/ghodss/yaml"
+
+	envoy_cache "github.com/envoyproxy/go-control-plane/pkg/cache"
+
+	"github.com/Kong/kuma/pkg/core"
+	kuma_log "github.com/Kong/kuma/pkg/log"
+
+	xds_client "github.com/Kong/kuma/pkg/test/xds/client"
+)
+
+func newRootCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "kuma-xds-client",
+		Short: "Kuma xDS client",
+		Long:  `Kuma xDS client.`,
+		PersistentPreRun: func(_ *cobra.Command, _ []string) {
+			core.SetLogger(core.NewLogger(kuma_log.DebugLevel))
+		},
+	}
+	// sub-commands
+	cmd.AddCommand(newRunCmd())
+	return cmd
+}
+
+func newRunCmd() *cobra.Command {
+	log := core.Log.WithName("kuma-xds-client").WithName("run")
+	args := struct {
+		xdsServerAddress string
+		configFile       string
+	}{
+		xdsServerAddress: "grpc://localhost:5678",
+		configFile:       "",
+	}
+	cmd := &cobra.Command{
+		Use:   "run",
+		Short: "Start xDS client(s) that simulate Envoy",
+		Long:  `Start xDS client(s) that simulate Envoy.`,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			configBytes, err := ioutil.ReadFile(args.configFile)
+			if err != nil {
+				return errors.Wrapf(err, "failed to read config file from %q", args.configFile)
+			}
+
+			type Node struct {
+				// ID of the Envoy node.
+				ID string `json:"id,omitempty"`
+			}
+			type Config struct {
+				// List of Envoy nodes to simulate.
+				Nodes []Node `json:"nodes,omitempty"`
+			}
+
+			config := Config{}
+			err = yaml.Unmarshal(configBytes, &config)
+			if err != nil {
+				return errors.Wrap(err, "failed to unmarshal config")
+			}
+
+			log.Info("going to start xDS clients (Envoy simulators)", "total", len(config.Nodes))
+
+			errCh := make(chan error, 1)
+
+			for i, node := range config.Nodes {
+				nodeLog := log.WithName("envoy-simulator").WithValues("idx", i, "ID", node.ID)
+				nodeLog.Info("creating an xDS client ...")
+
+				go func(i int, node Node) {
+					errCh <- func() (errs error) {
+						client, err := xds_client.New(args.xdsServerAddress)
+						if err != nil {
+							return errors.Wrap(err, "failed to connect to xDS server")
+						}
+						defer func() {
+							nodeLog.Info("closing a connection ...")
+							if err := client.Close(); err != nil {
+								errs = multierr.Append(errs, errors.Wrapf(err, "failed to close a connection"))
+							}
+						}()
+
+						nodeLog.Info("opening an xDS stream ...")
+						stream, err := client.StartStream()
+						if err != nil {
+							return errors.Wrap(err, "failed to start an xDS stream")
+						}
+						defer func() {
+							nodeLog.Info("closing an xDS stream ...")
+							if err := stream.Close(); err != nil {
+								errs = multierr.Append(errs, errors.Wrapf(err, "failed to close an xDS stream"))
+							}
+						}()
+
+						nodeLog.Info("requesting Listeners")
+						e := stream.Request(node.ID, envoy_cache.ListenerType)
+						if e != nil {
+							return errors.Wrapf(e, "failed to request %q", envoy_cache.ListenerType)
+						}
+
+						nodeLog.Info("requesting Clusters")
+						e = stream.Request(node.ID, envoy_cache.ClusterType)
+						if e != nil {
+							return errors.Wrapf(e, "failed to request %q", envoy_cache.ClusterType)
+						}
+
+						nodeLog.Info("requesting Endpoints")
+						e = stream.Request(node.ID, envoy_cache.EndpointType)
+						if e != nil {
+							return errors.Wrapf(e, "failed to request %q", envoy_cache.EndpointType)
+						}
+
+						for {
+							nodeLog.Info("waiting for a discovery response ...")
+							resp, err := stream.WaitForResources()
+							if err != nil {
+								return errors.Wrap(err, "failed to receive a discovery response")
+							}
+							nodeLog.Info("received xDS resources", "type", resp.TypeUrl, "version", resp.VersionInfo, "nonce", resp.Nonce, "resources", len(resp.Resources))
+
+							if err := stream.ACK(resp.TypeUrl); err != nil {
+								return errors.Wrap(err, "failed to ACK a discovery response")
+							}
+							nodeLog.Info("ACKed discovery response", "type", resp.TypeUrl, "version", resp.VersionInfo, "nonce", resp.Nonce)
+						}
+					}()
+				}(i, node)
+			}
+
+			err = <-errCh
+
+			return errors.Wrap(err, "one of xDS clients (Envoy simulators) terminated with an error")
+		},
+	}
+	// flags
+	cmd.PersistentFlags().StringVar(&args.xdsServerAddress, "xds-server-address", args.xdsServerAddress, "address of xDS server")
+	cmd.PersistentFlags().StringVar(&args.configFile, "config-file", args.configFile, "path to a config file")
+	_ = cmd.MarkFlagRequired("config-file")
+	return cmd
+}
+
+func main() {
+	if err := newRootCmd().Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
