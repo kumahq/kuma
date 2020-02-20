cd4c73809a6b998c06f82d6d40ac15316ce63baf
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2020-02-20 09:26:45 +0100

    refactor to UpdateHTTPConnectionManager

--- pkg/xds/envoy/listeners/tracing_configurer.go
+++ pkg/xds/envoy/listeners/tracing_configurer.go
@@ -1,15 +1,14 @@
 package listeners
 
 import (
-	"github.com/Kong/kuma/api/mesh/v1alpha1"
+	mesh_proto "github.com/Kong/kuma/api/mesh/v1alpha1"
+
 	envoy_listener "github.com/envoyproxy/go-control-plane/envoy/api/v2/listener"
 	envoy_hcm "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/http_connection_manager/v2"
 	envoy_type "github.com/envoyproxy/go-control-plane/envoy/type"
-	envoy_wellknown "github.com/envoyproxy/go-control-plane/pkg/wellknown"
-	"github.com/golang/protobuf/proto"
 )
 
-func Tracing(backend *v1alpha1.TracingBackend) FilterChainBuilderOpt {
+func Tracing(backend *mesh_proto.TracingBackend) FilterChainBuilderOpt {
 	return FilterChainBuilderOptFunc(func(config *FilterChainBuilderConfig) {
 		config.Add(&TracingConfigurer{
 			backend: backend,
@@ -18,7 +17,7 @@ func Tracing(backend *v1alpha1.TracingBackend) FilterChainBuilderOpt {
 }
 
 type TracingConfigurer struct {
-	backend *v1alpha1.TracingBackend
+	backend *mesh_proto.TracingBackend
 }
 
 func (c *TracingConfigurer) Configure(filterChain *envoy_listener.FilterChain) error {
@@ -26,12 +25,7 @@ func (c *TracingConfigurer) Configure(filterChain *envoy_listener.FilterChain) e
 		return nil
 	}
 
-	return UpdateFilterConfig(filterChain, envoy_wellknown.HTTPConnectionManager, func(filterConfig proto.Message) error {
-		hcm, ok := filterConfig.(*envoy_hcm.HttpConnectionManager)
-		if !ok {
-			return NewUnexpectedFilterConfigTypeError(filterConfig, &envoy_hcm.HttpConnectionManager{})
-		}
-
+	return UpdateHTTPConnectionManager(filterChain, func(hcm *envoy_hcm.HttpConnectionManager) error {
 		hcm.Tracing = &envoy_hcm.HttpConnectionManager_Tracing{}
 		if c.backend.Sampling != 0.0 {
 			hcm.Tracing.OverallSampling = &envoy_type.Percent{
