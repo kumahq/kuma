7317306b35d7784bd951792f535c8c5dc3a01c97
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2020-02-20 09:26:45 +0100

    refactor to UpdateHTTPConnectionManager

--- pkg/xds/envoy/listeners/http_outbound_route_configurer.go
+++ pkg/xds/envoy/listeners/http_outbound_route_configurer.go
@@ -1,12 +1,9 @@
 package listeners
 
 import (
-	"github.com/golang/protobuf/proto"
-
 	envoy_core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 	envoy_listener "github.com/envoyproxy/go-control-plane/envoy/api/v2/listener"
 	envoy_hcm "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/http_connection_manager/v2"
-	envoy_wellknown "github.com/envoyproxy/go-control-plane/pkg/wellknown"
 )
 
 func HttpOutboundRoute(routeName string) FilterChainBuilderOpt {
@@ -22,12 +19,7 @@ type HttpOutboundRouteConfigurer struct {
 }
 
 func (c *HttpOutboundRouteConfigurer) Configure(filterChain *envoy_listener.FilterChain) error {
-	return UpdateFilterConfig(filterChain, envoy_wellknown.HTTPConnectionManager, func(filterConfig proto.Message) error {
-		hcm, ok := filterConfig.(*envoy_hcm.HttpConnectionManager)
-		if !ok {
-			return NewUnexpectedFilterConfigTypeError(filterConfig, &envoy_hcm.HttpConnectionManager{})
-		}
-
+	return UpdateHTTPConnectionManager(filterChain, func(hcm *envoy_hcm.HttpConnectionManager) error {
 		hcm.RouteSpecifier = &envoy_hcm.HttpConnectionManager_Rds{
 			Rds: &envoy_hcm.Rds{
 				ConfigSource: &envoy_core.ConfigSource{
