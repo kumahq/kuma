aeb30fac76dcfad6985b757820bd2b38b1aefc10
Yaroslav Skopets <y.skopets@gmail.com>
2020-02-26 17:20:03 +0100

    kuma-injector: add support for `kuma.io/sidecar-injection: disabled` annotation on Pods to let users selectively opt out of side-car injection (#607)
    * kuma-injector: add support for `kuma.io/sidecar-injection: disabled` annotation on Pods to let users selectively opt out of side-car injection
    
    * docs: update CHANGELOG

--- app/kuma-injector/pkg/injector/metadata/annotations.go
+++ app/kuma-injector/pkg/injector/metadata/annotations.go
@@ -2,10 +2,18 @@ package metadata
 
 // Annotations that can be used by the end users.
 const (
-	// KumaMeshAnnotation defines an annotation that can be put on Pods
-	// in order to associate them with a particular Mesh.
-	// Annotation value must be a name of a Mesh resource.
+	// KumaMeshAnnotation defines a Pod annotation that
+	// associates a given Pod with a particular Mesh.
+	// Annotation value must be the name of a Mesh resource.
 	KumaMeshAnnotation = "kuma.io/mesh"
+
+	// KumaSidecarInjectionAnnotation defines a Pod annotation that
+	// gives users a chance to opt out of side-car injection
+	// into a given Pod by setting its value to KumaSidecarInjectionDisabled.
+	KumaSidecarInjectionAnnotation = "kuma.io/sidecar-injection"
+	// KumaSidecarInjectionDisabled defines a value of KumaSidecarInjectionAnnotation
+	// that will prevent Kuma from injecting a side-car into that Pod.
+	KumaSidecarInjectionDisabled = "disabled"
 )
 
 // Annotations that are being automatically set by the Kuma Sidecar Injector.
