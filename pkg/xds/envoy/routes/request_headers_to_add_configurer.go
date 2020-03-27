9248c03808a386b6f6ae16ea987b4219ef85dddd
lobkovilya <ilya.lobkov@konghq.com>
2020-03-27 20:47:14 +0700

    feat(kuma-cp) make configurer more specific for tags

--- pkg/xds/envoy/routes/request_headers_to_add_configurer.go
+++ pkg/xds/envoy/routes/request_headers_to_add_configurer.go
@@ -1,27 +1,28 @@
 package routes
 
 import (
+	"github.com/Kong/kuma/api/mesh/v1alpha1"
 	envoy_api_v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 	envoy_core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 )
 
-func RequestHeadersToAdd(headers map[string]string) RouteConfigurationBuilderOpt {
+const TagsHeader = "x-kuma-tags"
+
+func Tags(tags v1alpha1.MultiValueTagSet) RouteConfigurationBuilderOpt {
 	return RouteConfigurationBuilderOptFunc(func(config *RouteConfigurationBuilderConfig) {
-		config.Add(&RequestHeadersToAddConfigurer{
-			headers: headers,
+		config.Add(&TagsConfigurer{
+			tags: tags,
 		})
 	})
 }
 
-type RequestHeadersToAddConfigurer struct {
-	headers map[string]string
+type TagsConfigurer struct {
+	tags v1alpha1.MultiValueTagSet
 }
 
-func (r *RequestHeadersToAddConfigurer) Configure(rc *envoy_api_v2.RouteConfiguration) error {
-	for key, value := range r.headers {
-		rc.RequestHeadersToAdd = append(rc.RequestHeadersToAdd, &envoy_core.HeaderValueOption{
-			Header: &envoy_core.HeaderValue{Key: key, Value: value},
-		})
-	}
+func (r *TagsConfigurer) Configure(rc *envoy_api_v2.RouteConfiguration) error {
+	rc.RequestHeadersToAdd = append(rc.RequestHeadersToAdd, &envoy_core.HeaderValueOption{
+		Header: &envoy_core.HeaderValue{Key: TagsHeader, Value: r.tags.String()},
+	})
 	return nil
 }
