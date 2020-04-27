62ec66a10d097bc017179b90d18445269782d791
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2020-04-27 10:40:40 +0200

    chore(*) fix time in tests

--- pkg/core/alias.go
+++ pkg/core/alias.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"time"
+
 	kube_uuid "k8s.io/apimachinery/pkg/util/uuid"
 	kube_log "sigs.k8s.io/controller-runtime/pkg/log"
 	kube_signals "sigs.k8s.io/controller-runtime/pkg/manager/signals"
@@ -12,6 +14,7 @@ var (
 	Log       = kube_log.Log
 	NewLogger = kuma_log.NewLogger
 	SetLogger = kube_log.SetLogger
+	Now       = time.Now
 
 	SetupSignalHandler = kube_signals.SetupSignalHandler
 
