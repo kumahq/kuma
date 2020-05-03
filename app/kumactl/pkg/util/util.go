e217d5d44f0a46210d447ab4e437f89c0110205e
Tharun <rajendrantharun@live.com>
2020-05-03 12:12:02 +0530

    introduced helper function for duration formatting

---
+++ app/kumactl/pkg/util/util.go
@@ -0,0 +1,24 @@
+package util
+
+import (
+	"fmt"
+	"time"
+)
+
+// Duration returns a readable represantation of the provided time
+func Duration(d time.Duration) string {
+	if seconds := int(d.Seconds()); seconds < -1 {
+		return fmt.Sprintf("nil")
+	} else if seconds < 0 {
+		return fmt.Sprintf("0s")
+	} else if seconds < 60 {
+		return fmt.Sprintf("%ds", seconds)
+	} else if minutes := int(d.Minutes()); minutes < 60 {
+		return fmt.Sprintf("%dm", minutes)
+	} else if hours := int(d.Hours()); hours < 24 {
+		return fmt.Sprintf("%dh", hours)
+	} else if hours < 24*365 {
+		return fmt.Sprintf("%dd", hours/24)
+	}
+	return fmt.Sprintf("%dy", int(d.Hours()/24/365))
+}
