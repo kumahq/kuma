64bda6f0e1d383fb4db9e13d75dc305066b98f88
Nikolay Nikolaev <nikolay.nikolaev@konghq.com>
2020-06-13 10:20:05 +0300

    Fix e2e Local/Global tests (#826)
    Changes the way Local/Global config is handled in kumactl and kuma-cp. Also, fix testing to accommodate the Local/Global specifics.
    
    Introduces SetupDiagnosticsServer as a way to spin up the server that handles readiness and health check probes for K8s. This is independent of setting up the XDS server, which is not needed in Global mode.
    
    
    * chore(xds) SetupDiagnosticsServer separated in xds
    
    * chore(kumactl) default to kuma-system-global namespace for global mode
    
    * chore(kumactl) disable parts of app.yaml when deploying Global CP
    
    * chore(kuma-cp) reorder the Mode isolating servers and Validations
    
    * chore(kind) expand deployment options with global/local
    
    * chore(kuma-cp) set cfg.Mode before loading the config file
    
    * chore(*) tests for Global/Local port forwarding and GUI check
    
    
    Signed-off-by: Nikolay Nikolaev <nikolay.nikolaev@konghq.com>

--- app/kuma-cp/cmd/run.go
+++ app/kuma-cp/cmd/run.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"fmt"
 
+	api_server "github.com/Kong/kuma/pkg/api-server"
+
 	"github.com/spf13/cobra"
 
 	kuma_cmd "github.com/Kong/kuma/pkg/cmd"
@@ -11,7 +13,6 @@ import (
 
 	ui_server "github.com/Kong/kuma/app/kuma-ui/pkg/server"
 	admin_server "github.com/Kong/kuma/pkg/admin-server"
-	api_server "github.com/Kong/kuma/pkg/api-server"
 	"github.com/Kong/kuma/pkg/config"
 	kuma_cp "github.com/Kong/kuma/pkg/config/app/kuma-cp"
 	"github.com/Kong/kuma/pkg/core"
@@ -49,12 +50,12 @@ func newRunCmdWithOpts(opts runCmdOpts) *cobra.Command {
 				return err
 			}
 			cfg := kuma_cp.DefaultConfig()
+			cfg.Mode = args.kumaCpMode
 			err := config.Load(args.configPath, &cfg)
 			if err != nil {
 				runLog.Error(err, "could not load the configuration")
 				return err
 			}
-			cfg.Mode = args.kumaCpMode
 			rt, err := bootstrap.Bootstrap(cfg)
 			if err != nil {
 				runLog.Error(err, "unable to set up Control Plane runtime")
@@ -71,7 +72,14 @@ func newRunCmdWithOpts(opts runCmdOpts) *cobra.Command {
 				return err
 			}
 			runLog.Info(fmt.Sprintf("Current config %s", cfgBytes))
-			if cfg.Mode != config_core.Global {
+			switch cfg.Mode {
+			case config_core.Standalone:
+				if err := ui_server.SetupServer(rt); err != nil {
+					runLog.Error(err, "unable to set up GUI server")
+					return err
+				}
+				fallthrough
+			case config_core.Local:
 				if err := sds_server.SetupServer(rt); err != nil {
 					runLog.Error(err, "unable to set up SDS server")
 					return err
@@ -80,24 +88,28 @@ func newRunCmdWithOpts(opts runCmdOpts) *cobra.Command {
 					runLog.Error(err, "unable to set up xDS server")
 					return err
 				}
-				if err := api_server.SetupServer(rt); err != nil {
-					runLog.Error(err, "unable to set up API server")
-					return err
-				}
 				if err := mads_server.SetupServer(rt); err != nil {
 					runLog.Error(err, "unable to set up Monitoring Assignment server")
 					return err
 				}
-			}
-			if err := admin_server.SetupServer(rt); err != nil {
-				runLog.Error(err, "unable to set up Admin server")
-				return err
-			}
-			if cfg.Mode != config_core.Local {
+			case config_core.Global:
+				if err := xds_server.SetupDiagnosticsServer(rt); err != nil {
+					runLog.Error(err, "unable to set up xDS server")
+					return err
+				}
 				if err := ui_server.SetupServer(rt); err != nil {
 					runLog.Error(err, "unable to set up GUI server")
 					return err
 				}
+			}
+
+			if err := api_server.SetupServer(rt); err != nil {
+				runLog.Error(err, "unable to set up API server")
+				return err
+			}
+			if err := admin_server.SetupServer(rt); err != nil {
+				runLog.Error(err, "unable to set up Admin server")
+				return err
 			}
 
 			runLog.Info("starting Control Plane", "version", kuma_version.Build.Version)
