1044d82c480545293a83e05a936f701d8871fd37
Nikolay Nikolaev <nikolay.nikolaev@konghq.com>
2020-06-20 10:32:36 +0300

    chore(e2e) refactor multi-apps test to use setup.go primitives
    Signed-off-by: Nikolay Nikolaev <nikolay.nikolaev@konghq.com>

--- test/e2e/kuma_deploy_multi_apps_test.go
+++ test/e2e/kuma_deploy_multi_apps_test.go
@@ -1,8 +1,9 @@
 package e2e_test
 
 import (
+	"fmt"
+
 	"github.com/gruntwork-io/terratest/modules/k8s"
-	"github.com/gruntwork-io/terratest/modules/retry"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -12,89 +13,63 @@ import (
 
 var _ = Describe("Test App deployment", func() {
 
-	var clusters Clusters
+	namespaceWithSidecarInjection := func(namespace string) string {
+		return fmt.Sprintf(`
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: %s
+  labels:
+    kuma.io/sidecar-injection: "enabled"
+`, namespace)
+	}
+
+	var c1 Cluster
 
 	BeforeEach(func() {
-		var err error
-		clusters, err = NewK8sClusters(
+		clusters, err := NewK8sClusters(
 			[]string{Kuma1},
-			Verbose)
+			Silent)
 		Expect(err).ToNot(HaveOccurred())
 
-		err = clusters.CreateNamespace("kuma-test")
-		Expect(err).ToNot(HaveOccurred())
+		c1 = clusters.GetCluster(Kuma1)
 
-		err = clusters.LabelNamespaceForSidecarInjection("kuma-test")
-		Expect(err).ToNot(HaveOccurred())
-
-		_, err = clusters.DeployKuma()
-		Expect(err).ToNot(HaveOccurred())
-
-		err = clusters.VerifyKuma()
-		Expect(err).ToNot(HaveOccurred())
-
-		err = clusters.InjectDNS()
+		err = NewClusterSetup().
+			Install(Kuma()).
+			Install(KumaDNS()).
+			Install(Yaml(namespaceWithSidecarInjection(TestNamespace))).
+			Install(DemoClient()).
+			Install(EchoServer()).
+			Setup(c1)
 		Expect(err).ToNot(HaveOccurred())
 	})
 
 	AfterEach(func() {
-		err := clusters.DeleteNamespace("kuma-test")
-		Expect(err).ToNot(HaveOccurred())
-
-		_ = clusters.DeleteKuma()
+		//_ = c1.DeleteKuma()
+		//_ = k8s.KubectlDeleteFromStringE(c1.GetTesting(), c1.GetKubectlOptions(), namespaceWithSidecarInjection(TestNamespace))
 	})
 
 	It("Should deploy two apps", func() {
-		// setup
-		c := clusters.GetCluster(Kuma1)
-
-		// given
-		err := c.DeployApp(TestNamespace, "example-app")
-		Expect(err).ToNot(HaveOccurred())
-
-		err = c.DeployApp(TestNamespace, "example-client")
-		Expect(err).ToNot(HaveOccurred())
-
-		clientPods := k8s.ListPods(c.GetTesting(),
-			c.GetKubectlOptions(TestNamespace),
+		pods, err := k8s.ListPodsE(
+			c1.GetTesting(),
+			c1.GetKubectlOptions(TestNamespace),
 			metav1.ListOptions{
-				LabelSelector: "app=example-client",
-			})
-		Expect(len(clientPods)).To(Equal(1))
-
-		clientPod := clientPods[0]
-
-		k8s.WaitUntilPodAvailable(c.GetTesting(),
-			c.GetKubectlOptions(TestNamespace),
-			clientPod.GetName(),
-			defaultRetries, defaultTimeout)
-
-		// when
-		out, err := k8s.RunKubectlAndGetOutputE(c.GetTesting(),
-			c.GetKubectlOptions(TestNamespace),
-			"exec", clientPod.GetName(), "--", "/usr/bin/curl", "example-app")
-		// then
+				LabelSelector: fmt.Sprintf("app=%s", "demo-client"),
+			},
+		)
 		Expect(err).ToNot(HaveOccurred())
-		// and
-		Expect(out).To(ContainSubstring("Thank you for using nginx."))
+		Expect(pods).To(HaveLen(1))
 
-		retry.DoWithRetry(c.GetTesting(), "resolve example-app.mesh",
-			defaultRetries, defaultTimeout,
-			func() (string, error) {
-				out, err = k8s.RunKubectlAndGetOutputE(c.GetTesting(),
-					c.GetKubectlOptions("kuma-test"),
-					"exec", clientPod.GetName(),
-					"-c", "client", "--", "getent", "hosts", "example-app.mesh")
-				return out, err
-			})
+		clientPod := pods[0]
 
-		// when
-		out, err = k8s.RunKubectlAndGetOutputE(c.GetTesting(),
-			c.GetKubectlOptions("kuma-test"),
-			"exec", clientPod.GetName(), "--", "/usr/bin/curl", "example-app.mesh")
-		// then
+		_, stderr, err := c1.ExecWithRetries(TestNamespace, clientPod.GetName(), "demo-client",
+			"curl", "-v", "echo-server")
 		Expect(err).ToNot(HaveOccurred())
-		// and
-		Expect(out).To(ContainSubstring("Thank you for using nginx."))
+		Expect(stderr).To(ContainSubstring("HTTP/1.1 200 OK"))
+
+		_, stderr, err = c1.ExecWithRetries(TestNamespace, clientPod.GetName(), "demo-client",
+			"curl", "-v", "echo-server.mesh")
+		Expect(err).ToNot(HaveOccurred())
+		Expect(stderr).To(ContainSubstring("HTTP/1.1 200 OK"))
 	})
 })
