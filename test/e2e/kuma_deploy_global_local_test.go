1edf906ce1705467ae51610e56cdf5e0f7e60fad
Ilya Lobkov <ilya.lobkov@konghq.com>
2020-06-25 20:41:46 +0700

    feat(kuma-cp) disable test

--- test/e2e/kuma_deploy_global_local_test.go
+++ test/e2e/kuma_deploy_global_local_test.go
@@ -1,7 +1,11 @@
 package e2e_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/Kong/kuma/pkg/clusters/poller"
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
+	"net/http"
 
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	. "github.com/onsi/ginkgo"
@@ -33,6 +37,66 @@ var _ = Describe("Test Local and Global", func() {
 		Expect(err).ToNot(HaveOccurred())
 
 		_ = clusters.DeleteKuma()
+	})
+
+	// todo (lobkoviya): implement Cluster polling based on KDS
+	XIt("Should deploy Local and Global on 2 clusters", func() {
+		// given
+		c1 := clusters.GetCluster(Kuma1)
+		c2 := clusters.GetCluster(Kuma2)
+
+		global, err := c1.DeployKuma(core.Global)
+		Expect(err).ToNot(HaveOccurred())
+
+		local, err := c2.DeployKuma(core.Local)
+		Expect(err).ToNot(HaveOccurred())
+
+		// when
+		err = c1.VerifyKuma()
+		// then
+		Expect(err).ToNot(HaveOccurred())
+
+		// when
+		err = c2.VerifyKuma()
+		// then
+		Expect(err).ToNot(HaveOccurred())
+
+		err = global.AddCluster(local.GetName(), local.GetHostAPI())
+		Expect(err).ToNot(HaveOccurred())
+
+		err = c1.RestartKuma()
+		Expect(err).ToNot(HaveOccurred())
+
+		// then
+		logs1, err := global.GetKumaCPLogs()
+		Expect(err).ToNot(HaveOccurred())
+		Expect(logs1).To(ContainSubstring("\"mode\":\"global\""))
+
+		// and
+		logs2, err := local.GetKumaCPLogs()
+		Expect(err).ToNot(HaveOccurred())
+		Expect(logs2).To(ContainSubstring("\"mode\":\"local\""))
+
+		// when
+		status, response := http_helper.HttpGet(c1.GetTesting(), global.GetGlobaStatusAPI(), nil)
+		// then
+		Expect(status).To(Equal(http.StatusOK))
+
+		// when
+		clustersStatus := poller.Clusters{}
+		_ = json.Unmarshal([]byte(response), &clustersStatus)
+		// then
+
+		found := false
+		for _, cluster := range clustersStatus {
+			if cluster.URL == local.GetHostAPI() {
+				Expect(cluster.Active).To(BeTrue())
+				found = true
+				break
+			}
+		}
+		Expect(found).To(BeTrue())
+
 	})
 
 	It("should deploy Local and Global on 2 clusters and sync dataplanes", func() {
