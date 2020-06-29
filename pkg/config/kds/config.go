c47ac01a7822e32a3f0febf1a931380782bfd970
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2020-06-29 14:02:16 +0200

    add test to check override config

--- pkg/config/kds/config.go
+++ pkg/config/kds/config.go
@@ -75,6 +75,7 @@ func (c *KdsServerConfig) Validate() (errs error) {
 }
 
 type KdsClientConfig struct {
+	// RootCAFile defines a path to a file with PEM-encoded Root CA. Client will verify server by using it.
 	RootCAFile string `yaml:"rootCaFile" envconfig:"kuma_kds_client_root_ca_file"`
 }
 
