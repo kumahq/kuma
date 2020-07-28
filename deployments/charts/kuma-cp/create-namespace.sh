240810d549b35376aa4c38888350a5a52b7977ee
austin ce <austin.cawley@gmail.com>
2020-07-28 18:06:55 -0400

    feat(deployments) use a pre-install hook to patch the system namespace
    Signed-off-by: austin ce <austin.cawley@gmail.com>

--- deployments/charts/kuma-cp/create-namespace.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-NAMESPACE=${1:-kuma-system}
-
-kubectl create namespace "$NAMESPACE" || true
-
-kubectl patch namespace/"$NAMESPACE" \
-  --type merge \
-  --patch '{ "metadata": { "labels": { "kuma.io/system-namespace": "true" } } }'
