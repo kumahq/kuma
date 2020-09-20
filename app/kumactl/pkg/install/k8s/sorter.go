3298381cfb7677405db91fc334855288be67b720
Nikolay Nikolaev <nikolay.nikolaev@konghq.com>
2020-09-20 21:01:43 +0300

    chore(*) reuse Helm Kind sorterand bump to Helm 3.3.3
    Signed-off-by: Nikolay Nikolaev <nikolay.nikolaev@konghq.com>

--- app/kumactl/pkg/install/k8s/sorter.go
+++ app/kumactl/pkg/install/k8s/sorter.go
@@ -1,21 +1,31 @@
 package k8s
 
 import (
-	helm_manifest "k8s.io/helm/pkg/manifest"
-	helm_releaseutil "k8s.io/helm/pkg/releaseutil"
+	"log"
 
 	"github.com/kumahq/kuma/app/kumactl/pkg/install/data"
+	"helm.sh/helm/v3/pkg/chartutil"
+	"helm.sh/helm/v3/pkg/releaseutil"
 )
 
 func SortResourcesByKind(files []data.File) []data.File {
 	singleFile := data.JoinYAML(files)
-	resources := helm_releaseutil.SplitManifests(string(singleFile.Data))
-	manifests := helm_manifest.SplitManifests(resources)
-	SortByKind(manifests)
+	resources := releaseutil.SplitManifests(string(singleFile.Data))
 
-	result := make([]data.File, len(manifests))
+	hooks, manifests, err := releaseutil.SortManifests(resources, chartutil.VersionSet{"v1", "v1beta1", "v1alpha1"}, releaseutil.InstallOrder)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	result := make([]data.File, len(manifests)+len(hooks))
 	for i, manifest := range manifests {
 		result[i].Data = []byte(manifest.Content)
+	}
+
+	baseIdx := len(manifests) - 1
+	for i, hook := range hooks {
+		result[baseIdx+i].Data = []byte(hook.Manifest)
 	}
 	return result
 }
