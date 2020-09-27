90d27016d3f2d7e5e03ffb3e47ca7908025fc0d7
Nikolay Nikolaev <nikolay.nikolaev@konghq.com>
2020-09-27 21:21:30 +0300

    chore(*) fixed review
    Signed-off-by: Nikolay Nikolaev <nikolay.nikolaev@konghq.com>

--- app/kumactl/pkg/install/k8s/sorter.go
+++ app/kumactl/pkg/install/k8s/sorter.go
@@ -1,22 +1,19 @@
 package k8s
 
 import (
-	"log"
-
 	"helm.sh/helm/v3/pkg/chartutil"
 	"helm.sh/helm/v3/pkg/releaseutil"
 
 	"github.com/kumahq/kuma/app/kumactl/pkg/install/data"
 )
 
-func SortResourcesByKind(files []data.File) []data.File {
+func SortResourcesByKind(files []data.File) ([]data.File, error) {
 	singleFile := data.JoinYAML(files)
 	resources := releaseutil.SplitManifests(string(singleFile.Data))
 
 	hooks, manifests, err := releaseutil.SortManifests(resources, chartutil.VersionSet{"v1", "v1beta1", "v1alpha1"}, releaseutil.InstallOrder)
 	if err != nil {
-		log.Fatal(err)
-		return nil
+		return nil, err
 	}
 
 	result := make([]data.File, len(manifests)+len(hooks))
@@ -28,5 +25,5 @@ func SortResourcesByKind(files []data.File) []data.File {
 	for i, hook := range hooks {
 		result[baseIdx+i].Data = []byte(hook.Manifest)
 	}
-	return result
+	return result, nil
 }
