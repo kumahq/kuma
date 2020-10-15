d97aa6e9c710285ecec33afe8178d16f749e054f
Ilya Lobkov <ilya.lobkov@konghq.com>
2020-10-15 21:03:01 +0700

    feat(kuma-cp) review
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- pkg/plugins/runtime/universal/outbound_test.go
+++ pkg/plugins/runtime/universal/outbound_test.go
@@ -3,9 +3,10 @@ package universal_test
 import (
 	"context"
 
-	"github.com/kumahq/kuma/pkg/core/resources/model"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	"github.com/kumahq/kuma/pkg/core/resources/model"
 
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
 	"github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
