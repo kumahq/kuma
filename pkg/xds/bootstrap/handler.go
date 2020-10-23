a8d48cf9b5069bafe399a27e91f1ba4cc1a89886
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2020-10-23 16:09:53 +0200

    chore(kuma-dp) move token validation to server side (#1084)
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- pkg/xds/bootstrap/handler.go
+++ pkg/xds/bootstrap/handler.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"github.com/go-logr/logr"
+
 	"github.com/kumahq/kuma/pkg/core"
 	"github.com/kumahq/kuma/pkg/core/resources/store"
 	"github.com/kumahq/kuma/pkg/core/validators"
@@ -32,38 +34,16 @@ func (b *BootstrapHandler) Handle(resp http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	logger := log.WithValues("params", reqParams)
 	config, err := b.Generator.Generate(req.Context(), reqParams)
 	if err != nil {
-		if store.IsResourceNotFound(err) {
-			resp.WriteHeader(http.StatusNotFound)
-			return
-		}
-		if store.IsResourcePreconditionFailed(err) {
-			resp.WriteHeader(http.StatusUnprocessableEntity)
-			_, err = resp.Write([]byte(err.Error()))
-			if err != nil {
-				log.WithValues("params", reqParams).Error(err, "Error while writing the response")
-				return
-			}
-			return
-		}
-		if validators.IsValidationError(err) {
-			resp.WriteHeader(http.StatusUnprocessableEntity)
-			_, err = resp.Write([]byte(err.Error()))
-			if err != nil {
-				log.WithValues("params", reqParams).Error(err, "Error while writing the response")
-				return
-			}
-			return
-		}
-		log.WithValues("params", reqParams).Error(err, "Could not generate a bootstrap configuration")
-		resp.WriteHeader(http.StatusInternalServerError)
+		handleError(resp, err, logger)
 		return
 	}
 
 	bytes, err = proto.ToYAML(config)
 	if err != nil {
-		log.WithValues("params", reqParams).Error(err, "Could not convert to json")
+		logger.Error(err, "Could not convert to json")
 		resp.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -72,7 +52,40 @@ func (b *BootstrapHandler) Handle(resp http.ResponseWriter, req *http.Request) {
 	resp.Header().Set("content-type", "text/x-yaml")
 	_, err = resp.Write(bytes)
 	if err != nil {
-		log.WithValues("params", reqParams).Error(err, "Error while writing the response")
+		logger.Error(err, "Error while writing the response")
 		return
 	}
+}
+
+func handleError(resp http.ResponseWriter, err error, logger logr.Logger) {
+	if err == DpTokenRequired {
+		resp.WriteHeader(http.StatusUnprocessableEntity)
+		_, err = resp.Write([]byte(err.Error()))
+		if err != nil {
+			logger.Error(err, "Error while writing the response")
+		}
+		return
+	}
+	if store.IsResourceNotFound(err) {
+		resp.WriteHeader(http.StatusNotFound)
+		return
+	}
+	if store.IsResourcePreconditionFailed(err) {
+		resp.WriteHeader(http.StatusUnprocessableEntity)
+		_, err = resp.Write([]byte(err.Error()))
+		if err != nil {
+			logger.Error(err, "Error while writing the response")
+		}
+		return
+	}
+	if validators.IsValidationError(err) {
+		resp.WriteHeader(http.StatusUnprocessableEntity)
+		_, err = resp.Write([]byte(err.Error()))
+		if err != nil {
+			logger.Error(err, "Error while writing the response")
+		}
+		return
+	}
+	logger.Error(err, "Could not generate a bootstrap configuration")
+	resp.WriteHeader(http.StatusInternalServerError)
 }
