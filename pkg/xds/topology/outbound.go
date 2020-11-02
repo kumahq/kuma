1192f325cf65fc74f312a8022fc9d0ac54e50f2f
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2020-11-02 20:22:50 +0100

    adjust loadbalancing to many instances of one ingress
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- pkg/xds/topology/outbound.go
+++ pkg/xds/topology/outbound.go
@@ -1,6 +1,8 @@
 package topology
 
 import (
+	"fmt"
+
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
 	mesh_core "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
 	core_xds "github.com/kumahq/kuma/pkg/core/xds"
@@ -22,49 +24,100 @@ func BuildEndpointMap(
 	externalServices []*mesh_core.ExternalServiceResource,
 ) core_xds.EndpointMap {
 	outbound := core_xds.EndpointMap{}
+	ingressInstances := fillIngressOutbounds(outbound, dataplanes, zone, mesh)
+	endpointWeight := uint32(1)
+	if ingressInstances > 0 {
+		endpointWeight = ingressInstances
+	}
+	fillDataplaneOutbounds(outbound, dataplanes, mesh, endpointWeight)
+	fillExternalServicesOutbounds(outbound, externalServices, mesh)
+	return outbound
+}
+
+// endpointWeight defines default weight for in-cluster endpoint.
+// Examples of having service "backend":
+// 1) Standalone deployment, 2 instances in one cluster (zone1)
+//    All endpoints have to have the same weight (ex. 1) to achieve fair loadbalancing.
+//    Endpoints:
+//    * backend-zone1-1 - weight: 1
+//    * backend-zone1-2 - weight: 1
+// 2) Multi-zone deployment, 2 instances in "zone1" (local zone), 3 instances in "zone2" (remote zone) with 1 Ingress instance
+//    Endpoints:
+//    * backend-zone1-1 - weight: 1
+//    * backend-zone1-2 - weight: 1
+//    * ingress-zone2-1 - weight: 3 (all remote endpoints are aggregated to one Ingress, it needs to have weight of instances in other cluster)
+// 3) Multi-zone deployment, 2 instances in "zone1" (local zone), 2 instances in "zone2" (remote zone) with 1 Ingress instance
+//    Many instances of Ingress will forward the traffic to the same endpoints in "zone2" so we need to lower the weights.
+//    Since weights are integers, we cannot put fractional on ingress endpoints weights, we need to adjust "default" weight for local zone
+//    Endpoints:
+//    * backend-zone1-1 - weight: 2
+//    * backend-zone1-2 - weight: 2
+//    * ingress-zone2-1 - weight: 3
+//    * ingress-zone2-2 - weight: 3
+func fillDataplaneOutbounds(outbound core_xds.EndpointMap, dataplanes []*mesh_core.DataplaneResource, mesh *mesh_core.MeshResource, endpointWeight uint32) {
 	for _, dataplane := range dataplanes {
-		// Ingress routes the request by TLS SNI, therefore for cross cluster communication MTLS is required
-		// We ignore Ingress from endpoints if MTLS is disabled, otherwise we would fail anyway.
-		if dataplane.Spec.IsIngress() && dataplane.Spec.IsRemoteIngress(zone) && mesh.MTLSEnabled() && dataplane.Spec.HasPublicAddress() {
-			for _, ingress := range dataplane.Spec.Networking.GetIngress().GetAvailableServices() {
-				if ingress.Mesh != mesh.GetMeta().GetName() {
-					continue
-				}
-				service := ingress.Tags[mesh_proto.ServiceTag]
-				outbound[service] = append(outbound[service], core_xds.Endpoint{
-					Target:   dataplane.Spec.Networking.Ingress.PublicAddress,
-					Port:     dataplane.Spec.Networking.Ingress.PublicPort,
-					Tags:     ingress.Tags,
-					Weight:   ingress.Instances,
-					Locality: localityFromTags(mesh, priorityRemote, ingress.Tags),
-				})
-			}
+		if dataplane.Spec.IsIngress() {
 			continue
 		}
-		if !dataplane.Spec.IsIngress() {
-			for _, inbound := range dataplane.Spec.Networking.GetInbound() {
-				service := inbound.Tags[mesh_proto.ServiceTag]
-				iface := dataplane.Spec.Networking.ToInboundInterface(inbound)
-				// TODO(yskopets): do we need to dedup?
-				// TODO(yskopets): sort ?
-				outbound[service] = append(outbound[service], core_xds.Endpoint{
-					Target:   iface.DataplaneIP,
-					Port:     iface.DataplanePort,
-					Tags:     inbound.Tags,
-					Weight:   1,
-					Locality: localityFromTags(mesh, priorityLocal, inbound.Tags),
-				})
-			}
+		for _, inbound := range dataplane.Spec.Networking.GetInbound() {
+			service := inbound.Tags[mesh_proto.ServiceTag]
+			iface := dataplane.Spec.Networking.ToInboundInterface(inbound)
+			// TODO(yskopets): do we need to dedup?
+			// TODO(yskopets): sort ?
+			outbound[service] = append(outbound[service], core_xds.Endpoint{
+				Target:   iface.DataplaneIP,
+				Port:     iface.DataplanePort,
+				Tags:     inbound.Tags,
+				Weight:   endpointWeight,
+				Locality: localityFromTags(mesh, priorityLocal, inbound.Tags),
+			})
 		}
 	}
+}
 
+func fillIngressOutbounds(outbound core_xds.EndpointMap, dataplanes []*mesh_core.DataplaneResource, zone string, mesh *mesh_core.MeshResource) uint32 {
+	ingressInstances := map[string]bool{}
+	for _, dataplane := range dataplanes {
+		if !dataplane.Spec.IsIngress() {
+			continue
+		}
+		if !dataplane.Spec.IsRemoteIngress(zone) {
+			continue // we only need Ingress for other zones, we don't want to direct request to the same zone through Ingress
+		}
+		if !mesh.MTLSEnabled() {
+			// Ingress routes the request by TLS SNI, therefore for cross cluster communication MTLS is required
+			// We ignore Ingress from endpoints if MTLS is disabled, otherwise we would fail anyway.
+			continue
+		}
+		if !dataplane.Spec.HasPublicAddress() {
+			continue // Dataplane is not reachable yet from other clusters. This may happen when Ingress Service is pending waiting on External IP on Kubernetes.
+		}
+		ingressCoordinates := fmt.Sprintf("%s;%d", dataplane.Spec.Networking.Ingress.PublicAddress, dataplane.Spec.Networking.Ingress.PublicPort)
+		if ingressInstances[ingressCoordinates] {
+			continue // many Ingress instances can be placed in front of one load balancer (all instances can have the same public address and port). In this case we only need one Instance avoiding creating unnecessary duplicated endpoints
+		}
+		ingressInstances[ingressCoordinates] = true
+		for _, service := range dataplane.Spec.Networking.GetIngress().GetAvailableServices() {
+			if service.Mesh != mesh.GetMeta().GetName() {
+				continue
+			}
+			serviceName := service.Tags[mesh_proto.ServiceTag]
+			outbound[serviceName] = append(outbound[serviceName], core_xds.Endpoint{
+				Target:   dataplane.Spec.Networking.Ingress.PublicAddress,
+				Port:     dataplane.Spec.Networking.Ingress.PublicPort,
+				Tags:     service.Tags,
+				Weight:   service.Instances,
+				Locality: localityFromTags(mesh, priorityRemote, service.Tags),
+			})
+		}
+	}
+	return uint32(len(ingressInstances))
+}
+
+func fillExternalServicesOutbounds(outbound core_xds.EndpointMap, externalServices []*mesh_core.ExternalServiceResource, mesh *mesh_core.MeshResource) {
 	for _, externalService := range externalServices {
 		service := externalService.Spec.GetService()
-
-		tlsEnabled := false
-		if externalService.Spec.Networking.Tls != nil {
-			tlsEnabled = externalService.Spec.Networking.Tls.Enabled
-		}
+		tlsEnabled := externalService.Spec.GetNetworking().GetTls().GetEnabled()
 
 		tags := externalService.Spec.GetTags()
 		if tlsEnabled {
@@ -82,8 +135,6 @@ func BuildEndpointMap(
 			Locality: localityFromTags(mesh, priorityRemote, tags),
 		})
 	}
-
-	return outbound
 }
 
 func localityFromTags(mesh *mesh_core.MeshResource, priority uint32, tags map[string]string) *core_xds.Locality {
