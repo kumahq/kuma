772e5466ce48a39e9d53a2d17d5e90d9dc1e2823
Nikolay Nikolaev <nikolay.nikolaev@konghq.com>
2020-11-11 09:08:06 +0200

    fix(*) review
    Signed-off-by: Nikolay Nikolaev <nikolay.nikolaev@konghq.com>

--- pkg/config/diagnostics/config.go
+++ pkg/config/diagnostics/config.go
@@ -1,14 +1,12 @@
 package diagnostics
 
 import (
-	"github.com/pkg/errors"
-
 	"github.com/kumahq/kuma/pkg/config"
 )
 
 type DiagnosticsConfig struct {
 	// Port of Diagnostic Server for checking health and readiness of the Control Plane
-	ServerPort int `yaml:"serverPort" envconfig:"kuma_diagnostics_server_port"`
+	ServerPort uint32 `yaml:"serverPort" envconfig:"kuma_diagnostics_server_port"`
 	// If true, enables https://golang.org/pkg/net/http/pprof/ debug endpoints
 	DebugEndpoints bool `yaml:"debugEndpoints" envconfig:"kuma_diagnostics_debug_endpoints"`
 }
@@ -19,9 +17,6 @@ func (d *DiagnosticsConfig) Sanitize() {
 }
 
 func (d *DiagnosticsConfig) Validate() error {
-	if d.ServerPort < 0 {
-		return errors.New("DiagnosticPort cannot be negative")
-	}
 	return nil
 }
 
