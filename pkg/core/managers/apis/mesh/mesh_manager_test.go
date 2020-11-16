e2f2e5c803cd92cfae9a440fed398b5b6f18f38f
Nikolay Nikolaev <nikolay.nikolaev@konghq.com>
2020-11-16 16:56:20 +0200

    fix(*) test unification
    Signed-off-by: Nikolay Nikolaev <nikolay.nikolaev@konghq.com>

--- pkg/core/managers/apis/mesh/mesh_manager_test.go
+++ pkg/core/managers/apis/mesh/mesh_manager_test.go
@@ -101,21 +101,6 @@ var _ = Describe("Mesh Manager", func() {
 			// and default TrafficPermission for the mesh exists
 			err = resStore.Get(context.Background(), &core_mesh.TrafficPermissionResource{}, store.GetByKey("allow-all-mesh-1", meshName))
 			Expect(err).ToNot(HaveOccurred())
-		})
-
-		It("should create default traffic route", func() {
-			// given
-			meshName := "mesh-1"
-			resKey := model.ResourceKey{
-				Name: meshName,
-			}
-
-			// when
-			mesh := &core_mesh.MeshResource{}
-			err := resManager.Create(context.Background(), mesh, store.CreateBy(resKey))
-
-			// then
-			Expect(err).ToNot(HaveOccurred())
 
 			// and default TrafficRoute for the mesh exists
 			err = resStore.Get(context.Background(), &core_mesh.TrafficRouteResource{}, store.GetByKey("allow-all-mesh-1", meshName))
