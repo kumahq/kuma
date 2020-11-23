304f36bab2bf1b69d66bc51ea7ce50928c69141c
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2020-11-23 14:50:28 +0100

    fix(kuma-cp) create default mesh resources when default mesh is skipped (#1178)
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- pkg/core/managers/apis/mesh/mesh_manager.go
+++ pkg/core/managers/apis/mesh/mesh_manager.go
@@ -80,7 +80,7 @@ func (m *meshManager) Create(ctx context.Context, resource core_model.Resource, 
 	if err := m.store.Create(ctx, mesh, append(fs, core_store.CreatedAt(time.Now()))...); err != nil {
 		return err
 	}
-	if err := defaults_mesh.CreateDefaultMeshResources(m.otherManagers, opts.Name); err != nil {
+	if err := defaults_mesh.EnsureDefaultMeshResources(m.otherManagers, opts.Name); err != nil {
 		return err
 	}
 	return nil
