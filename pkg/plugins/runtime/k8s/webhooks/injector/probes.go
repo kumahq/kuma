3d02c394076c76432f74babda5f32bd1e75effcb
Ilya Lobkov <ilya.lobkov@konghq.com>
2020-11-23 17:03:26 +0700

    fix(kuma-cp) fix virtual probes disabling and envs (#1171)

--- pkg/plugins/runtime/k8s/webhooks/injector/probes.go
+++ pkg/plugins/runtime/k8s/webhooks/injector/probes.go
@@ -1,6 +1,9 @@
 package injector
 
 import (
+	"fmt"
+
+	"github.com/pkg/errors"
 	kube_core "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 
@@ -11,12 +14,16 @@ import (
 
 func (i *KumaInjector) overrideHTTPProbes(pod *kube_core.Pod) error {
 	log.WithValues("name", pod.Name, "namespace", pod.Namespace)
-	if ok, err := needVirtualProbes(pod, i.cfg); err != nil || !ok {
+	enabled, _, err := metadata.Annotations(pod.Annotations).GetEnabled(metadata.KumaVirtualProbesAnnotation)
+	if err != nil {
+		return err
+	}
+	if !enabled {
 		log.V(1).Info("skip adding virtual probes")
 		return err
 	}
 
-	port, err := virtualProbesPort(pod, i.cfg)
+	port, _, err := metadata.Annotations(pod.Annotations).GetUint32(metadata.KumaVirtualProbesPortAnnotation)
 	if err != nil {
 		return err
 	}
@@ -48,24 +55,48 @@ func overrideHTTPProbe(probe *kube_core.Probe, virtualPort uint32) error {
 	return nil
 }
 
-func needVirtualProbes(pod *kube_core.Pod, cfg runtime_k8s.Injector) (bool, error) {
-	enabled, exist, err := metadata.Annotations(pod.Annotations).GetEnabled(metadata.KumaVirtualProbesAnnotation)
+func setVirtualProbesEnabledAnnotation(annotations metadata.Annotations, pod *kube_core.Pod, cfg runtime_k8s.Injector) error {
+	str := func(b bool) string {
+		if b {
+			return metadata.AnnotationEnabled
+		}
+		return metadata.AnnotationDisabled
+	}
+
+	vpEnabled, vpExist, err := metadata.Annotations(pod.Annotations).GetEnabled(metadata.KumaVirtualProbesAnnotation)
 	if err != nil {
-		return false, err
+		return err
 	}
-	if exist {
-		return enabled, nil
+	gwEnabled, _, err := metadata.Annotations(pod.Annotations).GetEnabled(metadata.KumaGatewayAnnotation)
+	if err != nil {
+		return err
 	}
-	return cfg.VirtualProbesEnabled, nil
+
+	if gwEnabled {
+		if vpEnabled {
+			return errors.New("virtual probes can't be enabled in gateway mode")
+		}
+		annotations[metadata.KumaVirtualProbesAnnotation] = metadata.AnnotationDisabled
+		return nil
+	}
+
+	if vpExist {
+		annotations[metadata.KumaVirtualProbesAnnotation] = str(vpEnabled)
+		return nil
+	}
+	annotations[metadata.KumaVirtualProbesAnnotation] = str(cfg.VirtualProbesEnabled)
+	return nil
 }
 
-func virtualProbesPort(pod *kube_core.Pod, cfg runtime_k8s.Injector) (uint32, error) {
+func setVirtualProbesPortAnnotation(annotations metadata.Annotations, pod *kube_core.Pod, cfg runtime_k8s.Injector) error {
 	port, exist, err := metadata.Annotations(pod.Annotations).GetUint32(metadata.KumaVirtualProbesPortAnnotation)
 	if err != nil {
-		return 0, err
+		return err
 	}
 	if exist {
-		return port, nil
+		annotations[metadata.KumaVirtualProbesPortAnnotation] = fmt.Sprintf("%d", port)
+		return nil
 	}
-	return cfg.VirtualProbesPort, nil
+	annotations[metadata.KumaVirtualProbesPortAnnotation] = fmt.Sprintf("%d", cfg.VirtualProbesPort)
+	return nil
 }
