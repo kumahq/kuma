d280177d443999a8f1e1f4a9be3b68608e8dc0f6
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2020-11-30 15:01:13 +0100

    fix(kuma-cp) do not replace autogenerated certs (#1215)
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
    (cherry picked from commit dc26d880fc4062006ec94e258ffd26c7b0f44fe0)
    
    # Conflicts:
    #	app/kumactl/pkg/install/k8s/control-plane/helmtemplates_vfsdata.go

--- test/framework/k8s_controlplane.go
+++ test/framework/k8s_controlplane.go
@@ -176,6 +176,12 @@ func (c *K8sControlPlane) FinalizeAdd() error {
 }
 
 func (c *K8sControlPlane) InstallCP(args ...string) (string, error) {
+	// store the kumactl environment
+	oldEnv := c.kumactl.Env
+	c.kumactl.Env["KUBECONFIG"] = c.GetKubectlOptions().ConfigPath
+	defer func() {
+		c.kumactl.Env = oldEnv // restore kumactl environment
+	}()
 	return c.kumactl.KumactlInstallCP(c.mode, args...)
 }
 
@@ -183,14 +189,14 @@ func (c *K8sControlPlane) InjectDNS(args ...string) error {
 	// store the kumactl environment
 	oldEnv := c.kumactl.Env
 	c.kumactl.Env["KUBECONFIG"] = c.GetKubectlOptions().ConfigPath
+	defer func() {
+		c.kumactl.Env = oldEnv // restore kumactl environment
+	}()
 
 	yaml, err := c.kumactl.KumactlInstallDNS(args...)
 	if err != nil {
 		return err
 	}
-
-	// restore kumactl environment
-	c.kumactl.Env = oldEnv
 
 	return k8s.KubectlApplyFromStringE(c.t,
 		c.GetKubectlOptions(),
