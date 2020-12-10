ab308f0b7a9d4212f9d558400b5b84e82b1fb053
Nikolay Nikolaev <nikolay.nikolaev@konghq.com>
2020-12-10 18:33:32 +0200

    fix(*) conflicts
    Signed-off-by: Nikolay Nikolaev <nikolay.nikolaev@konghq.com>

--- api/mesh/v1alpha1/dataplane.pb.go
+++ api/mesh/v1alpha1/dataplane.pb.go
@@ -340,21 +340,11 @@ type Dataplane_Networking_Inbound struct {
 	// e.g. kuma.io/service=web, version=1.0.
 	// `kuma.io/service` tag is mandatory.
 	Tags map[string]string `protobuf:"bytes,2,rep,name=tags,proto3" json:"tags,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
-<<<<<<< HEAD
-	// Health is an optional field filled automatically by Kuma Control Plane on Kubernetes
-	// if Pod has ReadinessProbe configured. If 'health' is equal to nil we consider dataplane
-	// as healthy. Unhealthy dataplanes will be excluded from Endpoints Discovery Service (EDS)
-	Health               *Dataplane_Networking_Inbound_Health `protobuf:"bytes,7,opt,name=health,proto3" json:"health,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}                             `json:"-"`
-	XXX_unrecognized     []byte                               `json:"-"`
-	XXX_sizecache        int32                                `json:"-"`
-=======
 	// Health is an optional field filled automatically by Kuma Control Plane
 	// on Kubernetes if Pod has ReadinessProbe configured. If 'health' is
 	// equal to nil we consider dataplane as healthy. Unhealthy dataplanes
 	// will be excluded from Endpoints Discovery Service (EDS)
 	Health *Dataplane_Networking_Inbound_Health `protobuf:"bytes,7,opt,name=health,proto3" json:"health,omitempty"`
->>>>>>> 9d868cd8... chore(*) upgrade protocol buffers version (#1284)
 }
 
 func (x *Dataplane_Networking_Inbound) Reset() {
