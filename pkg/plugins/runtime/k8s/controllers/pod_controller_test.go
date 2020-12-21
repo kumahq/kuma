092f07f1909cffdb3cc8bceeb58efbf180e3290f
Ilya Lobkov <ilya.lobkov@konghq.com>
2020-12-21 21:57:53 +0700

    fix(kuma-cp) fix test
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- pkg/plugins/runtime/k8s/controllers/pod_controller_test.go
+++ pkg/plugins/runtime/k8s/controllers/pod_controller_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 
+	"github.com/kumahq/kuma/pkg/plugins/resources/k8s"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -174,12 +175,13 @@ var _ = Describe("PodReconciler", func() {
 			})
 
 		reconciler = &PodReconciler{
-			Client:          kubeClient,
-			EventRecorder:   kube_record.NewFakeRecorder(10),
-			Scheme:          k8sClientScheme,
-			Log:             core.Log.WithName("test"),
-			SystemNamespace: "kuma-system",
-			Persistence:     vips.NewPersistence(core_manager.NewResourceManager(memory.NewStore()), manager.NewConfigManager(memory.NewStore())),
+			Client:            kubeClient,
+			EventRecorder:     kube_record.NewFakeRecorder(10),
+			Scheme:            k8sClientScheme,
+			Log:               core.Log.WithName("test"),
+			SystemNamespace:   "kuma-system",
+			Persistence:       vips.NewPersistence(core_manager.NewResourceManager(memory.NewStore()), manager.NewConfigManager(memory.NewStore())),
+			ResourceConverter: k8s.NewSimpleConverter(),
 		}
 	})
 
