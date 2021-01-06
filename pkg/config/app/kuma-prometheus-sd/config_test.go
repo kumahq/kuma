1df77f3a36e43bff21cedf1a86e4c16fcfd91579
Charly Molter <charly.molter@gmail.com>
2021-01-06 18:53:16 +0100

    fix(kube-prometheus-sd) allow kuma-cp config to grpcs scheme (#1390)
    The configuration validation was only allowing `grpc` schemes
    in practice `grpc` and `grpcs` should both be allowed.
    
    * improve validation to allow grpcs scheme in the kube-prometheus-sd config
    
    Signed-off-by: Charly Molter <charly@koyeb.com>

--- pkg/config/app/kuma-prometheus-sd/config_test.go
+++ pkg/config/app/kuma-prometheus-sd/config_test.go
@@ -98,6 +98,19 @@ var _ = Describe("Config", func() {
 		err := config.Load(filepath.Join("testdata", "invalid-config.input.yaml"), &cfg)
 
 		// then
-		Expect(err.Error()).To(Equal(`Invalid configuration: .MonitoringAssignment is not valid: .Client is not valid: .Name must be non-empty; .URL must be a valid absolute URI; .URL must start with grpc://; .Prometheus is not valid: .OutputFile must be non-empty`))
+		Expect(err.Error()).To(Equal(`Invalid configuration: .MonitoringAssignment is not valid: .Client is not valid: .Name must be non-empty; .URL must be a valid absolute URI; .URL must start with grpc:// or grpcs://; .Prometheus is not valid: .OutputFile must be non-empty`))
+	})
+
+	It("should allow grpcs", func() {
+		// given
+		cfg := kuma_promsd.Config{}
+
+		// when
+		err := config.Load(filepath.Join("testdata", "valid-grpcs-config.input.yaml"), &cfg)
+
+		// then
+		Expect(err).ToNot(HaveOccurred())
+		// and
+		Expect(cfg.MonitoringAssignment.Client.URL).To(Equal("grpcs://kuma-control-plane.internal:5682"))
 	})
 })
