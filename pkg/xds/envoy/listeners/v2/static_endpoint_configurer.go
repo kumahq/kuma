a932c20086815a84285fd52ff39e931748310ea6
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2021-01-11 19:06:07 +0100

    feat(kuma-cp) expose envoy admin endpoints
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- pkg/xds/envoy/listeners/v2/static_endpoint_configurer.go
+++ pkg/xds/envoy/listeners/v2/static_endpoint_configurer.go
@@ -1,24 +1,26 @@
-package v3
+package v2
 
 import (
-	envoy_listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
-	envoy_route "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
-	envoy_hcm "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
+	envoy_api "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	envoy_listener "github.com/envoyproxy/go-control-plane/envoy/api/v2/listener"
+	envoy_route "github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
+	envoy_hcm "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/http_connection_manager/v2"
 	"github.com/golang/protobuf/ptypes/wrappers"
 
 	"github.com/kumahq/kuma/pkg/util/proto"
 	util_xds "github.com/kumahq/kuma/pkg/util/xds"
 )
 
-type PrometheusEndpointConfigurer struct {
+type StaticEndpointConfigurer struct {
 	StatsName   string
 	Path        string
 	ClusterName string
+	RewritePath string
 }
 
-var _ FilterChainConfigurer = &PrometheusEndpointConfigurer{}
+var _ FilterChainConfigurer = &StaticEndpointConfigurer{}
 
-func (c *PrometheusEndpointConfigurer) Configure(filterChain *envoy_listener.FilterChain) error {
+func (c *StaticEndpointConfigurer) Configure(filterChain *envoy_listener.FilterChain) error {
 	config := &envoy_hcm.HttpConnectionManager{
 		StatPrefix: util_xds.SanitizeMetric(c.StatsName),
 		CodecType:  envoy_hcm.HttpConnectionManager_AUTO,
@@ -26,7 +28,7 @@ func (c *PrometheusEndpointConfigurer) Configure(filterChain *envoy_listener.Fil
 			Name: "envoy.filters.http.router",
 		}},
 		RouteSpecifier: &envoy_hcm.HttpConnectionManager_RouteConfig{
-			RouteConfig: &envoy_route.RouteConfiguration{
+			RouteConfig: &envoy_api.RouteConfiguration{
 				VirtualHosts: []*envoy_route.VirtualHost{{
 					Name:    "envoy_admin",
 					Domains: []string{"*"},
@@ -41,7 +43,7 @@ func (c *PrometheusEndpointConfigurer) Configure(filterChain *envoy_listener.Fil
 								ClusterSpecifier: &envoy_route.RouteAction_Cluster{
 									Cluster: c.ClusterName,
 								},
-								PrefixRewrite: "/stats/prometheus", // well-known Admin API endpoint
+								PrefixRewrite: c.RewritePath,
 							},
 						},
 					}},
