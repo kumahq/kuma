a9e02ff30f551e3f4d50959cae483d85f3ef580b
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2021-01-14 20:00:31 +0100

    feat(kuma-cp): Resource counter based on Mesh insights
    Signed-off-by: Jacek Ewertowski <jacek.ewertowski1@gmail.com>

--- pkg/metrics/store/counter.go
+++ pkg/metrics/store/counter.go
@@ -7,8 +7,8 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/kumahq/kuma/pkg/core"
+	mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
 	"github.com/kumahq/kuma/pkg/core/resources/manager"
-	"github.com/kumahq/kuma/pkg/core/resources/registry"
 	"github.com/kumahq/kuma/pkg/core/runtime/component"
 	"github.com/kumahq/kuma/pkg/metrics"
 )
@@ -63,15 +63,24 @@ func (s *storeCounter) NeedLeaderElection() bool {
 }
 
 func (s *storeCounter) count() error {
-	for _, resType := range registry.Global().ListTypes() {
-		list, err := registry.Global().NewList(resType)
-		if err != nil {
-			return err
-		}
-		if err := s.resManager.List(context.Background(), list); err != nil {
-			return err
-		}
-		s.counts.WithLabelValues(string(resType)).Set(float64(len(list.GetItems())))
+	insights := &mesh.MeshInsightResourceList{}
+	err := s.resManager.List(context.Background(), insights)
+	if err != nil {
+		return err
 	}
+
+	resourceCounts := map[string]uint32{
+		string(mesh.MeshType): uint32(len(insights.GetItems())),
+	}
+	for _, meshInsight := range insights.Items {
+		resourceCounts[string(mesh.DataplaneType)] += meshInsight.Spec.GetDataplanes().GetTotal()
+		for policy, stats := range meshInsight.Spec.GetPolicies() {
+			resourceCounts[policy] += stats.GetTotal()
+		}
+	}
+	for resType, counter := range resourceCounts {
+		s.counts.WithLabelValues(resType).Set(float64(counter))
+	}
+
 	return nil
 }
