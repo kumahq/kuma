dd8ed7e009775497e7760b015899eb16985a548a
austin ce <austin.cawley@gmail.com>
2021-03-10 18:45:50 -0500

    feat(xds) add support for rest callbacks
    Signed-off-by: austin ce <austin.cawley@gmail.com>

--- pkg/util/xds/callbacks.go
+++ pkg/util/xds/callbacks.go
@@ -2,14 +2,16 @@ package xds
 
 import (
 	"context"
-
 	_struct "github.com/golang/protobuf/ptypes/struct"
 )
 
 // DiscoveryRequest defines interface over real Envoy's DiscoveryRequest.
 type DiscoveryRequest interface {
 	NodeId() string
+	// Node returns either a v2 or v3 Node
+	Node() interface{}
 	Metadata() *_struct.Struct
+	VersionInfo() string
 	GetTypeUrl() string
 	GetResponseNonce() string
 	HasErrors() bool
@@ -21,10 +23,10 @@ type DiscoveryResponse interface {
 	GetTypeUrl() string
 }
 
-// Callbacks defines Callbacks for XDS requests. The difference over real go-control-plane Callbacks is that it takes an DiscoveryRequest / DiscoveryResponse interface.
+// Callbacks defines Callbacks for xDS streaming requests. The difference over real go-control-plane Callbacks is that it takes an DiscoveryRequest / DiscoveryResponse interface.
 // It helps us to implement Callbacks once for many different versions of Envoy API.
 type Callbacks interface {
-	// OnStreamOpen is called once an xDS stream is open with a stream ID and the type URL (or "" for ADS).
+	// OnStreamOpen is called once an xDS stream is opened with a stream ID and the type URL (or "" for ADS).
 	// Returning an error will end processing and close the stream. OnStreamClosed will still be called.
 	OnStreamOpen(context.Context, int64, string) error
 	// OnStreamClosed is called immediately prior to closing an xDS stream with a stream ID.
@@ -34,4 +36,21 @@ type Callbacks interface {
 	OnStreamRequest(int64, DiscoveryRequest) error
 	// OnStreamResponse is called immediately prior to sending a response on a stream.
 	OnStreamResponse(int64, DiscoveryRequest, DiscoveryResponse)
+}
+
+// RestCallbacks defines rest.Callbacks for xDS fetch requests. The difference over real go-control-plane
+// Callbacks is that it takes an DiscoveryRequest / DiscoveryResponse interface.
+// It helps us to implement Callbacks once for many different versions of Envoy API.
+type RestCallbacks interface {
+	// OnFetchRequest is called when a new rest request comes in.
+	// Returning an error will end processing. OnFetchResponse will not be called.
+	OnFetchRequest(ctx context.Context, request DiscoveryRequest) error
+	// OnFetchResponse is called immediately prior to sending a rest response.
+	OnFetchResponse(request DiscoveryRequest, response DiscoveryResponse)
+}
+
+// MultiCallbacks implements callbacks for both rest and streaming xDS requests.
+type MultiCallbacks interface {
+	Callbacks
+	RestCallbacks
 }
--- pkg/util/xds/v3/callbacks.go
+++ pkg/util/xds/v3/callbacks.go
@@ -10,6 +10,8 @@ import (
 	"github.com/kumahq/kuma/pkg/util/xds"
 )
 
+// stream callbacks
+
 type adapterCallbacks struct {
 	callbacks xds.Callbacks
 }
@@ -46,6 +48,80 @@ func (a *adapterCallbacks) OnStreamResponse(streamID int64, request *envoy_sd.Di
 	a.callbacks.OnStreamResponse(streamID, &discoveryRequest{request}, &discoveryResponse{response})
 }
 
+// rest callbacks
+
+type adapterRestCallbacks struct {
+	callbacks xds.RestCallbacks
+}
+
+// AdaptRestCallbacks translate Kuma callbacks to real go-control-plane Callbacks
+func AdaptRestCallbacks(callbacks xds.RestCallbacks) envoy_xds.Callbacks {
+	return &adapterRestCallbacks{
+		callbacks: callbacks,
+	}
+}
+
+func (a *adapterRestCallbacks) OnFetchRequest(ctx context.Context, request *envoy_sd.DiscoveryRequest) error {
+	return a.callbacks.OnFetchRequest(ctx, &discoveryRequest{request})
+}
+
+func (a *adapterRestCallbacks) OnFetchResponse(request *envoy_sd.DiscoveryRequest, response *envoy_sd.DiscoveryResponse) {
+	a.callbacks.OnFetchResponse(&discoveryRequest{request}, &discoveryResponse{response})
+}
+
+func (a *adapterRestCallbacks) OnStreamOpen(ctx context.Context, streamID int64, typeURL string) error {
+	return nil
+}
+
+func (a *adapterRestCallbacks) OnStreamClosed(streamID int64) {
+}
+
+func (a *adapterRestCallbacks) OnStreamRequest(streamID int64, request *envoy_sd.DiscoveryRequest) error {
+	return nil
+}
+
+func (a *adapterRestCallbacks) OnStreamResponse(streamID int64, request *envoy_sd.DiscoveryRequest, response *envoy_sd.DiscoveryResponse) {
+}
+
+// Both rest and stream
+
+type adapterMultiCallbacks struct {
+	callbacks xds.MultiCallbacks
+}
+
+// AdaptMultiCallbacks translate Kuma callbacks to real go-control-plane Callbacks
+func AdaptMultiCallbacks(callbacks xds.MultiCallbacks) envoy_xds.Callbacks {
+	return &adapterMultiCallbacks{
+		callbacks: callbacks,
+	}
+}
+
+func (a *adapterMultiCallbacks) OnFetchRequest(ctx context.Context, request *envoy_sd.DiscoveryRequest) error {
+	return a.callbacks.OnFetchRequest(ctx, &discoveryRequest{request})
+}
+
+func (a *adapterMultiCallbacks) OnFetchResponse(request *envoy_sd.DiscoveryRequest, response *envoy_sd.DiscoveryResponse) {
+	a.callbacks.OnFetchResponse(&discoveryRequest{request}, &discoveryResponse{response})
+}
+
+func (a *adapterMultiCallbacks) OnStreamOpen(ctx context.Context, streamID int64, typeURL string) error {
+	return a.callbacks.OnStreamOpen(ctx, streamID, typeURL)
+}
+
+func (a *adapterMultiCallbacks) OnStreamClosed(streamID int64) {
+	a.callbacks.OnStreamClosed(streamID)
+}
+
+func (a *adapterMultiCallbacks) OnStreamRequest(streamID int64, request *envoy_sd.DiscoveryRequest) error {
+	return a.callbacks.OnStreamRequest(streamID, &discoveryRequest{request})
+}
+
+func (a *adapterMultiCallbacks) OnStreamResponse(streamID int64, request *envoy_sd.DiscoveryRequest, response *envoy_sd.DiscoveryResponse) {
+	a.callbacks.OnStreamResponse(streamID, &discoveryRequest{request}, &discoveryResponse{response})
+}
+
+// DiscoveryRequest facade
+
 type discoveryRequest struct {
 	*envoy_sd.DiscoveryRequest
 }
@@ -54,8 +130,16 @@ func (d *discoveryRequest) Metadata() *_struct.Struct {
 	return d.GetNode().GetMetadata()
 }
 
+func (d *discoveryRequest) VersionInfo() string {
+	return d.GetVersionInfo()
+}
+
 func (d *discoveryRequest) NodeId() string {
 	return d.GetNode().GetId()
+}
+
+func (d *discoveryRequest) Node() interface{} {
+	return d.GetNode()
 }
 
 func (d *discoveryRequest) HasErrors() bool {
