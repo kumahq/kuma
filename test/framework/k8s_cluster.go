324d8e71baf09c587b0c1d5828e131bdbfba5eb4
Bart Smykla <bartek@smykla.com>
2021-04-02 11:39:48 +0200

    feat(*) remove overwhelming logs when delete crds
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- test/framework/k8s_cluster.go
+++ test/framework/k8s_cluster.go
@@ -5,9 +5,11 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -532,6 +534,30 @@ func (c *K8sCluster) VerifyKuma() error {
 	return nil
 }
 
+func (c *K8sCluster) deleteCRDs() (errs error) {
+	var crdsYAML bytes.Buffer
+	cmd := exec.Command("kubectl", "get", "crds", "-o", "yaml", "--kubeconfig", c.kubeconfig)
+	cmd.Stdout = &crdsYAML
+
+	if err := cmd.Run(); err != nil {
+		errs = multierr.Append(errs, err)
+	} else if tmpfile, err := ioutil.TempFile("", "crds.yaml"); err != nil {
+		errs = multierr.Append(errs, err)
+	} else {
+		defer os.Remove(tmpfile.Name()) // clean up
+
+		if _, err := tmpfile.Write(crdsYAML.Bytes()); err != nil {
+			errs = multierr.Append(errs, err)
+		} else if err := tmpfile.Close(); err != nil {
+			errs = multierr.Append(errs, err)
+		} else if err := k8s.KubectlDeleteE(c.t, c.GetKubectlOptions(), tmpfile.Name()); err != nil {
+			errs = multierr.Append(errs, err)
+		}
+	}
+
+	return errs
+}
+
 func (c *K8sCluster) deleteKumaViaHelm(opts *deployOptions) (errs error) {
 	if opts.helmReleaseName == "" {
 		return errors.New("must supply a helm release name for cleanup")
@@ -551,14 +577,7 @@ func (c *K8sCluster) deleteKumaViaHelm(opts *deployOptions) (errs error) {
 
 	// HELM does not remove CRDs therefore we need to do it manually.
 	// It's important to remove CRDs to get rid of all "instances" of CRDs like default Mesh etc.
-	crdsYAML, err := k8s.RunKubectlAndGetOutputE(c.t, c.GetKubectlOptions(), "get", "crds", "-o", "yaml")
-	if err != nil {
-		errs = multierr.Append(errs, err)
-	} else if err := k8s.KubectlDeleteFromStringE(c.t, c.GetKubectlOptions(), crdsYAML); err != nil {
-		errs = multierr.Append(errs, err)
-	}
-
-	return errs
+	return c.deleteCRDs()
 }
 
 func (c *K8sCluster) deleteKumaViaKumactl(opts *deployOptions) error {
