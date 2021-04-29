19f148d6875f28097dd5cae40eff731ff4fd4316
Nikolay Nikolaev <nikolay.nikolaev@konghq.com>
2021-04-29 21:44:41 +0300

    chore(release) customize KUMA_DOCKER_REPO_ORG
    Signed-off-by: Nikolay Nikolaev <nikolay.nikolaev@konghq.com>

--- tools/releases/docker.sh
+++ tools/releases/docker.sh
@@ -3,7 +3,7 @@
 set -e
 
 [ -z "$KUMA_DOCKER_REPO" ] && KUMA_DOCKER_REPO="docker.io"
-KUMA_DOCKER_REPO_ORG=${KUMA_DOCKER_REPO}/kumahq
+[ -z "$KUMA_DOCKER_REPO_ORG" ] && KUMA_DOCKER_REPO_ORG=${KUMA_DOCKER_REPO}/kumahq
 KUMA_COMPONENTS=("kuma-cp" "kuma-dp" "kumactl" "kuma-init" "kuma-prometheus-sd")
 
 function msg_green() {
