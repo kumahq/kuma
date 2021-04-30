e1ae06a41a35fc23e5e485e53d2b2b3d60117e3f
Bart Smykla <bartek@smykla.com>
2021-04-30 16:51:08 +0200

    fix(kuma-cp) check for xds.Context in Bypass hook (#1914)
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/xds/hooks/resource_set.go
+++ pkg/xds/hooks/resource_set.go
@@ -9,6 +9,10 @@ import (
 // Since resourceSet is an argument, you can add new, remove or modify the existing resources
 // To support V2 and V3, search the resource set for typeUrls for proper version
 // If you want to for example modify only inbound listeners, search for the resource origin of OriginInbound
+//
+// WARNING: Please, be aware that Hooks are being called also in Ingress Dataplanes
+//			and when method Modify is being called, the passed MeshContext
+//			inside xds_context.Context will be empty
 type ResourceSetHook interface {
 	Modify(resourceSet *core_xds.ResourceSet, ctx xds_context.Context, proxy *core_xds.Proxy) error
 }
