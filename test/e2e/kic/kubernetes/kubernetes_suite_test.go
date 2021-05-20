b6597dc2144485c4825df738595fbdf7482cef38
Paul Parkanzky <paul.parkanzky@konghq.com>
2021-05-20 14:21:08 -0400

    tests(kumactl) Check that k8s test don't run without cluster
    Signed-off-by: Paul Parkanzky <paul.parkanzky@konghq.com>

--- test/e2e/kic/kubernetes/kubernetes_suite_test.go
+++ test/e2e/kic/kubernetes/kubernetes_suite_test.go
@@ -3,11 +3,17 @@ package kubernetes_test
 import (
 	"testing"
 
+	"github.com/kumahq/kuma/test/framework"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 func TestE2EKICKubernetes(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "KIC Kubernetes Suite")
+	if framework.IsK8sClustersStarted() {
+		RegisterFailHandler(Fail)
+		RunSpecs(t, "KIC Kubernetes Suite")
+	} else {
+		t.SkipNow()
+	}
 }
