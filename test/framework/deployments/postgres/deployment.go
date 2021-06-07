33c8b30477146e8e9318b27ac842d1df78bfee29
Bart Smykla <bartek@smykla.com>
2021-06-07 12:12:31 +0200

    chore(e2e) decouple deployments for postgres
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- test/framework/deployments/postgres/deployment.go
+++ test/framework/deployments/postgres/deployment.go
@@ -6,12 +6,25 @@ import (
 	. "github.com/kumahq/kuma/test/framework"
 )
 
-func Install() InstallFunc {
+type Postgres interface {
+	GetEnvVars() map[string]string
+}
+
+type PostgresDeployment interface {
+	Postgres
+	Deployment
+}
+
+func From(cluster Cluster, name string) Postgres {
+	return cluster.Deployment(AppPostgres + name).(Postgres)
+}
+
+func Install(name string) InstallFunc {
 	return func(cluster Cluster) error {
-		var deployment Deployment
+		var deployment PostgresDeployment
 		switch cluster.(type) {
 		case *UniversalCluster:
-			deployment = NewUniversalDeployment(cluster)
+			deployment = NewUniversalDeployment(cluster, name)
 		case *K8sCluster:
 			return errors.New("kubernetes cluster not supported for postgres deployment")
 		default:
