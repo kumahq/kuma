936a012e3f11cabc0f8570e23f6c289fd33d4fef
Ilya Lobkov <ilya.lobkov@konghq.com>
2021-06-15 22:00:25 +0700

    fix(kuma-cp) add test, fix logs
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- pkg/defaults/signing_key.go
+++ pkg/defaults/signing_key.go
@@ -12,21 +12,22 @@ import (
 func (d *defaultsComponent) createZoneIngressSigningKeyIfNotExist(ctx context.Context) error {
 	signingKey, err := zoneingress.CreateSigningKey()
 	if err != nil {
-		return errors.Wrap(err, "could not create a signing key")
+		return errors.Wrap(err, "could not create a Zone Ingress signing key")
 	}
 	key := zoneingress.SigningKeyResourceKey()
 	err = d.resManager.Get(ctx, signingKey, core_store.GetBy(key))
 	if err == nil {
+		log.V(1).Info("Zone Ingress signing key already exists. Skip creating zone ingress signing key.")
 		return nil
 	}
 	if !core_store.IsResourceNotFound(err) {
 		return errors.Wrap(err, "could not retrieve a resource")
 	}
-	log.Info("trying to create zone ingress signing key")
+	log.Info("trying to create a Zone Ingress signing key")
 	if err := d.resManager.Create(ctx, signingKey, core_store.CreateBy(key)); err != nil {
-		log.V(1).Info("could not create Zone Ingress signing key", "err", err)
+		log.V(1).Info("could not create a Zone Ingress signing key", "err", err)
 		return errors.Wrap(err, "could not create a resource")
 	}
-	log.Info("zone ingress signing key created")
+	log.Info("Zone Ingress signing key created")
 	return nil
 }
