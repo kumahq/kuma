c46f0727633e71b6ec304e0ca024b4da0e574042
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2021-06-17 13:16:41 +0200

    chore(*) Kuma 1.2.0
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- app/kumactl/cmd/install/install_tracing_test.go
+++ app/kumactl/cmd/install/install_tracing_test.go
@@ -2,7 +2,6 @@ package install_test
 
 import (
 	"bytes"
-	"fmt"
 	"path/filepath"
 
 	. "github.com/onsi/ginkgo"
@@ -40,7 +39,6 @@ var _ = Describe("kumactl install tracing", func() {
 
 			// then
 			Expect(err).ToNot(HaveOccurred())
-			fmt.Println(stderr.String())
 			Expect(stderr.Bytes()).To(BeNil())
 
 			// and output matches golden files
