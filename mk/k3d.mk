afb7df3547ea1ee6eb5e169d2d27b87dbf70f397
James Peach <james.peach@konghq.com>
2021-07-09 09:16:51 +1000

    fix(ci) set KUBECONFIG for kumactl install
    In #2084, we unexported KUBECONFIG so that targets would not use
    the setting from the external environment. However, "kumactl install"
    depends on KUBECONFIG, and not all the makefile targets were setting it
    explictly. Update the remaining uses so that "kumactl install" always
    targets the correct test cluster.
    
    Signed-off-by: James Peach <james.peach@konghq.com>

--- mk/k3d.mk
+++ mk/k3d.mk
@@ -50,7 +50,7 @@ k3d/load: image/kuma-cp image/kuma-dp image/kuma-init image/kuma-prometheus-sd i
 
 .PHONY: k3d/deploy/kuma
 k3d/deploy/kuma: build/kumactl k3d/load
-	@${BUILD_ARTIFACTS_DIR}/kumactl/kumactl install --mode $(KUMA_MODE) control-plane $(KUMACTL_INSTALL_CONTROL_PLANE_IMAGES) | KUBECONFIG=$(KIND_KUBECONFIG)  kubectl apply -f -
+	@KUBECONFIG=$(KIND_KUBECONFIG) $(BUILD_ARTIFACTS_DIR)/kumactl/kumactl install --mode $(KUMA_MODE) control-plane $(KUMACTL_INSTALL_CONTROL_PLANE_IMAGES) | KUBECONFIG=$(KIND_KUBECONFIG)  kubectl apply -f -
 	@KUBECONFIG=$(KIND_KUBECONFIG) kubectl wait --timeout=60s --for=condition=Available -n $(KUMA_NAMESPACE) deployment/kuma-control-plane
 	@KUBECONFIG=$(KIND_KUBECONFIG) kubectl wait --timeout=60s --for=condition=Ready -n $(KUMA_NAMESPACE) pods -l app=kuma-control-plane
 	@KUBECONFIG=$(KIND_KUBECONFIG) kumactl install dns | kubectl apply -f -
