dbe8366ded52d70c3f63ea3140adde70cb7731d4
James Peach <james.peach@konghq.com>
2021-07-30 17:35:21 +1000

    chore(*) add initial gateway plugin structure (#2412)
    Add a skeleton gateway plugin that can be enabled by a build-time
    feature flag. It is always enabled for tests, but disabled by default
    for other builds.
    
    Stub out some structure for Kuma gateway profile generators. We expect
    to need a listener generator and a route generator, and it probably make
    sense to separate them.
    
    Signed-off-by: James Peach <james.peach@konghq.com>

---
+++ pkg/plugins/runtime/gateway/listener_generator_test.go
@@ -0,0 +1,60 @@
+package gateway_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	envoy_types "github.com/envoyproxy/go-control-plane/pkg/cache/types"
+
+	core_mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
+	core_model "github.com/kumahq/kuma/pkg/core/resources/model"
+	"github.com/kumahq/kuma/pkg/core/runtime"
+	"github.com/kumahq/kuma/pkg/core/xds"
+	xds_context "github.com/kumahq/kuma/pkg/xds/context"
+	"github.com/kumahq/kuma/pkg/xds/envoy"
+	xds_server "github.com/kumahq/kuma/pkg/xds/server/v3"
+)
+
+var _ = Describe("Gateway Listener", func() {
+	var rt runtime.Runtime
+
+	BeforeEach(func() {
+		var err error
+
+		rt, err = BuildRuntime()
+		Expect(err).To(Succeed(), "build runtime instance")
+
+		Expect(StoreNamedFixture(rt, "mesh-default.yaml"))
+		Expect(StoreNamedFixture(rt, "dataplane-default.yaml"))
+	})
+
+	It("should not be implemented", func() {
+		serverCtx := xds_server.NewXdsContext()
+		reconciler := xds_server.DefaultReconciler(rt, serverCtx)
+
+		dp, err := FetchNamedFixture(rt, core_mesh.DataplaneType,
+			core_model.ResourceKey{Mesh: "default", Name: "default"})
+		Expect(err).To(Succeed())
+
+		proxy := xds.Proxy{
+			Id:         xds.FromResourceKey(core_model.MetaToResourceKey(dp.GetMeta())),
+			APIVersion: envoy.APIV3,
+			Dataplane:  dp.(*core_mesh.DataplaneResource),
+			Metadata:   nil,
+			Routing:    xds.Routing{},
+			Policies:   xds.MatchedPolicies{},
+		}
+
+		Expect(proxy.Dataplane.Spec.IsBuiltinGateway()).To(BeTrue())
+
+		// The gateway listener generator isn't implemented,
+		// so we expect this should fail.
+		err = reconciler.Reconcile(xds_context.Context{}, &proxy)
+		Expect(err).To(Not(Succeed()))
+		Expect(err.Error()).To(ContainSubstring("not implemented"))
+
+		snap, err := serverCtx.Cache().GetSnapshot(proxy.Id.String())
+		Expect(err).To(Not(Succeed()))
+		Expect(snap.Resources[envoy_types.Listener].Items).To(BeEmpty())
+	})
+})
