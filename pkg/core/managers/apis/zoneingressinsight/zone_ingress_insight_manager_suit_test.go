cc10d196a33a9b3bb093eb8887cc8a9b587b6c1b
Ilya Lobkov <ilya.lobkov@konghq.com>
2021-08-05 14:58:49 +0700

    fix(kuma-cp) add owner when create ZoneIngressInsight (#2456)

---
+++ pkg/core/managers/apis/zoneingressinsight/zone_ingress_insight_manager_suit_test.go
@@ -0,0 +1,11 @@
+package zoneingressinsight_test
+
+import (
+	"testing"
+
+	"github.com/kumahq/kuma/pkg/test"
+)
+
+func TestZoneIngressInsightManager(t *testing.T) {
+	test.RunSpecs(t, "Zone Ingress Insights Manager Suite")
+}
