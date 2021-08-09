f976e93282c6552a2eae66c76511b3de7064e5e0
Ilya Lobkov <lobkovilya@yandex.ru>
2021-08-09 18:05:18 +0700

    fix(kuma-cp) add unit test
    Signed-off-by: Ilya Lobkov <lobkovilya@yandex.ru>

--- pkg/gc/finalizer_test.go
+++ pkg/gc/finalizer_test.go
@@ -19,6 +19,8 @@ import (
 )
 
 var _ = Describe("Subscription Finalizer", func() {
+	sampleTime, _ := time.Parse(time.RFC3339, "2019-07-01T00:00:00+00:00")
+
 	var rm core_manager.ResourceManager
 	var finalizer component.Component
 
@@ -38,7 +40,6 @@ var _ = Describe("Subscription Finalizer", func() {
 	}
 
 	createZoneInsight := func() {
-		sampleTime, _ := time.Parse(time.RFC3339, "2019-07-01T00:00:00+00:00")
 		Expect(rm.Create(context.Background(), &system.ZoneInsightResource{
 			Spec: &system_proto.ZoneInsight{
 				Subscriptions: []*system_proto.KDSSubscription{
@@ -78,6 +79,22 @@ var _ = Describe("Subscription Finalizer", func() {
 		Expect(rm.Update(context.Background(), zoneInsight)).To(Succeed())
 	}
 
+	addNewSubscription := func() {
+		zoneInsight := system.NewZoneInsightResource()
+		Expect(
+			rm.Get(context.Background(), zoneInsight, store.GetByKey("zone-1", core_model.NoMesh)),
+		).To(Succeed())
+		zoneInsight.Spec.GetLastSubscription().SetDisconnectTime(sampleTime.Add(2 * time.Hour))
+		zoneInsight.Spec.Subscriptions = append(zoneInsight.Spec.Subscriptions, &system_proto.KDSSubscription{
+			Id:               "stream-id-3",
+			GlobalInstanceId: "cp-1",
+			ConnectTime:      proto.MustTimestampProto(sampleTime.Add(2 * time.Hour)),
+			Status:           system_proto.NewSubscriptionStatus(),
+			Generation:       0,
+		})
+		Expect(rm.Update(context.Background(), zoneInsight)).To(Succeed())
+	}
+
 	It("should finalize subscription after idle timeout", func() {
 		stop := make(chan struct{})
 		defer close(stop)
@@ -99,5 +116,25 @@ var _ = Describe("Subscription Finalizer", func() {
 		// finalizer should observe the generation didn't change and set DisconnectTime
 		ticks <- time.Time{}
 		Eventually(isOnline, "5s", "100ms").Should(BeFalse())
+	})
+
+	It("should not finalize subscription if generation is the same, but subscription id was changed", func() {
+		stop := make(chan struct{})
+		defer close(stop)
+		ticks := make(chan time.Time)
+		defer close(ticks)
+		startSubscriptionFinalizer(ticks, stop)
+
+		createZoneInsight()
+
+		// finalizer should memorize the current generation = 0
+		ticks <- time.Time{}
+		Eventually(isOnline, "5s", "100ms").Should(BeTrue())
+
+		addNewSubscription()
+
+		// generation is the same, but last subscriptionId was changed
+		ticks <- time.Time{}
+		Eventually(isOnline, "5s", "100ms").Should(BeTrue())
 	})
 })
