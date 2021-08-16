3f3c715ae2610a763c36edcdf59d1053b20fe56a
Charly Molter <charly.molter@konghq.com>
2021-08-16 15:53:08 +0200

    Feat(virtual-outbound): Add entity for virtual-outbound
    It currently doesn't do anything
    
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

---
+++ pkg/core/resources/apis/mesh/virtual_outbound_validator.go
@@ -0,0 +1,104 @@
+package mesh
+
+import (
+	"fmt"
+	"regexp"
+
+	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
+	"github.com/kumahq/kuma/pkg/core/validators"
+)
+
+func (t *VirtualOutboundResource) Validate() error {
+	var err validators.ValidationError
+	err.Add(t.validateSelectors())
+	err.Add(t.ValidateConf())
+	return err.OrNil()
+}
+
+func (t *VirtualOutboundResource) validateSelectors() validators.ValidationError {
+	return ValidateSelectors(validators.RootedAt("selectors"), t.Spec.GetSelectors(), ValidateSelectorsOpts{
+		RequireAtLeastOneSelector: true,
+		ValidateSelectorOpts: ValidateSelectorOpts{
+			RequireAtLeastOneTag: true,
+			RequireService:       true,
+		},
+	})
+}
+
+func (t *VirtualOutboundResource) ValidateConf() (err validators.ValidationError) {
+	root := validators.RootedAt("conf")
+	// host, port, parameters
+	conf := t.Spec.GetConf()
+
+	if conf == nil {
+		err.AddViolationAt(root, HasToBeDefinedViolation)
+		return
+	}
+	err.Add(t.validateParameters(root.Field("parameters")))
+	err.Add(t.validateHost(root.Field("host")))
+	err.Add(t.validatePort(root.Field("port")))
+	return
+}
+
+var parameterKeyName = regexp.MustCompile(`^[a-zA-Z0-9]+$`)
+
+func (t *VirtualOutboundResource) validateParameters(path validators.PathBuilder) (err validators.ValidationError) {
+	hasService := false
+	keys := map[string]bool{}
+	for k, v := range t.Spec.Conf.Parameters {
+		p := path.Index(k)
+		if keys[v.Name] {
+			err.AddViolationAt(p.Field("name"), `name is already used`)
+		}
+		keys[v.Name] = true
+		if v.TagKey != "" {
+			hasService = hasService || v.TagKey == mesh_proto.ServiceTag
+			if !tagNameCharacterSet.MatchString(v.TagKey) {
+				err.AddViolationAt(p.Field("tagKey"), `must be a valid tag name`)
+			}
+		} else if !tagNameCharacterSet.MatchString(v.Name) {
+			err.AddViolationAt(p.Field("name"), `must be a valid tag name if you are not setting tagKey`)
+		}
+		if !parameterKeyName.MatchString(v.Name) {
+			err.AddViolationAt(p.Field("name"), `must consist of alphanumeric characters to be used as a gotemplate key`)
+		}
+	}
+	if !hasService {
+		err.AddViolationAt(path, fmt.Sprintf(`must contain a parameter with %s as a tagKey`, mesh_proto.ServiceTag))
+	}
+	return
+}
+
+func (t *VirtualOutboundResource) validateHost(path validators.PathBuilder) (err validators.ValidationError) {
+	h := t.Spec.Conf.Host
+	if h == "" {
+		err.AddViolationAt(path, HasToBeDefinedViolation)
+		return
+	}
+	fakeTags := map[string]string{}
+	for _, v := range t.Spec.Conf.Parameters {
+		fakeTags[tagKeyOrName(v)] = "dummy"
+	}
+	_, lerr := t.EvalHost(fakeTags)
+	if lerr != nil {
+		err.AddViolationAt(path, fmt.Sprintf("template pre evaluation failed with error='%s'", lerr.Error()))
+	}
+	return
+}
+
+func (t *VirtualOutboundResource) validatePort(path validators.PathBuilder) (err validators.ValidationError) {
+	h := t.Spec.Conf.Port
+	if h == "" {
+		err.AddViolationAt(path, HasToBeDefinedViolation)
+		return
+	}
+	fakeTags := map[string]string{}
+	for _, v := range t.Spec.Conf.Parameters {
+		fakeTags[tagKeyOrName(v)] = "1"
+	}
+	_, lerr := t.EvalPort(fakeTags)
+	if lerr != nil {
+		err.AddViolationAt(path, fmt.Sprintf("template pre evaluation failed with error='%s'", lerr.Error()))
+	}
+	return
+}
---
+++ pkg/core/resources/apis/mesh/virtual_outbound_validator_test.go
@@ -0,0 +1,328 @@
+package mesh_test
+
+import (
+	"github.com/ghodss/yaml"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+
+	. "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
+	util_proto "github.com/kumahq/kuma/pkg/util/proto"
+)
+
+var _ = Describe("VirtualOutbound_validator", func() {
+	DescribeTable("should pass validation",
+		func(in string) {
+			// setup
+			virtualOutbound := NewVirtualOutboundResource()
+
+			// when
+			err := util_proto.FromYAML([]byte(in), virtualOutbound.Spec)
+			// then
+			Expect(err).ToNot(HaveOccurred())
+
+			// when
+			verr := virtualOutbound.Validate()
+
+			// then
+			Expect(verr).ToNot(HaveOccurred())
+		},
+		Entry("full example", `
+                selectors:
+                - match:
+                    region: eu
+                    kuma.io/service: "*"
+                conf:
+                  host: "foo.bar"
+                  port: "80"
+                  parameters:
+                    - name: "service"
+                      tagKey: "kuma.io/service"`,
+		),
+		Entry("empty host", `
+                selectors:
+                - match:
+                    region: eu
+                    kuma.io/service: "*"
+                conf:
+                  host: "{{.service}}.mesh"
+                  port: "{{.port}}"
+                  parameters:
+                    - name: "service"
+                      tagKey: kuma.io/service
+                    - name: "port"
+                      tagKey: kuma.io/port
+`,
+		),
+	)
+
+	type testCase struct {
+		input    string
+		expected string
+	}
+	DescribeTable("should validate all fields and return as much individual errors as possible",
+		func(given testCase) {
+			// setup
+			virtualOutbound := NewVirtualOutboundResource()
+
+			// when
+			err := util_proto.FromYAML([]byte(given.input), virtualOutbound.Spec)
+			// then
+			Expect(err).ToNot(HaveOccurred())
+
+			// when
+			verr := virtualOutbound.Validate()
+			// and
+			actual, err := yaml.Marshal(verr)
+
+			// then
+			Expect(err).ToNot(HaveOccurred())
+			// and
+			Expect(actual).To(MatchYAML(given.expected))
+		},
+		Entry("empty spec", testCase{
+			input: ``,
+			expected: `
+          violations:
+          - field: selectors
+            message: must have at least one element
+          - field: conf
+            message: has to be defined
+`,
+		}),
+		Entry("selectors without tags", testCase{
+			input: `
+                selectors:
+                - match: {}
+                conf:
+                  host: "foo.mesh"
+                  port: "80"
+                  parameters:
+                  - name: "service"
+                    tagKey: "kuma.io/service"
+`,
+			expected: `
+                violations:
+                  - field: selectors[0].match
+                    message: must have at least one tag
+                  - field: selectors[0].match
+                    message: mandatory tag "kuma.io/service" is missing
+`,
+		}),
+		Entry("selectors with empty tags values", testCase{
+			input: `
+                selectors:
+                - match:
+                    kuma.io/service:
+                    region:
+                conf:
+                  host: "foo.mesh"
+                  port: "80"
+                  parameters:
+                  - name: "service"
+                    tagKey: "kuma.io/service"
+`,
+			expected: `
+                violations:
+                - field: selectors[0].match["kuma.io/service"]
+                  message: tag value must be non-empty
+                - field: selectors[0].match["region"]
+                  message: tag value must be non-empty
+`,
+		}),
+		Entry("multiple selectors", testCase{
+			input: `
+                selectors:
+                - match:
+                    kuma.io/service:
+                    region:
+                - match: {}
+                conf:
+                  host: "foo.mesh"
+                  port: "80"
+                  parameters:
+                  - name: "service"
+                    tagKey: "kuma.io/service"
+`,
+			expected: `
+                violations:
+                - field: selectors[0].match["kuma.io/service"]
+                  message: tag value must be non-empty
+                - field: selectors[0].match["region"]
+                  message: tag value must be non-empty
+                - field: selectors[1].match
+                  message: must have at least one tag
+                - field: selectors[1].match
+                  message: mandatory tag "kuma.io/service" is missing
+`,
+		}),
+		Entry("bad host template", testCase{
+			input: `
+                selectors:
+                - match:
+                    kuma.io/service: "*"
+                conf:
+                  host: "foo.{{mesh"
+                  port: "80"
+                  parameters:
+                  - name: "service"
+                    tagKey: "kuma.io/service"
+`,
+			expected: `
+                violations:
+                - field: conf.host
+                  message: 'template pre evaluation failed with error=''failed compiling gotemplate error=''template: :1: function "mesh" not defined'''''
+`,
+		}),
+		Entry("bad port template", testCase{
+			input: `
+                selectors:
+                - match:
+                    kuma.io/service: "*"
+                conf:
+                  host: "foo.mesh"
+                  port: "{{port"
+                  parameters:
+                  - name: "service"
+                    tagKey: "kuma.io/service"
+`,
+			expected: `
+                violations:
+                - field: conf.port
+                  message: 'template pre evaluation failed with error=''failed compiling gotemplate error=''template: :1: function "port" not defined'''''
+`,
+		}),
+		Entry("port is not a number template", testCase{
+			input: `
+                selectors:
+                - match:
+                    kuma.io/service: "*"
+                conf:
+                  host: "foo.mesh"
+                  port: "{{.port}}a"
+                  parameters:
+                  - name: port
+                    tagKey: "service"
+                  - name: "service"
+                    tagKey: "kuma.io/service"
+`,
+			expected: `
+                violations:
+                - field: conf.port
+                  message: template pre evaluation failed with error='evaluation of template with parameters didn't evaluate to a parsable number result='1a''
+`,
+		}),
+		Entry("parameter is not good tag", testCase{
+			input: `
+                selectors:
+                - match:
+                    kuma.io/service: "*"
+                conf:
+                  host: "foo.mesh"
+                  port: "80"
+                  parameters:
+                    - name: port
+                      tagKey: "***"
+                    - name: service
+                      tagKey: "kuma.io/service"
+`,
+			expected: `
+                violations:
+                - field: 'conf.parameters[0].tagKey'
+                  message: must be a valid tag name
+`,
+		}),
+		Entry("parameter is not good template entry", testCase{
+			input: `
+                selectors:
+                - match:
+                    kuma.io/service: "*"
+                conf:
+                  host: "foo.mesh"
+                  port: "80"
+                  parameters:
+                  - name: _
+                    tagKey: "kuma.io/service"
+`,
+			expected: `
+                violations:
+                - field: 'conf.parameters[0].name'
+                  message: must consist of alphanumeric characters to be used as a gotemplate key
+`,
+		}),
+		Entry("parameter is not good template entry", testCase{
+			input: `
+                selectors:
+                - match:
+                    kuma.io/service: "*"
+                conf:
+                  host: "foo.mesh"
+                  port: "80"
+                  parameters:
+                  - name: srv
+                    tagKey: "kuma.io/service"
+                  - name: "kuma.io/port"
+`,
+			expected: `
+                violations:
+                - field: 'conf.parameters[1].name'
+                  message: must consist of alphanumeric characters to be used as a gotemplate key
+`,
+		}),
+		Entry("duplicate name not allowed", testCase{
+			input: `
+                selectors:
+                - match:
+                    kuma.io/service: "*"
+                conf:
+                  host: "foo.mesh"
+                  port: "80"
+                  parameters:
+                  - name: srv
+                    tagKey: "kuma.io/service"
+                  - name: srv
+`,
+			expected: `
+                violations:
+                - field: 'conf.parameters[1].name'
+                  message: name is already used
+`,
+		}),
+		Entry("empty parameter is not good template entry", testCase{
+			input: `
+                selectors:
+                - match:
+                    kuma.io/service: "*"
+                conf:
+                  host: "foo.mesh"
+                  port: "80"
+                  parameters:
+                    - name: ""
+                      tagKey: "kuma.io/service"
+`,
+			expected: `
+                violations:
+                - field: 'conf.parameters[0].name'
+                  message: must consist of alphanumeric characters to be used as a gotemplate key
+`,
+		}),
+		Entry("missing service tag in parameters", testCase{
+			input: `
+                selectors:
+                - match:
+                    kuma.io/service: "*"
+                conf:
+                  host: "foo.mesh"
+                  port: "80"
+                  parameters:
+                    - name: "foo"
+                      tagKey: "kuma.io/port"
+`,
+			expected: `
+                violations:
+                - field: conf.parameters
+                  message: must contain a parameter with kuma.io/service as a tagKey
+`,
+		}),
+	)
+})
