19f480396ce0351905af2b63bd1551f9defffe8e
Bart Smykla <bartek@smykla.com>
2021-08-18 12:49:10 +0200

    fix(e2e) get kds address for k8s fix (#2574)
    it was wrong, as with just UseLoadBalancer set it was ignored
    and the local port of kuma-cp used instead, which broke e2e tests
    on AKS
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- test/framework/k8s_controlplane.go
+++ test/framework/k8s_controlplane.go
@@ -226,16 +226,13 @@ func (c *K8sControlPlane) GetKDSServerAddress() string {
 	// As EKS and AWS generally returns dns records of load balancers instead of
 	//  IP addresses, accessing this data (hostname) was only tested there,
 	//  so the env var was created for that purpose
-	if UseLoadBalancer() && UseHostnameInsteadOfIP() {
+	if UseLoadBalancer() {
 		svc := c.GetKumaCPSvcs()[0]
 
-		ip, hostname := svc.Status.LoadBalancer.Ingress[0].IP, svc.Status.LoadBalancer.Ingress[0].Hostname
-		var address string
+		address := svc.Status.LoadBalancer.Ingress[0].IP
 
-		if ip != "" {
-			address = ip
-		} else if hostname != "" {
-			address = hostname
+		if UseHostnameInsteadOfIP() {
+			address = svc.Status.LoadBalancer.Ingress[0].Hostname
 		}
 
 		return "grpcs://" + address + ":" + strconv.FormatUint(loadBalancerKdsPort, 10)
