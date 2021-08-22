9344d1848cb4af7f6b3437114d4a601a93a4d833
Paul Parkanzky <paul.parkanzky@konghq.com>
2021-08-22 16:40:42 -0400

    feat(kuma-cp) Add k8s hooks for ratelimit validation
    Signed-off-by: Paul Parkanzky <paul.parkanzky@konghq.com>

--- pkg/plugins/runtime/k8s/plugin.go
+++ pkg/plugins/runtime/k8s/plugin.go
@@ -11,7 +11,9 @@ import (
 
 	config_core "github.com/kumahq/kuma/pkg/config/core"
 	"github.com/kumahq/kuma/pkg/core"
+	managers_externalservice "github.com/kumahq/kuma/pkg/core/managers/apis/external_service"
 	managers_mesh "github.com/kumahq/kuma/pkg/core/managers/apis/mesh"
+	managers_ratelimit "github.com/kumahq/kuma/pkg/core/managers/apis/ratelimit"
 	"github.com/kumahq/kuma/pkg/core/managers/apis/zone"
 	core_plugins "github.com/kumahq/kuma/pkg/core/plugins"
 	core_mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
@@ -245,6 +247,18 @@ func addValidators(mgr kube_ctrl.Manager, rt core_runtime.Runtime, converter k8s
 	}
 	k8sMeshValidator := k8s_webhooks.NewMeshValidatorWebhook(coreMeshValidator, converter, rt.ResourceManager())
 	composite.AddValidator(k8sMeshValidator)
+
+	rateLimitValidator := managers_ratelimit.RateLimitValidator{
+		Store: rt.ResourceStore(),
+	}
+	k8sRateLimitValidator := k8s_webhooks.NewRateLimitValidatorWebhook(rateLimitValidator, converter)
+	composite.AddValidator(k8sRateLimitValidator)
+
+	externalServiceValidator := managers_externalservice.ExternalServiceValidator{
+		Store: rt.ResourceStore(),
+	}
+	k8sExternalServiceValidator := k8s_webhooks.NewExternalServiceValidatorWebhook(externalServiceValidator, converter)
+	composite.AddValidator(k8sExternalServiceValidator)
 
 	coreZoneValidator := zone.Validator{Store: rt.ResourceStore()}
 	k8sZoneValidator := k8s_webhooks.NewZoneValidatorWebhook(coreZoneValidator)
