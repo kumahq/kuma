7fbb40b06d9acaaf55d90e82076bd58d72bd7105
Ilya Lobkov <ilya.lobkov@konghq.com>
2021-08-25 12:04:10 +0200

    fix(kuma-cp) get rid of regex for parsing IPs (#2681)
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>
    (cherry picked from commit cd08d733d3092be4d6a35fabd1c633aed0608834)

---
+++ test/e2e/mtls/permissive/permissive_mode_test.go
@@ -0,0 +1,9 @@
+package permissive_test
+
+import (
+	. "github.com/onsi/ginkgo"
+
+	"github.com/kumahq/kuma/test/e2e/mtls/permissive"
+)
+
+var _ = Describe("Test Permissive mTLS", permissive.PermissiveMode)
