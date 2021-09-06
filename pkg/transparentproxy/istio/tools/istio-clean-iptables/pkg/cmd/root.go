1e9f063176a0397610c3b05a02f75ef16ded63cf
James Peach <james.peach@konghq.com>
2021-09-07 09:44:49 +1000

    Fix tests.
    Signed-off-by: James Peach <james.peach@konghq.com>

--- pkg/transparentproxy/istio/tools/istio-clean-iptables/pkg/cmd/root.go
+++ pkg/transparentproxy/istio/tools/istio-clean-iptables/pkg/cmd/root.go
@@ -155,6 +155,8 @@ func init() {
 	rootCmd.Flags().Bool(constants.RedirectAllDNSTraffic, false, "Enable capture of all dns traffic by istio-agent ")
 
 	rootCmd.Flags().String(constants.AgentDNSListenerPort, constants.IstioAgentDNSListenerPort, "set listen port for DNS agent")
+
+	rootCmd.Flags().String(constants.DNSUpstreamTargetChain, constants.RETURN, "(optional) the iptables chain where the upstream DNS requests should be directed to. It is only applied for IP V4. Use with care.")
 }
 
 func GetCommand() *cobra.Command {
