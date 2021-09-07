e01685a2df6b268cd82c1c0f332ca4d3d2d33a35
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2021-09-07 09:46:28 +0200

    fix(kuma-cp) test versions compatibility on the release (#2707)
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
    (cherry picked from commit 8ae816b9714831294e28f8a98ee4415df1c174d9)

--- pkg/api-server/versions_ws.go
+++ pkg/api-server/versions_ws.go
@@ -2,52 +2,16 @@ package api_server
 
 import (
 	"github.com/emicklei/go-restful"
-)
 
-var Versions = []byte(`{
-  "kumaDp": {
-    "1.0.0": {
-      "envoy": "1.16.0"
-    },
-    "1.0.1": {
-      "envoy": "1.16.0"
-    },
-    "1.0.2": {
-      "envoy": "1.16.1"
-    },
-    "1.0.3": {
-      "envoy": "1.16.1"
-    },
-    "1.0.4": {
-      "envoy": "1.16.1"
-    },
-    "1.0.5": {
-      "envoy": "1.16.2"
-    },
-    "1.0.6": {
-      "envoy": "1.16.2"
-    },
-    "1.0.7": {
-      "envoy": "1.16.2"
-    },
-    "1.0.8": {
-      "envoy": "1.16.2"
-    },
-    "~1.1.0": {
-      "envoy": "~1.17.0"
-    },
-    "~1.2.0": {
-      "envoy": "~1.18.0"
-    }
-  }
-}`)
+	"github.com/kumahq/kuma/pkg/version"
+)
 
 func versionsWs() *restful.WebService {
 	ws := new(restful.WebService).Path("/versions")
 
 	ws.Route(ws.GET("").To(func(req *restful.Request, resp *restful.Response) {
 		resp.AddHeader("content-type", "application/json")
-		if _, err := resp.Write(Versions); err != nil {
+		if err := resp.WriteAsJson(version.CompatibilityMatrix); err != nil {
 			log.Error(err, "Could not write the index response")
 		}
 	}))
