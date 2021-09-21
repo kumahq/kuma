9c47cef75558732ee02f50212a5f7229fbd4b9a2
James Peach <james.peach@konghq.com>
2021-09-21 13:58:58 +1000

    feat(kuma-cp) generate GatewayRoute clusters
    Add cluster and load assignment support to the Gatewayroute generator.
    This makes simple end-to-end Gateway configuratinos work.
    
    Signed-off-by: James Peach <james.peach@konghq.com>

--- pkg/plugins/runtime/gateway/route_table_generator.go
+++ pkg/plugins/runtime/gateway/route_table_generator.go
@@ -1,12 +1,19 @@
 package gateway
 
 import (
+	"context"
 	"sort"
 
+	"github.com/pkg/errors"
+
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
+	"github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
 	core_xds "github.com/kumahq/kuma/pkg/core/xds"
 	"github.com/kumahq/kuma/pkg/plugins/runtime/gateway/route"
 	xds_context "github.com/kumahq/kuma/pkg/xds/context"
+	"github.com/kumahq/kuma/pkg/xds/envoy"
+	"github.com/kumahq/kuma/pkg/xds/envoy/clusters"
+	"github.com/kumahq/kuma/pkg/xds/generator"
 )
 
 // RouteTableGenerator generates Envoy xDS resources gateway routes from
@@ -19,7 +26,7 @@ func (*RouteTableGenerator) SupportsProtocol(mesh_proto.Gateway_Listener_Protoco
 }
 
 // GenerateHost generates xDS resources for the current route table.
-func (*RouteTableGenerator) GenerateHost(ctx xds_context.Context, info *GatewayResourceInfo) (*core_xds.ResourceSet, error) {
+func (r *RouteTableGenerator) GenerateHost(ctx xds_context.Context, info *GatewayResourceInfo) (*core_xds.ResourceSet, error) {
 	resources := ResourceAggregator{}
 
 	// Sort routing table entries so the most specific match comes first.
@@ -82,12 +89,128 @@ func (*RouteTableGenerator) GenerateHost(ctx xds_context.Context, info *GatewayR
 		info.Resources.VirtualHost.Configure(route.VirtualHostRoute(&routeBuilder))
 	}
 
-	// TODO(jpeach) walk the routing table and generate clusters
-	// (including the mirror cluster). Hopefully we can use the
-	// envoy.Cluster type for this.
+	destinations, err := makeRouteDestinations(&info.RouteTable)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := resources.Add(r.generateClusters(ctx, info, destinations)); err != nil {
+		return nil, err
+	}
+
+	if err := resources.Add(r.generateEndpointAssignments(ctx, info, destinations)); err != nil {
+		return nil, err
+	}
 
 	// TODO(jpeach) Once we drop TrafficRoute, generate resources
 	// for info.Resources.VirtualHost here instead of in generator.go
 
 	return resources.Get(), nil
+}
+
+func (r *RouteTableGenerator) generateEndpointAssignments(
+	ctx xds_context.Context,
+	info *GatewayResourceInfo,
+	destinations map[string]route.Destination,
+) (*core_xds.ResourceSet, error) {
+	resources := core_xds.NewResourceSet()
+
+	// TODO(jpeach) Don't generate load assignments for external services.
+
+	for name, dest := range destinations {
+		// The CLA cache needs an envoy.Cluster but only looks
+		// at the fields we populate here.
+		cluster := envoy.NewCluster(
+			envoy.WithName(name),
+			envoy.WithService(dest.Destination[mesh_proto.ServiceTag]),
+			envoy.WithTags(dest.Destination),
+		)
+
+		loadAssignment, err := ctx.ControlPlane.CLACache.GetCLA(
+			context.Background(),
+			ctx.Mesh.Resource.GetMeta().GetName(),
+			ctx.Mesh.Hash,
+			cluster,
+			info.Proxy.APIVersion,
+		)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to build LoadAssignment for cluster %q", name)
+		}
+
+		resources.Add(NewResource(name, loadAssignment))
+	}
+
+	return resources, nil
+}
+
+func (r *RouteTableGenerator) generateClusters(
+	ctx xds_context.Context,
+	info *GatewayResourceInfo,
+	destinations map[string]route.Destination,
+) (*core_xds.ResourceSet, error) {
+	resources := ResourceAggregator{}
+
+	for name, dest := range destinations {
+		protocol := generator.InferServiceProtocol([]core_xds.Endpoint{{
+			Tags: dest.Destination,
+		}})
+
+		builder := clusters.NewClusterBuilder(info.Proxy.APIVersion).Configure(
+			clusters.EdsCluster(name),
+			clusters.Timeout(protocol, nil /* TODO(jpeach) default timeout */),
+			clusters.CircuitBreaker(nil /* TODO(jpeach) uses default */),
+			clusters.OutlierDetection(nil /* TODO(jpeach) uses default */),
+			clusters.HealthCheck(protocol, nil /* TODO(jpeach) uses default */),
+			clusters.LB(nil /* TODO(jpeach) uses default Round Robin*/),
+		)
+
+		// Assuming this is an intra-Mesh service, enable mTLS.
+		builder.Configure(
+			clusters.ClientSideMTLS(ctx, dest.Destination[mesh_proto.ServiceTag], []envoy.Tags{dest.Destination}),
+		)
+
+		// TODO(jpeach) External services are "strict DNS" clusters and don't use mTLS.
+
+		switch protocol {
+		case mesh.ProtocolHTTP:
+			builder.Configure(clusters.Http())
+		case mesh.ProtocolHTTP2, mesh.ProtocolGRPC:
+			builder.Configure(clusters.Http2())
+		}
+
+		if err := resources.Add(BuildResourceSet(builder)); err != nil {
+			return nil, err
+		}
+	}
+
+	return resources.Get(), nil
+}
+
+// makeRouteDestinations builds a map of all the destinations in the
+// route table, indexed by cluster name. This de-duplicates the destinations
+// by name and ensures we only have to generate the name once.
+func makeRouteDestinations(table *route.Table) (map[string]route.Destination, error) {
+	destinations := map[string]route.Destination{}
+
+	for _, e := range table.Entries {
+		if m := e.Mirror; m != nil {
+			name, err := route.DestinationClusterName(m.Forward)
+			if err != nil {
+				return nil, err
+			}
+
+			destinations[name] = m.Forward
+		}
+
+		for _, d := range e.Action.Forward {
+			name, err := route.DestinationClusterName(d)
+			if err != nil {
+				return nil, err
+			}
+
+			destinations[name] = d
+		}
+	}
+
+	return destinations, nil
 }
