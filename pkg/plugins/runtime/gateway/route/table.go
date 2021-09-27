98166ecbe35c05d90ba10b7be4c91898a39fe659
James Peach <james.peach@konghq.com>
2021-09-27 14:35:41 +1000

    feat(kuma-cp) add GatewayRoute route generation (#2782)
    Add machinery to generate Envoy routes from GatewayRoute resources. This
    is split into a generator that translates GatewayRoute resources into a
    generic route table structure, and a generator that translates the generic
    structure into Envoy resources. The generic route table structure provides
    an API-independent way to configure Envoy.
    
    This change only generates routes (not cluster or endpoints), but the full
    semantics of GatewayRoute are supported, including path prefix matches,
    multiple match criteria and correct route match ordering.
    
    Signed-off-by: James Peach <james.peach@konghq.com>
    (cherry picked from commit 511eeb99307ee4a524c213b60ce542ec789087e7)

---
+++ pkg/plugins/runtime/gateway/route/table.go
@@ -0,0 +1,113 @@
+package route
+
+import (
+	"github.com/kumahq/kuma/pkg/xds/envoy"
+)
+
+// Table stores a collection of routing Entries, aka. a routing table.
+//
+// A route table is a simplified model of a collection of Envoy routes.
+// Although extracting a model is duplicative of the actual Envoy
+// model, this approach makes it possible to support different APIs
+// that combine to make a coherent routing table.
+//
+// The different elements of the routing table are deliberately defined
+// without any behavior or internal consistency checks. The API that
+// generates these elements should apply validation to ensure that it
+// generates route table elements that make sense and accurately capture
+// the right semantics.
+type Table struct {
+	Entries []Entry
+}
+
+// Entry is a single routing element. Incoming requests are matched by Match
+// and dispatched according to the Action. Other optional field specify
+// additional processing.
+type Entry struct {
+	Match  Match
+	Action Action
+
+	// Mirror specifies whether to mirror matching traffic.
+	Mirror *Mirror
+
+	// RequestHeaders specifies transformations on the HTTP
+	// request headers.
+	RequestHeaders *Headers
+}
+
+// KeyValue is a generic pairing of key and value strings. Route table
+// elements generally use this in preference to maps so that input ordering
+// is preserved and output does not change based on map iteration order.
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+// Pair combines key and value into a KeyValue.
+func Pair(key string, value string) KeyValue {
+	return KeyValue{
+		Key:   key,
+		Value: value,
+	}
+}
+
+// Match describes how to match a HTTP request.
+type Match struct {
+	ExactPath  string
+	PrefixPath string
+	RegexPath  string
+
+	Method string
+
+	ExactHeader []KeyValue // name -> value
+	RegexHeader []KeyValue // name -> regex
+
+	ExactQuery []KeyValue // param -> value
+	RegexQuery []KeyValue // param -> regex
+}
+
+// Action describes how a HTTP request should be dispatched.
+type Action struct {
+	Forward  []Destination
+	Redirect *Redirection
+	Respond  struct{} // TODO(jpeach) add DirectResponseAction support
+}
+
+// Redirection is an action that responds to a HTTP request with a HTTP
+// redirect response.
+type Redirection struct {
+	Status uint32 // HTTP status code.
+	Scheme string // URL scheme (optional).
+	Host   string // URL host (optional).
+	Port   uint32 // URL port (optional).
+	Path   string // URL path (optional).
+
+	StripQuery bool // Whether to strip the query string.
+}
+
+// Destination is a forwarding target (aka Cluster).
+type Destination struct {
+	Destination envoy.Tags
+	Weight      uint32
+
+	// TODO(jpeach) Kuma connection policies for traffic that is
+	// forwarded to this destination.
+	Policies []struct{}
+}
+
+// Headers is a set of operations to perform on HTTP message headers.
+type Headers struct {
+	// Append adds a value to a HTTP header field.
+	Append []KeyValue
+	// Replace adds a value to a HTTP header field, removing all other
+	// values for that field.
+	Replace []KeyValue
+	// Delete deletes a HTTP header field.
+	Delete []string
+}
+
+// Mirror specifies a traffic mirroring operation.
+type Mirror struct {
+	Forward    Destination
+	Percentage float64
+}
