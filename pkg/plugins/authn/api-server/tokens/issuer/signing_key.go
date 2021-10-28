039d5d0ec4eb8d9c9d6b89085dd26a292c9b8a10
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2021-10-28 12:32:18 +0200

    remove is
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- pkg/plugins/authn/api-server/tokens/issuer/signing_key.go
+++ pkg/plugins/authn/api-server/tokens/issuer/signing_key.go
@@ -5,7 +5,6 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"fmt"
-	"reflect"
 	"sort"
 	"strconv"
 	"strings"
@@ -34,10 +33,6 @@ type SigningKeyNotFound struct {
 
 func (s *SigningKeyNotFound) Error() string {
 	return fmt.Sprintf("there is no signing key with serial number %d. GlobalSecret of name %q is not found. If signing key was rotated, regenerate the token", s.SerialNumber, SigningKeyResourceKey(s.SerialNumber).Name)
-}
-
-func (a *SigningKeyNotFound) Is(err error) bool {
-	return reflect.TypeOf(a) == reflect.TypeOf(err)
 }
 
 func SigningKeyResourceKey(serialNumber int) model.ResourceKey {
