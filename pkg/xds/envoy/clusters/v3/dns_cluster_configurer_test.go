047f4cf372fcc401181ad29f575f81435860ce16
Sudeepto Roy <sudeepto.roy@gmail.com>
2021-11-01 19:15:32 +0530

    added test cases
    Signed-off-by: Sudeepto Roy <sudeepto.roy@gmail.com>

--- pkg/xds/envoy/clusters/v3/dns_cluster_configurer_test.go
+++ pkg/xds/envoy/clusters/v3/dns_cluster_configurer_test.go
@@ -2,6 +2,7 @@ package clusters_test
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
 	core_mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
@@ -12,12 +13,36 @@ import (
 
 var _ = Describe("DNSClusterConfigurer", func() {
 
-	It("should generate proper Envoy config", func() {
-		// given
-		clusterName := "test:cluster"
-		address := "google.com"
-		port := uint32(80)
-		expected := `
+	type testCase struct {
+		clusterName string
+		address     string
+		port        int32
+		expected    string
+		isHttp      bool
+	}
+
+	DescribeTable("should generate proper Envoy config",
+		func(given testCase) {
+			// when
+			cluster, err := clusters.NewClusterBuilder(envoy.APIV3).
+				Configure(clusters.DNSCluster(given.clusterName, given.address, uint32(given.port), given.isHttp)).
+				Configure(clusters.Timeout(core_mesh.ProtocolTCP, DefaultTimeout())).
+				Build()
+
+			// then
+			Expect(err).ToNot(HaveOccurred())
+
+			actual, err := util_proto.ToYAML(cluster)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(actual).To(MatchYAML(given.expected))
+		},
+		Entry("should generate proper Envoy config", testCase{
+			// given
+			clusterName: "test:cluster",
+			address:     "google.com",
+			port:        80,
+			isHttp:      false,
+			expected: `
         altStatName: test_cluster
         connectTimeout: 5s
         loadAssignment:
@@ -30,19 +55,35 @@ var _ = Describe("DNSClusterConfigurer", func() {
                     address: google.com
                     portValue: 80
         name: test:cluster
-        type: STRICT_DNS`
-
-		// when
-		cluster, err := clusters.NewClusterBuilder(envoy.APIV3).
-			Configure(clusters.DNSCluster(clusterName, address, port, false)).
-			Configure(clusters.Timeout(core_mesh.ProtocolTCP, DefaultTimeout())).
-			Build()
-
-		// then
-		Expect(err).ToNot(HaveOccurred())
-
-		actual, err := util_proto.ToYAML(cluster)
-		Expect(err).ToNot(HaveOccurred())
-		Expect(actual).To(MatchYAML(expected))
-	})
+        type: STRICT_DNS`,
+		}),
+		Entry("should generate proper Envoy config for https", testCase{
+			// given
+			clusterName: "test:cluster",
+			address:     "google.com",
+			port:        80,
+			isHttp:      true,
+			expected: `
+        altStatName: test_cluster
+        connectTimeout: 5s
+        loadAssignment:
+          clusterName: test:cluster
+          endpoints:
+          - lbEndpoints:
+            - endpoint:
+                address:
+                  socketAddress:
+                    address: google.com
+                    portValue: 80
+        name: test:cluster
+        type: STRICT_DNS
+        transportSocket:
+          name: envoy.transport_sockets.tls
+          typedConfig:
+            '@type': type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.UpstreamTlsContext
+            allowRenegotiation: true
+            sni: google.com
+        `,
+		}),
+	)
 })
