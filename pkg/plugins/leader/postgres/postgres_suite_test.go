ed92be4b5f6ad15dac1c79c642ac72b9bc33086f
Charly Molter <charly.molter@konghq.com>
2021-11-04 11:21:58 +0100

    chore(ci) remove some circleci jobs, skip testcontainer if no docker (#3084)
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- pkg/plugins/leader/postgres/postgres_suite_test.go
+++ pkg/plugins/leader/postgres/postgres_suite_test.go
@@ -3,9 +3,12 @@ package postgres
 import (
 	"testing"
 
+	"github.com/testcontainers/testcontainers-go"
+
 	"github.com/kumahq/kuma/pkg/test"
 )
 
 func TestPostgresLeader(t *testing.T) {
+	testcontainers.SkipIfProviderIsNotHealthy(t)
 	test.RunSpecs(t, "Postgres Leader Suite")
 }
