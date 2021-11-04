5a6d66b6e4845fed47973c9310ec66b95bd1de7e
Charly Molter <charly.molter@konghq.com>
2021-11-04 10:33:52 +0100

    chore(ci) remove some circleci jobs, skip testcontainer if no docker
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- pkg/plugins/resources/postgres/store_suite_test.go
+++ pkg/plugins/resources/postgres/store_suite_test.go
@@ -5,6 +5,7 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/testcontainers/testcontainers-go"
 
 	"github.com/kumahq/kuma/pkg/test"
 	test_postgres "github.com/kumahq/kuma/pkg/test/store/postgres"
@@ -13,6 +14,7 @@ import (
 var c test_postgres.PostgresContainer
 
 func TestPostgresStore(t *testing.T) {
+	testcontainers.SkipIfProviderIsNotHealthy(t)
 	BeforeSuite(func() {
 		c = test_postgres.PostgresContainer{WithTLS: true}
 		Expect(c.Start()).To(Succeed())
