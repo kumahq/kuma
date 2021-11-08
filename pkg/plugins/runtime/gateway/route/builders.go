f3272aa3625af12eefd6d790ad1ca360c0ceef66
James Peach <james.peach@konghq.com>
2021-11-08 15:15:43 +1100

    fix(kuma-cp) make cluster names contextually unique
    For Gateway, it is not good enough to create unique cluster names by
    combining the service name and tags. The same service name and tags can be
    referenced by different listeners and have different connection policies
    applied. When this happens, the different cluster policies are lost
    because the cluster resources are uniqified based on the resource name.
    
    The fix is to include a hash the contents of the cluster to in its name to
    guarantee that each unique cluster hane has a unique configuration. This
    makes the cluster names less friendly for humans, but gives us assurances
    that the connection policies always generate distinct clusters.
    
    Signed-off-by: James Peach <james.peach@konghq.com>

--- pkg/plugins/runtime/gateway/route/builders.go
+++ pkg/plugins/runtime/gateway/route/builders.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"sort"
 
+	envoy_cluster_v3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
 	envoy_config_route "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
 	"github.com/pkg/errors"
 
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
+	util_proto "github.com/kumahq/kuma/pkg/util/proto"
 	"github.com/kumahq/kuma/pkg/xds/envoy"
 )
 
@@ -61,24 +63,14 @@ func (f RouteMustConfigureFunc) Configure(r *envoy_config_route.Route) error {
 
 // DestinationClusterName generates a unique cluster name for the
 // destination. The destination must contain at least a service tag.
-func DestinationClusterName(d Destination) (string, error) {
+func DestinationClusterName(d *Destination, c *envoy_cluster_v3.Cluster) (string, error) {
 	serviceName := d.Destination[mesh_proto.ServiceTag]
 	if serviceName == "" {
 		return "", errors.Errorf("missing %s tag", mesh_proto.ServiceTag)
 	}
 
-	clusterName := serviceName
-
-	if len(d.Destination) == 1 {
-		return clusterName, nil
-	}
-
 	// If cluster is splitting the target service with selector tags,
-	// hash the tag names to generate a unique cluster name. This is
-	// a stateless uniqifier so that different xDS resource generators
-	// can use the same tags to arrive at the same cluster name without
-	// coordination.
-
+	// hash the tag names to generate a unique cluster name.
 	var keys []string
 	for k := range d.Destination {
 		keys = append(keys, k)
@@ -93,9 +85,15 @@ func DestinationClusterName(d Destination) (string, error) {
 		h.Write([]byte(d.Destination[k]))
 	}
 
+	any, err := util_proto.MarshalAnyDeterministic(c)
+	if err != nil {
+		return "", err
+	}
+
+	h.Write([]byte(any.GetTypeUrl()))
+	h.Write(any.GetValue())
+
 	// The qualifier is 16 hex digits. Unscientifically balancing the length
 	// of the hex against the likelihood of collisions.
-	clusterName = fmt.Sprintf("%s-%x", serviceName, h.Sum(nil)[:8])
-
-	return clusterName, nil
+	return fmt.Sprintf("%s-%x", serviceName, h.Sum(nil)[:8]), nil
 }
