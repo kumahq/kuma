5ba75978583deedebe0c22d81ced9fe5c455b95f
Ilya Lobkov <ilya.lobkov@konghq.com>
2021-11-17 14:54:48 +0100

    chore(*) scripts for build, publish and fetch Envoy binaries (#3110)
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>
    (cherry picked from commit 6f2071e2b709813b26a9a682f71d85bd70fd9310)

---
+++ tools/envoy/build_darwin.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+echo "Building Envoy for Darwin"
+
+mkdir -p "$(dirname ${BINARY_PATH})"
+
+SOURCE_DIR="${SOURCE_DIR}" "${KUMA_DIR:-.}/tools/envoy/fetch_sources.sh"
+
+pushd "${SOURCE_DIR}"
+
+BAZEL_BUILD_EXTRA_OPTIONS=${BAZEL_BUILD_EXTRA_OPTIONS:-""}
+read -ra BAZEL_BUILD_EXTRA_OPTIONS <<< "${BAZEL_BUILD_EXTRA_OPTIONS}"
+BAZEL_BUILD_OPTIONS=(
+    "--curses=no"
+    --show_task_finish
+    --verbose_failures
+    "--action_env=PATH=/usr/local/bin:/opt/local/bin:/usr/bin:/bin"
+    "--define" "wasm=disabled"
+    "${BAZEL_BUILD_EXTRA_OPTIONS[@]+"${BAZEL_BUILD_EXTRA_OPTIONS[@]}"}")
+bazel build "${BAZEL_BUILD_OPTIONS[@]}" -c opt //source/exe:envoy-static
+
+popd
+
+cp ${SOURCE_DIR}/bazel-bin/source/exe/envoy-static ${BINARY_PATH}
+
