f906d0bd31db159cdef522c670599bc1726514d9
Mike Beaumont <mjboamail@gmail.com>
2021-12-01 14:29:55 +0100

    ci: disable commitlint footer rules
    The specifics of the footer aren't super important for us.
    
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- .github/commitlint.config.js
+++ .github/commitlint.config.js
@@ -2,4 +2,8 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   helpUrl:
     "https://github.com/kumahq/kuma/blob/master/CONTRIBUTING.md#commit-message-format",
+  rules: {
+    "footer-max-line-length": [0],
+    "footer-leading-blank": [0],
+  },
 };
