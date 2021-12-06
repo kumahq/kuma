1152810775c984c4f3a7638562756df1e8d26054
Mike Beaumont <mjboamail@gmail.com>
2021-12-06 09:41:33 -0800

    ci(.github): remove unnecessary commit message rules
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- .github/commitlint.config.js
+++ .github/commitlint.config.js
@@ -3,7 +3,9 @@ module.exports = {
   helpUrl:
     "https://github.com/kumahq/kuma/blob/master/CONTRIBUTING.md#commit-message-format",
   rules: {
+    "body-max-line-length": [0],
     "footer-max-line-length": [0],
     "footer-leading-blank": [0],
+    "header-max-length": [0],
   },
 };
