bd363bf462c7405bafd1a888327ef4f624728612
James Peach <james.peach@konghq.com>
2021-12-07 12:24:01 +1100

    feat(kuma-cp): support Gateway request timeouts (#3426)
    Add support for setting the upstream request timeout for Gateway routes.
    This uses the same policy selection criteria that we use for upstream
    retries.
    
    This fixes #3318.
    
    Signed-off-by: James Peach <james.peach@konghq.com>

--- pkg/plugins/runtime/gateway/match/policy.go
+++ pkg/plugins/runtime/gateway/match/policy.go
@@ -6,6 +6,7 @@ import (
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
 	"github.com/kumahq/kuma/pkg/core/policy"
 	"github.com/kumahq/kuma/pkg/core/resources/model"
+	"github.com/kumahq/kuma/pkg/plugins/runtime/gateway/route"
 )
 
 // ToConnectionPolicies casts a ResourceList to a slice of ConnectionPolicy.
@@ -59,4 +60,59 @@ func OldestPolicy(policies []model.Resource) model.Resource {
 	})
 
 	return sorted[0]
+}
+
+// BestConnectionPolicyForDestination returns the retry policy for the collection
+// of forwarding target. This is conceptually a bit subtle because a
+// forwarding target can have multiple destinations, each of which is a
+// distinct service.  However, there are some relatively obvious rules that
+// we can use to determine policy.
+//
+// 1. If all the destinations are the same service, use that policy.
+// 2. If there are multiple destinations, prefer a wildcard policy.
+// 3. Everything else being equal, older policies are preferred.
+func BestConnectionPolicyForDestination(
+	destinations []route.Destination,
+	policyType model.ResourceType,
+) model.Resource {
+	seenNames := map[string]bool{}
+	servicePolicies := map[string][]model.Resource{}
+
+	// Index all the policies by service name.
+	for _, d := range destinations {
+		p, ok := d.Policies[policyType]
+		if !ok {
+			continue
+		}
+
+		if seenNames[p.GetMeta().GetName()] {
+			continue
+		}
+
+		// Index this policy by its destination service.
+		c := p.(policy.ConnectionPolicy)
+		for _, selector := range c.Destinations() {
+			svc := selector.GetMatch()[mesh_proto.ServiceTag]
+			if svc == d.Destination[mesh_proto.ServiceTag] || svc == mesh_proto.MatchAllTag {
+				servicePolicies[svc] = append(servicePolicies[svc], p)
+			}
+		}
+
+		seenNames[p.GetMeta().GetName()] = true
+	}
+
+	var candidates []model.Resource
+
+	// If we are forwarding to multiple services, no one service
+	// would be the most specific match, so we should choose the
+	// wildcard policy. Otherwise, we can just take the oldest of
+	// all the matches, since there's no better way to discriminate.
+	candidates = append(candidates, servicePolicies[mesh_proto.MatchAllTag]...)
+	if len(candidates) == 0 {
+		for _, p := range servicePolicies {
+			candidates = append(candidates, p...)
+		}
+	}
+
+	return OldestPolicy(candidates)
 }
