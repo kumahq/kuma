e18c7071eee7bee1d04b3f20a65a643175fea012
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2021-12-16 10:57:26 +0100

    fix(kuma-cp) do not override other dataplane with dp lifecycle (#3507)
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
    (cherry picked from commit f464b5bb3ab81e27ae4d2eaf90d272055f6826ed)

---
+++ test/e2e/auth/dp/auth_dp_suite_test.go
@@ -0,0 +1,21 @@
+package auth_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+
+	"github.com/kumahq/kuma/pkg/test"
+	auth "github.com/kumahq/kuma/test/e2e/auth/dp"
+	"github.com/kumahq/kuma/test/framework"
+)
+
+func TestE2EDpAuth(t *testing.T) {
+	if framework.IsK8sClustersStarted() {
+		test.RunSpecs(t, "E2E Auth DP Suite")
+	} else {
+		t.SkipNow()
+	}
+}
+
+var _ = Describe("Test Universal", auth.DpAuthUniversal)
