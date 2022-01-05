44dddb522591435bbdf1f9c333e13c762d0848db
James Peach <james.peach@konghq.com>
2022-01-05 15:30:55 +1100

    fix(kumactl): print meta before spec for Kuma resources (#3637)
    When printing Kuma resources as YAML, it's a better user experience to
    show the metadata fields (including the type and name) up front, rather
    than the fields being in an arbitrary order.
    
    This change is not a complete fix for the issue, but by tweaking the
    kumactl get printer, we can make a small change that improves the user
    experience in most cases.
    
    This updates #679.
    
    Signed-off-by: James Peach <james.peach@konghq.com>

--- app/kumactl/pkg/output/yaml/printer.go
+++ app/kumactl/pkg/output/yaml/printer.go
@@ -1,11 +1,14 @@
 package yaml
 
 import (
+	"bytes"
 	"io"
 
 	"github.com/ghodss/yaml"
 
 	"github.com/kumahq/kuma/app/kumactl/pkg/output"
+	"github.com/kumahq/kuma/pkg/core/resources/model/rest"
+	util_proto "github.com/kumahq/kuma/pkg/util/proto"
 )
 
 func NewPrinter() output.Printer {
@@ -16,11 +19,38 @@ var _ output.Printer = &printer{}
 
 type printer struct{}
 
-func (p *printer) Print(obj interface{}, out io.Writer) error {
+func print(obj interface{}, out io.Writer) error {
 	b, err := yaml.Marshal(obj)
 	if err != nil {
 		return err
 	}
 	_, err = out.Write(b)
 	return err
+}
+
+func (p *printer) Print(obj interface{}, out io.Writer) error {
+	// The common case is printing a single Kuma resource, in which
+	// case showing the meta and then the spec is more readable than
+	// showing fields in an arbitrary order. This partially addresses
+	// https://github.com/kumahq/kuma/issues/679.
+	switch obj := obj.(type) {
+	case *rest.Resource:
+		if err := print(obj.Meta, out); err != nil {
+			return err
+		}
+
+		b, err := util_proto.ToYAML(obj.Spec)
+		if err != nil {
+			return err
+		}
+		// Don't emit an empty YAML object that would cause a
+		// subsequent parse failure.
+		if len(b) == 0 || bytes.HasPrefix(b, []byte("{}")) {
+			return nil
+		}
+		_, err = out.Write(b)
+		return err
+	default:
+		return print(obj, out)
+	}
 }
