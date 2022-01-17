f50d5bed9b8fe0d60224f0f3064fbd2831c149e2
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2022-01-17 17:02:07 +0100

    remove kuma-master workflow
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- tools/releases/version.sh
+++ tools/releases/version.sh
@@ -2,8 +2,9 @@
 
 # Kuma version is built as follows:
 # 1) If a git tag is present on the current commit, then the version is a git tag
-# 2) If the branch starts with "release" (like "release-1.3"), then the version is $(last git tag)-$(short_git_hash) (for example: 1.4.1-450174242)
-# 3) If the branch does not start with "release", then the version is "dev-$(short_git_hash)" (for example: dev-450174242)
+# 2) If the branch starts with "release" (like "release-1.3"), then the version is "$lastGitTag-$shortHash" (for example: 1.4.1-450174242)
+#    At the same time, if $lastGitTag is not final tag (for example 1.4.0-rc1 or 1.4.0-preview1), the version is "dev-$shortHash"
+# 3) If the branch does not start with "release", then the version is "dev-$shortHash" (for example: dev-450174242)
 
 lastGitTag=`git describe --abbrev=0 --tags`
 shortHash=`git rev-parse --short HEAD`
