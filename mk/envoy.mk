5dd229abf9e414a091ed842dd55bfffcfa5a9d94
Charly Molter <charly.molter@konghq.com>
2022-02-23 14:07:44 +0100

    chore(envoy): update to 1.21.1 (#3909)
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- mk/envoy.mk
+++ mk/envoy.mk
@@ -2,7 +2,7 @@ KUMA_DIR ?= .
 
 BUILD_ENVOY_FROM_SOURCES ?= false
 
-ENVOY_TAG ?= v1.21.0 # commit hash or git tag
+ENVOY_TAG ?= v1.21.1 # commit hash or git tag
 # Remember to update pkg/version/compatibility.go
 ENVOY_VERSION = $(shell ${KUMA_DIR}/tools/envoy/version.sh ${ENVOY_TAG})
 
