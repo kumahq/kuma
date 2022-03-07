34e19e1529fcc92564baf411a467c7b71cfd0bae
parkanzky <42279121+parkanzky@users.noreply.github.com>
2022-03-07 10:05:14 -0500

    feat(kuma-cp): move envoy compat check to DP (#3963)
    Signed-off-by: Paul Parkanzky <paul.parkanzky@konghq.com>
    Signed-off-by: Sally Blich <sally.blich@walkme.com>

--- pkg/xds/bootstrap/parameters.go
+++ pkg/xds/bootstrap/parameters.go
@@ -3,27 +3,28 @@ package bootstrap
 import "time"
 
 type configParameters struct {
-	Id                 string
-	Service            string
-	AdminAddress       string
-	AdminPort          uint32
-	AdminAccessLogPath string
-	XdsHost            string
-	XdsPort            uint32
-	XdsConnectTimeout  time.Duration
-	AccessLogPipe      string
-	DataplaneToken     string
-	DataplaneResource  string
-	CertBytes          []byte
-	KumaDpVersion      string
-	KumaDpGitTag       string
-	KumaDpGitCommit    string
-	KumaDpBuildDate    string
-	EnvoyVersion       string
-	EnvoyBuild         string
-	HdsEnabled         bool
-	DynamicMetadata    map[string]string
-	DNSPort            uint32
-	EmptyDNSPort       uint32
-	ProxyType          string
+	Id                    string
+	Service               string
+	AdminAddress          string
+	AdminPort             uint32
+	AdminAccessLogPath    string
+	XdsHost               string
+	XdsPort               uint32
+	XdsConnectTimeout     time.Duration
+	AccessLogPipe         string
+	DataplaneToken        string
+	DataplaneResource     string
+	CertBytes             []byte
+	KumaDpVersion         string
+	KumaDpGitTag          string
+	KumaDpGitCommit       string
+	KumaDpBuildDate       string
+	EnvoyVersion          string
+	EnvoyBuild            string
+	EnvoyKumaDpCompatible bool
+	HdsEnabled            bool
+	DynamicMetadata       map[string]string
+	DNSPort               uint32
+	EmptyDNSPort          uint32
+	ProxyType             string
 }
