242ee6227136366c9b5b01cf68c9a1a712fd6d9d
Mike Beaumont <mjboamail@gmail.com>
2022-03-29 19:19:24 +0200

    test(kumactl): add inspect meshgateway test
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

---
+++ app/kumactl/cmd/inspect/inspect_meshgateway_test.go
@@ -0,0 +1,79 @@
+package inspect_test
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"os"
+	"path"
+	"path/filepath"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	gomega_types "github.com/onsi/gomega/types"
+
+	"github.com/kumahq/kuma/app/kumactl/cmd"
+	"github.com/kumahq/kuma/app/kumactl/pkg/resources"
+	api_server_types "github.com/kumahq/kuma/pkg/api-server/types"
+	test_kumactl "github.com/kumahq/kuma/pkg/test/kumactl"
+	"github.com/kumahq/kuma/pkg/test/matchers"
+	util_http "github.com/kumahq/kuma/pkg/util/http"
+)
+
+type testMeshGatewayInspectClient struct {
+	response api_server_types.GatewayInspectResult
+}
+
+func (t *testMeshGatewayInspectClient) InspectPolicies(ctx context.Context, mesh, name string) (api_server_types.GatewayInspectResult, error) {
+	return t.response, nil
+}
+
+var _ resources.MeshGatewayInspectClient = &testMeshGatewayInspectClient{}
+
+type testCase struct {
+	serverOutput string
+	goldenFile   string
+	matcher      func(path ...string) gomega_types.GomegaMatcher
+}
+
+var _ = DescribeTable("kumactl inspect meshgateway",
+	func(given testCase) {
+		rawResponse, err := os.ReadFile(path.Join("testdata", given.serverOutput))
+		Expect(err).ToNot(HaveOccurred())
+
+		response := api_server_types.GatewayInspectResult{}
+		Expect(json.Unmarshal(rawResponse, &response)).To(Succeed())
+
+		testClient := &testMeshGatewayInspectClient{
+			response: response,
+		}
+
+		rootCtx, err := test_kumactl.MakeRootContext(time.Now(), nil)
+		Expect(err).ToNot(HaveOccurred())
+
+		rootCtx.Runtime.NewMeshGatewayInspectClient = func(client util_http.Client) resources.MeshGatewayInspectClient {
+			return testClient
+		}
+
+		rootCmd := cmd.NewRootCmd(rootCtx)
+		buf := &bytes.Buffer{}
+		rootCmd.SetOut(buf)
+		// given
+		rootCmd.SetArgs([]string{
+			"--config-file", filepath.Join("..", "testdata", "sample-kumactl.config.yaml"),
+			"inspect", "meshgateway", "meshgateway-1"})
+
+		// when
+		err = rootCmd.Execute()
+
+		// then
+		Expect(err).ToNot(HaveOccurred())
+		Expect(buf.String()).To(given.matcher("testdata", given.goldenFile))
+	},
+	Entry("default output", testCase{
+		serverOutput: "inspect-meshgateway.server-response.json",
+		goldenFile:   "inspect-meshgateway.golden.txt",
+		matcher:      matchers.MatchGoldenEqual,
+	}),
+)
