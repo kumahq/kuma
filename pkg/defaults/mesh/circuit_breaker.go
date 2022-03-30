f5470e7f6e0bd3da2b5cf8b3a8d56cc877e0bbb6
Ilya Lobkov <ilya.lobkov@konghq.com>
2022-03-30 15:37:47 +0700

    fix(kuma-cp): default policy creation (#4073)
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>
    Signed-off-by: Sally Blich <sally.blich@walkme.com>

--- pkg/defaults/mesh/circuit_breaker.go
+++ pkg/defaults/mesh/circuit_breaker.go
@@ -3,24 +3,27 @@ package mesh
 import (
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
 	core_mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
+	"github.com/kumahq/kuma/pkg/core/resources/model"
 	util_proto "github.com/kumahq/kuma/pkg/util/proto"
 )
 
-var defaultCircuitBreakerResource = &core_mesh.CircuitBreakerResource{
-	Spec: &mesh_proto.CircuitBreaker{
-		Sources: []*mesh_proto.Selector{{
-			Match: mesh_proto.MatchAnyService(),
-		}},
-		Destinations: []*mesh_proto.Selector{{
-			Match: mesh_proto.MatchAnyService(),
-		}},
-		Conf: &mesh_proto.CircuitBreaker_Conf{
-			Thresholds: &mesh_proto.CircuitBreaker_Conf_Thresholds{
-				MaxConnections:     util_proto.UInt32(1024),
-				MaxPendingRequests: util_proto.UInt32(1024),
-				MaxRequests:        util_proto.UInt32(1024),
-				MaxRetries:         util_proto.UInt32(3),
+var defaultCircuitBreakerResource = func() model.Resource {
+	return &core_mesh.CircuitBreakerResource{
+		Spec: &mesh_proto.CircuitBreaker{
+			Sources: []*mesh_proto.Selector{{
+				Match: mesh_proto.MatchAnyService(),
+			}},
+			Destinations: []*mesh_proto.Selector{{
+				Match: mesh_proto.MatchAnyService(),
+			}},
+			Conf: &mesh_proto.CircuitBreaker_Conf{
+				Thresholds: &mesh_proto.CircuitBreaker_Conf_Thresholds{
+					MaxConnections:     util_proto.UInt32(1024),
+					MaxPendingRequests: util_proto.UInt32(1024),
+					MaxRequests:        util_proto.UInt32(1024),
+					MaxRetries:         util_proto.UInt32(3),
+				},
 			},
 		},
-	},
+	}
 }
--- pkg/defaults/mesh/traffic_permission.go
+++ pkg/defaults/mesh/traffic_permission.go
@@ -3,23 +3,26 @@ package mesh
 import (
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
 	core_mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
+	"github.com/kumahq/kuma/pkg/core/resources/model"
 )
 
-var defaultTrafficPermissionResource = &core_mesh.TrafficPermissionResource{
-	Spec: &mesh_proto.TrafficPermission{
-		Sources: []*mesh_proto.Selector{
-			{
-				Match: map[string]string{
-					mesh_proto.ServiceTag: "*",
+var defaultTrafficPermissionResource = func() model.Resource {
+	return &core_mesh.TrafficPermissionResource{
+		Spec: &mesh_proto.TrafficPermission{
+			Sources: []*mesh_proto.Selector{
+				{
+					Match: map[string]string{
+						mesh_proto.ServiceTag: "*",
+					},
+				},
+			},
+			Destinations: []*mesh_proto.Selector{
+				{
+					Match: map[string]string{
+						mesh_proto.ServiceTag: "*",
+					},
 				},
 			},
 		},
-		Destinations: []*mesh_proto.Selector{
-			{
-				Match: map[string]string{
-					mesh_proto.ServiceTag: "*",
-				},
-			},
-		},
-	},
+	}
 }
