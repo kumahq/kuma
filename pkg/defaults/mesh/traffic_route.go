b3515f4932e5360bd290a6e6970a96cec4bbf7cc
Ilya Lobkov <ilya.lobkov@konghq.com>
2022-03-30 15:37:47 +0700

    fix(kuma-cp): default policy creation (#4073)
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>
    Signed-off-by: Sally Blich <sally.blich@walkme.com>

--- pkg/defaults/mesh/traffic_route.go
+++ pkg/defaults/mesh/traffic_route.go
@@ -3,21 +3,25 @@ package mesh
 import (
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
 	core_mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
+	"github.com/kumahq/kuma/pkg/core/resources/model"
 )
 
-var defaultTrafficRouteResource = &core_mesh.TrafficRouteResource{
-	Spec: &mesh_proto.TrafficRoute{
-		Sources: []*mesh_proto.Selector{{
-			Match: mesh_proto.MatchAnyService(),
-		}},
-		Destinations: []*mesh_proto.Selector{{
-			Match: mesh_proto.MatchAnyService(),
-		}},
-		Conf: &mesh_proto.TrafficRoute_Conf{
-			Destination: mesh_proto.MatchAnyService(),
-			LoadBalancer: &mesh_proto.TrafficRoute_LoadBalancer{
-				LbType: &mesh_proto.TrafficRoute_LoadBalancer_RoundRobin_{},
+var defaultTrafficRouteResource = func() model.Resource {
+	return &core_mesh.TrafficRouteResource{
+		Spec: &mesh_proto.TrafficRoute{
+			Sources: []*mesh_proto.Selector{{
+				Match: mesh_proto.MatchAnyService(),
+			}},
+			Destinations: []*mesh_proto.Selector{{
+				Match: mesh_proto.MatchAnyService(),
+			}},
+			Conf: &mesh_proto.TrafficRoute_Conf{
+				Destination: mesh_proto.MatchAnyService(),
+				LoadBalancer: &mesh_proto.TrafficRoute_LoadBalancer{
+					LbType: &mesh_proto.TrafficRoute_LoadBalancer_RoundRobin_{},
+				},
 			},
 		},
-	},
+	}
+
 }
