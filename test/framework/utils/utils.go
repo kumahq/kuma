b340ae89c54d5121c85d15880f251b9cd7db08e6
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2022-04-14 13:01:14 +0200

    chore(e2e): never reuse ports
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- test/framework/utils/utils.go
+++ test/framework/utils/utils.go
@@ -5,15 +5,28 @@ import (
 	"math/rand"
 	"net"
 	"strings"
+	"sync"
 )
 
 func ShellEscape(arg string) string {
 	return fmt.Sprintf("'%s'", strings.ReplaceAll(arg, "'", "\\'"))
 }
 
+var usedPorts map[int]struct{}
+var mutex sync.Mutex
+
+func init() {
+	usedPorts = map[int]struct{}{}
+}
+
 func GetFreePort() (int, error) {
+	mutex.Lock()
+	defer mutex.Unlock()
 	for {
 		port := rand.Int()%50000 + 10000
+		if _, ok := usedPorts[port]; ok {
+			continue
+		}
 		address, err := net.ResolveTCPAddr("tcp", fmt.Sprintf(":%d", port))
 		if err != nil {
 			return 0, err
@@ -25,6 +38,7 @@ func GetFreePort() (int, error) {
 		if err := listener.Close(); err != nil {
 			return 0, err
 		}
+		usedPorts[port] = struct{}{}
 		return port, nil
 	}
 }
