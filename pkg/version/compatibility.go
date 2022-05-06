d90eb9a06663521a4fa2b19d9dd17cc62b34e290
Paul A. Parkanzky <paul.parkanzky@konghq.com>
2022-05-06 16:45:40 -0400

    feat(kuma-cp): validate DP compat on kuma backend (#4236)
    * feat(kuma-cp): validate DP compat on kuma backend
    
    Signed-off-by: Paul Parkanzky <paul.parkanzky@konghq.com>

--- pkg/version/compatibility.go
+++ pkg/version/compatibility.go
@@ -1,5 +1,16 @@
 package version
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Masterminds/semver/v3"
+
+	"github.com/kumahq/kuma/pkg/core"
+)
+
+var log = core.Log.WithName("version").WithName("compatibility")
+
 type DataplaneCompatibility struct {
 	Envoy string `json:"envoy"`
 }
@@ -65,3 +76,45 @@ var CompatibilityMatrix = Compatibility{
 }
 
 var DevVersionPrefix = "dev"
+
+// DeploymentVersionCompatible returns true if the given component version
+// is compatible with the installed version of Kuma CP.
+// For all binaries which share a common version (Kuma DP, CP, Zone CP...), we
+// support backwards compatibility of at most two prior minor versions.
+func DeploymentVersionCompatible(kumaVersionStr string, componentVersionStr string) bool {
+	if strings.Contains(kumaVersionStr, "dev") || strings.Contains(componentVersionStr, "dev") {
+		return true
+	}
+
+	kumaVersion, err := semver.NewVersion(kumaVersionStr)
+	if err != nil {
+		// Assume some kind of dev version
+		log.Info("cannot parse semantic version", "version", kumaVersionStr)
+		return true
+	}
+
+	componentVersion, err := semver.NewVersion(componentVersionStr)
+	if err != nil {
+		// Assume some kind of dev version
+		log.Info("cannot parse semantic version", "version", componentVersionStr)
+		return true
+	}
+
+	minMinor := int64(kumaVersion.Minor()) - 2
+	if minMinor < 0 {
+		minMinor = 0
+	}
+
+	maxMinor := kumaVersion.Minor() + 2
+
+	constraint, err := semver.NewConstraint(
+		fmt.Sprintf(">= %d.%d, <= %d.%d", kumaVersion.Major(), minMinor, kumaVersion.Major(), maxMinor),
+	)
+
+	if err != nil {
+		// Programmer error
+		panic(err)
+	}
+
+	return constraint.Check(componentVersion)
+}
