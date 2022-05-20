376d2db7f384502f972259aa658986225cae2dcd
Łukasz Dziedziak <lukidzi@gmail.com>
2022-05-19 19:23:44 -0500

    feat(*): added e2e test
    Signed-off-by: Łukasz Dziedziak <lukidzi@gmail.com>

---
+++ test/e2e_env/kubernetes/container_patch/container_patch.go
@@ -0,0 +1,104 @@
+package container_patch
+
+import (
+	"fmt"
+
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/kumahq/kuma/test/e2e_env/kubernetes/env"
+	. "github.com/kumahq/kuma/test/framework"
+	"github.com/kumahq/kuma/test/framework/deployments/testserver"
+)
+
+func ContainerPatch() {
+	const namespace = "container-patch"
+	const mesh = "container-patch"
+	const appName = "test-service"
+	const appNameWithPatch = "test-service-patched"
+
+	containerPatch := func(ns string) string {
+		return fmt.Sprintf(`apiVersion: kuma.io/v1alpha1
+kind: ContainerPatch
+metadata:
+  namespace: %s
+  name: container-patch-1
+spec:
+  sidecarPatch:
+    - op: add
+      path: /securityContext/privileged
+      value: "true"`, ns)
+	}
+	containerPatch2 := func(ns string) string {
+		return fmt.Sprintf(`apiVersion: kuma.io/v1alpha1
+kind: ContainerPatch
+metadata:
+  namespace: %s
+  name: container-patch-2
+spec:
+  initPatch:
+    - op: remove
+      path: /securityContext/runAsUser`, ns)
+	}
+	BeforeAll(func() {
+		err := NewClusterSetup().
+			Install(NamespaceWithSidecarInjection(namespace)).
+			Install(YamlK8s(containerPatch(namespace))).
+			Install(YamlK8s(containerPatch2(namespace))).
+			Install(MeshKubernetes(mesh)).
+			Install(testserver.Install(
+				testserver.WithNamespace(namespace),
+				testserver.WithMesh(mesh),
+				testserver.WithName(appNameWithPatch),
+				testserver.WithPodAnnotations(
+					map[string]string{"kuma.io/container-patches": "container-patch-1,container-patch-2"},
+				),
+			)).
+			Install(testserver.Install(
+				testserver.WithNamespace(namespace),
+				testserver.WithMesh(mesh),
+				testserver.WithName(appName),
+			)).
+			Setup(env.Cluster)
+		Expect(err).ToNot(HaveOccurred())
+	})
+	E2EAfterAll(func() {
+		Expect(env.Cluster.TriggerDeleteNamespace(namespace)).To(Succeed())
+		Expect(env.Cluster.DeleteMesh(mesh)).To(Succeed())
+	})
+
+	It("should apply container patch to kubernetes configuartion", func() {
+		// when
+		// container without container patch
+		podName, err := PodNameOfApp(env.Cluster, appName, namespace)
+		Expect(err).ToNot(HaveOccurred())
+		pod, err := k8s.GetPodE(env.Cluster.GetTesting(), env.Cluster.GetKubectlOptions(namespace), podName)
+		Expect(err).ToNot(HaveOccurred())
+
+		// then
+		Expect(len(pod.Spec.InitContainers)).To(Equal(1))
+		Expect(len(pod.Spec.Containers)).To(Equal(2))
+		// should have default value *int64 = 0
+		Expect(pod.Spec.InitContainers[0].SecurityContext.RunAsUser).To(Equal(new(int64)))
+		// should have Nil value
+		Expect(pod.Spec.Containers[1].SecurityContext.Privileged).To(BeNil())
+
+		// when
+		// container with patch
+		podName, err = PodNameOfApp(env.Cluster, appNameWithPatch, namespace)
+		Expect(err).ToNot(HaveOccurred())
+		pod, err = k8s.GetPodE(env.Cluster.GetTesting(), env.Cluster.GetKubectlOptions(namespace), podName)
+		Expect(err).ToNot(HaveOccurred())
+
+		// then
+		pointerTrue := new(bool)
+		*pointerTrue = true
+		Expect(len(pod.Spec.InitContainers)).To(Equal(1))
+		Expect(len(pod.Spec.Containers)).To(Equal(2))
+		// should doesn't have defined RunAsUser
+		Expect(pod.Spec.InitContainers[0].SecurityContext.RunAsUser).To(BeNil())
+		// should have value *true
+		Expect(pod.Spec.Containers[1].SecurityContext.Privileged).To(Equal(pointerTrue))
+	})
+}
--- test/e2e_env/kubernetes/kubernetes_suite_test.go
+++ test/e2e_env/kubernetes/kubernetes_suite_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/kumahq/kuma/pkg/config/core"
 	"github.com/kumahq/kuma/pkg/test"
+	"github.com/kumahq/kuma/test/e2e_env/kubernetes/container_patch"
 	"github.com/kumahq/kuma/test/e2e_env/kubernetes/env"
 	"github.com/kumahq/kuma/test/e2e_env/kubernetes/graceful"
 	"github.com/kumahq/kuma/test/e2e_env/kubernetes/healthcheck"
@@ -69,3 +70,4 @@ var _ = Describe("Virtual Probes", healthcheck.VirtualProbes, Ordered)
 var _ = Describe("Graceful", graceful.Graceful, Ordered)
 var _ = Describe("Jobs", jobs.Jobs)
 var _ = Describe("Membership", membership.Membership, Ordered)
+var _ = FDescribe("Container Patch", container_patch.ContainerPatch, Ordered)
