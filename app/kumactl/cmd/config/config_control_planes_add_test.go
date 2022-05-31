4d230405222b23bc7c99e5c923c679f9c958bf5b
Charly Molter <charly.molter@konghq.com>
2022-05-31 11:35:55 +0200

    fix(kumactl): make error more exclude when CLI and CP product differ
    Previously the error was cryptic and complex for folks to troubleshoot
    
    Fix #4347
    
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- app/kumactl/cmd/config/config_control_planes_add_test.go
+++ app/kumactl/cmd/config/config_control_planes_add_test.go
@@ -142,7 +142,7 @@ var _ = Describe("kumactl config control-planes add", func() {
 		It("should fail on invalid api server", func() {
 			// setup
 			server, port := setupCpServer(func(writer http.ResponseWriter, req *http.Request) {
-				_, err := writer.Write([]byte("{}"))
+				_, err := writer.Write([]byte(`{"tagLine": "OtherProduct"}`))
 				Expect(err).ToNot(HaveOccurred())
 			})
 			defer server.Close()
@@ -155,9 +155,9 @@ var _ = Describe("kumactl config control-planes add", func() {
 			// when
 			err := rootCmd.Execute()
 			// then
-			Expect(err).To(MatchError(`provided address is not valid Kuma Control Plane API Server`))
+			Expect(err).To(MatchError(`CLI is for Kuma when control plane is OtherProduct, please use a CLI aligned with your control plane`))
 			// and
-			Expect(outbuf.String()).To(Equal(`Error: provided address is not valid Kuma Control Plane API Server
+			Expect(outbuf.String()).To(Equal(`Error: CLI is for Kuma when control plane is OtherProduct, please use a CLI aligned with your control plane
 `))
 		})
 	})
