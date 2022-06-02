4a5b2e684ca13ae01f6146907d9953dcfd0560c7
Mike Beaumont <mjboamail@gmail.com>
2022-06-02 07:44:18 +0200

    test(e2e): add universal cross-mesh tests (#4415)
    * test(e2e): add universal cross-mesh tests
    * test(e2e): add check for outside mesh reachability
    
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>
    (cherry picked from commit 15e9f1fee40952f203d58a750b7927201777312d)

---
+++ test/e2e_env/universal/gateway/utils.go
@@ -0,0 +1,116 @@
+package gateway
+
+import (
+	"fmt"
+	"net/url"
+	"path"
+	"strings"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/kumahq/kuma/test/e2e_env/universal/env"
+	. "github.com/kumahq/kuma/test/framework"
+	"github.com/kumahq/kuma/test/framework/client"
+)
+
+func demoClientName(mesh string) string {
+	return fmt.Sprintf("demo-client-%s", mesh)
+}
+
+func successfullyProxyRequestToGateway(cluster Cluster, mesh, instance, gatewayAddr string) {
+	Logf("expecting 200 response from %q", gatewayAddr)
+	target := fmt.Sprintf("http://%s/%s",
+		gatewayAddr, path.Join("test", url.PathEscape(GinkgoT().Name())),
+	)
+
+	response, err := client.CollectResponse(
+		cluster, demoClientName(mesh), target,
+	)
+
+	Expect(err).NotTo(HaveOccurred())
+	Expect(response.Instance).To(Equal(instance))
+}
+
+func failToProxyRequestToGateway(cluster Cluster, containerName, gatewayAddr, host string) func(Gomega) {
+	return func(g Gomega) {
+		Logf("expecting 200 response from %q", gatewayAddr)
+		target := fmt.Sprintf("http://%s/%s",
+			gatewayAddr, path.Join("test", url.PathEscape(GinkgoT().Name())),
+		)
+
+		response, err := client.CollectFailure(
+			cluster, containerName, target,
+			client.Resolve(gatewayAddr, host),
+		)
+
+		g.Expect(err).ToNot(HaveOccurred())
+		g.Expect(response.Exitcode).To(Or(Equal(56), Equal(7), Equal(28)))
+	}
+}
+
+func mkGateway(name, mesh string, crossMesh bool, hostname, backendService string, port int) string {
+	meshGateway := fmt.Sprintf(`
+type: MeshGateway
+name: %s
+mesh: %s
+selectors:
+- match:
+    kuma.io/service: %s
+conf:
+  listeners:
+  - port: %d
+    protocol: HTTP
+    crossMesh: %t
+    hostname: %s
+`, name, mesh, name, port, crossMesh, hostname)
+
+	route := fmt.Sprintf(`
+type: MeshGatewayRoute
+name: %s
+mesh: %s
+selectors:
+- match:
+    kuma.io/service: %s
+conf:
+  http:
+    rules:
+    - matches:
+      - path:
+          match: PREFIX
+          value: /
+      backends:
+      - destination:
+          kuma.io/service: %s # Matches the echo-server we deployed.
+`, name, mesh, name, backendService)
+
+	return strings.Join([]string{meshGateway, route}, "\n---\n")
+}
+
+func mkGatewayDataplane(name, mesh string) InstallFunc {
+	return func(cluster Cluster) error {
+		token, err := env.Cluster.GetKuma().GenerateDpToken(mesh, name)
+		if err != nil {
+			return err
+		}
+
+		dataplane := fmt.Sprintf(`
+type: Dataplane
+name: {{ name }}
+mesh: %s
+networking:
+  address:  {{ address }}
+  gateway:
+    type: BUILTIN
+    tags:
+      kuma.io/service: %s
+`, mesh, name)
+
+		return env.Cluster.DeployApp(
+			WithName(name),
+			WithToken(token),
+			WithVerbose(),
+			WithYaml(dataplane),
+		)
+	}
+}
