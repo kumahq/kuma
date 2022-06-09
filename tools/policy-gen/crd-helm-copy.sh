7ecb4846199cd4435f3bbba2c74309fdcda7cb5d
Charly Molter <charly.molter@konghq.com>
2022-06-09 23:01:49 +0200

    chore: refactor policy generation (#4451)

--- tools/policy-gen/crd-helm-copy.sh
+++ tools/policy-gen/crd-helm-copy.sh
@@ -6,15 +6,20 @@ set -o nounset
 
 POLICY=$1
 
-POLICIES_DIR=pkg/plugins/policies
-POLICIES_CRD_DIR="${POLICIES_DIR}/${POLICY}/k8s/crd"
+POLICY_DIR=pkg/plugins/policies/${POLICY}
+POLICY_CRD_DIR="${POLICY_DIR}/k8s/crd"
 
-if [ "$(find "${POLICIES_CRD_DIR}" -type f | wc -l | xargs echo)" != 1 ]; then
+if [ ! -f "${POLICY_DIR}/plugin.go" ]; then
+  echo "Policy has skip registration, not copying crd to helm"
+  exit 0
+fi
+
+if [ "$(find "${POLICY_CRD_DIR}" -type f | wc -l | xargs echo)" != 1 ]; then
   echo "More than 1 file in crd directory"
   exit 1
 fi
 
-CRD_FILE="$(find "${POLICIES_CRD_DIR}" -type f)"
+CRD_FILE="$(find "${POLICY_CRD_DIR}" -type f)"
 
 HELM_CRD_DIR=deployments/charts/kuma/crds
 
