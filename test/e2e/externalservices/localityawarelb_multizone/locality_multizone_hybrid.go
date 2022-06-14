9d7248bf0a35138ad17a270263d11ce96cfbc432
Łukasz Dziedziak <lukidzi@gmail.com>
2022-06-14 11:57:20 +0200

    ci(*): changed test flow and refactor
    Signed-off-by: Łukasz Dziedziak <lukidzi@gmail.com>

--- test/e2e/externalservices/localityawarelb_multizone/locality_multizone_hybrid.go
+++ test/e2e/externalservices/localityawarelb_multizone/locality_multizone_hybrid.go
@@ -58,73 +58,72 @@ func InstallExternalService(name string) InstallFunc {
 	}
 }
 
-var _ = E2EBeforeSuite(func() {
-	k8sClusters, err := NewK8sClusters(
-		[]string{Kuma1},
-		Silent)
-	Expect(err).ToNot(HaveOccurred())
+func ExternalServicesOnMultizoneHybridWithLocalityAwareLb() {
+	BeforeAll(func() {
+		k8sClusters, err := NewK8sClusters(
+			[]string{Kuma1},
+			Silent)
+		Expect(err).ToNot(HaveOccurred())
 
-	universalClusters, err := NewUniversalClusters(
-		[]string{Kuma4, Kuma5},
-		Silent)
-	Expect(err).ToNot(HaveOccurred())
+		universalClusters, err := NewUniversalClusters(
+			[]string{Kuma4, Kuma5},
+			Silent)
+		Expect(err).ToNot(HaveOccurred())
 
-	// Global
-	global = universalClusters.GetCluster(Kuma5)
+		// Global
+		global = universalClusters.GetCluster(Kuma5)
 
-	Expect(NewClusterSetup().
-		Install(Kuma(config_core.Global)).
-		Install(YamlUniversal(meshMTLSOn(defaultMesh, "true"))).
-		Setup(global)).To(Succeed())
+		Expect(NewClusterSetup().
+			Install(Kuma(config_core.Global)).
+			Install(YamlUniversal(meshMTLSOn(defaultMesh, "true"))).
+			Setup(global)).To(Succeed())
 
-	E2EDeferCleanup(global.DismissCluster)
+		globalCP := global.GetKuma()
 
-	globalCP := global.GetKuma()
+		// K8s Cluster 1
+		zone1 = k8sClusters.GetCluster(Kuma1).(*K8sCluster)
+		Expect(NewClusterSetup().
+			Install(Kuma(config_core.Zone,
+				WithIngress(),
+				WithIngressEnvoyAdminTunnel(),
+				WithEgress(),
+				WithEgressEnvoyAdminTunnel(),
+				WithGlobalAddress(globalCP.GetKDSServerAddress()),
+			)).
+			Install(NamespaceWithSidecarInjection(TestNamespace)).
+			Setup(zone1)).To(Succeed())
 
-	// K8s Cluster 1
-	zone1 = k8sClusters.GetCluster(Kuma1).(*K8sCluster)
-	Expect(NewClusterSetup().
-		Install(Kuma(config_core.Zone,
-			WithIngress(),
-			WithIngressEnvoyAdminTunnel(),
-			WithEgress(),
-			WithEgressEnvoyAdminTunnel(),
-			WithGlobalAddress(globalCP.GetKDSServerAddress()),
-		)).
-		Install(NamespaceWithSidecarInjection(TestNamespace)).
-		Setup(zone1)).To(Succeed())
+		// Universal Cluster 4
+		zone4 = universalClusters.GetCluster(Kuma4).(*UniversalCluster)
+		Expect(err).ToNot(HaveOccurred())
 
-	E2EDeferCleanup(func() {
+		Expect(NewClusterSetup().
+			Install(Kuma(config_core.Zone, WithGlobalAddress(globalCP.GetKDSServerAddress()))).
+			Install(DemoClientUniversal(
+				"zone4-demo-client",
+				defaultMesh,
+				WithTransparentProxy(true),
+			)).
+			Install(IngressUniversal(globalCP.GenerateZoneIngressToken)).
+			Install(EgressUniversal(globalCP.GenerateZoneEgressToken)).
+			Install(InstallExternalService("external-service-in-zone1")).
+			Setup(zone4),
+		).To(Succeed())
+
+		err = NewClusterSetup().
+			Install(YamlUniversal(zoneExternalService(defaultMesh, zone4.GetApp("external-service-in-zone1").GetIP(), "external-service-in-zone1", "kuma-1-zone"))).
+			Setup(global)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	AfterAll(func() {
 		Expect(zone1.DeleteNamespace(TestNamespace)).To(Succeed())
 		Expect(zone1.DeleteKuma()).To(Succeed())
 		Expect(zone1.DismissCluster()).To(Succeed())
+		Expect(zone4.DismissCluster()).To(Succeed())
+		Expect(global.DismissCluster()).To(Succeed())
 	})
 
-	// Universal Cluster 4
-	zone4 = universalClusters.GetCluster(Kuma4).(*UniversalCluster)
-	Expect(err).ToNot(HaveOccurred())
-
-	Expect(NewClusterSetup().
-		Install(Kuma(config_core.Zone, WithGlobalAddress(globalCP.GetKDSServerAddress()))).
-		Install(DemoClientUniversal(
-			"zone4-demo-client",
-			defaultMesh,
-			WithTransparentProxy(true),
-		)).
-		Install(IngressUniversal(globalCP.GenerateZoneIngressToken)).
-		Install(EgressUniversal(globalCP.GenerateZoneEgressToken)).
-		Install(InstallExternalService("external-service-in-zone1")).
-		Setup(zone4),
-	).To(Succeed())
-	E2EDeferCleanup(zone4.DismissCluster)
-
-	err = NewClusterSetup().
-		Install(YamlUniversal(zoneExternalService(defaultMesh, zone4.GetApp("external-service-in-zone1").GetIP(), "external-service-in-zone1", "kuma-1-zone"))).
-		Setup(global)
-	Expect(err).ToNot(HaveOccurred())
-})
-
-func ExternalServicesOnMultizoneHybridWithLocalityAwareLb() {
 	BeforeEach(func() {
 		Expect(global.GetKumactlOptions().
 			KumactlApplyFromString(meshMTLSOn(defaultMesh, "true")),
@@ -136,21 +135,37 @@ func ExternalServicesOnMultizoneHybridWithLocalityAwareLb() {
 		Expect(k8sCluster.StartZoneIngress()).To(Succeed())
 	})
 	It("should fail request when ingress is down", func() {
+		// when
+		stdout, _, err := zone4.ExecWithCustomRetries("", "", "zone4-demo-client", 3, time.Second*3,
+			"curl", "--verbose", "--max-time", "3", "--fail", "external-service-in-zone1.mesh")
+
+		// then
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(stdout).To(ContainSubstring("HTTP/1.1 200 OK"))
+
 		// when ingress is down
 		Expect(zone1.(*K8sCluster).StopZoneIngress()).To(Succeed())
 
 		// then service is unreachable
-		_, _, err := zone4.ExecWithCustomRetries("", "", "zone4-demo-client", 3, time.Second*3,
+		_, _, err = zone4.ExecWithCustomRetries("", "", "zone4-demo-client", 3, time.Second*3,
 			"curl", "--verbose", "--max-time", "3", "--fail", "external-service-in-zone1.mesh")
 		Expect(err).Should(HaveOccurred())
 	})
 
 	It("should fail request when egress is down", func() {
+		// when
+		stdout, _, err := zone4.ExecWithCustomRetries("", "", "zone4-demo-client", 3, time.Second*3,
+			"curl", "--verbose", "--max-time", "3", "--fail", "external-service-in-zone1.mesh")
+
+		// then
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(stdout).To(ContainSubstring("HTTP/1.1 200 OK"))
+
 		// when egress is down
 		Expect(zone1.(*K8sCluster).StopZoneEgress()).To(Succeed())
 
 		// then service is unreachable
-		_, _, err := zone4.ExecWithCustomRetries("", "", "zone4-demo-client", 3, time.Second*3,
+		_, _, err = zone4.ExecWithCustomRetries("", "", "zone4-demo-client", 3, time.Second*3,
 			"curl", "--verbose", "--max-time", "3", "--fail", "external-service-in-zone1.mesh")
 		Expect(err).Should(HaveOccurred())
 	})
