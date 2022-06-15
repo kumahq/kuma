5f007c9fdb78140d283823ad2a23ebbcd65cb105
Charly Molter <charly.molter@konghq.com>
2022-06-15 11:43:11 +0200

    fix(ci): make version.sh script infer the correct version (#4463)
    - simplify script a little
    - only look at tags that are of the same `major.minor` than the branch
    - ignore tags that are not in format `x.y.z`
    - if there are no matching tag use 0 as a patch version
    - use version sort to make sure we always have the most recent release
    - use real semVer for all our versions
    - support tags with v prefix to make it compatible with go versions (we strip the starting v as it's not semver).
    - use a format that works for docker as well as semVer 2.0
    
    Signed-off-by: Charly Molter <charly.molter@konghq.com>
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>
    # Conflicts:
    #	tools/releases/helm.sh
    #	tools/releases/version.sh

--- tools/releases/version.sh
+++ tools/releases/version.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 
 # Kuma version is built as follows:
-# 1) If a git tag is present on the current commit, then the version is a git tag
-# 2) If the branch starts with "release" (like "release-1.3"), then the version is "$lastGitTag-$shortHash" (for example: 1.4.1-450174242)
-#    At the same time, if $lastGitTag is not final tag (for example 1.4.0-rc1 or 1.4.0-preview1), the version is "dev-$shortHash"
-# 3) If the branch does not start with "release", then the version is "dev-$shortHash" (for example: dev-450174242)
-
-lastGitTag=$(git describe --abbrev=0 --tags)
-shortHash=$(git rev-parse --short HEAD)
-currentBranch=$(git rev-parse --abbrev-ref HEAD)
+# 1) If a git tag is present on the current commit, then the version is a git tag (without a starting v)
+# 2) If the branch is "release-X.Y" look at the existing tags and use either X.Y.0-<shortHash> if there's none or
+#     the latest tag with a patch version increased by one (.e.g if latest tag is X.Y.1 the version will be `X.Y.2-<shortHash>`)
+# 3) In non release branch use `0.0.0-$shortHash`
 
 # Note: this format must be changed carefully, other scripts depend on it
+exactTag=$(git describe --exact-match --tags 2> /dev/null)
+# We only support tags of the format: "v?X.Y.Z(-<alphaNumericName>)?" all other tags will just be ignored and use the regular versioning scheme
+if [[ ${exactTag} =~ ^v?[0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z0-9]+)?$ ]]; then
+  echo "${exactTag/^v//}"
+  exit 0
+fi
 
-if git describe --exact-match --tags > /dev/null 2>&1; then # if we are on tag
-  echo "$lastGitTag"
+shortHash=$(git rev-parse --short HEAD 2> /dev/null)
+currentBranch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+if [[ ${currentBranch} == release-* ]]; then
+    releasePrefix=${currentBranch//release-/}
+    lastGitTag=$(git tag -l | grep -E "^v?${releasePrefix}\.[0-9]+$" | sed 's/^v//'| sort -V | tail -1)
+    if [[ ${lastGitTag} ]]; then
+      IFS=. read -r major minor patch <<< "${lastGitTag}"
+      echo "${major}.${minor}.$((++patch))-preview.${shortHash}"
+    else
+      echo "${releasePrefix}.0-preview.${shortHash}"
+    fi
 else
-  if [[ $lastGitTag =~ [0-9]+\.[0-9]+\.[0-9]+ ]] && [[ $currentBranch == release* ]]; then
-    # set field separator to dot and read parts of semver
-    IFS=. read -r major minor patch <<< "$lastGitTag"
-    echo "$major.$minor.$((++patch))-$shortHash"
-  else
-    echo "dev-$shortHash"
-  fi
+  echo "0.0.0-preview.${shortHash}"
 fi
