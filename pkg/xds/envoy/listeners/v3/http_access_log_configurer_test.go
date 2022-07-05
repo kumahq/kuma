6ef9593f6bde2e2f32ea14c9ed6e2900e72d5d37
Paul Parkanzky <paul.parkanzky@konghq.com>
2022-07-05 11:19:36 -0400

    tests(kuma-cp) add test for legacy TCP accesslog
    Signed-off-by: Paul Parkanzky <paul.parkanzky@konghq.com>

--- pkg/xds/envoy/listeners/v3/http_access_log_configurer_test.go
+++ pkg/xds/envoy/listeners/v3/http_access_log_configurer_test.go
@@ -16,14 +16,15 @@ import (
 var _ = Describe("HttpAccessLogConfigurer", func() {
 
 	type testCase struct {
-		listenerName     string
-		listenerAddress  string
-		listenerPort     uint32
-		listenerProtocol core_xds.SocketAddressProtocol
-		statsName        string
-		routeName        string
-		backend          *mesh_proto.LoggingBackend
-		expected         string
+		listenerName       string
+		listenerAddress    string
+		listenerPort       uint32
+		listenerProtocol   core_xds.SocketAddressProtocol
+		statsName          string
+		routeName          string
+		backend            *mesh_proto.LoggingBackend
+		legacyTcpAccessLog bool
+		expected           string
 	}
 
 	DescribeTable("should generate proper Envoy config",
@@ -32,11 +33,13 @@ var _ = Describe("HttpAccessLogConfigurer", func() {
 			mesh := "demo"
 			sourceService := "web"
 			destinationService := "backend"
+			metaData := &core_xds.DataplaneMetadata{}
+			if !given.legacyTcpAccessLog {
+				metaData.Features = core_xds.Features{core_xds.FeatureTCPAccessLogViaNamedPipe: true}
+			}
 			proxy := &core_xds.Proxy{
-				Id: *core_xds.BuildProxyId("web", "example"),
-				Metadata: &core_xds.DataplaneMetadata{
-					Features: core_xds.Features{core_xds.FeatureTCPAccessLogViaNamedPipe: true},
-				},
+				Id:       *core_xds.BuildProxyId("web", "example"),
+				Metadata: metaData,
 				Dataplane: &core_mesh.DataplaneResource{
 					Meta: &model.ResourceMeta{
 						Name: "dataplane0",
@@ -179,6 +182,61 @@ var _ = Describe("HttpAccessLogConfigurer", func() {
                             "%RESP(server):5%" "%TRAILER(grpc-message):7%" "DYNAMIC_METADATA(namespace:object:key):9" "FILTER_STATE(filter.state.key):12"
 
                       path: /tmp/kuma-al-dataplane0-demo.sock
+                  httpFilters:
+                  - name: envoy.filters.http.router
+                    typedConfig:
+                      '@type': type.googleapis.com/envoy.extensions.filters.http.router.v3.Router
+                  statPrefix: backend
+            name: outbound:127.0.0.1:27070
+            trafficDirection: OUTBOUND`,
+		}),
+		Entry("basic http_connection_manager with legacy tcp access log", testCase{
+			listenerName:    "outbound:127.0.0.1:27070",
+			listenerAddress: "127.0.0.1",
+			listenerPort:    27070,
+			statsName:       "backend",
+			routeName:       "outbound:backend",
+			backend: &mesh_proto.LoggingBackend{
+				Name: "tcp",
+				Format: `[%START_TIME%] "%REQ(X-REQUEST-ID)%" "%REQ(:AUTHORITY)%" "%REQ(ORIGIN)%" "%REQ(CONTENT-TYPE)%" "%KUMA_SOURCE_SERVICE%" "%KUMA_DESTINATION_SERVICE%" "%KUMA_SOURCE_ADDRESS%" "%KUMA_SOURCE_ADDRESS_WITHOUT_PORT%" "%UPSTREAM_HOST%"
+"%RESP(SERVER):5%" "%TRAILER(GRPC-MESSAGE):7%" "DYNAMIC_METADATA(namespace:object:key):9" "FILTER_STATE(filter.state.key):12"
+`, // intentional newline at the end
+				Type: mesh_proto.LoggingTcpType,
+				Conf: util_proto.MustToStruct(&mesh_proto.TcpLoggingBackendConfig{
+					Address: "127.0.0.1:1234",
+				}),
+			},
+			legacyTcpAccessLog: true,
+			expected: `
+            address:
+              socketAddress:
+                address: 127.0.0.1
+                portValue: 27070
+            filterChains:
+            - filters:
+              - name: envoy.filters.network.http_connection_manager
+                typedConfig:
+                  '@type': type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager
+                  accessLog:
+                  - name: envoy.access_loggers.http_grpc
+                    typedConfig:
+                      '@type': type.googleapis.com/envoy.extensions.access_loggers.grpc.v3.HttpGrpcAccessLogConfig
+                      additionalRequestHeadersToLog:
+                      - origin
+                      - content-type
+                      additionalResponseHeadersToLog:
+                      - server
+                      additionalResponseTrailersToLog:
+                      - grpc-message
+                      commonConfig:
+                        grpcService:
+                          envoyGrpc:
+                            clusterName: access_log_sink
+                        logName: |+
+                          127.0.0.1:1234;[%START_TIME%] "%REQ(x-request-id)%" "%REQ(:authority)%" "%REQ(origin)%" "%REQ(content-type)%" "web" "backend" "192.168.0.1:0" "192.168.0.1" "%UPSTREAM_HOST%"
+                          "%RESP(server):5%" "%TRAILER(grpc-message):7%" "DYNAMIC_METADATA(namespace:object:key):9" "FILTER_STATE(filter.state.key):12"
+
+                        transportApiVersion: V3
                   httpFilters:
                   - name: envoy.filters.http.router
                     typedConfig:
