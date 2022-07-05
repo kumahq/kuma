bb21e55c59453b27ff46f15efbc8f2d23ad4ec03
Paul Parkanzky <paul.parkanzky@konghq.com>
2022-07-04 22:19:35 -0400

    fix(kuma-cp): make check
    Signed-off-by: Paul Parkanzky <paul.parkanzky@konghq.com>

--- app/kuma-dp/pkg/dataplane/accesslogs/v3/interfaces.go
+++ app/kuma-dp/pkg/dataplane/accesslogs/v3/interfaces.go
@@ -2,15 +2,7 @@ package v3
 
 import (
 	"io"
-
-	"github.com/go-logr/logr"
 )
-
-// logHandler represents a contract between a log stream receiver and a log handler.
-type logHandler interface {
-	Handle(msg []byte) error
-	io.Closer
-}
 
 // logSender represents a contract between a log handler and a log sender.
 type logSender interface {
@@ -18,6 +10,3 @@ type logSender interface {
 	Send(entry []byte) error
 	io.Closer
 }
-
-// logHandlerFactoryFunc represents a factory of log handler implementations.
-type logHandlerFactoryFunc = func(log logr.Logger, address string) (logHandler, error)
