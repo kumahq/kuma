7038ffcb5f4466be6284bdd66b736a5afae819cf
slonka <slonka@users.noreply.github.com>
2022-07-14 11:52:27 +0200

    feat(cni): rename testfiles and use match golden json
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- app/cni/cmd/install/transform_test.go
+++ app/cni/cmd/install/transform_test.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"io/ioutil"
+	"path"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
+	"github.com/kumahq/kuma/pkg/test/matchers"
 )
 
 const kumaCniConfig = `{
@@ -22,54 +25,54 @@ const kumaCniConfig = `{
 var _ = Describe("testTransformJsonConfig", func() {
 	It("should properly manipulate CNI conflist file", func() {
 		// given
-		calicoConfig, _ := ioutil.ReadFile("testdata/10-calico.conflist")
-		expectedConfig, _ := ioutil.ReadFile("testdata/10-calico.conflist.golden")
+		calicoConfig, _ := ioutil.ReadFile(path.Join("testdata", "10-calico.conflist"))
+		expectedConfig := path.Join("testdata", "10-calico-cni-injected.conflist")
 
 		// when
 		result, err := transformJsonConfig(kumaCniConfig, calicoConfig)
 
 		// then
 		Expect(err).To(Not(HaveOccurred()))
-		Expect(result).To(MatchJSON(expectedConfig))
+		Expect(result).To(matchers.MatchGoldenJSON(expectedConfig))
 	})
 
 	It("should properly manipulate CNI conf file", func() {
 		// given
-		calicoConfig, _ := ioutil.ReadFile("testdata/10-flannel.conf")
-		expectedConfig, _ := ioutil.ReadFile("testdata/10-flannel.conf.golden")
+		calicoConfig, _ := ioutil.ReadFile(path.Join("testdata", "10-flannel.conf"))
+		expectedConfig := path.Join("testdata", "10-flannel-cni-injected.conf")
 
 		// when
 		result, _ := transformJsonConfig(kumaCniConfig, calicoConfig)
 
 		// then
-		Expect(result).To(MatchJSON(expectedConfig))
+		Expect(result).To(matchers.MatchGoldenJSON(expectedConfig))
 	})
 })
 
 var _ = Describe("revertConfig", func() {
 	It("should properly revert CNI conflist", func() {
 		// given
-		changedConfig, _ := ioutil.ReadFile("testdata/10-calico.conflist.golden")
-		originalConfig, _ := ioutil.ReadFile("testdata/10-calico.conflist")
+		changedConfig, _ := ioutil.ReadFile(path.Join("testdata", "10-calico-cni-injected.conflist"))
+		originalConfig := path.Join("testdata", "10-calico.conflist")
 
 		// when
 		result, err := revertConfigContents(changedConfig)
 
 		// then
 		Expect(err).To(Not(HaveOccurred()))
-		Expect(result).To(MatchJSON(originalConfig))
+		Expect(result).To(matchers.MatchGoldenJSON(originalConfig))
 	})
 
 	It("should properly revert CNI conf", func() {
 		// given
-		changedConfig, _ := ioutil.ReadFile("testdata/10-flannel.conf.golden")
-		originalConfig, _ := ioutil.ReadFile("testdata/10-flannel-clean.conf.golden")
+		changedConfig, _ := ioutil.ReadFile(path.Join("testdata", "10-flannel-cni-injected.conf"))
+		originalConfig := path.Join("testdata", "10-flannel-clean.conf")
 
 		// when
 		result, err := revertConfigContents(changedConfig)
 
 		// then
 		Expect(err).To(Not(HaveOccurred()))
-		Expect(result).To(MatchJSON(originalConfig))
+		Expect(result).To(matchers.MatchGoldenJSON(originalConfig))
 	})
 })
--- app/cni/cmd/install/validation_test.go
+++ app/cni/cmd/install/validation_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"path"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
@@ -8,13 +10,13 @@ import (
 var _ = Describe("validation", func() {
 	Context("isValidConflistFile", func() {
 		It("should return no error when a file is a valid conflist file", func() {
-			err := isValidConflistFile("testdata/10-calico.conflist")
+			err := isValidConflistFile(path.Join("testdata", "10-calico.conflist"))
 
 			Expect(err).To(Not(HaveOccurred()))
 		})
 
 		It("should return error when a file is an invalid conflist file", func() {
-			err := isValidConflistFile("testdata/10-flannel.conf")
+			err := isValidConflistFile(path.Join("testdata", "10-flannel.conf"))
 
 			Expect(err).To(HaveOccurred())
 		})
@@ -22,13 +24,13 @@ var _ = Describe("validation", func() {
 
 	Context("isValidConfFile", func() {
 		It("should return no error when a file is a valid conf file", func() {
-			err := isValidConfFile("testdata/10-flannel.conf")
+			err := isValidConfFile(path.Join("testdata", "10-flannel.conf"))
 
 			Expect(err).To(Not(HaveOccurred()))
 		})
 
 		It("should return false when a file is an invalid conf file", func() {
-			err := isValidConfFile("testdata/10-calico.conflist")
+			err := isValidConfFile(path.Join("testdata/", "0-calico.conflist"))
 
 			Expect(err).To(HaveOccurred())
 		})
@@ -36,19 +38,19 @@ var _ = Describe("validation", func() {
 
 	Context("checkInstall", func() {
 		It("should not return error when a file is a conflist file with kuma-cni installed", func() {
-			err := checkInstall("testdata/10-flannel.conf.golden", true)
+			err := checkInstall(path.Join("testdata", "10-flannel-cni-injected.conf"), true)
 
 			Expect(err).To(Not(HaveOccurred()))
 		})
 
 		It("should return true when a file is a conf file with kuma-cni", func() {
-			err := checkInstall("testdata/10-kuma-cni.conf", false)
+			err := checkInstall(path.Join("testdata", "10-kuma-cni.conf"), false)
 
 			Expect(err).To(Not(HaveOccurred()))
 		})
 
 		It("should return false when a file does not have kuma cni installed", func() {
-			err := checkInstall("testdata/10-flannel.conf", false)
+			err := checkInstall(path.Join("testdata", "10-flannel.conf"), false)
 
 			Expect(err).To(HaveOccurred())
 		})
