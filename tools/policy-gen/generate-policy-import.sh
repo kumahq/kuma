de9b84452448c0c3f27b1aed3986da55d475c2d3
Ilya Lobkov <ilya.lobkov@konghq.com>
2022-08-01 22:58:54 +0100

    docs(*): delete extra checks in bash script
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- tools/policy-gen/generate-policy-import.sh
+++ tools/policy-gen/generate-policy-import.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-set -o errexit
-set -o pipefail
-set -o nounset
-
 IMPORTS_FILE="pkg/plugins/policies/imports.go"
 
 imports=$(for i in "$@"; do [[ -f pkg/plugins/policies/${i}/plugin.go ]] && echo "_ \"github.com/kumahq/kuma/pkg/plugins/policies/${i}\""; done)
