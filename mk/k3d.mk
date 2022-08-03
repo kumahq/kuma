47e2bec91de77d6e2ebcc25e70cda55a6dcc357a
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2022-08-03 11:29:21 +0200

    chore: wait for default mesh in k3d/deploy/kuma
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- mk/k3d.mk
+++ mk/k3d.mk
@@ -93,6 +93,9 @@ k3d/deploy/kuma: build/kumactl k3d/load
 	@KUBECONFIG=$(KIND_KUBECONFIG) $(BUILD_ARTIFACTS_DIR)/kumactl/kumactl install --mode $(KUMA_MODE) control-plane $(KUMACTL_INSTALL_CONTROL_PLANE_IMAGES) | KUBECONFIG=$(KIND_KUBECONFIG)  kubectl apply -f -
 	@KUBECONFIG=$(KIND_KUBECONFIG) kubectl wait --timeout=60s --for=condition=Available -n $(KUMA_NAMESPACE) deployment/kuma-control-plane
 	@KUBECONFIG=$(KIND_KUBECONFIG) kubectl wait --timeout=60s --for=condition=Ready -n $(KUMA_NAMESPACE) pods -l app=kuma-control-plane
+	until \
+		 KUBECONFIG=$(KIND_KUBECONFIG) kubectl get mesh default ; \
+	do echo "Waiting for default mesh to be present" && sleep 1; done
 
 .PHONY: k3d/deploy/helm
 k3d/deploy/helm: k3d/load
