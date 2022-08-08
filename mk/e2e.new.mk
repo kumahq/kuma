62b650312c2c4103d43a63ed79716ce90b56beee
slonka <slonka@users.noreply.github.com>
2022-08-08 14:24:00 +0200

    fix(cni): exclude old k3s tests
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- mk/e2e.new.mk
+++ mk/e2e.new.mk
@@ -41,6 +41,10 @@ K8S_CLUSTER_TOOL=kind
 GINKGO_E2E_TEST_FLAGS += --label-filter="!kind-not-supported"
 endif
 
+ifeq ($(CI_K3S_VERSION), "v1.19.16-k3s1")
+GINKGO_E2E_TEST_FLAGS += --label-filter="!legacy-k3s-not-supported"
+endif
+
 ifdef IPV6
 KIND_CONFIG_IPV6=-ipv6
 endif
