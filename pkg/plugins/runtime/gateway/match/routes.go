b331642d127d9413cefc6016f0f894310afe9be1
Mike Beaumont <mjboamail@gmail.com>
2022-08-17 13:52:11 +0200

    fix(gateway): don't create invalid envoy config when routes and listeners don't match (#4837)
    * test(gateway): add failing test
    * fix(gateway): don't create invalid envoy config when routes and listeners don't match
    
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>
    (cherry picked from commit 8b483edf65b5f1a7e9e2248a5178e8fd94e2a04b)

--- pkg/plugins/runtime/gateway/match/routes.go
+++ pkg/plugins/runtime/gateway/match/routes.go
@@ -3,27 +3,19 @@ package match
 import (
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
 	"github.com/kumahq/kuma/pkg/core/policy"
-	"github.com/kumahq/kuma/pkg/core/resources/model"
+	core_mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
 )
 
 // Routes finds all the route resources of the given type that
 // have a `Sources` selector that matches the given listener tags.
-func Routes(routes model.ResourceList, listener mesh_proto.TagSelector) []model.Resource {
-	var matched []model.Resource
+func Routes(routes []*core_mesh.MeshGatewayRouteResource, listener mesh_proto.TagSelector) []*core_mesh.MeshGatewayRouteResource {
+	var matched []*core_mesh.MeshGatewayRouteResource
 
-	for _, i := range routes.GetItems() {
-		if c, ok := i.(policy.ConnectionPolicy); ok {
-			if _, ok := policy.MatchSelector(listener, c.Sources()); ok {
-				matched = append(matched, i)
-			}
-			continue
+	for _, i := range routes {
+		if _, ok := policy.MatchSelector(listener, i.Selectors()); ok {
+			matched = append(matched, i)
 		}
-		if c, ok := i.(policy.DataplanePolicy); ok {
-			if _, ok := policy.MatchSelector(listener, c.Selectors()); ok {
-				matched = append(matched, i)
-			}
-			continue
-		}
+		continue
 	}
 
 	return matched
