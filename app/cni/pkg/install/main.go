0646e0a357527013e9d006564813f25a132bd948
Krzysztof Słonka <slonka@users.noreply.github.com>
2022-08-18 16:17:43 +0200

    fix(cni): hook up log level to cni (#4849)
    * fix(cni): hook up log level to cni
    
    Signed-off-by: slonka <slonka@users.noreply.github.com>
    Co-authored-by: Bart Smykla <bartek@smykla.com>
    Co-authored-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
    (cherry picked from commit f568a0081e1a2e65eb502810d68ca00addd98466)

--- app/cni/pkg/install/main.go
+++ app/cni/pkg/install/main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/pkg/errors"
 	"go.uber.org/multierr"
 
-	"github.com/kumahq/kuma/pkg/core"
+	kuma_log "github.com/kumahq/kuma/pkg/log"
 	"github.com/kumahq/kuma/pkg/util/files"
 )
 
@@ -25,10 +25,11 @@ const (
 	secondaryBinDir    = "/host/secondary-bin-dir"
 	serviceAccountPath = "/var/run/secrets/kubernetes.io/serviceaccount"
 	readyFilePath      = "/tmp/ready"
+	defaultLogName     = "install-cni"
 )
 
 var (
-	log = core.NewLoggerTo(os.Stderr, 2).WithName("install-cni")
+	log = CreateNewLogger(defaultLogName, kuma_log.DebugLevel)
 )
 
 func removeBinFiles() error {
@@ -201,19 +202,6 @@ func tryWritingToDir(dir string) error {
 	return nil
 }
 
-func setupSignalCleanup(ic *InstallerConfig) {
-	osSignals := make(chan os.Signal, 1)
-	signal.Notify(osSignals,
-		syscall.SIGHUP,
-		syscall.SIGINT,
-		syscall.SIGTERM,
-		syscall.SIGQUIT)
-	go func() {
-		_ = <-osSignals
-		cleanup(ic)
-	}()
-}
-
 func Run() {
 	installerConfig, err := loadInstallerConfig()
 	if err != nil {
@@ -221,35 +209,48 @@ func Run() {
 		os.Exit(1)
 	}
 
+	err = SetLogLevel(&log, installerConfig.CniLogLevel, defaultLogName)
+	if err != nil {
+		log.Error(err, "error occurred during setting the log level")
+		os.Exit(2)
+	}
+
 	err = install(installerConfig)
 	if err != nil {
 		log.Error(err, "error occurred during cni installation")
-		os.Exit(1)
+		os.Exit(3)
 	}
 
 	err = atomic.WriteFile(readyFilePath, strings.NewReader(""))
 	if err != nil {
 		log.Error(err, "unable to mark as ready")
-		os.Exit(1)
+		os.Exit(4)
 	}
 
 	if err := runLoop(installerConfig); err != nil {
 		log.Error(err, "checking installation failed - exiting")
-		os.Exit(1)
+		os.Exit(5)
 	}
 }
 
 func runLoop(ic *InstallerConfig) error {
 	defer cleanup(ic)
-	setupSignalCleanup(ic)
+	osSignals := make(chan os.Signal, 1)
+	signal.Notify(osSignals, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
-	for ic.ShouldSleep {
-		time.Sleep(time.Duration(ic.CfgCheckInterval) * time.Second)
-		err := checkInstall(ic.MountedCniNetDir+"/"+ic.CniConfName, ic.ChainedCniPlugin)
-		if err != nil {
-			return err
-		}
+	if !ic.ShouldSleep {
+		return nil
 	}
 
-	return nil
+	for {
+		select {
+		case <-osSignals:
+			return nil
+		case <-time.After(time.Duration(ic.CfgCheckInterval) * time.Second):
+			err := checkInstall(ic.MountedCniNetDir+"/"+ic.CniConfName, ic.ChainedCniPlugin)
+			if err != nil {
+				return err
+			}
+		}
+	}
 }
