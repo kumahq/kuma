f3c20d59631e7b978d6dd63420a0e68d7a6a4d0e
Ilya Lobkov <ilya.lobkov@konghq.com>
2022-09-06 12:31:58 +0100

    feat(*): make check
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- pkg/core/resources/model/rest/list_test.go
+++ pkg/core/resources/model/rest/list_test.go
@@ -311,25 +311,25 @@ var _ = Describe("Unmarshal ResourceList", func() {
 				From: []*policies_api.MeshTrafficPermission_From{
 					{
 						TargetRef: &common_api.TargetRef{Kind: "Mesh"},
-						Default: &policies_api.MeshTrafficPermission_From_Conf{
+						Default: &policies_api.MeshTrafficPermission_Conf{
 							Action: "ALLOW",
 						},
 					},
 					{
 						TargetRef: &common_api.TargetRef{Kind: "MeshSubset", Tags: map[string]string{"kuma.io/zone": "us-east"}},
-						Default: &policies_api.MeshTrafficPermission_From_Conf{
+						Default: &policies_api.MeshTrafficPermission_Conf{
 							Action: "DENY_WITH_SHADOW_ALLOW",
 						},
 					},
 					{
 						TargetRef: &common_api.TargetRef{Kind: "MeshService", Name: "backend"},
-						Default: &policies_api.MeshTrafficPermission_From_Conf{
+						Default: &policies_api.MeshTrafficPermission_Conf{
 							Action: "ALLOW_WITH_SHADOW_DENY",
 						},
 					},
 					{
 						TargetRef: &common_api.TargetRef{Kind: "MeshServiceSubset", Name: "backend", Tags: map[string]string{"version": "v1"}},
-						Default: &policies_api.MeshTrafficPermission_From_Conf{
+						Default: &policies_api.MeshTrafficPermission_Conf{
 							Action: "DENY",
 						},
 					},
@@ -345,7 +345,7 @@ var _ = Describe("Unmarshal ResourceList", func() {
 				From: []*policies_api.MeshTrafficPermission_From{
 					{
 						TargetRef: &common_api.TargetRef{Kind: "MeshSubset", Tags: map[string]string{"kuma.io/zone": "us-east"}},
-						Default: &policies_api.MeshTrafficPermission_From_Conf{
+						Default: &policies_api.MeshTrafficPermission_Conf{
 							Action: "DENY",
 						},
 					},
