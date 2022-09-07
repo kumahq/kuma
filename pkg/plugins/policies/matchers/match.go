83e30eb96e812b0b54579c1d064da4c0d8daae16
Ilya Lobkov <ilya.lobkov@konghq.com>
2022-09-07 11:42:33 +0100

    feat(*): separate method for inbound policies
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- pkg/plugins/policies/matchers/match.go
+++ pkg/plugins/policies/matchers/match.go
@@ -18,21 +18,56 @@ type ResourceSpecWithTargetRef interface {
 	GetTargetRef() *common_proto.TargetRef
 }
 
-// MatchedPolicies match policies using the standard matchers using targetRef (madr-005)
-func MatchedPolicies(rType core_model.ResourceType, dpp *core_mesh.DataplaneResource, resources xds_context.Resources) (core_xds.TypedMatchingPolicies, error) {
-	policies := resources.ListOrEmpty(rType)
+// MatchedInboundPolicies returns an InboundPolicies map with a list of policies
+// set for each inbound. Should be used for policies that modify inbound listeners/cluster.
+func MatchedInboundPolicies(
+	rType core_model.ResourceType,
+	dpp *core_mesh.DataplaneResource,
+	resources xds_context.Resources,
+) (core_xds.TypedMatchingPolicies, error) {
+	inboundPoliciesMap, err := matchedInboundPolicies(rType, dpp, resources)
+	if err != nil {
+		return core_xds.TypedMatchingPolicies{}, err
+	}
+
+	for inbound := range inboundPoliciesMap {
+		sort.Sort(ByTargetRef(inboundPoliciesMap[inbound]))
+	}
+
+	return core_xds.TypedMatchingPolicies{
+		Type:            rType,
+		InboundPolicies: inboundPoliciesMap,
+	}, nil
+}
+
+// MatchedDataplanePolicies returns a DataplanePolicies list with policies
+// matched for the DPP. Should be used for policies that modify either outbound
+// listeners/clusters or DPP overall (like ProxyTemplate).
+// The result of this function is the same as MatchedInboundPolicies, but without
+// grouping policies by inbounds.
+func MatchedDataplanePolicies(
+	rType core_model.ResourceType,
+	dpp *core_mesh.DataplaneResource,
+	resources xds_context.Resources,
+) (core_xds.TypedMatchingPolicies, error) {
+	// policies matched for DPP is a union of policies matched for every inbound
+	inboundPoliciesMap, err := matchedInboundPolicies(rType, dpp, resources)
+	if err != nil {
+		return core_xds.TypedMatchingPolicies{}, err
+	}
+
+	matchedPoliciesByName := map[string]core_model.Resource{}
+	for _, policies := range inboundPoliciesMap {
+		for _, policy := range policies {
+			if _, ok := matchedPoliciesByName[policy.GetMeta().GetName()]; !ok {
+				matchedPoliciesByName[policy.GetMeta().GetName()] = policy
+			}
+		}
+	}
 
 	matchedPolicies := []core_model.Resource{}
-	for _, policy := range policies.GetItems() {
-		spec, ok := policy.GetSpec().(ResourceSpecWithTargetRef)
-		if !ok {
-			return core_xds.TypedMatchingPolicies{}, errors.Errorf("resource type %v doesn't support TargetRef matching", rType)
-		}
-
-		targetRef := spec.GetTargetRef()
-		if isTargetedBy(targetRef, dpp) {
-			matchedPolicies = append(matchedPolicies, policy)
-		}
+	for _, policy := range matchedPoliciesByName {
+		matchedPolicies = append(matchedPolicies, policy)
 	}
 
 	sort.Sort(ByTargetRef(matchedPolicies))
@@ -43,35 +78,64 @@ func MatchedPolicies(rType core_model.ResourceType, dpp *core_mesh.DataplaneReso
 	}, nil
 }
 
-func isTargetedBy(tr *common_proto.TargetRef, dpp *core_mesh.DataplaneResource) bool {
+func matchedInboundPolicies(
+	rType core_model.ResourceType,
+	dpp *core_mesh.DataplaneResource,
+	resources xds_context.Resources,
+) (map[mesh_proto.InboundInterface][]core_model.Resource, error) {
+	policies := resources.ListOrEmpty(rType)
+
+	matched := map[mesh_proto.InboundInterface][]core_model.Resource{}
+	for _, policy := range policies.GetItems() {
+		spec, ok := policy.GetSpec().(ResourceSpecWithTargetRef)
+		if !ok {
+			return nil, errors.Errorf("resource type %v doesn't support TargetRef matching", rType)
+		}
+
+		targetRef := spec.GetTargetRef()
+		for _, inbound := range inboundsSelectedByTargetRef(targetRef, dpp) {
+			matched[inbound] = append(matched[inbound], policy)
+		}
+	}
+
+	return matched, nil
+}
+
+func inboundsSelectedByTargetRef(tr *common_proto.TargetRef, dpp *core_mesh.DataplaneResource) []mesh_proto.InboundInterface {
 	switch tr.GetKindEnum() {
 	case common_proto.TargetRef_Mesh:
-		return true
+		// return all inbounds interfaces of the DPP
+		result := []mesh_proto.InboundInterface{}
+		for _, inbound := range dpp.Spec.GetNetworking().GetInbound() {
+			result = append(result, dpp.Spec.GetNetworking().ToInboundInterface(inbound))
+		}
+		return result
 	case common_proto.TargetRef_MeshSubset:
-		return isDataplaneSelectedBy(tr.GetTags(), dpp)
+		return inboundsSelectedByTags(tr.GetTags(), dpp)
 	case common_proto.TargetRef_MeshService:
-		return isDataplaneSelectedBy(map[string]string{
+		return inboundsSelectedByTags(map[string]string{
 			mesh_proto.ServiceTag: tr.GetName(),
 		}, dpp)
 	case common_proto.TargetRef_MeshServiceSubset:
 		trTags := tr.GetTags()
 		trTags[mesh_proto.ServiceTag] = tr.GetName()
-		return isDataplaneSelectedBy(trTags, dpp)
+		return inboundsSelectedByTags(trTags, dpp)
 	default:
-		return false
+		return []mesh_proto.InboundInterface{}
 	}
 }
 
-func isDataplaneSelectedBy(tags map[string]string, dpp *core_mesh.DataplaneResource) bool {
+func inboundsSelectedByTags(tags map[string]string, dpp *core_mesh.DataplaneResource) []mesh_proto.InboundInterface {
+	result := []mesh_proto.InboundInterface{}
 	for _, inbound := range dpp.Spec.GetNetworking().GetInbound() {
-		if isInboundSelectedBy(tags, inbound) {
-			return true
+		if isInboundSelectedByTags(tags, inbound) {
+			result = append(result, dpp.Spec.GetNetworking().ToInboundInterface(inbound))
 		}
 	}
-	return false
+	return result
 }
 
-func isInboundSelectedBy(tags map[string]string, inbound *mesh_proto.Dataplane_Networking_Inbound) bool {
+func isInboundSelectedByTags(tags map[string]string, inbound *mesh_proto.Dataplane_Networking_Inbound) bool {
 	for k, v := range tags {
 		if inboundValue, ok := inbound.Tags[k]; !ok || inboundValue != v {
 			return false
--- pkg/plugins/policies/matchers/match_test.go
+++ pkg/plugins/policies/matchers/match_test.go
@@ -2,6 +2,7 @@ package matchers_test
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"path"
 
@@ -15,78 +16,133 @@ import (
 	_ "github.com/kumahq/kuma/pkg/plugins/policies"
 	"github.com/kumahq/kuma/pkg/plugins/policies/matchers"
 	policies_api "github.com/kumahq/kuma/pkg/plugins/policies/meshtrafficpermission/api/v1alpha1"
-	matchers2 "github.com/kumahq/kuma/pkg/test/matchers"
+	test_matchers "github.com/kumahq/kuma/pkg/test/matchers"
 	"github.com/kumahq/kuma/pkg/util/yaml"
 	xds_context "github.com/kumahq/kuma/pkg/xds/context"
 )
 
-var _ = Describe("MatchedPolicies", func() {
+var _ = Describe("Match", func() {
 
-	type testCase struct {
-		dppFile                    string
-		resourceListResponseFile   string
-		matchedResourcesGoldenFile string
+	readDPP := func(file string) *core_mesh.DataplaneResource {
+		dppYaml, err := os.ReadFile(path.Join("testdata", file))
+		Expect(err).ToNot(HaveOccurred())
+
+		dpp, err := rest.YAML.UnmarshalCore(dppYaml)
+		Expect(err).ToNot(HaveOccurred())
+		return dpp.(*core_mesh.DataplaneResource)
 	}
 
-	DescribeTable("should return a list of policies ordered by levels for the given DPP",
-		func(given testCase) {
-			// given DPP resource
-			dppYaml, err := os.ReadFile(path.Join("testdata", given.dppFile))
-			Expect(err).ToNot(HaveOccurred())
+	readResourceListResponse := func(file string) xds_context.Resources {
+		responseBytes, err := os.ReadFile(path.Join("testdata", file))
+		Expect(err).ToNot(HaveOccurred())
 
-			dpp, err := rest.YAML.UnmarshalCore(dppYaml)
+		rawResources := yaml.SplitYAML(string(responseBytes))
+		resourceList := &policies_api.MeshTrafficPermissionResourceList{}
+		for _, rawResource := range rawResources {
+			resource, err := rest.YAML.UnmarshalCore([]byte(rawResource))
 			Expect(err).ToNot(HaveOccurred())
+			err = resourceList.AddItem(resource)
+			Expect(err).ToNot(HaveOccurred())
+		}
 
-			// given MeshTrafficPermissions
-			responseBytes, err := os.ReadFile(path.Join("testdata", given.resourceListResponseFile))
-			Expect(err).ToNot(HaveOccurred())
-			rawResources := yaml.SplitYAML(string(responseBytes))
-			resourceList := &policies_api.MeshTrafficPermissionResourceList{}
-			for _, rawResource := range rawResources {
-				resource, err := rest.YAML.UnmarshalCore([]byte(rawResource))
+		return xds_context.Resources{
+			MeshLocalResources: map[core_model.ResourceType]core_model.ResourceList{
+				policies_api.MeshTrafficPermissionType: resourceList,
+			},
+		}
+	}
+
+	printPolicies := func(policies []core_model.Resource) string {
+		matchedPolicyList := &policies_api.MeshTrafficPermissionResourceList{}
+		for _, policy := range policies {
+			Expect(matchedPolicyList.AddItem(policy)).To(Succeed())
+		}
+		bytesBuffer := &bytes.Buffer{}
+		err := kubectl_output.NewPrinter().Print(rest.From.ResourceList(matchedPolicyList), bytesBuffer)
+		Expect(err).ToNot(HaveOccurred())
+		return bytesBuffer.String()
+	}
+
+	Describe("MatchedDataplanePolicies", func() {
+
+		type testCase struct {
+			dppFile                    string
+			resourceListResponseFile   string
+			matchedResourcesGoldenFile string
+		}
+
+		DescribeTable("should return a list of policies ordered by levels for the given DPP",
+			func(given testCase) {
+				// given DPP resource
+				dpp := readDPP(given.dppFile)
+
+				// given MeshTrafficPermissions
+				resources := readResourceListResponse(given.resourceListResponseFile)
+
+				// when
+				policies, err := matchers.MatchedDataplanePolicies(policies_api.MeshTrafficPermissionType, dpp, resources)
 				Expect(err).ToNot(HaveOccurred())
-				err = resourceList.AddItem(resource)
+
+				// then
+				Expect(printPolicies(policies.DataplanePolicies)).To(test_matchers.MatchGoldenYAML(path.Join("testdata", given.matchedResourcesGoldenFile)))
+			},
+			Entry("01. policies select the dataplane without collisions", testCase{
+				dppFile:                    "01.dataplane.yaml",
+				resourceListResponseFile:   "01.response.yaml",
+				matchedResourcesGoldenFile: "01.golden.yaml",
+			}),
+			Entry("02. policies with the same levels select the dataplane", testCase{
+				dppFile:                    "02.dataplane.yaml",
+				resourceListResponseFile:   "02.response.yaml",
+				matchedResourcesGoldenFile: "02.golden.yaml",
+			}),
+			Entry("03. policy doesn't selects a union of inbound tags", testCase{
+				dppFile:                    "03.dataplane.yaml",
+				resourceListResponseFile:   "03.response.yaml",
+				matchedResourcesGoldenFile: "03.golden.yaml",
+			}),
+			Entry("04. policy doesn't select dataplane if at least one tag is not presented", testCase{
+				dppFile:                    "04.dataplane.yaml",
+				resourceListResponseFile:   "04.response.yaml",
+				matchedResourcesGoldenFile: "04.golden.yaml",
+			}),
+		)
+	})
+
+	Describe("MatchedInboundPolicies", func() {
+
+		type testCase struct {
+			dppFile                  string
+			resourceListResponseFile string
+			inboundToGoldenFile      map[string]string
+		}
+
+		DescribeTable("should return a map of inbounds with matched policies",
+			func(given testCase) {
+				// given DPP resource
+				dpp := readDPP(given.dppFile)
+
+				// given MeshTrafficPermissions
+				resources := readResourceListResponse(given.resourceListResponseFile)
+
+				// when
+				policies, err := matchers.MatchedInboundPolicies(policies_api.MeshTrafficPermissionType, dpp, resources)
 				Expect(err).ToNot(HaveOccurred())
-			}
-			resources := xds_context.Resources{
-				MeshLocalResources: map[core_model.ResourceType]core_model.ResourceList{
-					policies_api.MeshTrafficPermissionType: resourceList,
+
+				// then
+				for inbound, matched := range policies.InboundPolicies {
+					key := fmt.Sprintf("%v", inbound)
+					Expect(printPolicies(matched)).To(test_matchers.MatchGoldenYAML(path.Join("testdata", given.inboundToGoldenFile[key])))
+				}
+			},
+			Entry("05. each inbound is selected by policies", testCase{
+				dppFile:                  "05.dataplane.yaml",
+				resourceListResponseFile: "05.response.yaml",
+				inboundToGoldenFile: map[string]string{
+					":8080:8080": "05.golden.8080.yaml",
+					":8081:8081": "05.golden.8081.yaml",
 				},
-			}
+			}))
+	})
 
-			// when
-			policies, err := matchers.MatchedPolicies(policies_api.MeshTrafficPermissionType, dpp.(*core_mesh.DataplaneResource), resources)
-			Expect(err).ToNot(HaveOccurred())
-
-			// then
-			matchedPolicyList := &policies_api.MeshTrafficPermissionResourceList{}
-			for _, policy := range policies.DataplanePolicies {
-				Expect(matchedPolicyList.AddItem(policy)).To(Succeed())
-			}
-			bytesBuffer := &bytes.Buffer{}
-			err = kubectl_output.NewPrinter().Print(rest.From.ResourceList(matchedPolicyList), bytesBuffer)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(bytesBuffer.String()).To(matchers2.MatchGoldenYAML(path.Join("testdata", given.matchedResourcesGoldenFile)))
-		},
-		Entry("01. policies select the dataplane without collisions", testCase{
-			dppFile:                    "01.dataplane.yaml",
-			resourceListResponseFile:   "01.response.yaml",
-			matchedResourcesGoldenFile: "01.golden.yaml",
-		}),
-		Entry("02. policies with the same levels select the dataplane", testCase{
-			dppFile:                    "02.dataplane.yaml",
-			resourceListResponseFile:   "02.response.yaml",
-			matchedResourcesGoldenFile: "02.golden.yaml",
-		}),
-		Entry("03. policy doesn't selects a union of inbound tags", testCase{
-			dppFile:                    "03.dataplane.yaml",
-			resourceListResponseFile:   "03.response.yaml",
-			matchedResourcesGoldenFile: "03.golden.yaml",
-		}),
-		Entry("04. policy doesn't select dataplane if at least one tag is not presented", testCase{
-			dppFile:                    "04.dataplane.yaml",
-			resourceListResponseFile:   "04.response.yaml",
-			matchedResourcesGoldenFile: "04.golden.yaml",
-		}),
-	)
 })
