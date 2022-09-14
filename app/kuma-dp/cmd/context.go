d2d43314e463fb9278d9f9ccb5c73e34877c0e1d
Mike Beaumont <mjboamail@gmail.com>
2022-09-14 22:46:53 +0200

    refactor: add xds authentication customization  (#4990)
    * refactor(kuma-cp): add xds authentication customization
    * refactor(kuma-dp): add token generation customization
    * refactor(kuma-dp): put default logic in DataplaneTokenGenerator
    
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- app/kuma-dp/cmd/context.go
+++ app/kuma-dp/cmd/context.go
@@ -3,9 +3,13 @@ package cmd
 import (
 	"crypto/tls"
 	"net/http"
+	"path/filepath"
 	"runtime"
 	"time"
 
+	"github.com/pkg/errors"
+
+	kumadp_config "github.com/kumahq/kuma/app/kuma-dp/pkg/config"
 	"github.com/kumahq/kuma/app/kuma-dp/pkg/dataplane/envoy"
 	kumadp "github.com/kumahq/kuma/pkg/config/app/kuma-dp"
 	"github.com/kumahq/kuma/pkg/core/runtime/component"
@@ -19,11 +23,33 @@ type RootContext struct {
 	ComponentManager         component.Manager
 	BootstrapGenerator       envoy.BootstrapConfigFactoryFunc
 	BootstrapDynamicMetadata map[string]string
+	DataplaneTokenGenerator  func(*kumadp.Config) error
 	Config                   *kumadp.Config
 	LogLevel                 log.LogLevel
 }
 
 var features = []string{core_xds.FeatureTCPAccessLogViaNamedPipe}
+
+// defaultDataplaneTokenGenerator uses only given tokens or paths from the
+// config.
+func defaultDataplaneTokenGenerator(cfg *kumadp.Config) error {
+	if cfg.DataplaneRuntime.Token != "" {
+		path := filepath.Join(cfg.DataplaneRuntime.ConfigDir, cfg.Dataplane.Name)
+		if err := writeFile(path, []byte(cfg.DataplaneRuntime.Token), 0600); err != nil {
+			runLog.Error(err, "unable to create file with dataplane token")
+			return err
+		}
+		cfg.DataplaneRuntime.TokenPath = path
+	}
+
+	if cfg.DataplaneRuntime.TokenPath != "" {
+		if err := kumadp_config.ValidateTokenPath(cfg.DataplaneRuntime.TokenPath); err != nil {
+			return errors.Wrapf(err, "dataplane token is invalid, in Kubernetes you must mount a serviceAccount token, in universal you must start your proxy with a generated token.")
+		}
+	}
+
+	return nil
+}
 
 func DefaultRootContext() *RootContext {
 	config := kumadp.DefaultConfig()
@@ -35,5 +61,6 @@ func DefaultRootContext() *RootContext {
 		}, runtime.GOOS, features),
 		Config:                   &config,
 		BootstrapDynamicMetadata: map[string]string{},
+		DataplaneTokenGenerator:  defaultDataplaneTokenGenerator,
 	}
 }
