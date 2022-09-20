29248a659d600b3b93d05fa78e83eebbc0f1b80a
Mike Beaumont <mjboamail@gmail.com>
2022-09-20 14:46:59 +0200

    test: set XDSAuthenticator in test runtime (#5030)
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- pkg/test/runtime/runtime.go
+++ pkg/test/runtime/runtime.go
@@ -34,6 +34,7 @@ import (
 	leader_memory "github.com/kumahq/kuma/pkg/plugins/leader/memory"
 	resources_memory "github.com/kumahq/kuma/pkg/plugins/resources/memory"
 	tokens_access "github.com/kumahq/kuma/pkg/tokens/builtin/access"
+	universal_auth "github.com/kumahq/kuma/pkg/xds/auth/universal"
 	xds_hooks "github.com/kumahq/kuma/pkg/xds/hooks"
 	"github.com/kumahq/kuma/pkg/xds/secrets"
 )
@@ -83,6 +84,8 @@ func BuilderFor(appCtx context.Context, cfg kuma_cp.Config) (*core_runtime.Build
 
 	metrics, _ := metrics.NewMetrics("Standalone")
 	builder.WithMetrics(metrics)
+
+	builder.WithXDSAuthenticator(universal_auth.NewNoopAuthenticator())
 
 	builder.WithDataSourceLoader(datasource.NewDataSourceLoader(builder.ResourceManager()))
 	builder.WithCaManager("builtin", builtin.NewBuiltinCaManager(builder.ResourceManager()))
