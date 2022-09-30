ca5907731db4043f73745a59e042f8416af004b2
slonka <slonka@users.noreply.github.com>
2022-09-30 11:14:57 +0200

    feat(kumacp): configurer done
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- pkg/plugins/policies/meshtrace/plugin/v1alpha1/plugin.go
+++ pkg/plugins/policies/meshtrace/plugin/v1alpha1/plugin.go
@@ -1,13 +1,18 @@
 package v1alpha1
 
 import (
+	envoy_listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
+	envoy_resource "github.com/envoyproxy/go-control-plane/pkg/resource/v3"
+	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
 	"github.com/kumahq/kuma/pkg/core"
 	core_plugins "github.com/kumahq/kuma/pkg/core/plugins"
 	core_mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
-	core_xds "github.com/kumahq/kuma/pkg/core/xds"
+	"github.com/kumahq/kuma/pkg/core/xds"
 	"github.com/kumahq/kuma/pkg/plugins/policies/matchers"
 	api "github.com/kumahq/kuma/pkg/plugins/policies/meshtrace/api/v1alpha1"
+	plugin_xds "github.com/kumahq/kuma/pkg/plugins/policies/meshtrace/plugin/xds"
 	xds_context "github.com/kumahq/kuma/pkg/xds/context"
+	"github.com/kumahq/kuma/pkg/xds/generator"
 )
 
 var _ core_plugins.PolicyPlugin = &plugin{}
@@ -20,11 +25,110 @@ func NewPlugin() core_plugins.Plugin {
 	return &plugin{}
 }
 
-func (p plugin) MatchedPolicies(dataplane *core_mesh.DataplaneResource, resources xds_context.Resources) (core_xds.TypedMatchingPolicies, error) {
+func (p plugin) MatchedPolicies(dataplane *core_mesh.DataplaneResource, resources xds_context.Resources) (xds.TypedMatchingPolicies, error) {
 	return matchers.MatchedPolicies(api.MeshTraceType, dataplane, resources)
 }
 
-func (p plugin) Apply(rs *core_xds.ResourceSet, ctx xds_context.Context, proxy *core_xds.Proxy) error {
+func (p plugin) Apply(rs *xds.ResourceSet, ctx xds_context.Context, proxy *xds.Proxy) error {
+	policies, ok := proxy.Policies.Dynamic[api.MeshTraceType]
+	if !ok {
+		return nil
+	}
+
+	listeners := gatherListeners(rs)
+
+	if err := applyToInbounds(policies.FromRules, listeners.inbound, proxy.Dataplane); err != nil {
+		return err
+	}
+
 	log.Info("apply is not implemented")
+	return nil
+}
+
+type listeners struct {
+	inbound  map[xds.InboundListener]*envoy_listener.Listener
+	outbound map[mesh_proto.OutboundInterface]*envoy_listener.Listener
+}
+
+func gatherListeners(rs *xds.ResourceSet) listeners {
+	listeners := listeners{
+		inbound:      map[xds.InboundListener]*envoy_listener.Listener{},
+		outbound:     map[mesh_proto.OutboundInterface]*envoy_listener.Listener{},
+	}
+
+	for _, res := range rs.Resources(envoy_resource.ListenerType) {
+		listener := res.Resource.(*envoy_listener.Listener)
+		address := listener.GetAddress().GetSocketAddress()
+
+		switch res.Origin {
+		case generator.OriginOutbound:
+			listeners.outbound[mesh_proto.OutboundInterface{
+				DataplaneIP:   address.GetAddress(),
+				DataplanePort: address.GetPortValue(),
+			}] = listener
+		case generator.OriginInbound:
+			listeners.inbound[xds.InboundListener{
+				Address: address.GetAddress(),
+				Port:    address.GetPortValue(),
+			}] = listener
+		default:
+			continue
+		}
+	}
+
+	return listeners
+}
+
+func applyToInbounds(
+	rules xds.FromRules, inboundListeners map[xds.InboundListener]*envoy_listener.Listener, dataplane *core_mesh.DataplaneResource,
+) error {
+	for _, inbound := range dataplane.Spec.GetNetworking().GetInbound() {
+		iface := dataplane.Spec.Networking.ToInboundInterface(inbound)
+
+		listenerKey := xds.InboundListener{
+			Address: iface.DataplaneIP,
+			Port:    iface.DataplanePort,
+		}
+		listener, ok := inboundListeners[listenerKey]
+		if !ok {
+			continue
+		}
+
+		serviceName := inbound.GetTags()[mesh_proto.ServiceTag]
+
+		if err := configureInbound(rules.Rules[listenerKey], dataplane, xds.MeshService(serviceName), listener); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func configureInbound(
+	fromRules xds.Rules,
+	dataplane *core_mesh.DataplaneResource,
+	subset xds.Subset,
+	listener *envoy_listener.Listener,
+) error {
+	serviceName := dataplane.Spec.GetIdentifyingService()
+
+	var conf *api.MeshTrace_Conf
+	if computed := fromRules.Compute(subset); computed != nil {
+		conf = computed.(*api.MeshTrace_Conf)
+	} else {
+		return nil
+	}
+
+	configurer := plugin_xds.Configurer{
+		Conf: conf,
+		Service: serviceName,
+	}
+
+	for _, chain := range listener.FilterChains {
+		if err := configurer.Configure(chain); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
---
+++ pkg/plugins/policies/meshtrace/plugin/xds/configurer.go
@@ -0,0 +1,177 @@
+package xds
+
+import (
+    tracingv3 "github.com/envoyproxy/go-control-plane/envoy/type/tracing/v3"
+    api "github.com/kumahq/kuma/pkg/plugins/policies/meshtrace/api/v1alpha1"
+    v3 "github.com/kumahq/kuma/pkg/xds/envoy/listeners/v3"
+    net_url "net/url"
+
+    envoy_listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
+    envoy_trace "github.com/envoyproxy/go-control-plane/envoy/config/trace/v3"
+    envoy_hcm "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
+    envoy_type "github.com/envoyproxy/go-control-plane/envoy/type/v3"
+    "github.com/pkg/errors"
+
+    "github.com/kumahq/kuma/pkg/util/proto"
+)
+
+type Configurer struct {
+    Conf *api.MeshTrace_Conf
+
+    // Opaque string which envoy will assign to tracer collector cluster, on those
+    // which support association of named "service" tags on traces. Consumed by datadog.
+    Service          string
+}
+
+var _ v3.FilterChainConfigurer = &Configurer{}
+
+func (c *Configurer) Configure(filterChain *envoy_listener.FilterChain) error {
+    if c.Conf.Backends[0] == nil {
+        return nil
+    }
+    
+    backend := c.Conf.Backends[0]
+
+    return v3.UpdateHTTPConnectionManager(filterChain, func(hcm *envoy_hcm.HttpConnectionManager) error {
+        hcm.Tracing = &envoy_hcm.HttpConnectionManager_Tracing{}
+        
+        if c.Conf.Sampling != nil {
+            hcm.Tracing.OverallSampling = &envoy_type.Percent{
+                Value: float64(c.Conf.Sampling.Overall),
+            }
+            hcm.Tracing.ClientSampling = &envoy_type.Percent{
+                Value: float64(c.Conf.Sampling.Client),
+            }
+            hcm.Tracing.RandomSampling = &envoy_type.Percent{
+                Value: float64(c.Conf.Sampling.Random),
+            }
+        }
+
+        if c.Conf.Tags != nil {
+            hcm.Tracing.CustomTags = mapTags(c.Conf.Tags)
+        }
+
+        if backend.GetZipkin() != nil {
+            tracing, err := zipkinConfig(backend.Zipkin)
+            if err != nil {
+                return err
+            }
+            hcm.Tracing.Provider = tracing
+            
+        }
+
+        if backend.GetDatadog() != nil {
+            tracing, err := datadogConfig(c.Service)
+            if err != nil {
+                return err
+            }
+            hcm.Tracing.Provider = tracing
+        }
+        
+        return nil
+    })
+}
+
+func datadogConfig(serviceName string) (*envoy_trace.Tracing_Http, error) {
+    datadogConfig := envoy_trace.DatadogConfig{
+        CollectorCluster: getTracingClusterName("datadog"),
+        ServiceName:      serviceName,
+    }
+    datadogConfigAny, err := proto.MarshalAnyDeterministic(&datadogConfig)
+    if err != nil {
+        return nil, err
+    }
+    tracingConfig := &envoy_trace.Tracing_Http{
+        Name: "envoy.datadog",
+        ConfigType: &envoy_trace.Tracing_Http_TypedConfig{
+            TypedConfig: datadogConfigAny,
+        },
+    }
+    return tracingConfig, nil
+}
+
+func zipkinConfig(zipkin *api.MeshTrace_ZipkinBackend) (*envoy_trace.Tracing_Http, error) {
+    url, err := net_url.ParseRequestURI(zipkin.Url)
+    if err != nil {
+        return nil, errors.Wrap(err, "invalid URL of Zipkin")
+    }
+
+    zipkinConfig := envoy_trace.ZipkinConfig{
+        CollectorCluster:         getTracingClusterName("zipkin"),
+        CollectorEndpoint:        url.Path,
+        TraceId_128Bit:           zipkin.TraceId128Bit,
+        CollectorEndpointVersion: apiVersion(zipkin, url),
+        SharedSpanContext:        zipkin.SharedSpanContext,
+        CollectorHostname:        url.Host,
+    }
+    zipkinConfigAny, err := proto.MarshalAnyDeterministic(&zipkinConfig)
+    if err != nil {
+        return nil, err
+    }
+    tracingConfig := &envoy_trace.Tracing_Http{
+        Name: "envoy.zipkin",
+        ConfigType: &envoy_trace.Tracing_Http_TypedConfig{
+            TypedConfig: zipkinConfigAny,
+        },
+    }
+    return tracingConfig, nil
+}
+
+func apiVersion(zipkin *api.MeshTrace_ZipkinBackend, url *net_url.URL) envoy_trace.ZipkinConfig_CollectorEndpointVersion {
+    if zipkin.ApiVersion == "" { // try to infer it from the URL
+        if url.Path == "/api/v2/spans" {
+            return envoy_trace.ZipkinConfig_HTTP_JSON
+        }
+    } else {
+        switch zipkin.ApiVersion {
+        case "httpJson":
+            return envoy_trace.ZipkinConfig_HTTP_JSON
+        case "httpProto":
+            return envoy_trace.ZipkinConfig_HTTP_PROTO
+        }
+    }
+    return envoy_trace.ZipkinConfig_HTTP_JSON
+}
+
+func getTracingClusterName(name string) string {
+    return "meshtrace:" + name
+}
+
+func mapTags(tags []*api.MeshTrace_Tag) []*tracingv3.CustomTag {
+
+    var customTags []*tracingv3.CustomTag
+
+    for _, tag := range tags {
+
+        if tag.Header != nil {
+            customTags = append(customTags, mapHeaderTag(tag.Name, tag.Header))
+        } else {
+            customTags = append(customTags, mapLiteralTag(tag.Name, tag.Literal))
+        }
+    }
+
+    return customTags
+}
+
+func mapLiteralTag(name, literal string) *tracingv3.CustomTag {
+    return &tracingv3.CustomTag{
+        Tag: name,
+        Type: &tracingv3.CustomTag_Literal_{
+            Literal: &tracingv3.CustomTag_Literal{
+                Value: literal,
+            },
+        },
+    }
+}
+
+func mapHeaderTag(name string, header *api.MeshTrace_HeaderTag) *tracingv3.CustomTag{
+    return &tracingv3.CustomTag{
+        Tag: name,
+        Type: &tracingv3.CustomTag_RequestHeader{
+            RequestHeader: &tracingv3.CustomTag_Header{
+                Name:         header.Name,
+                DefaultValue: header.Default,
+            },
+        },
+    }
+}
