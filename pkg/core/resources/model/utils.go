2c230fab4dd88f3f0cf49a4148e3b99400b31dd0
Ilya Lobkov <ilya.lobkov@konghq.com>
2022-10-19 12:47:18 +0100

    fix(*): remove generic type
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- pkg/core/resources/model/utils.go
+++ pkg/core/resources/model/utils.go
@@ -13,74 +13,56 @@ import (
 )
 
 func ToJSON(spec ResourceSpec) ([]byte, error) {
-	m := &marshal[[]byte]{protoFn: util_proto.ToJSON, fn: json.Marshal}
-	return m.ResourceSpec(spec)
+	if msg, ok := spec.(proto.Message); ok {
+		return util_proto.ToJSON(msg)
+	} else {
+		return json.Marshal(spec)
+	}
 }
 
 func ToYAML(spec ResourceSpec) ([]byte, error) {
-	m := &marshal[[]byte]{protoFn: util_proto.ToYAML, fn: yaml.Marshal}
-	return m.ResourceSpec(spec)
+	if msg, ok := spec.(proto.Message); ok {
+		return util_proto.ToYAML(msg)
+	} else {
+		return yaml.Marshal(spec)
+	}
 }
 
 func ToAny(spec ResourceSpec) (*anypb.Any, error) {
-	m := &marshal[*anypb.Any]{
-		protoFn: util_proto.MarshalAnyDeterministic,
-		fn: func(a any) (*anypb.Any, error) {
-			bytes, err := json.Marshal(a)
-			if err != nil {
-				return nil, err
-			}
-			return &anypb.Any{
-				Value: bytes,
-			}, nil
-		},
+	if msg, ok := spec.(proto.Message); ok {
+		return util_proto.MarshalAnyDeterministic(msg)
+	} else {
+		bytes, err := json.Marshal(spec)
+		if err != nil {
+			return nil, err
+		}
+		return &anypb.Any{
+			Value: bytes,
+		}, nil
 	}
-	return m.ResourceSpec(spec)
 }
 
 func FromJSON(src []byte, spec ResourceSpec) error {
-	u := &unmarshal[[]byte]{protoFn: util_proto.FromJSON, fn: json.Unmarshal}
-	return u.ResourceSpec(src, spec)
+	if msg, ok := spec.(proto.Message); ok {
+		return util_proto.FromJSON(src, msg)
+	} else {
+		return json.Unmarshal(src, spec)
+	}
 }
 
 func FromYAML(src []byte, spec ResourceSpec) error {
-	u := &unmarshal[[]byte]{protoFn: util_proto.FromYAML, fn: yaml.Unmarshal}
-	return u.ResourceSpec(src, spec)
+	if msg, ok := spec.(proto.Message); ok {
+		return util_proto.FromYAML(src, msg)
+	} else {
+		return yaml.Unmarshal(src, spec)
+	}
 }
 
 func FromAny(src *anypb.Any, spec ResourceSpec) error {
-	u := &unmarshal[*anypb.Any]{
-		protoFn: util_proto.UnmarshalAnyTo,
-		fn: func(a *anypb.Any, dst any) error {
-			return json.Unmarshal(a.Value, dst)
-		},
-	}
-	return u.ResourceSpec(src, spec)
-}
-
-type marshal[T any] struct {
-	protoFn func(proto.Message) (T, error)
-	fn      func(any) (T, error)
-}
-
-func (m *marshal[T]) ResourceSpec(spec ResourceSpec) (T, error) {
 	if msg, ok := spec.(proto.Message); ok {
-		return m.protoFn(msg)
+		return util_proto.UnmarshalAnyTo(src, msg)
 	} else {
-		return m.fn(spec)
-	}
-}
-
-type unmarshal[T any] struct {
-	protoFn func(T, proto.Message) error
-	fn      func(T, any) error
-}
-
-func (m *unmarshal[T]) ResourceSpec(src T, spec ResourceSpec) error {
-	if msg, ok := spec.(proto.Message); ok {
-		return m.protoFn(src, msg)
-	} else {
-		return m.fn(src, spec)
+		return json.Unmarshal(src.Value, spec)
 	}
 }
 
