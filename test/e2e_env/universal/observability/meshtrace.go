991ca346d5e6a13e3403bdd297ad1de54548a2de
Mike Beaumont <mjboamail@gmail.com>
2022-11-02 11:28:09 +0100

    feat(MeshTrace): `MeshGateway` support  (#5243)
    * feat(MeshTrace): MeshGateway support
    * test(e2e): MeshGateway and MeshTrace
    
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- test/e2e_env/universal/observability/meshtrace.go
+++ test/e2e_env/universal/observability/meshtrace.go
@@ -2,11 +2,14 @@ package observability
 
 import (
 	"fmt"
+	"net"
+	"strconv"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	"github.com/kumahq/kuma/test/e2e_env/universal/env"
+	"github.com/kumahq/kuma/test/e2e_env/universal/gateway"
 	. "github.com/kumahq/kuma/test/framework"
 	obs "github.com/kumahq/kuma/test/framework/deployments/observability"
 )
@@ -34,12 +37,20 @@ func PluginTest() {
 	obsDeployment := "obs-meshtrace"
 	var obsClient obs.Observability
 
+	GatewayAddressPort := func(appName string, port int) string {
+		ip := env.Cluster.GetApp(appName).GetIP()
+		return net.JoinHostPort(ip, strconv.Itoa(port))
+	}
+
 	BeforeAll(func() {
 		err := NewClusterSetup().
 			Install(obs.Install(obsDeployment)).
 			Install(MeshUniversal(mesh)).
 			Install(TestServerUniversal("test-server", mesh, WithArgs([]string{"echo", "--instance", "universal1"}))).
 			Install(DemoClientUniversal(AppModeDemoClient, mesh, WithTransparentProxy(true))).
+			Install(GatewayProxyUniversal(mesh, "edge-gateway")).
+			Install(YamlUniversal(gateway.MkGateway("edge-gateway", mesh, false, "example.kuma.io", "test-server", 8080))).
+			Install(gateway.GatewayClientAppUniversal("gateway-client")).
 			Setup(env.Cluster)
 		obsClient = obs.From(obsDeployment, env.Cluster)
 		Expect(err).ToNot(HaveOccurred())
@@ -64,8 +75,23 @@ func PluginTest() {
 			}
 			// then traces are published
 			return obsClient.TracedServices()
-		}, "30s", "1s").Should(Equal([]string{
+		}, "30s", "1s").Should(ContainElements([]string{
 			"demo-client",
+			"jaeger-query",
+			"test-server",
+		}))
+	})
+
+	It("should emit MeshGateway traces to jaeger", func() {
+		err := YamlUniversal(traceAll(mesh, obsClient.ZipkinCollectorURL()))(env.Cluster)
+		Expect(err).ToNot(HaveOccurred())
+
+		Eventually(func() ([]string, error) {
+			gateway.ProxySimpleRequests(env.Cluster, "universal1",
+				GatewayAddressPort("edge-gateway", 8080), "example.kuma.io")
+			return obsClient.TracedServices()
+		}, "30s", "1s").Should(ContainElements([]string{
+			"edge-gateway",
 			"jaeger-query",
 			"test-server",
 		}))
