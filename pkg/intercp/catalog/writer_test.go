93ce602260a9716fb63c3c4ec8f1f316079facc2
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2022-12-08 11:05:46 +0100

    fix: race in tests
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- pkg/intercp/catalog/writer_test.go
+++ pkg/intercp/catalog/writer_test.go
@@ -15,8 +15,8 @@ import (
 var _ = Describe("Writer", func() {
 
 	var c catalog.Catalog
-	var heartbeats *catalog.Heartbeats
 	var closeCh chan struct{}
+	var heartbeatCancelFunc context.CancelFunc
 
 	leader := catalog.Instance{
 		Id:          "instance-2",
@@ -25,81 +25,71 @@ var _ = Describe("Writer", func() {
 		Leader:      true,
 	}
 
+	instance := catalog.Instance{
+		Id:          "instance-1",
+		Address:     "192.168.0.1",
+		InterCpPort: 1234,
+	}
+
 	BeforeEach(func() {
 		store := memory.NewStore()
 		resManager := manager.NewResourceManager(store)
 		c = catalog.NewConfigCatalog(resManager)
-		heartbeats = catalog.NewHeartbeats()
+		heartbeats := catalog.NewHeartbeats()
 		closeCh = make(chan struct{})
 		writer := catalog.NewWriter(c, heartbeats, leader, 100*time.Millisecond)
 		go func() {
 			defer GinkgoRecover()
 			Expect(writer.Start(closeCh)).To(Succeed())
 		}()
+
+		ctx, fn := context.WithCancel(context.Background())
+		heartbeatCancelFunc = fn
+		go func() {
+			t := time.NewTicker(10 * time.Millisecond)
+			for {
+				select {
+				case <-t.C:
+					heartbeats.Add(instance)
+				case <-ctx.Done():
+					return
+				}
+			}
+		}()
 	})
 
 	AfterEach(func() {
 		close(closeCh)
+		heartbeatCancelFunc()
 	})
 
-	Context("with active heartbeat from instance", func() {
-		var heartbeatCancelFunc context.CancelFunc
+	It("should write to catalog", func() {
+		Eventually(func(g Gomega) {
+			instances, err := c.Instances(context.Background())
+			g.Expect(err).ToNot(HaveOccurred())
+			g.Expect(instances).To(HaveLen(2))
+			g.Expect(instances[0]).To(Equal(instance))
+			g.Expect(instances[1]).To(Equal(leader))
+		}, "10s", "100ms").Should(Succeed())
+	})
 
-		instance := catalog.Instance{
-			Id:          "instance-1",
-			Address:     "192.168.0.1",
-			InterCpPort: 1234,
-		}
+	It("should remove instance from the catalog once hearth-beating stop", func() {
+		// given 2 instances in catalog
+		Eventually(func(g Gomega) {
+			instances, err := c.Instances(context.Background())
+			g.Expect(err).ToNot(HaveOccurred())
+			g.Expect(instances).To(HaveLen(2))
+		}, "10s", "100ms").Should(Succeed())
 
-		BeforeEach(func() {
-			ctx, fn := context.WithCancel(context.Background())
-			heartbeatCancelFunc = fn
+		// when
+		heartbeatCancelFunc()
 
-			go func() {
-				t := time.NewTicker(10 * time.Millisecond)
-				for {
-					select {
-					case <-t.C:
-						heartbeats.Add(instance)
-					case <-ctx.Done():
-						return
-					}
-				}
-			}()
-		})
-
-		AfterEach(func() {
-			heartbeatCancelFunc()
-		})
-
-		It("should write to catalog", func() {
-			Eventually(func(g Gomega) {
-				instances, err := c.Instances(context.Background())
-				g.Expect(err).ToNot(HaveOccurred())
-				g.Expect(instances).To(HaveLen(2))
-				g.Expect(instances[0]).To(Equal(instance))
-				g.Expect(instances[1]).To(Equal(leader))
-			}, "10s", "100ms").Should(Succeed())
-		})
-
-		It("should remove instance from the catalog once hearth-beating stop", func() {
-			// given 2 instances in catalog
-			Eventually(func(g Gomega) {
-				instances, err := c.Instances(context.Background())
-				g.Expect(err).ToNot(HaveOccurred())
-				g.Expect(instances).To(HaveLen(2))
-			}, "10s", "100ms").Should(Succeed())
-
-			// when
-			heartbeatCancelFunc()
-
-			// then
-			Eventually(func(g Gomega) {
-				instances, err := c.Instances(context.Background())
-				g.Expect(err).ToNot(HaveOccurred())
-				g.Expect(instances).To(HaveLen(1))
-				g.Expect(instances[0]).To(Equal(leader))
-			}, "10s", "100ms").Should(Succeed())
-		})
+		// then
+		Eventually(func(g Gomega) {
+			instances, err := c.Instances(context.Background())
+			g.Expect(err).ToNot(HaveOccurred())
+			g.Expect(instances).To(HaveLen(1))
+			g.Expect(instances[0]).To(Equal(leader))
+		}, "10s", "100ms").Should(Succeed())
 	})
 })
