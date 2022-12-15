29b7b7d07a4bf66acae452d8653f500c6048ea3b
slonka <slonka@users.noreply.github.com>
2022-12-15 17:02:57 +0100

    ci: add helm to path since installer checks it
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- mk/dependencies/helm.sh
+++ mk/dependencies/helm.sh
@@ -4,5 +4,6 @@ set -e
 
 OUTPUT_DIR=$1/bin
 VERSION="3.8.2"
+export PATH="$OUTPUT_DIR:$PATH" # install script checks if helm is in your path
 curl --fail --location -s https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | \
 	HELM_INSTALL_DIR=${OUTPUT_DIR} DESIRED_VERSION=v${VERSION} USE_SUDO=false bash
