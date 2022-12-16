48014298706b74fbcf3d49d8f3f9f96e97e67a59
Łukasz Dziedziak <lukidzi@gmail.com>
2022-12-16 08:44:02 -0500

    chore(deps): upgrade k3d
    Signed-off-by: Łukasz Dziedziak <lukidzi@gmail.com>

--- mk/dependencies/k3d.sh
+++ mk/dependencies/k3d.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OUTPUT_DIR=$1/bin
-VERSION="5.3.0"
+VERSION="5.4.1"
 # see https://raw.githubusercontent.com/rancher/k3d/main/install.sh
 curl --fail --location -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | \
           PATH=${OUTPUT_DIR}:${PATH} TAG=v${VERSION} USE_SUDO="false" K3D_INSTALL_DIR="${OUTPUT_DIR}" bash
