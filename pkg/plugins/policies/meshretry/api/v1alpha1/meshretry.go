555b09ddb00f043d9c728f5d5b6a95570a00441a
Ilya Lobkov <ilya.lobkov@konghq.com>
2022-12-19 13:40:48 +0000

    feat: add comments
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- pkg/plugins/policies/meshretry/api/v1alpha1/meshretry.go
+++ pkg/plugins/policies/meshretry/api/v1alpha1/meshretry.go
@@ -44,23 +44,65 @@ type TCP struct {
 type HTTPRetryOn string
 
 var (
-	ALL_5XX                    HTTPRetryOn = "5XX"
-	GATEWAY_ERROR              HTTPRetryOn = "GATEWAY_ERROR"
-	RESET                      HTTPRetryOn = "RESET"
-	RETRIABLE_4XX              HTTPRetryOn = "RETRIABLE_4XX"
-	CONNECT_FAILURE            HTTPRetryOn = "CONNECT_FAILURE"
-	ENVOY_RATELIMITED          HTTPRetryOn = "ENVOY_RATELIMITED"
-	REFUSED_STREAM             HTTPRetryOn = "REFUSED_STREAM"
+	// ALL_5XX means Envoy will attempt a retry if the upstream server responds with
+	// any 5xx response code, or does not respond at all (disconnect/reset/read timeout).
+	// (Includes CONNECT_FAILURE and REFUSED_STREAM)
+	ALL_5XX HTTPRetryOn = "5XX"
+
+	// GATEWAY_ERROR is similar to the 5XX policy but will only retry requests
+	// that result in a 502, 503, or 504.
+	GATEWAY_ERROR HTTPRetryOn = "GATEWAY_ERROR"
+
+	// RESET means Envoy will attempt a retry if the upstream server does not respond at all
+	// (disconnect/reset/read timeout.)
+	RESET HTTPRetryOn = "RESET"
+
+	// RETRIABLE_4XX means Envoy will attempt a retry if the upstream server responds with
+	// a retriable 4xx response code. Currently, the only response code in this category is 409.
+	RETRIABLE_4XX HTTPRetryOn = "RETRIABLE_4XX"
+
+	// CONNECT_FAILURE means Envoy will attempt a retry if a request is failed because of
+	// a connection failure to the upstream server (connect timeout, etc.). (Included in 5XX)
+	CONNECT_FAILURE HTTPRetryOn = "CONNECT_FAILURE"
+
+	// ENVOY_RATELIMITED means Envoy will retry if the header x-envoy-ratelimited is present.
+	ENVOY_RATELIMITED HTTPRetryOn = "ENVOY_RATELIMITED"
+
+	// REFUSED_STREAM means Envoy will attempt a retry if the upstream server resets the stream
+	// with a REFUSED_STREAM error code. This reset type indicates that a request is safe to retry.
+	// (Included in 5XX)
+	REFUSED_STREAM HTTPRetryOn = "REFUSED_STREAM"
+
+	// HTTP3_POST_CONNECT_FAILURE means Envoy will attempt a retry if a request is sent over
+	// HTTP/3 to the upstream server and failed after getting connected.
 	HTTP3_POST_CONNECT_FAILURE HTTPRetryOn = "HTTP3_POST_CONNECT_FAILURE"
-	HTTP_METHOD_CONNECT        HTTPRetryOn = "HTTP_METHOD_CONNECT"
-	HTTP_METHOD_DELETE         HTTPRetryOn = "HTTP_METHOD_DELETE"
-	HTTP_METHOD_GET            HTTPRetryOn = "HTTP_METHOD_GET"
-	HTTP_METHOD_HEAD           HTTPRetryOn = "HTTP_METHOD_HEAD"
-	HTTP_METHOD_OPTIONS        HTTPRetryOn = "HTTP_METHOD_OPTIONS"
-	HTTP_METHOD_PATCH          HTTPRetryOn = "HTTP_METHOD_PATCH"
-	HTTP_METHOD_POST           HTTPRetryOn = "HTTP_METHOD_POST"
-	HTTP_METHOD_PUT            HTTPRetryOn = "HTTP_METHOD_PUT"
-	HTTP_METHOD_TRACE          HTTPRetryOn = "HTTP_METHOD_TRACE"
+
+	// HTTP_METHOD_CONNECT means Envoy will attempt a retry if the used HTTP method is CONNECT.
+	HTTP_METHOD_CONNECT HTTPRetryOn = "HTTP_METHOD_CONNECT"
+
+	// HTTP_METHOD_DELETE means Envoy will attempt a retry if the used HTTP method is DELETE.
+	HTTP_METHOD_DELETE HTTPRetryOn = "HTTP_METHOD_DELETE"
+
+	// HTTP_METHOD_GET means Envoy will attempt a retry if the used HTTP method is GET.
+	HTTP_METHOD_GET HTTPRetryOn = "HTTP_METHOD_GET"
+
+	// HTTP_METHOD_HEAD means Envoy will attempt a retry if the used HTTP method is HEAD.
+	HTTP_METHOD_HEAD HTTPRetryOn = "HTTP_METHOD_HEAD"
+
+	// HTTP_METHOD_OPTIONS means Envoy will attempt a retry if the used HTTP method is OPTIONS.
+	HTTP_METHOD_OPTIONS HTTPRetryOn = "HTTP_METHOD_OPTIONS"
+
+	// HTTP_METHOD_PATCH means Envoy will attempt a retry if the used HTTP method is PATCH.
+	HTTP_METHOD_PATCH HTTPRetryOn = "HTTP_METHOD_PATCH"
+
+	// HTTP_METHOD_POST means Envoy will attempt a retry if the used HTTP method is POST.
+	HTTP_METHOD_POST HTTPRetryOn = "HTTP_METHOD_POST"
+
+	// HTTP_METHOD_PUT means Envoy will attempt a retry if the used HTTP method is PUT.
+	HTTP_METHOD_PUT HTTPRetryOn = "HTTP_METHOD_PUT"
+
+	// HTTP_METHOD_TRACE means Envoy will attempt a retry if the used HTTP method is TRACE.
+	HTTP_METHOD_TRACE HTTPRetryOn = "HTTP_METHOD_TRACE"
 )
 
 type HTTP struct {
@@ -92,11 +134,25 @@ type HTTP struct {
 type GRPCRetryOn string
 
 var (
-	CANCELED           GRPCRetryOn = "CANCELED"
-	DEADLINE_EXCEEDED  GRPCRetryOn = "DEADLINE_EXCEEDED"
-	INTERNAL           GRPCRetryOn = "INTERNAL"
+	// CANCELED means Envoy will attempt a retry if the gRPC status code in
+	// the response headers is “cancelled” (1)
+	CANCELED GRPCRetryOn = "CANCELED"
+
+	// DEADLINE_EXCEEDED Envoy will attempt a retry if the gRPC status code in
+	// the response headers is “deadline-exceeded” (4)
+	DEADLINE_EXCEEDED GRPCRetryOn = "DEADLINE_EXCEEDED"
+
+	// INTERNAL Envoy will attempt to retry if the gRPC status code in the
+	// response headers is “internal” (13)
+	INTERNAL GRPCRetryOn = "INTERNAL"
+
+	// RESOURCE_EXHAUSTED means Envoy will attempt a retry if the gRPC status code
+	// in the response headers is “resource-exhausted” (8)
 	RESOURCE_EXHAUSTED GRPCRetryOn = "RESOURCE_EXHAUSTED"
-	UNAVAILABLE        GRPCRetryOn = "UNAVAILABLE"
+
+	// UNAVAILABLE means Envoy will attempt a retry if the gRPC status code in
+	// the response headers is “unavailable” (14)
+	UNAVAILABLE GRPCRetryOn = "UNAVAILABLE"
 )
 
 type GRPC struct {
