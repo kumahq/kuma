710333d4f2d5478151fbd224f184146fdafc8444
Łukasz Dziedziak <lukidzi@gmail.com>
2022-12-19 13:09:46 -0500

    chore(deps): install dev tools and split if more repos
    Signed-off-by: Łukasz Dziedziak <lukidzi@gmail.com>

--- tools/dev/install-dev-tools.sh
+++ tools/dev/install-dev-tools.sh
@@ -8,11 +8,13 @@ TOOLS_DEPS_DIRS="$3"
 TOOLS_DEPS_LOCK_FILE="$4"
 GOOS="$5"
 GOARCH="$6"
+# We cannot provide list of arguments not as a string, so we join them with a comma
+TOOLS_DEPS_DIRS=${TOOLS_DEPS_DIRS//,/ }
 
 mkdir -p "$CI_TOOLS_BIN_DIR" "$CI_TOOLS_DIR"/protos
 
 # Also compute a hash to use for caching
-FILES=$(find "$TOOLS_DEPS_DIRS" -name '*.sh' | sort)
+FILES=$(find $TOOLS_DEPS_DIRS -name '*.sh' | sort)
 for i in ${FILES}; do OS="$GOOS" ARCH="$GOARCH" "$i" "${CI_TOOLS_DIR}"; done
 for i in ${FILES}; do echo "---${i}"; cat "${i}"; done | git hash-object --stdin > "$TOOLS_DEPS_LOCK_FILE"
 
