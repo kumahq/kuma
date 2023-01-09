570d9f70ced4b4d801343afc584327e0eceefcf5
Mike Beaumont <mjboamail@gmail.com>
2023-01-09 17:57:10 +0100

    feat(MeshHTTPRoute): add basic validation
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

---
+++ pkg/plugins/policies/meshhttproute/api/v1alpha1/validation.go
@@ -0,0 +1,44 @@
+package v1alpha1
+
+import (
+	common_api "github.com/kumahq/kuma/api/common/v1alpha1"
+	"github.com/kumahq/kuma/pkg/core/validators"
+	matcher_validators "github.com/kumahq/kuma/pkg/plugins/policies/matchers/validators"
+)
+
+func (r *MeshHTTPRouteResource) validate() error {
+	var verr validators.ValidationError
+	path := validators.RootedAt("spec")
+	verr.AddErrorAt(path.Field("targetRef"), validateTop(r.Spec.TargetRef))
+	verr.AddErrorAt(path.Field("to"), validateRules(path, r.Spec.To))
+	return verr.OrNil()
+}
+
+func validateTop(targetRef common_api.TargetRef) validators.ValidationError {
+	return matcher_validators.ValidateTargetRef(targetRef, &matcher_validators.ValidateTargetRefOpts{
+		SupportedKinds: []common_api.TargetRefKind{
+			common_api.Mesh,
+			common_api.MeshSubset,
+			common_api.MeshService,
+			common_api.MeshServiceSubset,
+		},
+	})
+}
+
+func validateToRef(targetRef common_api.TargetRef) validators.ValidationError {
+	return matcher_validators.ValidateTargetRef(targetRef, &matcher_validators.ValidateTargetRefOpts{
+		SupportedKinds: []common_api.TargetRefKind{
+			common_api.MeshService,
+		},
+	})
+}
+
+func validateRules(path validators.PathBuilder, tos []To) validators.ValidationError {
+	var errs validators.ValidationError
+
+	for i, to := range tos {
+		errs.AddErrorAt(path.Index(i), validateToRef(to.TargetRef))
+	}
+
+	return errs
+}
