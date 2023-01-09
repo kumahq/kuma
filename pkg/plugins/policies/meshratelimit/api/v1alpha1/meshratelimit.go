5a5969f0074f1e587075c630f01d2f9f509fa0b9
Ayush Prakash Senapati <a.p.senapati008@gmail.com>
2023-01-09 16:58:44 +0530

    fix: make targetRef required (#5593)
    Signed-off-by: AyushSenapati <a.p.senapati008@gmail.com>
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>
    Co-authored-by: Mike Beaumont <mjboamail@gmail.com>

--- pkg/plugins/policies/meshratelimit/api/v1alpha1/meshratelimit.go
+++ pkg/plugins/policies/meshratelimit/api/v1alpha1/meshratelimit.go
@@ -12,7 +12,7 @@ type MeshRateLimit struct {
 	// TargetRef is a reference to the resource the policy takes an effect on.
 	// The resource could be either a real store object or virtual resource
 	// defined inplace.
-	TargetRef common_api.TargetRef `json:"targetRef,omitempty"`
+	TargetRef common_api.TargetRef `json:"targetRef"`
 	// From list makes a match between clients and corresponding configurations
 	From []From `json:"from,omitempty"`
 }
@@ -20,7 +20,7 @@ type MeshRateLimit struct {
 type From struct {
 	// TargetRef is a reference to the resource that represents a group of
 	// clients.
-	TargetRef common_api.TargetRef `json:"targetRef,omitempty"`
+	TargetRef common_api.TargetRef `json:"targetRef"`
 	// Default is a configuration specific to the group of clients referenced in
 	// 'targetRef'
 	Default Conf `json:"default,omitempty"`
--- tools/policy-gen/bootstrap/root.go
+++ tools/policy-gen/bootstrap/root.go
@@ -176,7 +176,7 @@ type {{ .name }} struct {
 	// TargetRef is a reference to the resource the policy takes an effect on.
 	// The resource could be either a real store object or virtual resource
 	// defined inplace.
-	TargetRef common_api.TargetRef` + " `json:\"targetRef,omitempty\"`" + `
+	TargetRef common_api.TargetRef` + " `json:\"targetRef\"`" + `
 	{{- end }}
 	{{- if .generateTo }}
 	// To list makes a match between the consumed services and corresponding configurations
@@ -192,7 +192,7 @@ type {{ .name }} struct {
 type To struct {
 	// TargetRef is a reference to the resource that represents a group of
 	// destinations.
-	TargetRef common_api.TargetRef` + " `json:\"targetRef,omitempty\"`" + `
+	TargetRef common_api.TargetRef` + " `json:\"targetRef\"`" + `
 	// Default is a configuration specific to the group of destinations referenced in
 	// 'targetRef'
 	Default Conf` + " `json:\"default,omitempty\"`" + `
@@ -204,7 +204,7 @@ type To struct {
 type From struct {
 	// TargetRef is a reference to the resource that represents a group of
 	// clients.
-	TargetRef common_api.TargetRef` + " `json:\"targetRef,omitempty\"`" + `
+	TargetRef common_api.TargetRef` + " `json:\"targetRef\"`" + `
 	// Default is a configuration specific to the group of clients referenced in
 	// 'targetRef'
 	Default Conf` + " `json:\"default,omitempty\"`" + `
