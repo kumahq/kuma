88ff1823ff52dc43ac472c35e7113c1f6af744a7
Lukasz Dziedziak <lukidzi@gmail.com>
2023-01-09 19:59:30 -0500

    chore(deps): regenerate files
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/xds/server/v3/resource_warming_forcer.go
+++ pkg/xds/server/v3/resource_warming_forcer.go
@@ -19,39 +19,41 @@ import (
 var warmingForcerLog = xdsServerLog.WithName("warming-forcer")
 
 // The problem
-//   When you send Cluster of type EDS to Envoy, it updates the config, but Cluster is marked as warming and is not used until you send EDS request.
-//   https://www.envoyproxy.io/docs/envoy/latest/api-docs/xds_protocol#xds-protocol-resource-warming
-//   The main problem is when you update the Cluster itself (for example enable mTLS or change a cluster property via ProxyTemplate)
-//   If you don't send EDS request after that (no endpoint has changed) then the cluster is stuck in warming state indefinitely.
+//
+//	When you send Cluster of type EDS to Envoy, it updates the config, but Cluster is marked as warming and is not used until you send EDS request.
+//	https://www.envoyproxy.io/docs/envoy/latest/api-docs/xds_protocol#xds-protocol-resource-warming
+//	The main problem is when you update the Cluster itself (for example enable mTLS or change a cluster property via ProxyTemplate)
+//	If you don't send EDS request after that (no endpoint has changed) then the cluster is stuck in warming state indefinitely.
 //
 // The solution
-//   The easiest solution would be to just set a new version of endpoints also when cluster changes (in pkg/xds/server/SnapshotReconciler) . The problem is that go-control-plane does not support resource ordering
-//   * https://github.com/envoyproxy/go-control-plane/issues/59
-//   * https://github.com/envoyproxy/go-control-plane/issues/218
-//   * https://github.com/envoyproxy/go-control-plane/issues/235
-//   Therefore even if we were to set a new version of EDS + CDS on one snapshot, there is no guarantee that EDS will be delivered after CDS.
 //
-//   The alternative solution is this based on a callbacks.
-//   Nonce is a sequence indicator of a sent DiscoveryResponse on a stream. We use ADS therefore every single DiscoveryResponse regardless of a type is sent with incremented nonce.
-//   Typical sequence of CDS + EDS looks like this:
-//   1) Envoy sends DiscoveryRequest [type=CDS, version="", nonce=""] // ask for the clusters
-//   2) Kuma sends DiscoveryResponse [type=CDS, version="UUID-1", nonce="1"] // response with clusters
-//   3) Envoy sends DiscoveryRequest [type=EDS, version="", nonce=""] // ask for the endpoints
-//   4) Envoy sends DiscoveryRequest [type=CDS, version="UUID-1", nonce="1"] // confirmations that it received clusters (ACK)
-//   5) Kuma sends DiscoveryResponse [type=EDS, version="UUID-2", nonce="2"] // response with endpoints
-//   6) Envoy sends DiscoveryRequest [type=EDS, version="UUID-2", nonce="2"] // confirmations that it received endpoints (ACK)
+//	The easiest solution would be to just set a new version of endpoints also when cluster changes (in pkg/xds/server/SnapshotReconciler) . The problem is that go-control-plane does not support resource ordering
+//	* https://github.com/envoyproxy/go-control-plane/issues/59
+//	* https://github.com/envoyproxy/go-control-plane/issues/218
+//	* https://github.com/envoyproxy/go-control-plane/issues/235
+//	Therefore even if we were to set a new version of EDS + CDS on one snapshot, there is no guarantee that EDS will be delivered after CDS.
 //
-//   Then if we send a Cluster update (continuing the flow above)
-//   7) Kuma sends DiscoveryResponse [type=CDS, version="UUID-2", nonce="3"] // response with cluster update
-//   8) Envoy sends DiscoveryRequest [type=CDS, version="UUID-2", nonce="3"] // ACK
-//   9) Envoy sends DiscoveryRequest [type=EDS, version="UUID-2", nonce="2"] // Envoy sends a request which looks like the second ACK for the previous endpoints
+//	The alternative solution is this based on a callbacks.
+//	Nonce is a sequence indicator of a sent DiscoveryResponse on a stream. We use ADS therefore every single DiscoveryResponse regardless of a type is sent with incremented nonce.
+//	Typical sequence of CDS + EDS looks like this:
+//	1) Envoy sends DiscoveryRequest [type=CDS, version="", nonce=""] // ask for the clusters
+//	2) Kuma sends DiscoveryResponse [type=CDS, version="UUID-1", nonce="1"] // response with clusters
+//	3) Envoy sends DiscoveryRequest [type=EDS, version="", nonce=""] // ask for the endpoints
+//	4) Envoy sends DiscoveryRequest [type=CDS, version="UUID-1", nonce="1"] // confirmations that it received clusters (ACK)
+//	5) Kuma sends DiscoveryResponse [type=EDS, version="UUID-2", nonce="2"] // response with endpoints
+//	6) Envoy sends DiscoveryRequest [type=EDS, version="UUID-2", nonce="2"] // confirmations that it received endpoints (ACK)
 //
-//   Updated Cluster is now in warming state until we send DiscoveryResponse with EDS.
-//   We could sent the same DiscoveryResponse again: DiscoveryRequest [type=EDS, version="UUID-2", nonce="2"], but there is no API in go-control-plane to do it.
-//   Instead we set a new version of the Endpoints to force a new EDS exchange:
-//   10) Kuma sends DiscoveryResponse [type=EDS, version="UUID-3", nonce="3"] // triggered because we set snapshot with a new version
-//   11) Envoy sends DiscoveryRequest [type=EDS, version="UUID-3", nonce="3"] // ACK
-//   After this exchange, cluster is now out of the warming state.
+//	Then if we send a Cluster update (continuing the flow above)
+//	7) Kuma sends DiscoveryResponse [type=CDS, version="UUID-2", nonce="3"] // response with cluster update
+//	8) Envoy sends DiscoveryRequest [type=CDS, version="UUID-2", nonce="3"] // ACK
+//	9) Envoy sends DiscoveryRequest [type=EDS, version="UUID-2", nonce="2"] // Envoy sends a request which looks like the second ACK for the previous endpoints
+//
+//	Updated Cluster is now in warming state until we send DiscoveryResponse with EDS.
+//	We could sent the same DiscoveryResponse again: DiscoveryRequest [type=EDS, version="UUID-2", nonce="2"], but there is no API in go-control-plane to do it.
+//	Instead we set a new version of the Endpoints to force a new EDS exchange:
+//	10) Kuma sends DiscoveryResponse [type=EDS, version="UUID-3", nonce="3"] // triggered because we set snapshot with a new version
+//	11) Envoy sends DiscoveryRequest [type=EDS, version="UUID-3", nonce="3"] // ACK
+//	After this exchange, cluster is now out of the warming state.
 //
 // The same problem is with Listeners and Routes (change of the Listener that uses RDS requires RDS DiscoveryResponse), but since we don't use RDS now, the implementation is for EDS only.
 // More reading of how Envoy is trying to solve it https://github.com/envoyproxy/envoy/issues/13009
