8daf83eecfcdb7284a431768e524c7ffe499803e
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2023-01-11 16:59:26 +0100

    chore: simply test
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- test/e2e/externalservices/externalservices_kubernetes_without_egress.go
+++ test/e2e/externalservices/externalservices_kubernetes_without_egress.go
@@ -25,7 +25,7 @@ spec:
         type: builtin
   networking:
     outbound:
-      passthrough: %s
+      passthrough: false
   routing:
     zoneEgress: %s
 `
@@ -52,7 +52,7 @@ spec:
 		cluster = NewK8sCluster(NewTestingT(), Kuma1, Silent)
 		err := NewClusterSetup().
 			Install(Kuma(core.Standalone)).
-			Install(YamlK8s(fmt.Sprintf(meshDefaulMtlsOn, "false", "false"))).
+			Install(YamlK8s(fmt.Sprintf(meshDefaulMtlsOn, "false"))).
 			Install(NamespaceWithSidecarInjection(TestNamespace)).
 			Install(DemoClientK8s("default", TestNamespace)).
 			Install(Namespace(externalServicesNamespace)).
@@ -77,42 +77,26 @@ spec:
 		Expect(cluster.DismissCluster()).To(Succeed())
 	})
 
-	trafficBlocked := func(g Gomega) {
-		_, _, err := cluster.Exec(TestNamespace, clientPodName, "demo-client",
-			"curl", "-v", "-m", "3", "--fail", "external-service.external-services.svc.cluster.local:80")
-		g.Expect(err).To(HaveOccurred())
-	}
-
-	trafficAllowed := func(g Gomega) {
-		_, stderr, err := cluster.Exec(TestNamespace, clientPodName, "demo-client",
-			"curl", "-v", "-m", "3", "--fail", "external-service.external-services.svc.cluster.local:80")
-		g.Expect(err).ToNot(HaveOccurred())
-		g.Expect(stderr).To(ContainSubstring("HTTP/1.1 200 OK"))
-	}
-
-	It("should route to external-service", func() {
-		// given the mesh with passthrough enabled and no egress disabled
-		Expect(cluster.Install(YamlK8s(fmt.Sprintf(meshDefaulMtlsOn, "true", "false")))).To(Succeed())
-
-		// then communication outside of the Mesh works, because it goes through passthrough
-		Eventually(trafficAllowed, "30s", "1s").Should(Succeed())
-
-		// when passthrough is disabled
-		Expect(cluster.Install(YamlK8s(fmt.Sprintf(meshDefaulMtlsOn, "false", "false")))).To(Succeed())
-
-		// then accessing the external service is no longer possible
-		Eventually(trafficBlocked, "30s", "1s").Should(Succeed())
-
-		// when apply external service
+	It("should block the traffic when zone egress is not deployed", func() {
+		// when external service is defined without passthrough and zone routing
 		Expect(cluster.Install(YamlK8s(externalService))).To(Succeed())
 
 		// then communication outside of the Mesh works, because it goes directly from the client to a service
-		Eventually(trafficAllowed, "30s", "1s").Should(Succeed())
+		Eventually(func(g Gomega) {
+			_, stderr, err := cluster.Exec(TestNamespace, clientPodName, "demo-client",
+				"curl", "-v", "-m", "3", "--fail", "external-service.external-services.svc.cluster.local:80")
+			g.Expect(err).ToNot(HaveOccurred())
+			g.Expect(stderr).To(ContainSubstring("HTTP/1.1 200 OK"))
+		}, "30s", "1s").Should(Succeed())
 
 		// when passthrough is disabled on the Mesh and zone egress enabled
-		Expect(cluster.Install(YamlK8s(fmt.Sprintf(meshDefaulMtlsOn, "false", "true")))).To(Succeed())
+		Expect(cluster.Install(YamlK8s(fmt.Sprintf(meshDefaulMtlsOn, "true")))).To(Succeed())
 
 		// then accessing the external service is no longer possible, because zone egress is not deployed
-		Eventually(trafficBlocked, "30s", "1s").Should(Succeed())
+		Eventually(func(g Gomega) {
+			_, _, err := cluster.Exec(TestNamespace, clientPodName, "demo-client",
+				"curl", "-v", "-m", "3", "--fail", "external-service.external-services.svc.cluster.local:80")
+			g.Expect(err).To(HaveOccurred())
+		}, "30s", "1s").Should(Succeed())
 	})
 }
