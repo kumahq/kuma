66fab88ca0b566bfb3463edf30feb172c2b2cef5
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2023-01-11 13:48:37 +0100

    test(e2e): better error when pod is not available
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- test/framework/k8s.go
+++ test/framework/k8s.go
@@ -2,8 +2,11 @@ package framework
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/retry"
+	"github.com/gruntwork-io/terratest/modules/testing"
 	"github.com/pkg/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -40,4 +43,32 @@ func PodIPOfApp(cluster Cluster, name string, namespace string) (string, error) 
 		return "", errors.Errorf("expected %d pods, got %d", 1, len(pods))
 	}
 	return pods[0].Status.PodIP, nil
+}
+
+// WaitUntilPodAvailableE it's a "fork" of original WaitUntilPodAvailableE from terratest with more information why the pod failed
+// Remove when https://github.com/gruntwork-io/terratest/pull/1223 is merged.
+func WaitUntilPodAvailableE(t testing.TestingT, options *k8s.KubectlOptions, podName string, retries int, sleepBetweenRetries time.Duration) error {
+	statusMsg := fmt.Sprintf("Wait for pod %s to be provisioned.", podName)
+	message, err := retry.DoWithRetryE(
+		t,
+		statusMsg,
+		retries,
+		sleepBetweenRetries,
+		func() (string, error) {
+			pod, err := k8s.GetPodE(t, options, podName)
+			if err != nil {
+				return "", err
+			}
+			if !k8s.IsPodAvailable(pod) {
+				return "", errors.Errorf("Pod %s is not available, reason: %s, message: %s", pod.Name, pod.Status.Reason, pod.Status.Message)
+			}
+			return "Pod is now available", nil
+		},
+	)
+	if err != nil {
+		Logf("Timedout waiting for Pod to be provisioned: %s", err)
+		return err
+	}
+	Logf(message)
+	return nil
 }
