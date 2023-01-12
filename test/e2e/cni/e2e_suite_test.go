a9b92570619ca11070adea93faf98b8eec562d16
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2023-01-12 13:12:10 +0100

    chore: rebalance
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- test/e2e/cni/e2e_suite_test.go
+++ test/e2e/cni/e2e_suite_test.go
@@ -13,5 +13,5 @@ func TestE2E(t *testing.T) {
 	test.RunE2ESpecs(t, "E2E CNI Suite")
 }
 
-var _ = Describe("Taint controller", Label("job-4"), Label("kind-not-supported"), Label("legacy-k3s-not-supported"), cni.AppDeploymentWithCniAndTaintController)
+var _ = Describe("Taint controller", Label("job-3"), Label("kind-not-supported"), Label("legacy-k3s-not-supported"), cni.AppDeploymentWithCniAndTaintController)
 var _ = Describe("Old CNI", Label("job-0"), Label("arm-not-supported"), Label("legacy-k3s-not-supported"), Label("kind-not-supported"), cni.AppDeploymentWithCniAndNoTaintController)
