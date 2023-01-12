bbc7260e2b7d910b72e82984964e94f5b163940e
Marcin Skalski <marcin.skalski@konghq.com>
2023-01-12 10:56:43 +0100

    feat(kuma-cp): add DataSource that does not depend on protobuf to common api
    Signed-off-by: Marcin Skalski <marcin.skalski@konghq.com>

---
+++ api/common/v1alpha1/datasource.go
@@ -0,0 +1,11 @@
+package v1alpha1
+
+// DataSource defines the source of bytes to use.
+type DataSource struct {
+	// Data source is a secret with given Secret key.
+	Secret *string `json:"secret,omitempty"`
+	// Data source is inline bytes.
+	Inline *[]byte `json:"inline,omitempty"`
+	// Data source is inline string`
+	InlineString *string `json:"inlineString,omitempty"`
+}
