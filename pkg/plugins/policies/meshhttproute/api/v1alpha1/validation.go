5490fb904c9ff56c4a0cff0ae9b4b9ff1594df4d
Mike Beaumont <mjboamail@gmail.com>
2023-01-12 09:51:12 +0100

    chore: add validation skeleton
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- pkg/plugins/policies/meshhttproute/api/v1alpha1/validation.go
+++ pkg/plugins/policies/meshhttproute/api/v1alpha1/validation.go
@@ -10,7 +10,7 @@ func (r *MeshHTTPRouteResource) validate() error {
 	var verr validators.ValidationError
 	path := validators.RootedAt("spec")
 	verr.AddErrorAt(path.Field("targetRef"), validateTop(r.Spec.TargetRef))
-	verr.AddErrorAt(path.Field("to"), validateRules(r.Spec.To))
+	verr.AddErrorAt(path.Field("to"), validateTos(r.Spec.To))
 	return verr.OrNil()
 }
 
@@ -33,12 +33,45 @@ func validateToRef(targetRef common_api.TargetRef) validators.ValidationError {
 	})
 }
 
-func validateRules(tos []To) validators.ValidationError {
+func validateTos(tos []To) validators.ValidationError {
 	var errs validators.ValidationError
 
 	for i, to := range tos {
-		errs.AddErrorAt(validators.PathBuilder{}.Index(i).Field("targetRef"), validateToRef(to.TargetRef))
+		path := validators.Root().Index(i)
+		errs.AddErrorAt(path.Field("targetRef"), validateToRef(to.TargetRef))
+		errs.AddErrorAt(path.Field("rules"), validateRules(to.Rules))
 	}
 
 	return errs
+}
+
+func validateRules(rules []Rule) validators.ValidationError {
+	var errs validators.ValidationError
+
+	for i, rule := range rules {
+		path := validators.Root().Index(i)
+		errs.AddErrorAt(path.Field("matches"), validateMatches(rule.Matches))
+	}
+
+	return errs
+}
+
+func validateMatches(matches []Match) validators.ValidationError {
+	var errs validators.ValidationError
+
+	for i, match := range matches {
+		path := validators.Root().Index(i)
+		errs.AddErrorAt(path.Field("path"), validatePath(match.Path))
+		errs.AddErrorAt(path.Field("method"), validateMethod(match.Method))
+	}
+
+	return errs
+}
+
+func validatePath(match *PathMatch) validators.ValidationError {
+	return validators.ValidationError{}
+}
+
+func validateMethod(match *Method) validators.ValidationError {
+	return validators.ValidationError{}
 }
