8fe2ccf4f8d58b5d8f099f79ec78ccfd5db7fac8
slonka <slonka@users.noreply.github.com>
2023-01-24 11:54:48 +0100

    chore: fix version sh pipe exit
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- tools/releases/version.sh
+++ tools/releases/version.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o pipefail
-
 function envoy_version() {
+  set -o errexit
+  set -o pipefail
+  set -o nounset
   # Returns Envoy version by ENVOY_TAG:
   # - if ENVOY_TAG is a real git tag like 'v1.20.0' then the version is equal to '1.20.0' (without the first letter 'v').
   # - if ENVOY_TAG is a commit hash then the version will look like '1.20.1-dev-b16d390f'
@@ -21,6 +21,9 @@ function envoy_version() {
   else
     echo "${ENVOY_VERSION}-${ENVOY_TAG:0:8}"
   fi
+  set +o errexit
+  set +o pipefail
+  set +o nounset
 }
 
 function version_info() {
