369f7cc5c357cb8411addd06fc0bcb757035e603
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-01-26 17:51:46 +0100

    chore: back-ports GUI files (#5832)
    * chore: back-ports GUI files
    * fix(ci): format files
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/kuma-ui/pkg/resources/data/assets/formatForCLI-4055776e.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-a8834e9c.js";function e(o,t='" | kumactl apply -f -'){const n='echo "',r=a(o);return`${n}${r}${t}`}export{e as f};
--- app/kuma-ui/pkg/resources/data/assets/rate-limits-916d4fa9.js
+++
@@ -1,1 +0,0 @@
-const o=0,t=[],s={total:0,items:t};export{s as default,t as items,o as total};
