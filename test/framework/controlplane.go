b442b97ad66036f0c682cf7ddd044d661dbdc675
slonka <slonka@users.noreply.github.com>
2023-02-06 12:21:20 +0100

    feat(helm): make check passes
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- test/framework/controlplane.go
+++ test/framework/controlplane.go
@@ -1,20 +1,19 @@
 package framework
 
 import (
-    "encoding/base64"
-    "encoding/json"
+	"encoding/base64"
+	"encoding/json"
 )
 
 func ExtractTokenStringFromResponse(output string) (string, error) {
-    var secret map[string]string
-    if err := json.Unmarshal([]byte(output), &secret); err != nil {
-        return "", err
-    }
-    data := secret["data"]
-    token, err := base64.StdEncoding.DecodeString(data)
-    if err != nil {
-        return "", err
-    }
-    return string(token), nil
+	var secret map[string]string
+	if err := json.Unmarshal([]byte(output), &secret); err != nil {
+		return "", err
+	}
+	data := secret["data"]
+	token, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		return "", err
+	}
+	return string(token), nil
 }
-
