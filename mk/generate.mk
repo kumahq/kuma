7eada632fc584c2999eed769c3030961bc99aac0
Charly Molter <charly.molter@konghq.com>
2023-02-07 15:01:04 +0100

    ci(make): make some parameters optional in Makefile
    This was making it harder to depend on this
    
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- mk/generate.mk
+++ mk/generate.mk
@@ -1,5 +1,5 @@
 ENVOY_IMPORTS := ./pkg/xds/envoy/imports.go
-PROTO_DIRS := ./pkg/config ./api
+PROTO_DIRS ?= ./pkg/config ./api
 
 CONTROLLER_GEN := go run -mod=mod sigs.k8s.io/controller-tools/cmd/controller-gen
 RESOURCE_GEN := go run -mod=mod $(TOOLS_DIR)/resource-gen/main.go
