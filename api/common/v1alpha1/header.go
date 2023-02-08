abe938ac809e4c0915bbca0888e83007ff50005d
Mike Beaumont <mjboamail@gmail.com>
2023-02-08 17:03:29 +0100

    feat: don't require a header value field be set
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- api/common/v1alpha1/header.go
+++ api/common/v1alpha1/header.go
@@ -19,7 +19,7 @@ const (
 	HeaderMatchPrefix            HeaderMatchType = "Prefix"
 )
 
-// HeaderMatch describes how to select a HTTP route by matching HTTP request
+// HeaderMatch describes how to select an HTTP route by matching HTTP request
 // headers.
 type HeaderMatch struct {
 	// Type specifies how to match against the value of the header.
@@ -33,5 +33,5 @@ type HeaderMatch struct {
 	Name HeaderName `json:"name"`
 
 	// Value is the value of HTTP Header to be matched.
-	Value HeaderValue `json:"value"`
+	Value HeaderValue `json:"value,omitempty"`
 }
