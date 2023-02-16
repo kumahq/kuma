0eaa2e5c8ce2e32543acdd01c4820989d2fb76e2
Mike Beaumont <mjboamail@gmail.com>
2023-02-16 18:32:06 +0100

    test(e2e): make multizone MeshHTTPRoute test fail
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- test/e2e_env/multizone/meshhttproute/test.go
+++ test/e2e_env/multizone/meshhttproute/test.go
@@ -38,6 +38,15 @@ func Test() {
 			Setup(multizone.UniZone2)
 		Expect(err).ToNot(HaveOccurred())
 
+		err = NewClusterSetup().
+			Install(TestServerUniversal("dp-echo-3", meshName,
+				WithArgs([]string{"echo", "--instance", "alias-zone2"}),
+				WithServiceName("alias-test-server"),
+				WithServiceVersion("v2"),
+			)).
+			Setup(multizone.UniZone2)
+		Expect(err).ToNot(HaveOccurred())
+
 		Expect(DeleteMeshResources(multizone.Global, meshName, core_mesh.TrafficRouteResourceTypeDescriptor)).To(Succeed())
 	})
 
@@ -72,7 +81,7 @@ spec:
           default:
             backendRefs:
               - kind: MeshServiceSubset
-                name: test-server
+                name: alias-test-server
                 weight: 100
                 tags:
                   kuma.io/zone: kuma-5
@@ -83,8 +92,9 @@ spec:
 			g.Expect(err).ToNot(HaveOccurred())
 			g.Expect(response).To(
 				And(
-					Not(HaveKey(MatchRegexp(`.*zone1.*`))),
-					HaveKeyWithValue(MatchRegexp(`.*zone2.*`), Not(BeNil())),
+					Not(HaveKey(MatchRegexp(`^zone1.*`))),
+					Not(HaveKey(MatchRegexp(`^zone2.*`))),
+					HaveKeyWithValue(MatchRegexp(`^alias-zone2.*`), Not(BeNil())),
 				),
 			)
 		}, "30s", "500ms").Should(Succeed())
