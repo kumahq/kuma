9ec37db29c33498d1443d5eb97b051fb3f4f63bb
Bart Smykla <bartek@smykla.com>
2023-02-17 07:01:34 +0100

    chore: get rid of ebpf/fetch.sh script
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- mk/ebpf.mk
+++ mk/ebpf.mk
@@ -1,41 +1,52 @@
-MERBRIDGE_RELEASE_TAG ?= main-20aa4b03cf256a261d1ebfa3af9f390186bec3ae
-MERBRIDGE_RELEASE_REPO ?= https://github.com/kumahq/merbridge
+# Explanation of used GNU Make concepts used in the file:
+#
+# "| $(DIRECTORY_NAME)" is an order-only prerequisite, which means prerequisite
+#   that is never checked when determining if the target is out of date;
+#   even order-only prerequisites marked as phony will not cause the target
+#   to be rebuilt. In our case it's used to make $(DIRECTORY_NAME) directory,
+#   if it doesn't exist.
+# ref. https://www.gnu.org/software/make/manual/html_node/Prerequisite-Types.html
+#
+# "$(@D)" is the directory part of the file name of the target, with the trailing
+#   slash removed. If the value of $(@) is "dir/foo.o" then $(@D) will be
+#   equal "dir". This value will be equal "." if $(@) does not contain a slash.
+# ref. https://www.gnu.org/software/make/manual/html_node/Automatic-Variables.html#Automatic-Variables
+
+RELEASE_TAG ?= main-20aa4b03cf256a261d1ebfa3af9f390186bec3ae
+RELEASE_REPO ?= https://github.com/kumahq/merbridge
 TARBALL_NAME ?= all.tar.gz
-
-BUILD_OUTPUT ?= $(KUMA_DIR)/build/ebpf/$(MERBRIDGE_RELEASE_TAG)
-
+# You can provide your own url if the tarball with all ebpf programs should be
+# fetched from somewhere else
+TARBALL_URL ?= $(RELEASE_REPO)/releases/download/$(RELEASE_TAG)/$(TARBALL_NAME)
+# Where should be placed directory with $(RELEASE_TAG) as name placed. We don't
+# allow to overwrite the final $(BUILD_OUTPUT_WITH_TAG) variable, as if someone
+# by mistake remove $(RELEASE_TAG) from the path, it may result in situation
+# where without realizing, ebpf programs are not re-fetched when $(RELEASE_TAG)
+# changes
+BUILD_OUTPUT ?= $(KUMA_DIR)/build/ebpf
+BUILD_OUTPUT_WITH_TAG = $(BUILD_OUTPUT)/$(RELEASE_TAG)
 # Path where ebpf programs should be placed, to be compiled in when building
 # kumactl
 COMPILE_IN_PATH ?= $(KUMA_DIR)/pkg/transparentproxy/ebpf/programs
 
-# We are placing ebpf programs inside $(BUILD_OUTPUT) directory first,
-# as by default it contains $(MERBRIDGE_RELEASE_TAG) in the path, which means
+# We are placing ebpf programs inside $(BUILD_OUTPUT_WITH_TAG) directory first,
+# as by default it contains $(RELEASE_TAG) in the path, which means
 # if the tag changes, we will re-fetch programs
 .PHONY: build/ebpf
-build/ebpf: $(BUILD_OUTPUT)/mb_* $(COMPILE_IN_PATH)/mb_*
+build/ebpf: $(BUILD_OUTPUT_WITH_TAG)/mb_* $(COMPILE_IN_PATH)/mb_*
 
-# "| $(DIRECTORY_NAME)" is an order-only prerequisite, which means prerequisite
-# that is never checked when determining if the target is out of date;
-# even order-only prerequisites marked as phony will not cause the target
-# to be rebuilt. In our case it's used to make $(DIRECTORY_NAME) directory, if
-# it doesn't exist
-# ref. https://www.gnu.org/software/make/manual/html_node/Prerequisite-Types.html
-
-$(BUILD_OUTPUT)/mb_*: | $(BUILD_OUTPUT)
-	MERBRIDGE_TAG=$(MERBRIDGE_RELEASE_TAG) \
-	OUTPUT_PATH=$(@D) \
-	RELEASE_REPO=$(MERBRIDGE_RELEASE_REPO) \
-	TARBALL_NAME=$(TARBALL_NAME) \
-	$(KUMA_DIR)/tools/ebpf/fetch.sh
+$(BUILD_OUTPUT_WITH_TAG)/mb_*: | $(BUILD_OUTPUT_WITH_TAG)
+	curl --progress-bar --location $(TARBALL_URL) | tar -C $(@D) -xz
 
 $(COMPILE_IN_PATH)/mb_*: | $(COMPILE_IN_PATH)
-	cp $(BUILD_OUTPUT)/mb_* $(COMPILE_IN_PATH)
+	cp $(BUILD_OUTPUT_WITH_TAG)/mb_* $(COMPILE_IN_PATH)
 
-# Make (COMPILE_IN_PATH) $(BUILD_OUTPUT) directories if they don't exist
-$(COMPILE_IN_PATH) $(BUILD_OUTPUT):
+# Make $(COMPILE_IN_PATH) $(BUILD_OUTPUT_WITH_TAG) directories if they don't
+# exist
+$(COMPILE_IN_PATH) $(BUILD_OUTPUT_WITH_TAG):
 	mkdir -p $@
 
 .PHONY: clean/ebpf
 clean/ebpf :
-	-rm -rf $(BUILD_OUTPUT) $(COMPILE_IN_PATH)
+	-rm -rf $(BUILD_OUTPUT_WITH_TAG) $(COMPILE_IN_PATH)
 
