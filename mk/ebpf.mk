fc125bc4f764d3b34ec80e4b02081091e644714d
Bart Smykla <bartek@smykla.com>
2023-02-20 08:19:53 +0100

    chore: adjust ebpf build paths
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- mk/ebpf.mk
+++ mk/ebpf.mk
@@ -23,11 +23,11 @@ TARBALL_URL ?= $(RELEASE_REPO)/releases/download/$(RELEASE_TAG)/$(TARBALL_NAME)
 # by mistake remove $(RELEASE_TAG) from the path, it may result in situation
 # where without realizing, ebpf programs are not re-fetched when $(RELEASE_TAG)
 # changes
-BUILD_OUTPUT ?= build/ebpf
+BUILD_OUTPUT ?= ./build/ebpf
 BUILD_OUTPUT_WITH_TAG = $(BUILD_OUTPUT)/$(RELEASE_TAG)
 # Path where ebpf programs should be placed, to be compiled in when building
 # kumactl
-COMPILE_IN_PATH ?= pkg/transparentproxy/ebpf/programs
+COMPILE_IN_PATH ?= ./pkg/transparentproxy/ebpf/programs
 
 # We are placing ebpf programs inside $(BUILD_OUTPUT_WITH_TAG) directory first,
 # as by default it contains $(RELEASE_TAG) in the path, which means
