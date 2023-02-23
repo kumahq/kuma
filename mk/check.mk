bcd2eea3fb845fad7fbb5ea232926e6cc0d9286f
Mike Beaumont <mjboamail@gmail.com>
2023-02-23 10:33:55 +0100

    ci(.github): take least fixed version for security updates (#6080)
    * ci(.github): take least fixed version for security updates
    * ci: put update command into script
    
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>
    (cherry picked from commit 54cf27071ac87da59f1f798fa66614c33afe9a0c)
    
    # Conflicts:
    #	.github/workflows/update-insecure-dependencies.yaml

--- mk/check.mk
+++ mk/check.mk
@@ -58,3 +58,7 @@ check: format helm-lint golangci-lint shellcheck kube-lint ## Dev: Run code chec
 		git ls-files --other --directory --exclude-standard --no-empty-directory && \
 		false \
 	)
+
+.PHONY: update-vulnerable-dependencies
+update-vulnerable-dependencies:
+	@./tools/ci/update-vulnerable-dependencies.sh
