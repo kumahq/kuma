e850522cc0fb8d779e33b6697ee5b880cc9bdec0
Bart Smykla <bartek@smykla.com>
2023-02-27 07:53:43 +0100

    chore: extrapolate format/common make target
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- mk/check.mk
+++ mk/check.mk
@@ -46,8 +46,11 @@ ginkgo/unfocus:
 ginkgo/lint:
 	go run $(TOOLS_DIR)/ci/check_test_files.go
 
+.PHONY: format/common
+format/common: generate docs tidy ginkgo/unfocus ginkgo/lint
+
 .PHONY: format
-format: fmt generate docs tidy ginkgo/unfocus ginkgo/lint
+format: fmt format/common
 
 .PHONY: kube-lint
 kube-lint:
@@ -58,7 +61,7 @@ hadolint:
 	find ./tools/releases/dockerfiles/ -type f -iname "Dockerfile*" | grep -v dockerignore | xargs -I {} $(HADOLINT) {}
 
 .PHONY: check
-check: generate docs tidy ginkgo/unfocus ginkgo/lint helm-lint golangci-lint shellcheck kube-lint hadolint ## Dev: Run code checks (go fmt, go vet, ...)
+check: format/common helm-lint golangci-lint shellcheck kube-lint hadolint ## Dev: Run code checks (go fmt, go vet, ...)
 	# fail if Git working tree is dirty or there are untracked files
 	git diff --quiet || \
 	git ls-files --other --directory --exclude-standard --no-empty-directory | wc -l | read UNTRACKED_FILES; if [ "$$UNTRACKED_FILES" != "0" ]; then false; fi || \
