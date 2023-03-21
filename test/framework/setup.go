618f665ff9cc88183fc17c00d960782c4a38d11b
slonka <slonka@users.noreply.github.com>
2023-03-21 14:11:21 +0100

    feat(persistance): fix missing driver
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- test/framework/setup.go
+++ test/framework/setup.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/retry"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/pkg/errors"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
