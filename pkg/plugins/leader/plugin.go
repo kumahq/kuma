0163ecddc00134627664ab6846e0156364a0c4a5
slonka <slonka@users.noreply.github.com>
2023-03-21 14:59:20 +0100

    feat(persistance): handle different drivers in pglock
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- pkg/plugins/leader/plugin.go
+++ pkg/plugins/leader/plugin.go
@@ -3,10 +3,12 @@ package leader
 import (
 	"time"
 
+
 	"cirello.io/pglock"
 	"github.com/pkg/errors"
 
 	"github.com/kumahq/kuma/pkg/config/core/resources/store"
+	"github.com/kumahq/kuma/pkg/config/plugins/resources/postgres"
 	core_runtime "github.com/kumahq/kuma/pkg/core/runtime"
 	"github.com/kumahq/kuma/pkg/core/runtime/component"
 	common_postgres "github.com/kumahq/kuma/pkg/plugins/common/postgres"
@@ -17,16 +19,30 @@ import (
 func NewLeaderElector(b *core_runtime.Builder) (component.LeaderElector, error) {
 	switch b.Config().Store.Type {
 	case store.PostgresStore:
-		db, err := common_postgres.ConnectToDb(*b.Config().Store.Postgres)
+		cfg := *b.Config().Store.Postgres
+		db, err := common_postgres.ConnectToDb(cfg)
 		if err != nil {
 			return nil, errors.Wrap(err, "could not connect to postgres")
 		}
-		client, err := pglock.New(db,
-			pglock.WithLeaseDuration(5*time.Second),
-			pglock.WithHeartbeatFrequency(1*time.Second),
-			pglock.WithOwner(b.GetInstanceId()),
-			pglock.WithLogger(&leader_postgres.KumaPqLockLogger{}),
-		)
+		var client *pglock.Client
+		switch cfg.DriverName {
+		case postgres.DriverNamePgx:
+			client, err = pglock.UnsafeNew(db,
+				pglock.WithLeaseDuration(5*time.Second),
+				pglock.WithHeartbeatFrequency(1*time.Second),
+				pglock.WithOwner(b.GetInstanceId()),
+				pglock.WithLogger(&leader_postgres.KumaPqLockLogger{}),
+			)
+		case postgres.DriverNamePq:
+			client, err = pglock.New(db,
+				pglock.WithLeaseDuration(5*time.Second),
+				pglock.WithHeartbeatFrequency(1*time.Second),
+				pglock.WithOwner(b.GetInstanceId()),
+				pglock.WithLogger(&leader_postgres.KumaPqLockLogger{}),
+			)
+		default:
+			err = errors.Errorf("unknown driver %s", cfg.DriverName)
+		}
 		if err != nil {
 			return nil, errors.Wrap(err, "could not create postgres lock client")
 		}
