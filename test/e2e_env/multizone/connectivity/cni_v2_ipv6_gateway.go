6ed9bf40b7f0e091a5cf5157bfb72b0fde632a9b
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2023-03-29 12:25:46 +0200

    fix(e2e): adjust e2e test
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- test/e2e_env/multizone/connectivity/cni_v2_ipv6_gateway.go
+++ test/e2e_env/multizone/connectivity/cni_v2_ipv6_gateway.go
@@ -7,7 +7,6 @@ import (
 	"github.com/kumahq/kuma/pkg/plugins/runtime/k8s/metadata"
 	. "github.com/kumahq/kuma/test/framework"
 	"github.com/kumahq/kuma/test/framework/client"
-	"github.com/kumahq/kuma/test/framework/deployments/democlient"
 	"github.com/kumahq/kuma/test/framework/deployments/testserver"
 	"github.com/kumahq/kuma/test/framework/envs/multizone"
 )
@@ -22,12 +21,14 @@ func GatewayIPV6CNIV2() {
 
 		err := NewClusterSetup().
 			Install(NamespaceWithSidecarInjection(namespace)).
-			Install(democlient.Install(
-				democlient.WithNamespace(namespace),
-				democlient.WithMesh(meshName),
-				democlient.WithPodAnnotations(map[string]string{
+			Install(testserver.Install(
+				testserver.WithName("demo-client"),
+				testserver.WithNamespace(namespace),
+				testserver.WithMesh(meshName),
+				testserver.WithPodAnnotations(map[string]string{
 					metadata.KumaGatewayAnnotation: "enabled",
 				}),
+				testserver.WithEchoArgs("echo", "--instance", "demo-client"),
 			)).
 			Install(testserver.Install(
 				testserver.WithNamespace(namespace),
@@ -45,7 +46,7 @@ func GatewayIPV6CNIV2() {
 
 	It("client should communicate with server", func() {
 		Eventually(func(g Gomega) {
-			response, err := client.CollectEchoResponse(multizone.KubeZone2, "demo-client", "http://test-server_gw-ipv6-cniv2_svc_80.mesh",
+			response, err := client.CollectResponse(multizone.KubeZone2, "demo-client", "http://test-server_gw-ipv6-cniv2_svc_80.mesh",
 				client.FromKubernetesPod(meshName, "demo-client"),
 			)
 			g.Expect(err).ToNot(HaveOccurred())
