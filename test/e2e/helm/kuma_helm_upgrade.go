7c834a9f1f4d27f2c0cee4fc91663ba427e7493f
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2023-03-31 15:16:49 +0200

    test(e2e): fix helm upgrade test (#6422)
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- test/e2e/helm/kuma_helm_upgrade.go
+++ test/e2e/helm/kuma_helm_upgrade.go
@@ -26,7 +26,7 @@ func UpgradingWithHelmChart() {
 		initialChartVersion string
 	}
 
-	PDescribeTable(
+	DescribeTable(
 		"should successfully upgrade Kuma via Helm",
 		func(given testCase) {
 			t := NewTestingT()
@@ -46,14 +46,17 @@ func UpgradingWithHelmChart() {
 					WithHelmReleaseName(releaseName),
 					WithHelmChartVersion(given.initialChartVersion),
 					WithoutHelmOpt("global.image.tag"),
-					WithHelmOpt("global.image.registry", Config.KumaImageRegistry),
 				)).
 				Setup(cluster)
 			Expect(err).ToNot(HaveOccurred())
 
 			k8sCluster := cluster.(*K8sCluster)
 
-			err = k8sCluster.UpgradeKuma(core.Standalone, WithHelmReleaseName(releaseName), WithHelmChartPath(Config.HelmChartPath))
+			err = k8sCluster.UpgradeKuma(core.Standalone,
+				WithHelmReleaseName(releaseName),
+				WithHelmChartPath(Config.HelmChartPath),
+				ClearNoHelmOpts(),
+			)
 			Expect(err).ToNot(HaveOccurred())
 
 			// when
