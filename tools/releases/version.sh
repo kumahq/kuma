81138ad333c4757e359bd00580182fc06b8a9d0e
ilya.lobkov@konghq.com <ilya.lobkov@konghq.com>
2023-04-05 10:54:29 +0100

    chore(deps): update Envoy
    Signed-off-by: ilya.lobkov@konghq.com <ilya.lobkov@konghq.com>

--- tools/releases/version.sh
+++ tools/releases/version.sh
@@ -8,7 +8,7 @@ function envoy_version() {
   # - if ENVOY_TAG is a real git tag like 'v1.20.0' then the version is equal to '1.20.0' (without the first letter 'v').
   # - if ENVOY_TAG is a commit hash then the version will look like '1.20.1-dev-b16d390f'
 
-  ENVOY_TAG=${ENVOY_TAG:-"v1.25.3"}
+  ENVOY_TAG=${ENVOY_TAG:-"v1.24.4"}
   ENVOY_VERSION=$(curl --silent --location "https://raw.githubusercontent.com/envoyproxy/envoy/${ENVOY_TAG}/VERSION.txt")
 
   # for envoy versions older than v1.22.0 file 'VERSION.txt' used to be called 'VERSION'
