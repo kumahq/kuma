f30a71d87edbf888ad051c89756395ef4c5daf76
Bart Smykla <bartek@smykla.com>
2023-04-21 08:44:21 +0200

    chore: adjust makefile for reusabillity + add -j
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- mk/test-container-structure.mk
+++ mk/test-container-structure.mk
@@ -1,10 +1,13 @@
-TEST_CONFIGS_PATH ?= tools/container-structure-test
+TEST_CONFIGS_PATH_ROOT ?= tools/container-structure-test
 
 define TEST_CONTAINER_STRUCTURES
+TEST_CONFIGS_PATH_FILENAME_$(1) ?= $(1)
+TEST_CONFIGS_PATH_$(1) ?= $(KUMA_DIR)/$(TEST_CONFIGS_PATH_ROOT)/$$(TEST_CONFIGS_PATH_FILENAME_$(1)).yaml
+
 .PHONY: test/container-structure/$(1)/$(2)
 test/container-structure/$(1)/$(2): docker/$(1)/$(2)/load
 	$(CONTAINER_STRUCTURE_TEST) test \
-		--config $(KUMA_DIR)/$(TEST_CONFIGS_PATH)/$(1).yaml \
+		--config $$(TEST_CONFIGS_PATH_$(1)) \
 		--image $(call build_image,$(1),$(2))
 endef
 
