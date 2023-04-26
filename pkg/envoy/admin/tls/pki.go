c0132c2c5a254d32eea5fa80ed415cfc6d181726
Matthieu MOREL <matthieu.morel35@gmail.com>
2023-04-26 13:48:49 +0200

    feat(tls): remove commonName in certificate generation
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- pkg/envoy/admin/tls/pki.go
+++ pkg/envoy/admin/tls/pki.go
@@ -70,7 +70,7 @@ func GenerateClientCert(ca tls.Certificate) (util_tls.KeyPair, error) {
 	if err != nil {
 		return util_tls.KeyPair{}, err
 	}
-	return util_tls.NewCert(*rootCert, ca.PrivateKey.(*rsa.PrivateKey), ClientCertSAN, util_tls.ClientCertType, util_tls.DefaultKeyType, ClientCertSAN)
+	return util_tls.NewCert(*rootCert, ca.PrivateKey.(*rsa.PrivateKey), util_tls.ClientCertType, util_tls.DefaultKeyType, ClientCertSAN)
 }
 
 func GenerateServerCert(ca tls.Certificate, hosts ...string) (util_tls.KeyPair, error) {
@@ -78,9 +78,5 @@ func GenerateServerCert(ca tls.Certificate, hosts ...string) (util_tls.KeyPair, 
 	if err != nil {
 		return util_tls.KeyPair{}, err
 	}
-	var commonName string
-	if len(hosts) > 0 {
-		commonName = hosts[0]
-	}
-	return util_tls.NewCert(*rootCert, ca.PrivateKey.(*rsa.PrivateKey), commonName, util_tls.ServerCertType, util_tls.DefaultKeyType, hosts...)
+	return util_tls.NewCert(*rootCert, ca.PrivateKey.(*rsa.PrivateKey), util_tls.ServerCertType, util_tls.DefaultKeyType, hosts...)
 }
