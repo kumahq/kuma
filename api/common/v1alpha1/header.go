80dfccde893a72878ed6328a80b8e9d571dae98e
Mike Beaumont <mjboamail@gmail.com>
2023-05-05 12:16:51 +0200

    chore(policies): allow uppercase letters in header names
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- api/common/v1alpha1/header.go
+++ api/common/v1alpha1/header.go
@@ -3,7 +3,7 @@ package v1alpha1
 
 // +kubebuilder:validation:MinLength=1
 // +kubebuilder:validation:MaxLength=256
-// +kubebuilder:validation:Pattern=`^[a-z0-9!#$%&'*+\-.^_\x60|~]+$`
+// +kubebuilder:validation:Pattern=`^[a-zA-Z0-9!#$%&'*+\-.^_\x60|~]+$`
 type HeaderName string
 
 type HeaderValue string
