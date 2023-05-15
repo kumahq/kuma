e829b7bbf5eb8d09cc3cf6e9a2d11118a398b9bd
Lukasz Dziedziak <lukidzi@gmail.com>
2023-05-15 12:51:45 +0200

    fix(kuma-cp): code review changes
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/insights/test/test_event_reader.go
+++ pkg/insights/test/test_event_reader.go
@@ -10,6 +10,9 @@ func (t *TestEventReader) Recv() <-chan events.Event {
 	return t.Ch
 }
 
+// This method should be called only once. In tests, we
+// have only one call to subscribe, and we want to avoid
+// closing the channel twice, as it may lead to a panic.
 func (t *TestEventReader) Close() {
 	close(t.Ch)
 }
