e4943162837d851e59e4ec99729255aa511b6082
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2023-05-17 12:44:51 +0200

    test(e2e): add option to set init scripts for postgres (#6784)
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- test/framework/deployments/postgres/kubernetes.go
+++ test/framework/deployments/postgres/kubernetes.go
@@ -1,6 +1,8 @@
 package postgres
 
 import (
+	"fmt"
+
 	"github.com/gruntwork-io/terratest/modules/helm"
 
 	"github.com/kumahq/kuma/test/framework"
@@ -33,6 +35,23 @@ func (t *k8SDeployment) Deploy(cluster framework.Cluster) error {
 			"postgresql.primary.fullname":             t.options.primaryName,
 		},
 		KubectlOptions: cluster.GetKubectlOptions(t.options.namespace),
+	}
+	if t.options.initScript != "" {
+		initScriptCfg := fmt.Sprintf(`
+kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: init-script
+  namespace: %s
+data:
+  0001-load.sql: "%s"
+`, t.options.namespace, t.options.initScript)
+		if err := cluster.Install(framework.YamlK8s(initScriptCfg)); err != nil {
+			return err
+		}
+		helmOpts.SetValues["primary.initdb.scriptsConfigMap"] = "init-script"
+		helmOpts.SetValues["primary.initdb.user"] = "postgres"
+		helmOpts.SetValues["primary.initdb.password"] = t.options.postgresPassword
 	}
 
 	err := helm.AddRepoE(cluster.GetTesting(), helmOpts, "bitnami", "https://charts.bitnami.com/bitnami")
