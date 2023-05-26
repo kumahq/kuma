96cc53030b942a851767742e6ec304a4d16de4ce
Bart Smykla <bartek@smykla.com>
2023-05-26 06:01:05 +0200

    chore(MeshTCPRoute): fix GetDefault for the policy (#6845)
    Rules may be empty, which might cause out of index access errors, so
    instead of assuming that there will always be at least one rule, or
    addressing this case in the `GetDefault` function, we will address it in
    the plugin logic instead.
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/plugins/policies/meshtcproute/api/v1alpha1/helpers.go
+++ pkg/plugins/policies/meshtcproute/api/v1alpha1/helpers.go
@@ -1,5 +1,20 @@
 package v1alpha1
 
+import (
+	"github.com/kumahq/kuma/pkg/util/pointer"
+)
+
 func (x *To) GetDefault() interface{} {
+	if len(x.Rules) == 0 {
+		return Rule{
+			Default: RuleConf{
+				BackendRefs: []BackendRef{{
+					TargetRef: x.TargetRef,
+					Weight:    pointer.To(uint(1)),
+				}},
+			},
+		}
+	}
+
 	return x.Rules[0]
 }
