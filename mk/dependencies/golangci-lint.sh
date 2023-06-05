343df0839915a1dd527ac96b10a295471bab9df7
Matthieu MOREL <matthieu.morel35@gmail.com>
2023-06-05 21:04:56 +0200

    ci(lint): add golangci-lint Github action
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- mk/dependencies/golangci-lint.sh
+++ mk/dependencies/golangci-lint.sh
@@ -2,12 +2,12 @@
 set -e
 
 OUTPUT_BIN_DIR=$1/bin
-VERSION="1.51.2"
+VERSION=$(cat .golangci-lint-version)
 
 golangcilint="${OUTPUT_BIN_DIR}"/golangci-lint
 
-if [ -e "${golangcilint}" ] && [ "$(${golangcilint} version --format short)" == "${VERSION}" ]; then
+if [ -e "${golangcilint}" ] && [ "v$(${golangcilint} version --format short)" == "${VERSION}" ]; then
   echo "golangci-lint ${VERSION} is already installed at ${OUTPUT_BIN_DIR}"
   exit
 fi
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "${OUTPUT_BIN_DIR}" "v${VERSION}"
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "${OUTPUT_BIN_DIR}" "${VERSION}"
