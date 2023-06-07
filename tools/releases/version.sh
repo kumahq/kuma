36f1198565c2b053ef154512824c0be29b0ea80f
Łukasz Dziedziak <lukidzi@gmail.com>
2023-06-07 15:06:48 +0200

    chore(deps): upgrade envoy version to 1.26.2 (#6938)
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- tools/releases/version.sh
+++ tools/releases/version.sh
@@ -8,7 +8,11 @@ function envoy_version() {
   # - if ENVOY_TAG is a real git tag like 'v1.20.0' then the version is equal to '1.20.0' (without the first letter 'v').
   # - if ENVOY_TAG is a commit hash then the version will look like '1.20.1-dev-b16d390f'
 
+<<<<<<< HEAD
   ENVOY_TAG=${ENVOY_TAG:-"v1.22.10"}
+=======
+  ENVOY_TAG=${ENVOY_TAG:-"v1.26.2"}
+>>>>>>> 28a441aac (chore(deps): upgrade envoy version to 1.26.2 (#6938))
   ENVOY_VERSION=$(curl --silent --location "https://raw.githubusercontent.com/envoyproxy/envoy/${ENVOY_TAG}/VERSION.txt")
 
   # for envoy versions older than v1.22.0 file 'VERSION.txt' used to be called 'VERSION'
