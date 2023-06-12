18ea55e3e0bb422fb1f1cb4391d699e799512484
Bart Smykla <bartek@smykla.com>
2023-06-12 13:27:27 +0200

    Revert "chore(MeshHTTPRoute): default catch all path in route match"
    This reverts commit 56fe057a40b74186b7862fe1e6db18676f03d3cd.
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/plugins/policies/meshhttproute/api/v1alpha1/helpers.go
+++ pkg/plugins/policies/meshhttproute/api/v1alpha1/helpers.go
@@ -5,33 +5,7 @@ type PolicyDefault struct {
 }
 
 func (x *To) GetDefault() interface{} {
-	var rules []Rule
-
-	for _, rule := range x.Rules {
-		var matches []Match
-
-		for _, match := range rule.Matches {
-			if match.Path == nil {
-				// According to Envoy docs, match must have precisely one of
-				// prefix, path, safe_regex, connect_matcher,
-				// path_separated_prefix, path_match_policy set, so when policy
-				// doesn't specify explicit type of matching, we are assuming
-				// "catch all" path (any path starting with "/").
-				match.Path = &PathMatch{
-					Value: "/",
-					Type:  PathPrefix,
-				}
-			}
-
-			matches = append(matches, match)
-		}
-
-		rule.Matches = matches
-
-		rules = append(rules, rule)
-	}
-
 	return PolicyDefault{
-		Rules: rules,
+		Rules: x.Rules,
 	}
 }
