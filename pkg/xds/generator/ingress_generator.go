c1d98c0f5e061fcca7f0b52923fa2c99f19558aa
Bart Smykla <bartek@smykla.com>
2023-06-15 10:24:32 +0200

    chore: revert removal of deprecated TcpProxy... configurer
    It was causing import cycle
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/xds/generator/ingress_generator.go
+++ pkg/xds/generator/ingress_generator.go
@@ -7,7 +7,6 @@ import (
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
 	core_mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
 	core_xds "github.com/kumahq/kuma/pkg/core/xds"
-	plugins_xds "github.com/kumahq/kuma/pkg/plugins/policies/core/xds"
 	xds_context "github.com/kumahq/kuma/pkg/xds/context"
 	envoy_common "github.com/kumahq/kuma/pkg/xds/envoy"
 	envoy_clusters "github.com/kumahq/kuma/pkg/xds/envoy/clusters"
@@ -115,16 +114,21 @@ func (i IngressGenerator) generateLDS(
 			}
 			sniUsed[sni] = true
 
-			split := plugins_xds.NewSplitBuilder().
-				WithClusterName(clusterName).
-				WithLBMetadata(meshDestination.WithoutTags(mesh_proto.ServiceTag)).
-				Build()
+			cluster := envoy_common.NewCluster(
+				envoy_common.WithName(clusterName),
+				envoy_common.WithTags(
+					meshDestination.WithoutTags(mesh_proto.ServiceTag),
+				),
+			)
 
 			filterChain := envoy_listeners.FilterChain(
 				envoy_listeners.NewFilterChainBuilder(apiVersion).Configure(
 					envoy_listeners.MatchTransportProtocol("tls"),
 					envoy_listeners.MatchServerNames(sni),
-					envoy_listeners.TCPProxy(clusterName, split),
+					envoy_listeners.TcpProxyDeprecatedWithMetadata(
+						clusterName,
+						cluster,
+					),
 				),
 			)
 
