10abc8ecd15f61fef516b218762e1ef2034f6c58
Matthieu MOREL <matthieu.morel35@gmail.com>
2023-07-25 16:17:58 +0200

    refactor(xds): virtualhost builder (#7183)
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- pkg/xds/envoy/virtualhosts/configurer.go
+++ pkg/xds/envoy/virtualhosts/configurer.go
@@ -2,22 +2,13 @@ package virtualhosts
 
 import (
 	envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
-	envoy_route "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	envoy_config_route_v3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
 
 	core_mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
 	util_proto "github.com/kumahq/kuma/pkg/util/proto"
 	envoy_common "github.com/kumahq/kuma/pkg/xds/envoy"
-	v3 "github.com/kumahq/kuma/pkg/xds/envoy/routes/v3"
+	envoy_routes_v3 "github.com/kumahq/kuma/pkg/xds/envoy/routes/v3"
 )
-
-func CommonVirtualHost(name string) VirtualHostBuilderOpt {
-	return AddVirtualHostConfigurer(
-		VirtualHostMustConfigureFunc(func(vh *envoy_route.VirtualHost) {
-			vh.Name = name
-			vh.Domains = []string{"*"}
-		}),
-	)
-}
 
 func DomainNames(domainNames ...string) VirtualHostBuilderOpt {
 	if len(domainNames) == 0 {
@@ -25,7 +16,7 @@ func DomainNames(domainNames ...string) VirtualHostBuilderOpt {
 	}
 
 	return AddVirtualHostConfigurer(
-		VirtualHostMustConfigureFunc(func(vh *envoy_route.VirtualHost) {
+		VirtualHostMustConfigureFunc(func(vh *envoy_config_route_v3.VirtualHost) {
 			vh.Domains = domainNames
 		}),
 	)
@@ -33,14 +24,14 @@ func DomainNames(domainNames ...string) VirtualHostBuilderOpt {
 
 func Routes(routes envoy_common.Routes) VirtualHostBuilderOpt {
 	return AddVirtualHostConfigurer(
-		&v3.RoutesConfigurer{
+		&RoutesConfigurer{
 			Routes: routes,
 		})
 }
 
 // Redirect for paths that match to matchPath returns 301 status code with new port and path
 func Redirect(matchPath, newPath string, allowGetOnly bool, port uint32) VirtualHostBuilderOpt {
-	return AddVirtualHostConfigurer(&v3.RedirectConfigurer{
+	return AddVirtualHostConfigurer(&RedirectConfigurer{
 		MatchPath:    matchPath,
 		NewPath:      newPath,
 		Port:         port,
@@ -51,8 +42,8 @@ func Redirect(matchPath, newPath string, allowGetOnly bool, port uint32) Virtual
 // RequireTLS specifies that this virtual host must only accept TLS connections.
 func RequireTLS() VirtualHostBuilderOpt {
 	return AddVirtualHostConfigurer(
-		VirtualHostMustConfigureFunc(func(vh *envoy_route.VirtualHost) {
-			vh.RequireTls = envoy_route.VirtualHost_ALL
+		VirtualHostMustConfigureFunc(func(vh *envoy_config_route_v3.VirtualHost) {
+			vh.RequireTls = envoy_config_route_v3.VirtualHost_ALL
 		}),
 	)
 }
@@ -60,7 +51,7 @@ func RequireTLS() VirtualHostBuilderOpt {
 // SetResponseHeader unconditionally sets the named response header to the given value.
 func SetResponseHeader(name string, value string) VirtualHostBuilderOpt {
 	return AddVirtualHostConfigurer(
-		VirtualHostMustConfigureFunc(func(vh *envoy_route.VirtualHost) {
+		VirtualHostMustConfigureFunc(func(vh *envoy_config_route_v3.VirtualHost) {
 			hsts := &envoy_config_core_v3.HeaderValueOption{
 				Append: util_proto.Bool(false),
 				Header: &envoy_config_core_v3.HeaderValue{
@@ -76,15 +67,15 @@ func SetResponseHeader(name string, value string) VirtualHostBuilderOpt {
 
 func Retry(retry *core_mesh.RetryResource, protocol core_mesh.Protocol) VirtualHostBuilderOpt {
 	return AddVirtualHostConfigurer(
-		VirtualHostMustConfigureFunc(func(vh *envoy_route.VirtualHost) {
-			vh.RetryPolicy = v3.RetryConfig(retry, protocol)
+		VirtualHostMustConfigureFunc(func(vh *envoy_config_route_v3.VirtualHost) {
+			vh.RetryPolicy = envoy_routes_v3.RetryConfig(retry, protocol)
 		}),
 	)
 }
 
 func Route(matchPath, newPath, cluster string, allowGetOnly bool) VirtualHostBuilderOpt {
 	return AddVirtualHostConfigurer(
-		&v3.RouteConfigurer{
+		&RouteConfigurer{
 			MatchPath:    matchPath,
 			NewPath:      newPath,
 			Cluster:      cluster,
--- pkg/xds/envoy/virtualhosts/routes_configurer.go
+++ pkg/xds/envoy/virtualhosts/routes_configurer.go
@@ -1,28 +1,30 @@
-package v3
+package virtualhosts
 
 import (
 	"sort"
 
 	envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
-	envoy_route "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
-	envoy_type_matcher "github.com/envoyproxy/go-control-plane/envoy/type/matcher/v3"
+	envoy_config_route_v3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	envoy_type_matcher_v3 "github.com/envoyproxy/go-control-plane/envoy/type/matcher/v3"
 	"google.golang.org/protobuf/types/known/anypb"
 
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
 	util_proto "github.com/kumahq/kuma/pkg/util/proto"
 	envoy_common "github.com/kumahq/kuma/pkg/xds/envoy"
+	envoy_routes_v3 "github.com/kumahq/kuma/pkg/xds/envoy/routes/v3"
 )
 
 type RoutesConfigurer struct {
 	Routes envoy_common.Routes
 }
 
-func (c RoutesConfigurer) Configure(virtualHost *envoy_route.VirtualHost) error {
+func (c RoutesConfigurer) Configure(virtualHost *envoy_config_route_v3.VirtualHost) error {
 	for i := range c.Routes {
 		route := c.Routes[i]
-		envoyRoute := &envoy_route.Route{
+		envoyRoute := &envoy_config_route_v3.Route{
 			Match: c.routeMatch(route.Match),
-			Action: &envoy_route.Route_Route{
+			Name:  envoy_common.AnonymousResource,
+			Action: &envoy_config_route_v3.Route_Route{
 				Route: c.routeAction(route.Clusters, route.Modify),
 			},
 		}
@@ -40,7 +42,7 @@ func (c RoutesConfigurer) Configure(virtualHost *envoy_route.VirtualHost) error 
 	return nil
 }
 
-func (c RoutesConfigurer) setHeadersModifications(route *envoy_route.Route, modify *mesh_proto.TrafficRoute_Http_Modify) {
+func (c RoutesConfigurer) setHeadersModifications(route *envoy_config_route_v3.Route, modify *mesh_proto.TrafficRoute_Http_Modify) {
 	for _, add := range modify.GetRequestHeaders().GetAdd() {
 		route.RequestHeadersToAdd = append(route.RequestHeadersToAdd, &envoy_config_core_v3.HeaderValueOption{
 			Header: &envoy_config_core_v3.HeaderValue{
@@ -68,15 +70,15 @@ func (c RoutesConfigurer) setHeadersModifications(route *envoy_route.Route, modi
 	}
 }
 
-func (c RoutesConfigurer) routeMatch(match *mesh_proto.TrafficRoute_Http_Match) *envoy_route.RouteMatch {
-	envoyMatch := &envoy_route.RouteMatch{}
+func (c RoutesConfigurer) routeMatch(match *mesh_proto.TrafficRoute_Http_Match) *envoy_config_route_v3.RouteMatch {
+	envoyMatch := &envoy_config_route_v3.RouteMatch{}
 
 	if match.GetPath() != nil {
 		c.setPathMatcher(match.GetPath(), envoyMatch)
 	} else {
 		// Path match is required on Envoy config so if there is only matching by header in TrafficRoute, we need to place
 		// the default route match anyways.
-		envoyMatch.PathSpecifier = &envoy_route.RouteMatch_Prefix{
+		envoyMatch.PathSpecifier = &envoy_config_route_v3.RouteMatch_Prefix{
 			Prefix: "/",
 		}
 	}
@@ -96,29 +98,29 @@ func (c RoutesConfigurer) routeMatch(match *mesh_proto.TrafficRoute_Http_Match) 
 	return envoyMatch
 }
 
-func (c RoutesConfigurer) headerMatcher(name string, matcher *mesh_proto.TrafficRoute_Http_Match_StringMatcher) *envoy_route.HeaderMatcher {
-	headerMatcher := &envoy_route.HeaderMatcher{
+func (c RoutesConfigurer) headerMatcher(name string, matcher *mesh_proto.TrafficRoute_Http_Match_StringMatcher) *envoy_config_route_v3.HeaderMatcher {
+	headerMatcher := &envoy_config_route_v3.HeaderMatcher{
 		Name: name,
 	}
 	switch matcher.MatcherType.(type) {
 	case *mesh_proto.TrafficRoute_Http_Match_StringMatcher_Prefix:
-		headerMatcher.HeaderMatchSpecifier = &envoy_route.HeaderMatcher_PrefixMatch{
+		headerMatcher.HeaderMatchSpecifier = &envoy_config_route_v3.HeaderMatcher_PrefixMatch{
 			PrefixMatch: matcher.GetPrefix(),
 		}
 	case *mesh_proto.TrafficRoute_Http_Match_StringMatcher_Exact:
-		stringMatcher := envoy_type_matcher.StringMatcher{
-			MatchPattern: &envoy_type_matcher.StringMatcher_Exact{
+		stringMatcher := envoy_type_matcher_v3.StringMatcher{
+			MatchPattern: &envoy_type_matcher_v3.StringMatcher_Exact{
 				Exact: matcher.GetExact(),
 			},
 		}
-		headerMatcher.HeaderMatchSpecifier = &envoy_route.HeaderMatcher_StringMatch{
+		headerMatcher.HeaderMatchSpecifier = &envoy_config_route_v3.HeaderMatcher_StringMatch{
 			StringMatch: &stringMatcher,
 		}
 	case *mesh_proto.TrafficRoute_Http_Match_StringMatcher_Regex:
-		headerMatcher.HeaderMatchSpecifier = &envoy_route.HeaderMatcher_SafeRegexMatch{
-			SafeRegexMatch: &envoy_type_matcher.RegexMatcher{
-				EngineType: &envoy_type_matcher.RegexMatcher_GoogleRe2{
-					GoogleRe2: &envoy_type_matcher.RegexMatcher_GoogleRE2{},
+		headerMatcher.HeaderMatchSpecifier = &envoy_config_route_v3.HeaderMatcher_SafeRegexMatch{
+			SafeRegexMatch: &envoy_type_matcher_v3.RegexMatcher{
+				EngineType: &envoy_type_matcher_v3.RegexMatcher_GoogleRe2{
+					GoogleRe2: &envoy_type_matcher_v3.RegexMatcher_GoogleRE2{},
 				},
 				Regex: matcher.GetRegex(),
 			},
@@ -129,22 +131,22 @@ func (c RoutesConfigurer) headerMatcher(name string, matcher *mesh_proto.Traffic
 
 func (c RoutesConfigurer) setPathMatcher(
 	matcher *mesh_proto.TrafficRoute_Http_Match_StringMatcher,
-	routeMatch *envoy_route.RouteMatch,
+	routeMatch *envoy_config_route_v3.RouteMatch,
 ) {
 	switch matcher.MatcherType.(type) {
 	case *mesh_proto.TrafficRoute_Http_Match_StringMatcher_Prefix:
-		routeMatch.PathSpecifier = &envoy_route.RouteMatch_Prefix{
+		routeMatch.PathSpecifier = &envoy_config_route_v3.RouteMatch_Prefix{
 			Prefix: matcher.GetPrefix(),
 		}
 	case *mesh_proto.TrafficRoute_Http_Match_StringMatcher_Exact:
-		routeMatch.PathSpecifier = &envoy_route.RouteMatch_Path{
+		routeMatch.PathSpecifier = &envoy_config_route_v3.RouteMatch_Path{
 			Path: matcher.GetExact(),
 		}
 	case *mesh_proto.TrafficRoute_Http_Match_StringMatcher_Regex:
-		routeMatch.PathSpecifier = &envoy_route.RouteMatch_SafeRegex{
-			SafeRegex: &envoy_type_matcher.RegexMatcher{
-				EngineType: &envoy_type_matcher.RegexMatcher_GoogleRe2{
-					GoogleRe2: &envoy_type_matcher.RegexMatcher_GoogleRE2{},
+		routeMatch.PathSpecifier = &envoy_config_route_v3.RouteMatch_SafeRegex{
+			SafeRegex: &envoy_type_matcher_v3.RegexMatcher{
+				EngineType: &envoy_type_matcher_v3.RegexMatcher_GoogleRe2{
+					GoogleRe2: &envoy_type_matcher_v3.RegexMatcher_GoogleRE2{},
 				},
 				Regex: matcher.GetRegex(),
 			},
@@ -161,8 +163,8 @@ func (c RoutesConfigurer) hasExternal(clusters []envoy_common.Cluster) bool {
 	return false
 }
 
-func (c RoutesConfigurer) routeAction(clusters []envoy_common.Cluster, modify *mesh_proto.TrafficRoute_Http_Modify) *envoy_route.RouteAction {
-	routeAction := &envoy_route.RouteAction{}
+func (c RoutesConfigurer) routeAction(clusters []envoy_common.Cluster, modify *mesh_proto.TrafficRoute_Http_Modify) *envoy_config_route_v3.RouteAction {
+	routeAction := &envoy_config_route_v3.RouteAction{}
 	if len(clusters) != 0 {
 		// Timeout can be configured only per outbound listener. So all clusters in the split
 		// must have the same timeout. That's why we can take the timeout from the first cluster.
@@ -170,26 +172,26 @@ func (c RoutesConfigurer) routeAction(clusters []envoy_common.Cluster, modify *m
 		routeAction.Timeout = util_proto.Duration(cluster.Timeout().GetHttp().GetRequestTimeout().AsDuration())
 	}
 	if len(clusters) == 1 {
-		routeAction.ClusterSpecifier = &envoy_route.RouteAction_Cluster{
+		routeAction.ClusterSpecifier = &envoy_config_route_v3.RouteAction_Cluster{
 			Cluster: clusters[0].Name(),
 		}
 	} else {
-		var weightedClusters []*envoy_route.WeightedCluster_ClusterWeight
+		var weightedClusters []*envoy_config_route_v3.WeightedCluster_ClusterWeight
 		for _, c := range clusters {
 			cluster := c.(*envoy_common.ClusterImpl)
-			weightedClusters = append(weightedClusters, &envoy_route.WeightedCluster_ClusterWeight{
+			weightedClusters = append(weightedClusters, &envoy_config_route_v3.WeightedCluster_ClusterWeight{
 				Name:   cluster.Name(),
 				Weight: util_proto.UInt32(cluster.Weight()),
 			})
 		}
-		routeAction.ClusterSpecifier = &envoy_route.RouteAction_WeightedClusters{
-			WeightedClusters: &envoy_route.WeightedCluster{
+		routeAction.ClusterSpecifier = &envoy_config_route_v3.RouteAction_WeightedClusters{
+			WeightedClusters: &envoy_config_route_v3.WeightedCluster{
 				Clusters: weightedClusters,
 			},
 		}
 	}
 	if c.hasExternal(clusters) {
-		routeAction.HostRewriteSpecifier = &envoy_route.RouteAction_AutoHostRewrite{
+		routeAction.HostRewriteSpecifier = &envoy_config_route_v3.RouteAction_AutoHostRewrite{
 			AutoHostRewrite: util_proto.Bool(true),
 		}
 	}
@@ -197,16 +199,16 @@ func (c RoutesConfigurer) routeAction(clusters []envoy_common.Cluster, modify *m
 	return routeAction
 }
 
-func (c RoutesConfigurer) setModifications(routeAction *envoy_route.RouteAction, modify *mesh_proto.TrafficRoute_Http_Modify) {
+func (c RoutesConfigurer) setModifications(routeAction *envoy_config_route_v3.RouteAction, modify *mesh_proto.TrafficRoute_Http_Modify) {
 	if modify.GetPath() != nil {
 		switch modify.GetPath().Type.(type) {
 		case *mesh_proto.TrafficRoute_Http_Modify_Path_RewritePrefix:
 			routeAction.PrefixRewrite = modify.GetPath().GetRewritePrefix()
 		case *mesh_proto.TrafficRoute_Http_Modify_Path_Regex:
-			routeAction.RegexRewrite = &envoy_type_matcher.RegexMatchAndSubstitute{
-				Pattern: &envoy_type_matcher.RegexMatcher{
-					EngineType: &envoy_type_matcher.RegexMatcher_GoogleRe2{
-						GoogleRe2: &envoy_type_matcher.RegexMatcher_GoogleRE2{},
+			routeAction.RegexRewrite = &envoy_type_matcher_v3.RegexMatchAndSubstitute{
+				Pattern: &envoy_type_matcher_v3.RegexMatcher{
+					EngineType: &envoy_type_matcher_v3.RegexMatcher_GoogleRe2{
+						GoogleRe2: &envoy_type_matcher_v3.RegexMatcher_GoogleRE2{},
 					},
 					Regex: modify.GetPath().GetRegex().GetPattern(),
 				},
@@ -218,15 +220,15 @@ func (c RoutesConfigurer) setModifications(routeAction *envoy_route.RouteAction,
 	if modify.GetHost() != nil {
 		switch modify.GetHost().Type.(type) {
 		case *mesh_proto.TrafficRoute_Http_Modify_Host_Value:
-			routeAction.HostRewriteSpecifier = &envoy_route.RouteAction_HostRewriteLiteral{
+			routeAction.HostRewriteSpecifier = &envoy_config_route_v3.RouteAction_HostRewriteLiteral{
 				HostRewriteLiteral: modify.GetHost().GetValue(),
 			}
 		case *mesh_proto.TrafficRoute_Http_Modify_Host_FromPath:
-			routeAction.HostRewriteSpecifier = &envoy_route.RouteAction_HostRewritePathRegex{
-				HostRewritePathRegex: &envoy_type_matcher.RegexMatchAndSubstitute{
-					Pattern: &envoy_type_matcher.RegexMatcher{
-						EngineType: &envoy_type_matcher.RegexMatcher_GoogleRe2{
-							GoogleRe2: &envoy_type_matcher.RegexMatcher_GoogleRE2{},
+			routeAction.HostRewriteSpecifier = &envoy_config_route_v3.RouteAction_HostRewritePathRegex{
+				HostRewritePathRegex: &envoy_type_matcher_v3.RegexMatchAndSubstitute{
+					Pattern: &envoy_type_matcher_v3.RegexMatcher{
+						EngineType: &envoy_type_matcher_v3.RegexMatcher_GoogleRe2{
+							GoogleRe2: &envoy_type_matcher_v3.RegexMatcher_GoogleRE2{},
 						},
 						Regex: modify.GetHost().GetFromPath().GetPattern(),
 					},
@@ -241,7 +243,7 @@ func (c *RoutesConfigurer) typedPerFilterConfig(route *envoy_common.Route) (map[
 	typedPerFilterConfig := map[string]*anypb.Any{}
 
 	if route.RateLimit != nil {
-		rateLimit, err := NewRateLimitConfiguration(RateLimitConfigurationFromProto(route.RateLimit))
+		rateLimit, err := envoy_routes_v3.NewRateLimitConfiguration(envoy_routes_v3.RateLimitConfigurationFromProto(route.RateLimit))
 		if err != nil {
 			return nil, err
 		}
