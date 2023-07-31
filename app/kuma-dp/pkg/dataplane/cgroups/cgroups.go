f876298d55682683924c41990911e8b63d1f69da
slonka <slonka@users.noreply.github.com>
2023-07-31 13:29:52 +0200

    chore(lint): change indent
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- app/kuma-dp/pkg/dataplane/cgroups/cgroups.go
+++ app/kuma-dp/pkg/dataplane/cgroups/cgroups.go
@@ -1,10 +1,10 @@
 package cgroups
 
 import (
-    "path/filepath"
-    "sync"
+	"path/filepath"
+	"sync"
 
-    "golang.org/x/sys/unix"
+	"golang.org/x/sys/unix"
 )
 
 // TAKEN FROM https://github.com/containerd/cgroups/blob/v1.1.0/utils.go
@@ -16,41 +16,41 @@ type CGMode int
 const unifiedMountpoint = "/sys/fs/cgroup"
 
 const (
-    // Unavailable cgroup mountpoint
-    Unavailable CGMode = iota
-    // Legacy cgroups v1
-    Legacy
-    // Hybrid with cgroups v1 and v2 controllers mounted
-    Hybrid
-    // Unified with only cgroups v2 mounted
-    Unified
+	// Unavailable cgroup mountpoint
+	Unavailable CGMode = iota
+	// Legacy cgroups v1
+	Legacy
+	// Hybrid with cgroups v1 and v2 controllers mounted
+	Hybrid
+	// Unified with only cgroups v2 mounted
+	Unified
 )
 
 var (
-    checkMode sync.Once
-    cgMode    CGMode
+	checkMode sync.Once
+	cgMode    CGMode
 )
 
 // Mode returns the cgroups mode running on the host
 func Mode() CGMode {
-    checkMode.Do(func() {
-        var st unix.Statfs_t
-        if err := unix.Statfs(unifiedMountpoint, &st); err != nil {
-            cgMode = Unavailable
-            return
-        }
-        switch st.Type {
-        case unix.CGROUP2_SUPER_MAGIC:
-            cgMode = Unified
-        default:
-            cgMode = Legacy
-            if err := unix.Statfs(filepath.Join(unifiedMountpoint, "unified"), &st); err != nil {
-                return
-            }
-            if st.Type == unix.CGROUP2_SUPER_MAGIC {
-                cgMode = Hybrid
-            }
-        }
-    })
-    return cgMode
+	checkMode.Do(func() {
+		var st unix.Statfs_t
+		if err := unix.Statfs(unifiedMountpoint, &st); err != nil {
+			cgMode = Unavailable
+			return
+		}
+		switch st.Type {
+		case unix.CGROUP2_SUPER_MAGIC:
+			cgMode = Unified
+		default:
+			cgMode = Legacy
+			if err := unix.Statfs(filepath.Join(unifiedMountpoint, "unified"), &st); err != nil {
+				return
+			}
+			if st.Type == unix.CGROUP2_SUPER_MAGIC {
+				cgMode = Hybrid
+			}
+		}
+	})
+	return cgMode
 }
