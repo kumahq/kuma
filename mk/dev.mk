de19893b4d51954287c1e04d37804d1175406280
slonka <slonka@users.noreply.github.com>
2023-08-03 11:19:50 +0200

    chore(release): resolve conflicts
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- mk/dev.mk
+++ mk/dev.mk
@@ -24,16 +24,9 @@ endif
 CI_TOOLS_BIN_DIR=$(CI_TOOLS_DIR)/bin
 
 # Change here and `make check` ensures these are used for CI
-<<<<<<< HEAD
 K8S_MIN_VERSION = v1.20.15-k3s1
 K8S_MAX_VERSION = v1.26.1-k3s1
-GO_VERSION := 1.20.6
-=======
-K8S_MIN_VERSION = v1.22.9-k3s1
-K8S_MAX_VERSION = v1.27.1-k3s1
-export GO_VERSION=1.20.7
-export GOLANGCI_LINT_VERSION=v1.53.3
->>>>>>> 95b033a3d (chore(deps): update to go 1.20.7 (#7429))
+GO_VERSION := 1.20.7
 GOOS := $(shell go env GOOS)
 GOARCH := $(shell go env GOARCH)
 
