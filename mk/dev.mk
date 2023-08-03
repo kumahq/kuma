eb99257af7ab4948e861ec5e91053a8b5e13076c
Krzysztof Słonka <slonka@users.noreply.github.com>
2023-08-03 09:00:01 +0200

    chore(deps): update to go 1.20.7 (#7429)
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- mk/dev.mk
+++ mk/dev.mk
@@ -16,6 +16,14 @@ ifdef XDG_DATA_HOME
 endif
 CI_TOOLS_BIN_DIR=$(CI_TOOLS_DIR)/bin
 
+<<<<<<< HEAD
+=======
+# Change here and `make check` ensures these are used for CI
+K8S_MIN_VERSION = v1.22.9-k3s1
+K8S_MAX_VERSION = v1.27.1-k3s1
+export GO_VERSION=1.20.7
+export GOLANGCI_LINT_VERSION=v1.53.3
+>>>>>>> 95b033a3d (chore(deps): update to go 1.20.7 (#7429))
 GOOS := $(shell go env GOOS)
 GOARCH := $(shell go env GOARCH)
 
