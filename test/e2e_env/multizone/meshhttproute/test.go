4b025a61f2fbc904a4ac3c806ecf566a222d2bc0
Mike Beaumont <mjboamail@gmail.com>
2023-08-15 19:30:26 +0200

    test(e2e): refactor MeshHTTPRoute cross zone for multiple meshes
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- test/e2e_env/multizone/meshhttproute/test.go
+++ test/e2e_env/multizone/meshhttproute/test.go
@@ -14,62 +14,69 @@ import (
 )
 
 func Test() {
-	meshName := "meshhttproute"
+	noEgressMeshName := "meshhttproute"
 
 	BeforeAll(func() {
 		// Global
-		Expect(multizone.Global.Install(MTLSMeshUniversal(meshName))).To(Succeed())
-		Expect(WaitForMesh(meshName, multizone.Zones())).To(Succeed())
+		Expect(multizone.Global.Install(MTLSMeshUniversal(noEgressMeshName))).To(Succeed())
+		for _, meshName := range []string{noEgressMeshName} {
+			Expect(WaitForMesh(meshName, multizone.Zones())).To(Succeed())
 
-		err := NewClusterSetup().
-			Install(DemoClientUniversal(AppModeDemoClient, meshName, WithTransparentProxy(true))).
-			Install(TestServerUniversal("dp-echo-1", meshName,
-				WithArgs([]string{"echo", "--instance", "zone1"}),
-				WithServiceVersion("v1"),
-			)).
-			Setup(multizone.UniZone1)
-		Expect(err).ToNot(HaveOccurred())
+			err := NewClusterSetup().
+				Install(DemoClientUniversal(AppModeDemoClient+meshName, meshName, WithTransparentProxy(true), WithServiceName(AppModeDemoClient))).
+				Install(TestServerUniversal("dp-echo-1"+meshName, meshName,
+					WithArgs([]string{"echo", "--instance", "zone1"}),
+					WithServiceVersion("v1"),
+				)).
+				Setup(multizone.UniZone1)
+			Expect(err).ToNot(HaveOccurred())
 
-		err = NewClusterSetup().
-			Install(TestServerUniversal("dp-echo-2", meshName,
-				WithArgs([]string{"echo", "--instance", "zone2-v1"}),
-				WithServiceVersion("v1"),
-			)).
-			Setup(multizone.UniZone2)
-		Expect(err).ToNot(HaveOccurred())
+			err = NewClusterSetup().
+				Install(TestServerUniversal("dp-echo-2"+meshName, meshName,
+					WithArgs([]string{"echo", "--instance", "zone2-v1"}),
+					WithServiceVersion("v1"),
+				)).
+				Setup(multizone.UniZone2)
+			Expect(err).ToNot(HaveOccurred())
 
-		err = NewClusterSetup().
-			Install(TestServerUniversal("dp-echo-3", meshName,
-				WithArgs([]string{"echo", "--instance", "zone2-v2"}),
-				WithServiceVersion("v2"),
-			)).
-			Setup(multizone.UniZone2)
-		Expect(err).ToNot(HaveOccurred())
+			err = NewClusterSetup().
+				Install(TestServerUniversal("dp-echo-3"+meshName, meshName,
+					WithArgs([]string{"echo", "--instance", "zone2-v2"}),
+					WithServiceVersion("v2"),
+				)).
+				Setup(multizone.UniZone2)
+			Expect(err).ToNot(HaveOccurred())
 
-		err = NewClusterSetup().
-			Install(TestServerUniversal("dp-echo-4", meshName,
-				WithArgs([]string{"echo", "--instance", "alias-zone2"}),
-				WithServiceName("alias-test-server"),
-				WithServiceVersion("v2"),
-			)).
-			Setup(multizone.UniZone2)
-		Expect(err).ToNot(HaveOccurred())
+			err = NewClusterSetup().
+				Install(TestServerUniversal("dp-echo-4"+meshName, meshName,
+					WithArgs([]string{"echo", "--instance", "alias-zone2"}),
+					WithServiceName("alias-test-server"),
+					WithServiceVersion("v2"),
+				)).
+				Setup(multizone.UniZone2)
+			Expect(err).ToNot(HaveOccurred())
 
-		Expect(DeleteMeshResources(multizone.Global, meshName, core_mesh.TrafficRouteResourceTypeDescriptor)).To(Succeed())
+			Expect(DeleteMeshResources(multizone.Global, meshName, core_mesh.TrafficRouteResourceTypeDescriptor)).To(Succeed())
+		}
 	})
 
 	E2EAfterEach(func() {
-		Expect(DeleteMeshResources(multizone.Global, meshName, v1alpha1.MeshHTTPRouteResourceTypeDescriptor)).To(Succeed())
+		for _, meshName := range []string{noEgressMeshName} {
+			Expect(DeleteMeshResources(multizone.Global, meshName, v1alpha1.MeshHTTPRouteResourceTypeDescriptor)).To(Succeed())
+		}
 	})
 
 	E2EAfterAll(func() {
-		Expect(multizone.UniZone2.DeleteMeshApps(meshName)).To(Succeed())
-		Expect(multizone.UniZone1.DeleteMeshApps(meshName)).To(Succeed())
-		Expect(multizone.Global.DeleteMesh(meshName)).To(Succeed())
+		for _, meshName := range []string{noEgressMeshName} {
+			Expect(multizone.UniZone2.DeleteMeshApps(meshName)).To(Succeed())
+			Expect(multizone.UniZone1.DeleteMeshApps(meshName)).To(Succeed())
+			Expect(multizone.Global.DeleteMesh(meshName)).To(Succeed())
+		}
 	})
 
-	It("should use MeshHTTPRoute for cross-zone communication", func() {
-		Expect(YamlUniversal(fmt.Sprintf(`
+	DescribeTable("MeshHTTPRoute should work cross-zone",
+		func(meshName string) {
+			Expect(YamlUniversal(fmt.Sprintf(`
 type: MeshHTTPRoute
 name: route-1
 mesh: %s
@@ -95,18 +102,20 @@ spec:
                   kuma.io/zone: kuma-5
 `, meshName))(multizone.Global)).To(Succeed())
 
-		Eventually(func(g Gomega) {
-			response, err := client.CollectResponsesByInstance(multizone.UniZone1, "demo-client", "test-server.mesh")
-			g.Expect(err).ToNot(HaveOccurred())
-			g.Expect(response).To(
-				And(
-					Not(HaveKey(MatchRegexp(`^zone1.*`))),
-					Not(HaveKey(MatchRegexp(`^zone2.*`))),
-					HaveKeyWithValue(MatchRegexp(`^alias-zone2.*`), Not(BeNil())),
-				),
-			)
-		}, "30s", "500ms").Should(Succeed())
-	})
+			Eventually(func(g Gomega) {
+				response, err := client.CollectResponsesByInstance(multizone.UniZone1, AppModeDemoClient+meshName, "test-server.mesh")
+				g.Expect(err).ToNot(HaveOccurred())
+				g.Expect(response).To(
+					And(
+						Not(HaveKey(MatchRegexp(`^zone1.*`))),
+						Not(HaveKey(MatchRegexp(`^zone2.*`))),
+						HaveKeyWithValue(MatchRegexp(`^alias-zone2.*`), Not(BeNil())),
+					),
+				)
+			}, "30s", "500ms").Should(Succeed())
+		},
+		Entry("without zoneEgress", noEgressMeshName),
+	)
 
 	It("should use MeshHTTPRoute for cross-zone with MeshServiceSubset", func() {
 		Expect(YamlUniversal(fmt.Sprintf(`
@@ -138,10 +147,10 @@ spec:
                 weight: 1
                 tags:
                   version: v2
-`, meshName))(multizone.Global)).To(Succeed())
+`, noEgressMeshName))(multizone.Global)).To(Succeed())
 
 		Eventually(func(g Gomega) {
-			response, err := client.CollectResponsesByInstance(multizone.UniZone1, "demo-client", "test-server.mesh")
+			response, err := client.CollectResponsesByInstance(multizone.UniZone1, AppModeDemoClient+noEgressMeshName, "test-server.mesh")
 			g.Expect(err).ToNot(HaveOccurred())
 			g.Expect(response).To(
 				And(
