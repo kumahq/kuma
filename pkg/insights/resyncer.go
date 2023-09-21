83bcba367332b060d3222cb3509a014c852a8bb5
Lukasz Dziedziak <lukidzi@gmail.com>
2023-09-21 09:17:40 +0200

    fix(kuma-cp): take proper context for resync
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/insights/resyncer.go
+++ pkg/insights/resyncer.go
@@ -265,7 +265,7 @@ func (r *resyncer) Start(stop <-chan struct{}) error {
 					}
 
 					externalServices := &core_mesh.ExternalServiceResourceList{}
-					if err := r.rm.List(ctx, externalServices, store.ListByMesh(event.mesh)); err != nil {
+					if err := r.rm.List(tenantCtx, externalServices, store.ListByMesh(event.mesh)); err != nil {
 						log.Error(err, "unable to get ExternalServices to recompute insights", "event", event)
 						continue
 					}
