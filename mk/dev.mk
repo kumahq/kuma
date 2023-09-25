620ce6407e88f5d5d9e2b31d9212af097d1b6f95
Mike Beaumont <mjboamail@gmail.com>
2023-09-25 17:25:03 +0200

    build(mk): use go.mod as source of truth for go version in makefiles (#7843)
    * build(mk): use go.mod as source of truth for go version
    * ci: use go-version-file in github actions
    * ci: don't check go version with actions/setup-go
    
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- mk/dev.mk
+++ mk/dev.mk
@@ -21,7 +21,7 @@ CI_TOOLS_BIN_DIR=$(CI_TOOLS_DIR)/bin
 # Change here and `make check` ensures these are used for CI
 K8S_MIN_VERSION = v1.23.17-k3s1
 K8S_MAX_VERSION = v1.27.1-k3s1
-export GO_VERSION=1.21.1
+export GO_VERSION=$(shell go mod edit -json | jq -r .Go)
 export GOLANGCI_LINT_VERSION=v1.54.1
 GOOS := $(shell go env GOOS)
 GOARCH := $(shell go env GOARCH)
