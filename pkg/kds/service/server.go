93e36b60b0fb0058ce3e73319341e818b6202a30
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2023-09-26 13:52:51 +0200

    feat(kds): better error handling (#7868)
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- pkg/kds/service/server.go
+++ pkg/kds/service/server.go
@@ -1,13 +1,26 @@
 package service
 
 import (
+<<<<<<< HEAD
+=======
+	"context"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/sethvargo/go-retry"
+>>>>>>> 3ab585c92 (feat(kds): better error handling (#7868))
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
 	"github.com/kumahq/kuma/pkg/core"
 	"github.com/kumahq/kuma/pkg/kds/util"
 	util_grpc "github.com/kumahq/kuma/pkg/util/grpc"
 )
+
+var log = core.Log.WithName("kds-service")
 
 type GlobalKDSServiceServer struct {
 	envoyAdminRPCs EnvoyAdminRPCs
@@ -48,19 +61,49 @@ func (g *GlobalKDSServiceServer) streamEnvoyAdminRPC(
 ) error {
 	zone, err := util.ClientIDFromIncomingCtx(stream.Context())
 	if err != nil {
-		return err
+		return status.Error(codes.InvalidArgument, err.Error())
 	}
+<<<<<<< HEAD
 	core.Log.Info("Envoy Admin RPC stream started", "rpc", rpcName, "zone", zone)
 	rpc.ClientConnected(zone, stream)
 	defer rpc.ClientDisconnected(zone)
+=======
+	clientID := ClientID(stream.Context(), zone)
+	logger := log.WithValues("rpc", rpcName, "clientID", clientID)
+	logger.Info("Envoy Admin RPC stream started")
+	rpc.ClientConnected(clientID, stream)
+	if err := g.storeStreamConnection(stream.Context(), zone, rpcName, g.instanceID); err != nil {
+		logger.Error(err, "could not store stream connection")
+		return status.Error(codes.Internal, "could not store stream connection")
+	}
+	defer func() {
+		rpc.ClientDisconnected(clientID)
+		// stream.Context() is cancelled here, we need to use another ctx
+		ctx := multitenant.CopyIntoCtx(stream.Context(), context.Background())
+		if err := g.storeStreamConnection(ctx, zone, rpcName, ""); err != nil {
+			logger.Error(err, "could not clear stream connection information in ZoneInsight")
+		}
+	}()
+>>>>>>> 3ab585c92 (feat(kds): better error handling (#7868))
 	for {
 		resp, err := recv()
-		if err != nil {
-			return err
+		if err == io.EOF {
+			return nil
 		}
+<<<<<<< HEAD
 		core.Log.V(1).Info("Envoy Admin RPC response received", "rpc", rpc, "zone", zone, "requestId", resp.GetRequestId())
 		if err := rpc.ResponseReceived(zone, resp); err != nil {
 			return err
+=======
+		if err != nil {
+			logger.Error(err, "could not receive a message")
+			return status.Error(codes.Internal, "could not receive a message")
+		}
+		logger.V(1).Info("Envoy Admin RPC response received", "requestId", resp.GetRequestId())
+		if err := rpc.ResponseReceived(clientID, resp); err != nil {
+			logger.Error(err, "could not mark the response as received")
+			return status.Error(codes.InvalidArgument, "could not mark the response as received")
+>>>>>>> 3ab585c92 (feat(kds): better error handling (#7868))
 		}
 	}
 }
---
+++ pkg/kds/v2/server/kds.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"context"
+
+	envoy_cache "github.com/envoyproxy/go-control-plane/pkg/cache/v3"
+	"github.com/envoyproxy/go-control-plane/pkg/server/delta/v3"
+	"github.com/envoyproxy/go-control-plane/pkg/server/stream/v3"
+	envoy_server "github.com/envoyproxy/go-control-plane/pkg/server/v3"
+	"github.com/go-logr/logr"
+
+	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
+)
+
+// Server is common for global and zone
+type Server interface {
+	ZoneToGlobal(stream.DeltaStream) error
+	mesh_proto.KDSSyncServiceServer
+}
+
+func NewServer(config envoy_cache.Cache, callbacks envoy_server.Callbacks, log logr.Logger) Server {
+	deltaServer := delta.NewServer(context.Background(), config, callbacks)
+	return &server{Server: deltaServer}
+}
+
+var _ Server = &server{}
+
+type server struct {
+	delta.Server
+	mesh_proto.UnimplementedKDSSyncServiceServer
+}
+
+func (s *server) GlobalToZoneSync(stream mesh_proto.KDSSyncService_GlobalToZoneSyncServer) error {
+	errorStream := NewErrorRecorderStream(stream)
+	err := s.Server.DeltaStreamHandler(errorStream, "")
+	if err == nil {
+		err = errorStream.Err()
+	}
+	return err
+}
+
+// Delta xDS server expects `KDSSyncService_ZoneToGlobalSyncServer` to have Send(*v3.DeltaDiscoveryResponse)
+// and Recv() (*v3.DeltaDiscoveryRequest, error) but proto has different definition to make it works for
+// synchronization from Zone to Global.
+func (s *server) ZoneToGlobalSync(stream mesh_proto.KDSSyncService_ZoneToGlobalSyncServer) error {
+	panic("not implemented")
+}
+
+// ZoneToGlobal is the custom implementation for `ZoneToGlobalSync` to support running delta server
+// on zone while kds.proto has different definition of `KDSSyncService_ZoneToGlobalSyncServer` then
+// expected by delta xDS server.
+func (s *server) ZoneToGlobal(stream stream.DeltaStream) error {
+	errorStream := NewErrorRecorderStream(stream)
+	err := s.Server.DeltaStreamHandler(errorStream, "")
+	if err == nil {
+		err = errorStream.Err()
+	}
+	return err
+}
