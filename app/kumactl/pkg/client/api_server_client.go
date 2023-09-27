4da7571e2e6086612218d607e7b80ad97446a859
Charly Molter <charly.molter@konghq.com>
2023-09-27 10:15:23 +0200

    feat(kds): add user-agent with useful version info (#7886)
    This is useful to debug KDS problems with access log on gateways
    
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- app/kumactl/pkg/client/api_server_client.go
+++ app/kumactl/pkg/client/api_server_client.go
@@ -12,7 +12,13 @@ import (
 )
 
 func ApiServerClient(coordinates *config_proto.ControlPlaneCoordinates_ApiServer, timeout time.Duration) (util_http.Client, error) {
+<<<<<<< HEAD
 	headers := make(map[string]string)
+=======
+	headers := map[string]string{
+		"User-Agent": kuma_version.Build.UserAgent("kumactl"),
+	}
+>>>>>>> 01b999035 (feat(kds): add user-agent with useful version info (#7886))
 	baseURL, err := url.Parse(coordinates.Url)
 	if err != nil {
 		return nil, errors.Wrapf(err, "Failed to parse API Server URL")
