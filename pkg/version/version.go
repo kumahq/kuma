126915f380aadbe4ac0dcaf6c1a345bccf8e840f
Charly Molter <charly.molter@konghq.com>
2023-09-27 10:15:23 +0200

    feat(kds): add user-agent with useful version info (#7886)
    This is useful to debug KDS problems with access log on gateways
    
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,6 +2,7 @@ package version
 
 import (
 	"fmt"
+	"runtime"
 	"strings"
 )
 
@@ -34,9 +35,23 @@ func FormatDetailedProductInfo() string {
 	)
 }
 
+<<<<<<< HEAD
 var (
 	Build BuildInfo
 )
+=======
+func (b BuildInfo) UserAgent(component string) string {
+	return fmt.Sprintf("%s/%s (%s; %s; %s/%s)",
+		component,
+		b.Version,
+		runtime.GOOS,
+		runtime.GOARCH,
+		b.Product,
+		b.GitCommit[:7])
+}
+
+var Build BuildInfo
+>>>>>>> 01b999035 (feat(kds): add user-agent with useful version info (#7886))
 
 func init() {
 	Build = BuildInfo{
