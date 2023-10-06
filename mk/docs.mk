44ff21a5c12366dfafdb72b877f897f3a3be4ed7
Charly Molter <charly.molter@konghq.com>
2023-10-06 13:06:57 +0200

    experiment
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- mk/docs.mk
+++ mk/docs.mk
@@ -56,11 +56,12 @@ docs/generated/openapi.yaml:
 	rm -rf $(OAPI_TMP_DIR)
 	mkdir -p $(dir $@)
 	mkdir -p $(OAPI_TMP_DIR)
-ifdef BASE_API
-	cp $(BASE_API) $(OAPI_TMP_DIR)/
-endif
 	for i in $(API_DIRS); do mkdir -p $(OAPI_TMP_DIR)/$$(echo $${i} | cut -d: -f2); cp -r $$(echo $${i} | cut -d: -f1) $(OAPI_TMP_DIR)/$$(echo $${i} | cut -d: -f2); done
 	mkdir -p $(OAPI_TMP_DIR)/policies
 	for i in $$( find $(POLICIES_DIR) -name '*.yaml' | grep '/api/'); do DIR=$(OAPI_TMP_DIR)/policies/$$(echo $${i} | awk -F/ '{print $$(NF-3)}'); mkdir -p $${DIR}; cp $${i} $${DIR}/$$(echo $${i} | awk -F/ '{print $$(NF)}'); done
-	ls -R $(OAPI_TMP_DIR)
+ifdef BASE_API
+	cp $(BASE_API) $(OAPI_TMP_DIR)/
+	docker run -v $$PWD/$(dir $(BASE_API)):/base -v $(OAPI_TMP_DIR):/specs ghcr.io/kumahq/openapi-tool:pr-31 generate /base/$(notdir $(BASE_API)) '/specs/**/*.yaml' > $@
+else
 	docker run -v $(OAPI_TMP_DIR):/specs ghcr.io/kumahq/openapi-tool:pr-31 generate '/specs/**/*.yaml' > $@
+endif
