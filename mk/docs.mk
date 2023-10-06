04c4f3b50409ee1e436f86587b56a3912afe83ff
Charly Molter <charly.molter@konghq.com>
2023-10-06 13:37:59 +0200

    troubleshooting
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- mk/docs.mk
+++ mk/docs.mk
@@ -60,8 +60,8 @@ docs/generated/openapi.yaml:
 	mkdir -p $(OAPI_TMP_DIR)/policies
 	for i in $$( find $(POLICIES_DIR) -name '*.yaml' | grep '/api/'); do DIR=$(OAPI_TMP_DIR)/policies/$$(echo $${i} | awk -F/ '{print $$(NF-3)}'); mkdir -p $${DIR}; cp $${i} $${DIR}/$$(echo $${i} | awk -F/ '{print $$(NF)}'); done
 ifdef BASE_API
-	cp $(BASE_API) $(OAPI_TMP_DIR)/
-	docker run --rm -v $$PWD/$(dir $(BASE_API)):/base -v $(OAPI_TMP_DIR):/specs ghcr.io/kumahq/openapi-tool:pr-31 generate /base/$(notdir $(BASE_API)) '/specs/**/*.yaml' > $@
+	docker run --rm -v $$PWD/$(dir $(BASE_API)):/base -v $(OAPI_TMP_DIR):/specs ghcr.io/kumahq/openapi-tool:pr-31 generate /base/$(notdir $(BASE_API)) '/specs/**/*.yaml'  '!/specs/kuma/**' > $@
 else
-	docker run --rm -v $(OAPI_TMP_DIR):/specs ghcr.io/kumahq/openapi-tool:pr-31 generate '/specs/**/*.yaml'
+	docker run --rm -v $(OAPI_TMP_DIR):/specs alpine ls -R '/specs/'
+#	docker run --rm -v $(OAPI_TMP_DIR):/specs ghcr.io/kumahq/openapi-tool:pr-31 generate '/specs/**/*.yaml' > $@
 endif
