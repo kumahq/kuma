4216f715a34f355d353da78f1fb72bc5ebcce00b
slonka <slonka@users.noreply.github.com>
2023-10-11 14:00:16 +0200

    chore(deps): update dev.mk to use go.mod version
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- mk/dev.mk
+++ mk/dev.mk
@@ -26,7 +26,7 @@ CI_TOOLS_BIN_DIR=$(CI_TOOLS_DIR)/bin
 # Change here and `make check` ensures these are used for CI
 K8S_MIN_VERSION = v1.20.15-k3s1
 K8S_MAX_VERSION = v1.26.1-k3s1
-GO_VERSION := 1.21.1
+export GO_VERSION=$(shell go mod edit -json | jq -r .Go)
 GOOS := $(shell go env GOOS)
 GOARCH := $(shell go env GOARCH)
 
