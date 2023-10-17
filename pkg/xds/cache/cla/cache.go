9eecee5a1511928e2d9a9fdfe13d60d0db9e2ab0
Lukasz Dziedziak <lukidzi@gmail.com>
2023-10-17 12:28:59 +0200

    feat(kuma-cp): turn off cla cache
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/xds/cache/cla/cache.go
+++ pkg/xds/cache/cla/cache.go
@@ -2,7 +2,6 @@ package cla
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"google.golang.org/protobuf/proto"
@@ -11,7 +10,6 @@ import (
 	"github.com/kumahq/kuma/pkg/core/xds"
 	"github.com/kumahq/kuma/pkg/metrics"
 	"github.com/kumahq/kuma/pkg/xds/cache/once"
-	"github.com/kumahq/kuma/pkg/xds/cache/sha256"
 	envoy_common "github.com/kumahq/kuma/pkg/xds/envoy"
 	envoy_endpoints "github.com/kumahq/kuma/pkg/xds/envoy/endpoints"
 )
@@ -37,30 +35,26 @@ func NewCache(
 }
 
 func (c *Cache) GetCLA(ctx context.Context, meshName, meshHash string, cluster envoy_common.Cluster, apiVersion xds.APIVersion, endpointMap xds.EndpointMap) (proto.Message, error) {
-	key := sha256.Hash(fmt.Sprintf("%s:%s:%s:%s", apiVersion, meshName, cluster.Hash(), meshHash))
+	matchTags := map[string]string{}
+	for tag, val := range cluster.Tags() {
+		if tag != mesh_proto.ServiceTag {
+			matchTags[tag] = val
+		}
+	}
 
-	elt, err := c.cache.GetOrRetrieve(ctx, key, once.RetrieverFunc(func(ctx context.Context, key string) (interface{}, error) {
-		matchTags := map[string]string{}
-		for tag, val := range cluster.Tags() {
-			if tag != mesh_proto.ServiceTag {
-				matchTags[tag] = val
+	// For the majority of cases we don't have custom tags, we can just take a slice
+	endpoints := endpointMap[cluster.Service()]
+	if len(matchTags) > 0 {
+		endpoints = []xds.Endpoint{}
+		for _, endpoint := range endpointMap[cluster.Service()] {
+			if endpoint.ContainsTags(matchTags) {
+				endpoints = append(endpoints, endpoint)
 			}
 		}
-
-		// For the majority of cases we don't have custom tags, we can just take a slice
-		endpoints := endpointMap[cluster.Service()]
-		if len(matchTags) > 0 {
-			endpoints = []xds.Endpoint{}
-			for _, endpoint := range endpointMap[cluster.Service()] {
-				if endpoint.ContainsTags(matchTags) {
-					endpoints = append(endpoints, endpoint)
-				}
-			}
-		}
-		return envoy_endpoints.CreateClusterLoadAssignment(cluster.Name(), endpoints, apiVersion)
-	}))
+	}
+	elt, err := envoy_endpoints.CreateClusterLoadAssignment(cluster.Name(), endpoints, apiVersion)
 	if err != nil {
 		return nil, err
 	}
-	return elt.(proto.Message), nil
+	return elt, nil
 }
