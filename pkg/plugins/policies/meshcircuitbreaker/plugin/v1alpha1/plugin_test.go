fda0b66f28b9cd2e39a80431d476c2ca81f33f27
Mike Beaumont <mjboamail@gmail.com>
2023-10-23 10:13:57 +0200

    test(MeshCircuitBreaker): adjust for new cluster names
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- pkg/plugins/policies/meshcircuitbreaker/plugin/v1alpha1/plugin_test.go
+++ pkg/plugins/policies/meshcircuitbreaker/plugin/v1alpha1/plugin_test.go
@@ -378,9 +378,9 @@ var _ = Describe("MeshCircuitBreaker", func() {
 					Resource: test_xds.ClusterWithName("other-service"),
 				},
 				{
-					Name:     "other-service-_0_",
+					Name:     "other-service-5ab6003f",
 					Origin:   generator.OriginOutbound,
-					Resource: test_xds.ClusterWithName("other-service-_0_"),
+					Resource: test_xds.ClusterWithName("other-service-5ab6003f"),
 				},
 			},
 			toRules: core_rules.ToRules{
