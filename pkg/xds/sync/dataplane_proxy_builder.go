31492bef0853f066b4eb72225bd08db49e0431c8
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2023-10-27 13:49:16 +0200

    static reachable services takes precedence
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- pkg/xds/sync/dataplane_proxy_builder.go
+++ pkg/xds/sync/dataplane_proxy_builder.go
@@ -120,17 +120,21 @@ func (p *DataplaneProxyBuilder) resolveVIPOutbounds(meshContext xds_context.Mesh
 	var outbounds []*mesh_proto.Dataplane_Networking_Outbound
 	for _, outbound := range meshContext.VIPOutbounds {
 		service := outbound.GetService()
-		if len(reachableServices) != 0 && !reachableServices[service] {
-			// ignore VIP outbound if reachableServices is defined and not specified
-			// Reachable services takes precedence over reachable services graph.
-			continue
+		if len(reachableServices) != 0 {
+			if !reachableServices[service] {
+				// ignore VIP outbound if reachableServices is defined and not specified
+				// Reachable services takes precedence over reachable services graph.
+				continue
+			}
+		} else {
+			// static reachable services takes precedence over the graph
+			if !xds_context.CanReachFromAny(meshContext.ReachableServicesGraph, dpTagSets, outbound.Tags) {
+				continue
+			}
 		}
 		if dataplane.UsesInboundInterface(net.ParseIP(outbound.Address), outbound.Port) {
 			// Skip overlapping outbound interface with inbound.
 			// This may happen for example with Headless service on Kubernetes (outbound is a PodIP not ClusterIP, so it's the same as inbound).
-			continue
-		}
-		if !xds_context.CanReachFromAny(meshContext.ReachableServicesGraph, dpTagSets, outbound.Tags) {
 			continue
 		}
 		outbounds = append(outbounds, outbound)
