999eb00acd415d9426567b0e24cc600011674ee7
Bart Smykla <bartek@smykla.com>
2023-10-30 09:43:36 +0100

    chore: ignore getting kube hosts for ExternalName
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/plugins/runtime/k8s/controllers/kube_hosts_converter.go
+++ pkg/plugins/runtime/k8s/controllers/kube_hosts_converter.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 
+	kube_core "k8s.io/api/core/v1"
 	kube_client "sigs.k8s.io/controller-runtime/pkg/client"
 
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
@@ -31,6 +32,22 @@ func KubeHosts(
 		if err != nil {
 			converterLog.Error(err, "could not get K8S Service for service tag")
 			continue // one invalid Dataplane definition should not break the entire mesh
+		}
+
+		// Services of ExternalName type should not have any selectors.
+		// Kubernetes does not validate this, so in rare cases, a service of
+		// ExternalName type could point to a workload inside the mesh. If this
+		// happens, we will add the service to the VIPs config map, but we will
+		// not be able to obtain its IP address. As a result, the key in the map
+		// will be incorrect (e.g., "1:"). We do not currently support
+		// ExternalName services, so we can safely skip them from processing.
+		if service.Spec.Type == kube_core.ServiceTypeExternalName {
+			converterLog.V(1).Info(
+				"ignoring hostnames for unsupported ExternalName service",
+				"name", service.GetName(),
+				"namespace", service.GetNamespace(),
+			)
+			continue
 		}
 
 		// Do not generate outbounds for service-less
---
+++ test/e2e_env/kubernetes/externalname-services/externalname-services.go
@@ -0,0 +1,64 @@
+package externalservices
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	. "github.com/kumahq/kuma/test/framework"
+	"github.com/kumahq/kuma/test/framework/deployments/testserver"
+	"github.com/kumahq/kuma/test/framework/envs/kubernetes"
+)
+
+func ExternalNameServices() {
+	meshName := "externalname-services"
+	systemNamespace := "externalname-services-system"
+	namespace := "externalname-services"
+
+	externalNameService := fmt.Sprintf(`
+apiVersion: v1
+kind: Service
+metadata:
+  name: externalname-service
+  namespace: %s
+spec:
+  type: ExternalName
+  externalName: foo.bar
+  ports: 
+  - appProtocol: tcp
+    port: 3000
+    protocol: TCP
+    targetPort: 3000
+  selector:
+    app: test-server
+`, namespace)
+
+	BeforeAll(func() {
+		Expect(NewClusterSetup().
+			Install(MeshKubernetes(meshName)).
+			Install(Namespace(systemNamespace)).
+			Install(NamespaceWithSidecarInjection(namespace)).
+			Install(testserver.Install(
+				testserver.WithName("test-server"),
+				testserver.WithNamespace(namespace),
+			)).
+			Setup(kubernetes.Cluster),
+		).To(Succeed())
+	})
+
+	E2EAfterAll(func() {
+		Expect(kubernetes.Cluster.TriggerDeleteNamespace(namespace)).To(Succeed())
+		Expect(kubernetes.Cluster.TriggerDeleteNamespace(systemNamespace)).To(Succeed())
+		Expect(kubernetes.Cluster.DeleteMesh(meshName)).To(Succeed())
+	})
+
+	It("should route to external-service", func() {
+		// when
+		Expect(kubernetes.Cluster.Install(YamlK8s(externalNameService))).To(Succeed())
+
+		// then
+		Consistently(kubernetes.Cluster.GetKumaCPLogs, "15s", "1s").
+			ShouldNot(ContainSubstring("could not parse hostname entry"))
+	})
+}
