a114efba8a0cea0a2a24f2d1946732c1f9bb088a
Marcin Skalski <skalskimarcin33@gmail.com>
2023-11-09 14:53:36 +0100

    feat(kuma-cp): review
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- pkg/plugins/policies/core/xds/endpoints.go
+++ pkg/plugins/policies/core/xds/endpoints.go
@@ -4,9 +4,9 @@ import (
 	clusterv3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
 	endpointv3 "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"
 	envoy_resource "github.com/envoyproxy/go-control-plane/pkg/resource/v3"
-	"github.com/kumahq/kuma/pkg/plugins/runtime/gateway/metadata"
 
 	"github.com/kumahq/kuma/pkg/core/xds"
+	"github.com/kumahq/kuma/pkg/plugins/runtime/gateway/metadata"
 	"github.com/kumahq/kuma/pkg/xds/envoy/tags"
 	"github.com/kumahq/kuma/pkg/xds/generator"
 	"github.com/kumahq/kuma/pkg/xds/generator/egress"
