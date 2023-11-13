07fcbed400fad5113c3d2140b337bb657a88e091
Lukasz Dziedziak <lukidzi@gmail.com>
2023-11-13 06:07:58 +0100

    feat(kuma-cp): map BadRequest error to 400 (#8312)
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/core/rest/errors/error_handler.go
+++ pkg/core/rest/errors/error_handler.go
@@ -33,7 +33,7 @@ func HandleError(ctx context.Context, response *restful.Response, err error, tit
 			Title:  title,
 			Detail: "Not found",
 		}
-	case errors.Is(err, &rest.InvalidResourceError{}) || errors.Is(err, &registry.InvalidResourceTypeError{}) || errors.Is(err, &store.PreconditionError{}):
+	case errors.Is(err, &rest.InvalidResourceError{}), errors.Is(err, &registry.InvalidResourceTypeError{}), errors.Is(err, &store.PreconditionError{}), errors.Is(err, &BadRequest{}):
 		kumaErr = &types.Error{
 			Status: 400,
 			Title:  "Bad Request",
