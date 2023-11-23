71cbc3b732a8e93b6ee12b9ead49031600bfeba4
Charly Molter <charly.molter@konghq.com>
2023-11-23 17:01:48 +0100

    sync version
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- mk/check.mk
+++ mk/check.mk
@@ -33,6 +33,7 @@ golangci-lint-fmt:
 .PHONY: fmt/ci
 fmt/ci:
 	$(CI_TOOLS_BIN_DIR)/yq -i '.parameters.go_version.default = "$(GO_VERSION)" | .parameters.first_k8s_version.default = "$(K8S_MIN_VERSION)" | .parameters.last_k8s_version.default = "$(K8S_MAX_VERSION)"' .circleci/config.yml
+	$(CI_TOOLS_BIN_DIR)/yq -i '.env.K8S_MIN_VERSION = "$(K8S_MIN_VERSION)" | .env.K8S_MAX_VERSION = "$(K8S_MAX_VERSION)"' .github/workflows/build-test-distribute.yaml
 	find .github/workflows -name '*ml' | xargs -n 1 $(CI_TOOLS_BIN_DIR)/yq -i '(.jobs.* | select(. | has("steps")) | .steps[] | select(.uses == "golangci/golangci-lint-action*") | .with.version) |= "$(GOLANGCI_LINT_VERSION)"'
 
 .PHONY: helm-lint
