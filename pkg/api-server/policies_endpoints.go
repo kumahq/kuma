34778a163c9e432a7d059d6941a5782fae8c3092
Charly Molter <charly.molter@konghq.com>
2023-12-04 15:26:47 +0100

    feat(api-server): add /_resources endpoint
    Similar to the previous /policies but does it for all resources.
    This should enable us to more easily write automation around resources
    that are not limited to policies.
    
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- pkg/api-server/policies_endpoints.go
+++ pkg/api-server/policies_endpoints.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/emicklei/go-restful/v3"
 
+	api_types "github.com/kumahq/kuma/api/openapi/types"
+	api_common "github.com/kumahq/kuma/api/openapi/types/common"
 	"github.com/kumahq/kuma/pkg/api-server/types"
 	"github.com/kumahq/kuma/pkg/core/resources/model"
 )
 
-func addPoliciesWsEndpoints(ws *restful.WebService, federatedZone bool, readOnly bool, defs []model.ResourceTypeDescriptor) {
+func addPoliciesWsEndpoints(ws *restful.WebService, isGlobal bool, federatedZone bool, readOnly bool, defs []model.ResourceTypeDescriptor) {
 	ws.Route(ws.GET("/policies").To(func(req *restful.Request, resp *restful.Response) {
 		response := types.PoliciesResponse{}
 		for _, def := range defs {
@@ -33,7 +35,35 @@ func addPoliciesWsEndpoints(ws *restful.WebService, federatedZone bool, readOnly
 		})
 
 		if err := resp.WriteAsJson(response); err != nil {
-			log.Error(err, "Could not write the index response")
+			log.Error(err, "Could not write the response")
+		}
+	}))
+	ws.Route(ws.GET("/_resources").To(func(req *restful.Request, resp *restful.Response) {
+		response := api_types.ResourceTypeDescriptionList{}
+		for _, def := range defs {
+			td := api_common.ResourceTypeDescription{
+				Name:                string(def.Name),
+				ReadOnly:            readOnly || federatedZone || def.ReadOnly,
+				Path:                def.WsPath,
+				SingularDisplayName: def.SingularDisplayName,
+				PluralDisplayName:   def.PluralDisplayName,
+				Scope:               api_common.ResourceTypeDescriptionScope(def.Scope),
+				IncludeInDump:       isGlobal || def.DumpForGlobal,
+			}
+			if def.IsPolicy {
+				td.Policy = &api_common.PolicyDescription{
+					HasToTargetRef:   def.HasToTargetRef,
+					HasFromTargetRef: def.HasFromTargetRef,
+					IsTargetRef:      def.IsTargetRefBased,
+				}
+			}
+			response.Resources = append(response.Resources, td)
+		}
+		sort.SliceStable(response.Resources, func(i, j int) bool {
+			return response.Resources[i].Name < response.Resources[j].Name
+		})
+		if err := resp.WriteAsJson(response); err != nil {
+			log.Error(err, "Could not write the response")
 		}
 	}))
 }
