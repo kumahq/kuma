b81edc42482c77b1c20791a3804ffc4011fe7689
Bart Smykla <bartek@smykla.com>
2023-12-05 08:40:27 +0100

    chore(MeshTimeout): improve policy's openapi schema (#8532)
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/plugins/policies/meshtimeout/api/v1alpha1/meshtimeout.go
+++ pkg/plugins/policies/meshtimeout/api/v1alpha1/meshtimeout.go
@@ -11,7 +11,7 @@ import (
 type MeshTimeout struct {
 	// TargetRef is a reference to the resource the policy takes an effect on.
 	// The resource could be either a real store object or virtual resource
-	// defined inplace.
+	// defined in-place.
 	TargetRef common_api.TargetRef `json:"targetRef"`
 	// To list makes a match between the consumed services and corresponding configurations
 	To []To `json:"to,omitempty"`
@@ -39,11 +39,13 @@ type From struct {
 
 type Conf struct {
 	// ConnectionTimeout specifies the amount of time proxy will wait for an TCP connection to be established.
-	// Default value is 5 seconds. Cannot be set to 0.
+	// Cannot be set to "0".
+	// +kubebuilder:default="5s"
 	ConnectionTimeout *k8s.Duration `json:"connectionTimeout,omitempty"`
 	// IdleTimeout is defined as the period in which there are no bytes sent or received on connection
-	// Setting this timeout to 0 will disable it. Be cautious when disabling it because
-	// it can lead to connection leaking. Default value is 1h.
+	// Setting this timeout to "0s" will disable it. Be cautious when disabling it because
+	// it can lead to connection leaking.
+	// +kubebuilder:default="1h"
 	IdleTimeout *k8s.Duration `json:"idleTimeout,omitempty"`
 	// Http provides configuration for HTTP specific timeouts
 	Http *Http `json:"http,omitempty"`
@@ -52,17 +54,19 @@ type Conf struct {
 type Http struct {
 	// RequestTimeout The amount of time that proxy will wait for the entire request to be received.
 	// The timer is activated when the request is initiated, and is disarmed when the last byte of the request is sent,
-	// OR when the response is initiated. Setting this timeout to 0 will disable it.
-	// Default is 15s.
+	// OR when the response is initiated. Setting this timeout to "0s" will disable it.
+	// +kubebuilder:default="15s"
 	RequestTimeout *k8s.Duration `json:"requestTimeout,omitempty"`
 	// StreamIdleTimeout is the amount of time that proxy will allow a stream to exist with no activity.
-	// Setting this timeout to 0 will disable it. Default is 30m
+	// Setting this timeout to "0s" will disable it.
+	// +kubebuilder:default="30s"
 	StreamIdleTimeout *k8s.Duration `json:"streamIdleTimeout,omitempty"`
 	// MaxStreamDuration is the maximum time that a stream’s lifetime will span.
-	// Setting this timeout to 0 will disable it. Disabled by default.
+	// Setting this timeout to "0s" will disable it.
+	// +kubebuilder:default="0s"
 	MaxStreamDuration *k8s.Duration `json:"maxStreamDuration,omitempty"`
 	// MaxConnectionDuration is the time after which a connection will be drained and/or closed,
-	// starting from when it was first established. Setting this timeout to 0 will disable it.
-	// Disabled by default.
+	// starting from when it was first established. Setting this timeout to "0s" will disable it.
+	// +kubebuilder:default="0s"
 	MaxConnectionDuration *k8s.Duration `json:"maxConnectionDuration,omitempty"`
 }
