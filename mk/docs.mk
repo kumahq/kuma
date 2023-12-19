6ec581c901764d79a3e3fc2c6c0d46f7b5b98df8
Charly Molter <charly.molter@konghq.com>
2023-12-19 16:55:11 +0100

    ci(make): remove old version of docs
    These are not used anymore (we only use what's in docs/generated/raw)
    
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- mk/docs.mk
+++ mk/docs.mk
@@ -6,32 +6,11 @@ clean/docs:
 	rm -rf docs/generated
 
 .PHONY: docs
-docs: docs/generated/cmd docs/generated/kuma-cp.md docs/generated/resources helm-docs docs/generated/raw docs/generated/openapi.yaml ## Dev: Generate local documentation
+docs: helm-docs docs/generated/raw docs/generated/openapi.yaml ## Dev: Generate local documentation
 
 .PHONY: helm-docs
 helm-docs: ## Dev: Runs helm-docs generator
 	$(HELM_DOCS) -s="file" --chart-search-root=./deployments/charts
-
-DOCS_CMD_FORMAT ?= markdown
-.PHONY: docs/generated/cmd
-docs/generated/cmd:
-	DESTDIR=$@ FORMAT=$(DOCS_CMD_FORMAT) go run $(TOOLS_DIR)/docs/generate.go
-
-.PHONY: docs/generated/resources
-docs/generated/resources: ## Generate Mesh API reference
-	mkdir -p $@ && $(PROTOC) \
-		--kumadoc_out=$@ \
-		--plugin=protoc-gen-kumadoc=$(PROTOC_GEN_KUMADOC) \
-		$(DOCS_PROTOS)
-
-.PHONY: docs/generated/kuma-cp.md
-docs/generated/kuma-cp.md: ## Generate Mesh API reference
-	@mkdir -p $(@D)
-	@echo "# Control-Plane configuration" > $@
-	@echo "Here are all options to configure the control-plane:" >> $@
-	@echo '```yaml' >> $@
-	@cat $(DOCS_CP_CONFIG) >> $@
-	@echo '```' >> $@
 
 .PHONY: docs/generated/raw
 docs/generated/raw:
