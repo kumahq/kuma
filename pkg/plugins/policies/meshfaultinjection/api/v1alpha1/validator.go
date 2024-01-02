03f213a8850a27225c2e27e0802ae3f082817c90
Mike Beaumont <mjboamail@gmail.com>
2024-01-02 11:15:23 +0100

    chore(policies): use new flag for gateway listener tags
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- pkg/plugins/policies/meshfaultinjection/api/v1alpha1/validator.go
+++ pkg/plugins/policies/meshfaultinjection/api/v1alpha1/validator.go
@@ -25,6 +25,7 @@ func validateTop(targetRef common_api.TargetRef) validators.ValidationError {
 			common_api.MeshService,
 			common_api.MeshServiceSubset,
 		},
+		GatewayListenerTagsAllowed: true,
 	})
 	return targetRefErr
 }
--- pkg/plugins/policies/meshhttproute/api/v1alpha1/validation.go
+++ pkg/plugins/policies/meshhttproute/api/v1alpha1/validation.go
@@ -28,6 +28,7 @@ func validateTop(targetRef common_api.TargetRef) validators.ValidationError {
 			common_api.MeshService,
 			common_api.MeshServiceSubset,
 		},
+		GatewayListenerTagsAllowed: true,
 	})
 }
 
