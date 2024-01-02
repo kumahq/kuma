e81d6618620a41d0b02d73b5afc38a54f5d19c82
Mike Beaumont <mjboamail@gmail.com>
2024-01-02 14:01:49 +0100

    fix(MeshCircuitBreaker): allow MeshGateway through validation (#8732)
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- pkg/plugins/policies/meshcircuitbreaker/api/v1alpha1/validator.go
+++ pkg/plugins/policies/meshcircuitbreaker/api/v1alpha1/validator.go
@@ -26,6 +26,7 @@ func validateTop(targetRef common_api.TargetRef) validators.ValidationError {
 			common_api.Mesh,
 			common_api.MeshSubset,
 			common_api.MeshService,
+			common_api.MeshGateway,
 			common_api.MeshServiceSubset,
 		},
 	})
