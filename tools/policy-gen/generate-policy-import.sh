2d1134283e80144f44bea0f1445caf30550cafc7
slonka <slonka@users.noreply.github.com>
2024-01-05 14:59:39 +0100

    feat(toggles): fix policy file generation
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- tools/policy-gen/generate-policy-import.sh
+++ tools/policy-gen/generate-policy-import.sh
@@ -5,17 +5,33 @@ GO_MODULE=$1
 
 IMPORTS_FILE="pkg/plugins/policies/imports.go"
 
-imports=$(for i in "${@:2}"; do [[ -f pkg/plugins/policies/${i}/zz_generated.plugin.go ]] && echo "_ \"${GO_MODULE}/pkg/plugins/policies/${i}\""; done)
+imports=$(for i in "${@:2}"; do [[ -f pkg/plugins/policies/${i}/zz_generated.plugin.go ]] && echo "\"${GO_MODULE}/pkg/plugins/policies/${i}\""; done)
 if [[ $imports == "" ]]; then
   rm -f "${IMPORTS_FILE}"
   exit 0
 fi
+
+mappings=$(for i in "${@:2}"; do [[ -f pkg/plugins/policies/${i}/zz_generated.plugin.go ]] && echo "\"${i}\": ${i}.InitPlugin,"; done)
 
 echo "package policies
 
 import (
 $imports
 )
+
+var nameToModule = map[string]func(bool){
+  $mappings
+}
+
+
+func InitPolicies(enabledPluginPolicies []string) {
+	for _, policy := range enabledPluginPolicies {
+		initializer, ok := nameToModule[policy]
+		if ok {
+			initializer(true)
+		}
+	}
+}
 " > "${IMPORTS_FILE}"
 
 gofmt -w "${IMPORTS_FILE}"
