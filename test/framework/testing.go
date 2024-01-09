feb5707e30e7561f836a2c84e1ebfc7c66bc390f
Charly Molter <charly.molter@konghq.com>
2024-01-09 18:29:14 +0100

    test(e2e): avoid flake on postgres tests
    Looks like in some cases the containers are not fully
    removed before we create the new ones. We make sure to use
    different container names to avoid these kind of flakes
    
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- test/framework/testing.go
+++ test/framework/testing.go
@@ -3,6 +3,7 @@ package framework
 import (
 	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/onsi/ginkgo/v2"
+	"k8s.io/apimachinery/pkg/util/rand"
 )
 
 type TestingT struct {
@@ -19,6 +20,10 @@ func (i *TestingT) Helper() {
 
 func (i *TestingT) Name() string {
 	return i.desc.FullText()
+}
+
+func (i *TestingT) Hash() string {
+	return rand.SafeEncodeString(i.Name())
 }
 
 // Logf logs a test progress message.
