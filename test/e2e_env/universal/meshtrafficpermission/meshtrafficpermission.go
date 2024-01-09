7d05d533a306a29ed3389b067a048c43aab076ac
Bart Smykla <bartek@smykla.com>
2024-01-09 13:22:53 +0100

    chore: change status code 503 to 403 in e2e test
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- test/e2e_env/universal/meshtrafficpermission/meshtrafficpermission.go
+++ test/e2e_env/universal/meshtrafficpermission/meshtrafficpermission.go
@@ -57,7 +57,7 @@ func MeshTrafficPermissionUniversal() {
 				universal.Cluster, "demo-client", "test-server.mesh",
 			)
 			g.Expect(err).ToNot(HaveOccurred())
-			g.Expect(response.ResponseCode).To(Equal(503))
+			g.Expect(response.ResponseCode).To(Equal(403))
 		}).Should(Succeed())
 	}
 
