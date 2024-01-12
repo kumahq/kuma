9a5910f3ccc3cdbc1e286bab5de7231db863e0d0
slonka <slonka@users.noreply.github.com>
2024-01-12 10:09:53 +0100

    feat(toggles): make test pass
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- pkg/config/loader_test.go
+++ pkg/config/loader_test.go
@@ -709,6 +709,10 @@ proxy:
     globalDownstreamMaxConnections: 1
 eventBus:
   bufferSize: 30
+policies:
+  pluginPoliciesEnabled:
+    - meshaccesslog
+    - meshcircuitbreaker
 tracing:
   openTelemetry:
     enabled: true
@@ -975,6 +979,7 @@ tracing:
 				"KUMA_TRACING_OPENTELEMETRY_ENDPOINT":                                                      "otel-collector:4317",
 				"KUMA_TRACING_OPENTELEMETRY_ENABLED":                                                       "true",
 				"KUMA_EVENT_BUS_BUFFER_SIZE":                                                               "30",
+				"KUMA_PLUGIN_POLICIES_ENABLED":                                                             "meshaccesslog,meshcircuitbreaker",
 			},
 			yamlFileConfig: "",
 		}),
