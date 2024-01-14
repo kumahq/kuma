82af71906ada79787f2750838a3537ed7d4220ce
slonka <slonka@users.noreply.github.com>
2024-01-14 09:38:42 +0100

    feat(toggles): change to plural
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- tools/policy-gen/generate-policy-config.sh
+++ tools/policy-gen/generate-policy-config.sh
@@ -4,7 +4,16 @@ set -e
 
 POLICIES_FILE="pkg/config/plugins/policies/policies.go"
 
-policies=$(for i in "${@:1}"; do [[ -f pkg/plugins/policies/${i}/zz_generated.plugin.go ]] && echo "\"${i}\","; done)
+policies=$(for i in "${@:1}"; do
+  if [[ -f pkg/plugins/policies/${i}/zz_generated.plugin.go ]]; then
+    policy_dir="pkg/plugins/policies/${i}"
+    policy_crd_dir="${policy_dir}/k8s/crd"
+    policy_crd_file="$(find "${policy_crd_dir}" -type f)"
+    plural=$(yq e '.spec.names.plural' "$policy_crd_file")
+    echo "\"$plural\","
+  fi
+done)
+
 if [[ $policies == "" ]]; then
   rm -f "${POLICIES_FILE}"
   exit 0
