fdd9b052240a3c47b51abce675bd30bb3388a8df
Lukasz Dziedziak <lukidzi@gmail.com>
2024-01-18 10:40:38 -0500

    feat(policy): add possibility to target only gateways/sidecars (#8868)
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/core/resources/apis/mesh/validators.go
+++ pkg/core/resources/apis/mesh/validators.go
@@ -357,10 +357,12 @@ func ValidateTargetRef(
 		err.Add(validateName(ref.Name))
 		err.Add(disallowedField("mesh", ref.Mesh, ref.Kind))
 		err.Add(disallowedField("tags", ref.Tags, ref.Kind))
+		err.Add(disallowedField("proxyTypes", ref.ProxyTypes, ref.Kind))
 	case common_api.MeshServiceSubset, common_api.MeshGateway:
 		err.Add(requiredField("name", ref.Name, ref.Kind))
 		err.Add(validateName(ref.Name))
 		err.Add(disallowedField("mesh", ref.Mesh, ref.Kind))
+		err.Add(disallowedField("proxyTypes", ref.ProxyTypes, ref.Kind))
 		err.Add(ValidateTags(validators.RootedAt("tags"), ref.Tags, ValidateTagsOpts{}))
 		if ref.Kind == common_api.MeshGateway && len(ref.Tags) > 0 && !opts.GatewayListenerTagsAllowed {
 			err.Add(disallowedField("tags", ref.Tags, ref.Kind))
@@ -383,7 +385,7 @@ func validateName(value string) validators.ValidationError {
 	return err
 }
 
-func disallowedField[T ~string | ~map[string]string](
+func disallowedField[T ~string | ~map[string]string | ~[]common_api.TargetRefProxyType](
 	name string,
 	value T,
 	kind common_api.TargetRefKind,
@@ -411,11 +413,13 @@ func requiredField[T ~string | ~map[string]string](
 	return err
 }
 
-func isSet[T ~string | ~map[string]string](value T) bool {
+func isSet[T ~string | ~map[string]string | ~[]common_api.TargetRefProxyType](value T) bool {
 	switch v := any(value).(type) {
 	case string:
 		return v != ""
 	case map[string]string:
+		return len(v) > 0
+	case []common_api.TargetRefProxyType:
 		return len(v) > 0
 	default:
 		return false
