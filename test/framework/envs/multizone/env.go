2aac9370ce8f5d7b63e876d34cc895b3019cf641
Lukasz Dziedziak <lukidzi@gmail.com>
2024-01-22 08:17:50 -0500

    fix test
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- test/framework/envs/multizone/env.go
+++ test/framework/envs/multizone/env.go
@@ -54,9 +54,7 @@ type State struct {
 func SetupAndGetState() []byte {
 	Global = NewUniversalCluster(NewTestingT(), Kuma3, Silent)
 	E2EDeferCleanup(Global.DismissCluster) // clean up any containers if needed
-	globalOptions := append([]framework.KumaDeploymentOption{
-		framework.WithEnv("KUMA_DEFAULTS_CREATE_MESH_ROUTING_RESOURCES", "false"),
-	},
+	globalOptions := append([]framework.KumaDeploymentOption{},
 		framework.KumaDeploymentOptionsFromConfig(framework.Config.KumaCpConfig.Multizone.Global)...)
 	Expect(Global.Install(Kuma(core.Global, globalOptions...))).To(Succeed())
 
