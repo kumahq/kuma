35d07d65df0c41d5b8276e73ee5aff95639162bc
Mike Beaumont <mjboamail@gmail.com>
2024-01-23 13:28:51 +0100

    test(gateway): add unit tests
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- pkg/plugins/runtime/gateway/match/hostname_match_test.go
+++ pkg/plugins/runtime/gateway/match/hostname_match_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 var _ = Describe("Hostname matching", func() {
-	DescribeTable("should match",
+	DescribeTable("Matches should match",
 		func(hostname string, candidate string) {
 			Expect(match.Hostnames(hostname, candidate)).To(BeTrue())
 		},
@@ -21,7 +21,7 @@ var _ = Describe("Hostname matching", func() {
 		Entry("larger exact matches smaller wild", "foo.example.com", "*.com"),
 		Entry("smaller wild matches larger exact", "foo.examples.com", "*.com"),
 	)
-	DescribeTable("should not match",
+	DescribeTable("Matches should not match",
 		func(hostname string, candidate string) {
 			Expect(match.Hostnames(hostname, candidate)).To(BeFalse())
 		},
@@ -29,5 +29,18 @@ var _ = Describe("Hostname matching", func() {
 		Entry("first wild with rest unequal", "*.example.com", "foo.examples.com"),
 		Entry("second wild with rest unequal", "foo.example.com", "*.examples.com"),
 		Entry("both wild with rest unequal", "*.example.com", "*.examples.com"),
+	)
+	DescribeTable("Contains",
+		func(hostname string, candidate string, expect bool) {
+			Expect(match.Contains(hostname, candidate)).To(Equal(expect))
+		},
+		Entry("equal exact succeeds", "foo.example.com", "foo.example.com", true),
+		Entry("exact unequal fails", "foo.example.com", "bar.example.com", false),
+		Entry("first wild with rest unequal fails", "*.foo.com", "foo.bar.com", false),
+		Entry("first wild with rest equal succeeds", "*.bar.com", "foo.bar.com", true),
+		Entry("second wild with rest equal fails", "foo.bar.com", "*.bar.com", false),
+		Entry("both wildcards succeeds", "*", "*", true),
+		Entry("larger wildcard contains smaller", "*.com", "*.foo.com", true),
+		Entry("smaller wildcard doesn't contain larger", "*.foo.com", "*.com", false),
 	)
 })
