ce0108ff3084902f5202666847a0ac17fade4a2e
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2024-02-02 14:38:58 +0100

    fix(kubernetes): create builtin CA once (#9124)
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- pkg/plugins/runtime/k8s/controllers/mesh_controller_test.go
+++ pkg/plugins/runtime/k8s/controllers/mesh_controller_test.go
@@ -13,6 +13,7 @@ import (
 	kube_client_fake "sigs.k8s.io/controller-runtime/pkg/client/fake"
 	kube_reconcile "sigs.k8s.io/controller-runtime/pkg/reconcile"
 
+	core_ca "github.com/kumahq/kuma/pkg/core/ca"
 	"github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
 	"github.com/kumahq/kuma/pkg/core/resources/apis/system"
 	resources_manager "github.com/kumahq/kuma/pkg/core/resources/manager"
@@ -20,17 +21,25 @@ import (
 	core_store "github.com/kumahq/kuma/pkg/core/resources/store"
 	secret_cipher "github.com/kumahq/kuma/pkg/core/secrets/cipher"
 	secret_manager "github.com/kumahq/kuma/pkg/core/secrets/manager"
+<<<<<<< HEAD:pkg/plugins/runtime/k8s/controllers/mesh_defaults_controller_test.go
+=======
+	bootstrap_k8s "github.com/kumahq/kuma/pkg/plugins/bootstrap/k8s"
+	ca_builtin "github.com/kumahq/kuma/pkg/plugins/ca/builtin"
+	v1alpha12 "github.com/kumahq/kuma/pkg/plugins/policies/meshretry/api/v1alpha1"
+>>>>>>> 126ed951a (fix(kubernetes): create builtin CA once (#9124)):pkg/plugins/runtime/k8s/controllers/mesh_controller_test.go
 	"github.com/kumahq/kuma/pkg/plugins/resources/k8s"
 	"github.com/kumahq/kuma/pkg/plugins/runtime/k8s/controllers"
 	secrets_k8s "github.com/kumahq/kuma/pkg/plugins/secrets/k8s"
+	"github.com/kumahq/kuma/pkg/test/resources/samples"
 )
 
-var _ = Describe("MeshDefaultsReconciler", func() {
+var _ = Describe("MeshReconciler", func() {
 	var kubeClient kube_client.Client
 	var resourceManager resources_manager.ResourceManager
 	var reconciler kube_reconcile.Reconciler
+	var builtinCaManager core_ca.Manager
 
-	BeforeEach(func() {
+	BeforeAll(func() {
 		kubeClient = kube_client_fake.NewClientBuilder().
 			WithScheme(k8sClientScheme).
 			WithIndex(&kube_core.Secret{}, "type",
@@ -47,35 +56,40 @@ var _ = Describe("MeshDefaultsReconciler", func() {
 
 		resourceManager = resources_manager.NewResourceManager(store)
 		customizableManager := resources_manager.NewCustomizableResourceManager(resourceManager, nil)
-		customizableManager.Customize(
-			system.SecretType,
-			secret_manager.NewSecretManager(
-				secretStore,
-				secret_cipher.None(),
-				secret_manager.ValidateDelete(func(ctx context.Context, secretName string, secretMesh string) error { return nil }),
-				false,
-			),
+		secretManager := secret_manager.NewSecretManager(
+			secretStore,
+			secret_cipher.None(),
+			secret_manager.ValidateDelete(func(ctx context.Context, secretName string, secretMesh string) error { return nil }),
+			false,
 		)
 
-		reconciler = &controllers.MeshDefaultsReconciler{
+		customizableManager.Customize(
+			system.SecretType,
+			secretManager,
+		)
+
+		builtinCaManager = ca_builtin.NewBuiltinCaManager(secretManager)
+		reconciler = &controllers.MeshReconciler{
 			ResourceManager: customizableManager,
 			Log:             logr.Discard(),
 			Extensions:      context.Background(),
+<<<<<<< HEAD:pkg/plugins/runtime/k8s/controllers/mesh_defaults_controller_test.go
+=======
+			K8sStore:        true,
+			SystemNamespace: "kuma-system",
+			CaManagers: core_ca.Managers{
+				"builtin": builtinCaManager,
+			},
+>>>>>>> 126ed951a (fix(kubernetes): create builtin CA once (#9124)):pkg/plugins/runtime/k8s/controllers/mesh_controller_test.go
 		}
 	})
 
 	createMesh := func() {
+		mesh := mesh.NewMeshResource()
+		mesh.Spec = samples.MeshMTLS().Spec
 		Expect(
-			resourceManager.Create(context.Background(), mesh.NewMeshResource(), core_store.CreateByKey("default", core_model.NoMesh)),
+			resourceManager.Create(context.Background(), mesh, core_store.CreateByKey("default", core_model.NoMesh)),
 		).To(Succeed())
-	}
-
-	hasTrafficPermissions := func() bool {
-		trafficPermissions := &mesh.TrafficPermissionResourceList{}
-		Expect(
-			resourceManager.List(context.Background(), trafficPermissions, core_store.ListByMesh("default")),
-		).To(Succeed())
-		return len(trafficPermissions.Items) == 1
 	}
 
 	reconcile := func() {
@@ -87,6 +101,7 @@ var _ = Describe("MeshDefaultsReconciler", func() {
 		Expect(err).ToNot(HaveOccurred())
 	}
 
+<<<<<<< HEAD:pkg/plugins/runtime/k8s/controllers/mesh_defaults_controller_test.go
 	deleteTrafficPermission := func() {
 		Expect(
 			resourceManager.Delete(context.Background(), mesh.NewTrafficPermissionResource(),
@@ -106,5 +121,23 @@ var _ = Describe("MeshDefaultsReconciler", func() {
 
 		reconcile()
 		Expect(hasTrafficPermissions()).To(BeFalse())
+=======
+	Context("on reconcile mesh", func() {
+		BeforeAll(func() {
+			createMesh()
+			reconcile()
+		})
+
+		It("should create a default policy", func() {
+			meshRetries := &v1alpha12.MeshRetryResourceList{}
+			Expect(resourceManager.List(context.Background(), meshRetries, core_store.ListByMesh("default"))).To(Succeed())
+			Expect(meshRetries.Items).To(HaveLen(1))
+		})
+
+		It("should create default CA", func() {
+			_, err := builtinCaManager.GetRootCert(context.Background(), "default", samples.MeshMTLS().Spec.Mtls.Backends[0])
+			Expect(err).ToNot(HaveOccurred())
+		})
+>>>>>>> 126ed951a (fix(kubernetes): create builtin CA once (#9124)):pkg/plugins/runtime/k8s/controllers/mesh_controller_test.go
 	})
-})
+}, Ordered)
