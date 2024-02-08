9ed9f8f851275648e2e1619c406891d3ab5380c5
Charly Molter <charly.molter@konghq.com>
2024-02-08 14:51:07 +0100

    fix(MeshGateway): fix MeshTCPRoute on MeshGateway (#9167)
    - MeshTCPRoute when applied to a MeshGateway would panic
    thus making it unusable. This fixes this and backfills e2e tests
    - Refactor tests to make them easier to maintain
    
    Fix #9154
    
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- test/e2e_env/kubernetes/gateway/gateway.go
+++ test/e2e_env/kubernetes/gateway/gateway.go
@@ -47,6 +47,8 @@ spec:
     - port: 8082
       protocol: HTTP
       hostname: '*'
+    - port: 8083
+      protocol: TCP
 `
 	httpsSecret := func() string {
 		cert, key, err := CreateCertsFor("example.kuma.io")
@@ -65,6 +67,7 @@ data:
 type: system.kuma.io/secret
 `, Config.KumaNamespace, secretData)
 	}
+	var clusterIP string
 
 	BeforeAll(func() {
 		err := NewClusterSetup().
@@ -75,11 +78,28 @@ type: system.kuma.io/secret
 				testserver.WithName("demo-client"),
 				testserver.WithNamespace(clientNamespace),
 			)).
+			Install(testserver.Install(
+				testserver.WithName("echo-server"),
+				testserver.WithMesh(meshName),
+				testserver.WithNamespace(namespace),
+				testserver.WithEchoArgs("echo", "--instance", "echo-server"),
+			)).
 			Install(YamlK8s(httpsSecret())).
 			Install(YamlK8s(meshGateway)).
 			Install(YamlK8s(MkGatewayInstance("simple-gateway", namespace, meshName))).
 			Setup(kubernetes.Cluster)
 		Expect(err).ToNot(HaveOccurred())
+
+		Eventually(func(g Gomega) {
+			var err error
+			clusterIP, err = k8s.RunKubectlAndGetOutputE(
+				kubernetes.Cluster.GetTesting(),
+				kubernetes.Cluster.GetKubectlOptions(namespace),
+				"get", "service", "simple-gateway", "-ojsonpath={.spec.clusterIP}",
+			)
+			g.Expect(err).ToNot(HaveOccurred())
+			g.Expect(clusterIP).ToNot(BeEmpty())
+		}, "30s", "1s").Should(Succeed())
 	})
 
 	E2EAfterAll(func() {
@@ -88,8 +108,14 @@ type: system.kuma.io/secret
 		Expect(kubernetes.Cluster.DeleteMesh(meshName)).To(Succeed())
 	})
 
+<<<<<<< HEAD
 	route := func(name, path, destination string) string {
 		return fmt.Sprintf(`
+=======
+	meshGatewayRoutes := func(name, path, destination string) []string {
+		return []string{
+			fmt.Sprintf(`
+>>>>>>> 67ee1be51 (fix(MeshGateway): fix MeshTCPRoute on MeshGateway (#9167))
 apiVersion: kuma.io/v1alpha1
 kind: MeshGatewayRoute
 metadata:
@@ -122,6 +148,7 @@ spec:
 `, name, path, destination, path, destination)
 	}
 
+<<<<<<< HEAD
 	Context("Mesh service", func() {
 		BeforeAll(func() {
 			err := NewClusterSetup().
@@ -134,8 +161,236 @@ spec:
 				Install(YamlK8s(route("internal-service", "/", "echo-server_simple-gateway_svc_80"))).
 				Setup(kubernetes.Cluster)
 			Expect(err).ToNot(HaveOccurred())
+=======
+	httpRoute := func(name, path, destination string) []string {
+		return []string{
+			fmt.Sprintf(`
+apiVersion: kuma.io/v1alpha1
+kind: MeshHTTPRoute
+metadata:
+  name: %s
+  namespace: %s
+  labels:
+    kuma.io/mesh: simple-gateway
+spec:
+  targetRef:
+    kind: MeshGateway
+    name: simple-gateway
+  to:
+  - targetRef:
+      kind: Mesh
+    rules:
+    - matches:
+      - path:
+          type: PathPrefix
+          value: "%s"
+      default:
+        backendRefs:
+        - kind: MeshService
+          name: "%s"
+  - targetRef:
+      kind: Mesh
+    hostnames:
+    - specific.kuma.io
+    rules:
+    - matches:
+      - path:
+          type: PathPrefix
+          value: "%s"
+      default:
+        filters:
+        - type: RequestHeaderModifier
+          requestHeaderModifier:
+            add:
+            - name: x-specific-hostname-header
+              value: "true"
+        backendRefs:
+        - kind: MeshService
+          name: "%s"
+`, name, Config.KumaNamespace, path, destination, path, destination),
+			fmt.Sprintf(`
+apiVersion: kuma.io/v1alpha1
+kind: MeshHTTPRoute
+metadata:
+  name: %s
+  namespace: %s
+  labels:
+    kuma.io/mesh: simple-gateway
+spec:
+  targetRef:
+    kind: MeshGateway
+    name: simple-gateway
+    tags:
+      hostname: otherexample.kuma.io 
+  to:
+  - targetRef:
+      kind: Mesh
+    rules:
+    - matches:
+      - path:
+          type: PathPrefix
+          value: "%s-specific-listener"
+      default:
+        filters:
+        - type: RequestHeaderModifier
+          requestHeaderModifier:
+            add:
+            - name: x-listener-by-hostname-header
+              value: "true"
+        backendRefs:
+        - kind: MeshService
+          name: "%s"
+`, name+"-hostname-specific", Config.KumaNamespace, path, destination),
+		}
+	}
+
+	basicRouting := func(name string, routeYAMLs []string) {
+		Context(fmt.Sprintf("Mesh service - %s", name), func() {
+			BeforeAll(func() {
+				Expect(NewClusterSetup().
+					Install(YamlK8s(routeYAMLs...)).
+					Setup(kubernetes.Cluster),
+				).To(Succeed())
+			})
+
+			E2EAfterAll(func() {
+				Expect(NewClusterSetup().
+					Install(DeleteYamlK8s(routeYAMLs...)).
+					Setup(kubernetes.Cluster),
+				).To(Succeed())
+			})
+
+			It("should proxy to service via HTTP without port in host", func() {
+				Eventually(func(g Gomega) {
+					response, err := client.CollectEchoResponse(
+						kubernetes.Cluster, "demo-client",
+						"http://simple-gateway.simple-gateway:8080/",
+						client.WithHeader("host", "example.kuma.io"),
+						client.FromKubernetesPod(clientNamespace, "demo-client"),
+					)
+
+					g.Expect(err).ToNot(HaveOccurred())
+					g.Expect(response.Instance).To(Equal("echo-server"))
+					g.Expect(response.Received.Headers).ToNot(HaveKey("X-Specific-Hostname-Header"))
+				}, "1m", "1s").Should(Succeed())
+			})
+			It("should proxy to service via HTTP with port in host", func() {
+				Eventually(func(g Gomega) {
+					response, err := client.CollectEchoResponse(
+						kubernetes.Cluster, "demo-client",
+						"http://simple-gateway.simple-gateway:8080/",
+						client.WithHeader("host", "example.kuma.io:8080"),
+						client.FromKubernetesPod(clientNamespace, "demo-client"),
+					)
+
+					g.Expect(err).ToNot(HaveOccurred())
+					g.Expect(response.Instance).To(Equal("echo-server"))
+					g.Expect(response.Received.Headers).ToNot(HaveKey("X-Specific-Hostname-Header"))
+				}, "30s", "1s").Should(Succeed())
+			})
+
+			It("should proxy to service via HTTPS", func() {
+				Eventually(func(g Gomega) {
+					response, err := client.CollectEchoResponse(
+						kubernetes.Cluster, "demo-client",
+						"https://example.kuma.io:8081/",
+						client.Resolve("example.kuma.io:8081", clusterIP),
+						client.FromKubernetesPod(clientNamespace, "demo-client"),
+						client.Insecure(),
+					)
+
+					g.Expect(err).ToNot(HaveOccurred())
+					g.Expect(response.Instance).To(Equal("echo-server"))
+					g.Expect(response.Received.Headers).ToNot(HaveKey("X-Specific-Hostname-Header"))
+				}, "30s", "1s").Should(Succeed())
+			})
+
+			It("should automatically set host header from service address", func() {
+				Eventually(func(g Gomega) {
+					response, err := client.CollectEchoResponse(
+						kubernetes.Cluster, "demo-client",
+						"http://simple-gateway.simple-gateway:8082/",
+						client.WithHeader("host", "example.kuma.io"),
+						client.FromKubernetesPod(clientNamespace, "demo-client"),
+					)
+
+					g.Expect(err).ToNot(HaveOccurred())
+					g.Expect(response.Received.Headers["Host"]).To(HaveLen(1))
+					g.Expect(response.Received.Headers["Host"]).To(ContainElements("example.kuma.io"))
+					g.Expect(response.Received.Headers).ToNot(HaveKey("X-Specific-Hostname-Header"))
+				}, "30s", "1s").Should(Succeed())
+			})
+
+			It("should route based on host name", func() {
+				Eventually(func(g Gomega) {
+					response, err := client.CollectEchoResponse(
+						kubernetes.Cluster, "demo-client",
+						"http://simple-gateway.simple-gateway:8082/",
+						client.WithHeader("host", "specific.kuma.io"),
+						client.FromKubernetesPod(clientNamespace, "demo-client"),
+					)
+
+					g.Expect(err).ToNot(HaveOccurred())
+					g.Expect(response.Received.Headers["Host"]).To(HaveLen(1))
+					g.Expect(response.Received.Headers["Host"]).To(ContainElements("specific.kuma.io"))
+					g.Expect(response.Received.Headers["X-Specific-Hostname-Header"]).To(ContainElements("true"))
+				}, "30s", "1s").Should(Succeed())
+			})
+
+			It("should match routes by SNI", func() {
+				Eventually(func(g Gomega) {
+					response, err := client.CollectEchoResponse(
+						kubernetes.Cluster, "demo-client",
+						"https://otherexample.kuma.io:8081/-specific-listener",
+						client.Resolve("otherexample.kuma.io:8081", clusterIP),
+						client.FromKubernetesPod(clientNamespace, "demo-client"),
+						client.Insecure(),
+					)
+
+					g.Expect(err).ToNot(HaveOccurred())
+					g.Expect(response.Received.Headers["Host"]).To(HaveLen(1))
+					g.Expect(response.Received.Headers["Host"]).To(ContainElements("otherexample.kuma.io:8081"))
+					g.Expect(response.Received.Headers["X-Listener-By-Hostname-Header"]).To(ContainElements("true"))
+				}, "30s", "1s").Should(Succeed())
+			})
+
+			It("should isolate routes by SNI", func() {
+				Eventually(func(g Gomega) {
+					response, err := client.CollectEchoResponse(
+						kubernetes.Cluster, "demo-client",
+						"https://example.kuma.io:8081/-specific-listener",
+						client.Resolve("example.kuma.io:8081", clusterIP),
+						client.FromKubernetesPod(clientNamespace, "demo-client"),
+						client.Insecure(),
+					)
+
+					g.Expect(err).ToNot(HaveOccurred())
+					g.Expect(response.Received.Headers["Host"]).To(HaveLen(1))
+					g.Expect(response.Received.Headers["Host"]).To(ContainElements("example.kuma.io:8081"))
+					g.Expect(response.Received.Headers["X-Listener-By-Hostname-Header"]).NotTo(ContainElements("true"))
+				}, "30s", "1s").Should(Succeed())
+			})
+
+			It("should check both SNI and Host", func() {
+				Consistently(func(g Gomega) {
+					status, err := client.CollectFailure(
+						kubernetes.Cluster, "demo-client",
+						"https://otherexample.kuma.io:8081/-specific-listener",
+						client.Resolve("otherexample.kuma.io:8081", clusterIP),
+						// Note the header differs from the SNI
+						client.WithHeader("host", "example.kuma.io"),
+						client.FromKubernetesPod(clientNamespace, "demo-client"),
+						client.Insecure(),
+					)
+
+					g.Expect(err).ToNot(HaveOccurred())
+					g.Expect(status.ResponseCode).To(Equal(404))
+				}, "30s", "1s").Should(Succeed())
+			})
+>>>>>>> 67ee1be51 (fix(MeshGateway): fix MeshTCPRoute on MeshGateway (#9167))
 		})
 
+<<<<<<< HEAD
 		It("should proxy to service via HTTP without port in host", func() {
 			Eventually(func(g Gomega) {
 				response, err := client.CollectEchoResponse(
@@ -192,6 +447,10 @@ spec:
 			}, "30s", "1s").Should(Succeed())
 		})
 	})
+=======
+	basicRouting("MeshGatewayRoute", meshGatewayRoutes("internal-service", "/", "echo-server_simple-gateway_svc_80"))
+	basicRouting("MeshHTTPRoute", httpRoute("internal-service", "/", "echo-server_simple-gateway_svc_80"))
+>>>>>>> 67ee1be51 (fix(MeshGateway): fix MeshTCPRoute on MeshGateway (#9167))
 
 	Context("Rate Limit", func() {
 		rt := `apiVersion: kuma.io/v1alpha1
@@ -210,6 +469,7 @@ spec:
     http:
       requests: 5
       interval: 10s`
+		routes := meshGatewayRoutes("rt-echo-server", "/rt", "rt-echo-server_simple-gateway_svc_80")
 
 		BeforeAll(func() {
 			err := NewClusterSetup().
@@ -220,7 +480,18 @@ spec:
 					testserver.WithEchoArgs("echo", "--instance", "rt-echo-server"),
 				)).
 				Install(YamlK8s(rt)).
+<<<<<<< HEAD
 				Install(YamlK8s(route("rt-echo-server", "/rt", "rt-echo-server_simple-gateway_svc_80"))).
+=======
+				Install(YamlK8s(routes...)).
+				Setup(kubernetes.Cluster)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		AfterAll(func() {
+			err := NewClusterSetup().
+				Install(DeleteYamlK8s(routes...)).
+>>>>>>> 67ee1be51 (fix(MeshGateway): fix MeshTCPRoute on MeshGateway (#9167))
 				Setup(kubernetes.Cluster)
 			Expect(err).ToNot(HaveOccurred())
 		})
@@ -238,6 +509,62 @@ spec:
 
 				g.Expect(err).ToNot(HaveOccurred())
 				g.Expect(response.ResponseCode).To(Equal(429))
+			}, "30s", "1s").Should(Succeed())
+		})
+	})
+
+	Context("TCPRoute", func() {
+		routes := []string{
+			fmt.Sprintf(`
+apiVersion: kuma.io/v1alpha1
+kind: MeshTCPRoute
+metadata:
+  name: simple-gateway-tcp-route
+  namespace: %s
+  labels:
+    kuma.io/mesh: simple-gateway
+spec:
+  targetRef:
+    kind: MeshGateway
+    name: simple-gateway
+  to:
+    - targetRef:
+        kind: Mesh
+      rules:
+        - default:
+            backendRefs:
+              - kind: MeshService
+                name: test-tcp-server_simple-gateway_svc_80 
+`, Config.KumaNamespace),
+		}
+		BeforeAll(func() {
+			err := NewClusterSetup().
+				Install(testserver.Install(
+					testserver.WithName("test-tcp-server"),
+					testserver.WithServicePortAppProtocol("tcp"),
+					testserver.WithMesh(meshName),
+					testserver.WithNamespace(namespace),
+				)).
+				Install(YamlK8s(routes...)).
+				Setup(kubernetes.Cluster)
+			Expect(err).ToNot(HaveOccurred())
+		})
+		AfterAll(func() {
+			err := NewClusterSetup().
+				Install(DeleteYamlK8s(routes...)).
+				Setup(kubernetes.Cluster)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("should work on TCP service", func() {
+			Eventually(func(g Gomega) {
+				response, err := client.CollectEchoResponse(
+					kubernetes.Cluster, "demo-client",
+					"http://simple-gateway.simple-gateway:8083/",
+					client.FromKubernetesPod(clientNamespace, "demo-client"),
+				)
+				g.Expect(err).ToNot(HaveOccurred())
+				g.Expect(response.Instance).To(HavePrefix("test-tcp-server"))
 			}, "30s", "1s").Should(Succeed())
 		})
 	})
@@ -269,8 +596,13 @@ spec:
 		})
 
 		It("should proxy to service via HTTP", func() {
+<<<<<<< HEAD
 			route := route("es-echo-server", "/external-service", "external-service")
 			setup := NewClusterSetup().Install(YamlK8s(route))
+=======
+			route := meshGatewayRoutes("es-echo-server", "/external-service", "external-service")
+			setup := NewClusterSetup().Install(YamlK8s(route...))
+>>>>>>> 67ee1be51 (fix(MeshGateway): fix MeshTCPRoute on MeshGateway (#9167))
 			Expect(setup.Setup(kubernetes.Cluster)).To(Succeed())
 
 			Eventually(func(g Gomega) {
@@ -289,8 +621,13 @@ spec:
 		})
 
 		It("should automatically set host header from external service address when rewrite.hostToBackendHostname is set to true", func() {
+<<<<<<< HEAD
 			route := route("es-echo-server", "/external-service", "external-service")
 			setup := NewClusterSetup().Install(YamlK8s(route))
+=======
+			route := meshGatewayRoutes("es-echo-server", "/external-service", "external-service")
+			setup := NewClusterSetup().Install(YamlK8s(route...))
+>>>>>>> 67ee1be51 (fix(MeshGateway): fix MeshTCPRoute on MeshGateway (#9167))
 			Expect(setup.Setup(kubernetes.Cluster)).To(Succeed())
 
 			// don't rewrite host to backend hostname
