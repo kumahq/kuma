b2fab56f33d1e6730c7367410c14f6813465855d
Lukasz Dziedziak <lukidzi@gmail.com>
2024-02-14 11:35:56 -0500

    fix(kuma-cp): copy annotations when adding/update k8s object (#9254)
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/plugins/resources/k8s/store.go
+++ pkg/plugins/resources/k8s/store.go
@@ -55,6 +55,13 @@ func (s *KubernetesStore) Create(ctx context.Context, r core_model.Resource, fs 
 	if err != nil {
 		return err
 	}
+<<<<<<< HEAD
+=======
+
+	labels, annotations := splitLabelsAndAnnotations(opts.Labels, obj.GetAnnotations())
+	obj.GetObjectMeta().SetLabels(labels)
+	obj.GetObjectMeta().SetAnnotations(annotations)
+>>>>>>> f564e79f6 (fix(kuma-cp): copy annotations when adding/update k8s object (#9254))
 	obj.SetMesh(opts.Mesh)
 	obj.GetObjectMeta().SetName(name)
 	obj.GetObjectMeta().SetNamespace(namespace)
@@ -91,6 +98,14 @@ func (s *KubernetesStore) Update(ctx context.Context, r core_model.Resource, fs 
 		return errors.Wrapf(err, "failed to convert core model of type %s into k8s counterpart", r.Descriptor().Name)
 	}
 
+<<<<<<< HEAD
+=======
+	labels, annotations := splitLabelsAndAnnotations(opts.Labels, obj.GetAnnotations())
+	obj.GetObjectMeta().SetLabels(labels)
+	obj.GetObjectMeta().SetAnnotations(annotations)
+	obj.SetMesh(r.GetMeta().GetMesh())
+
+>>>>>>> f564e79f6 (fix(kuma-cp): copy annotations when adding/update k8s object (#9254))
 	if err := s.Client.Update(ctx, obj); err != nil {
 		if kube_apierrs.IsConflict(err) {
 			return store.ErrorResourceConflict(r.Descriptor().Name, r.GetMeta().GetName(), r.GetMeta().GetMesh())
@@ -222,6 +237,24 @@ func k8sNameNamespace(coreName string, scope k8s_model.Scope) (string, string, e
 	}
 }
 
+<<<<<<< HEAD
+=======
+// Kuma resource labels are generally stored on Kubernetes as labels, except "kuma.io/display-name".
+// We store it as an annotation because the resource name on k8s is limited by 253 and the label value is limited by 63.
+func splitLabelsAndAnnotations(coreLabels map[string]string, currentAnnotations map[string]string) (map[string]string, map[string]string) {
+	labels := maps.Clone(coreLabels)
+	annotations := maps.Clone(currentAnnotations)
+	if annotations == nil {
+		annotations = map[string]string{}
+	}
+	if v, ok := labels[v1alpha1.DisplayName]; ok {
+		annotations[v1alpha1.DisplayName] = v
+		delete(labels, v1alpha1.DisplayName)
+	}
+	return labels, annotations
+}
+
+>>>>>>> f564e79f6 (fix(kuma-cp): copy annotations when adding/update k8s object (#9254))
 var _ core_model.ResourceMeta = &KubernetesMetaAdapter{}
 
 type KubernetesMetaAdapter struct {
