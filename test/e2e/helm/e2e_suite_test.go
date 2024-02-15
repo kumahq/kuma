6bb5004565b0a59b9f02d03b2096020051b2c210
Lukasz Dziedziak <lukidzi@gmail.com>
2024-02-15 14:31:18 -0500

    test
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- test/e2e/helm/e2e_suite_test.go
+++ test/e2e/helm/e2e_suite_test.go
@@ -15,7 +15,7 @@ func TestE2E(t *testing.T) {
 
 var (
 	_ = Describe("Test Zone and Global with Helm chart", Label("job-2"), helm.ZoneAndGlobalWithHelmChart, Ordered)
-	_ = Describe("Test Zone and Global universal mode with Helm chart", Label("job-0"), helm.ZoneAndGlobalInUniversalModeWithHelmChart, Ordered)
+	_ = Describe("Test Zone and Global universal mode with Helm chart", Label("job-2"), helm.ZoneAndGlobalInUniversalModeWithHelmChart, Ordered)
 	// Skipped as it fails with: error while running command: exit status 1; Error: INSTALLATION FAILED: rendered manifests contain a resource that already exists. Unable to continue with install: ServiceAccount "kuma-control-plane" in namespace "kuma-system" exists and cannot be imported into the current release: invalid ownership metadata; annotation validation error: key "meta.helm.sh/release-name" must equal "kuma-c0vo8o": current value is "kuma-8yy3uv"
 	// Likely something needs to be improved to be able to run this test
 	_ = PDescribe("Test Global and Zone universal mode with Helm chart", Label("job-0"), helm.GlobalAndZoneInUniversalModeWithHelmChart, Ordered)
