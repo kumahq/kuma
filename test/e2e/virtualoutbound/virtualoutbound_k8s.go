c56136a1c731c1b6c9f430295cb12d35cf9effe9
Lukasz Dziedziak <lukidzi@gmail.com>
2024-02-15 14:56:28 -0500

    change namespace
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- test/e2e/virtualoutbound/virtualoutbound_k8s.go
+++ test/e2e/virtualoutbound/virtualoutbound_k8s.go
@@ -13,6 +13,7 @@ import (
 
 func VirtualOutboundOnK8s() {
 	var k8sCluster Cluster
+	namespace := "virtual-outbound"
 
 	BeforeEach(func() {
 		k8sCluster = NewK8sCluster(NewTestingT(), Kuma1, Silent)
@@ -21,16 +22,16 @@ func VirtualOutboundOnK8s() {
 			Install(Kuma(config_core.Zone,
 				WithEnv("KUMA_DNS_SERVER_SERVICE_VIP_ENABLED", "false"),
 			)).
-			Install(NamespaceWithSidecarInjection(TestNamespace)).
-			Install(democlient.Install(democlient.WithNamespace(TestNamespace), democlient.WithMesh("default"))).
-			Install(testserver.Install(testserver.WithStatefulSet(true), testserver.WithReplicas(2))).
+			Install(NamespaceWithSidecarInjection(namespace)).
+			Install(democlient.Install(democlient.WithNamespace(namespace), democlient.WithMesh("default"))).
+			Install(testserver.Install(testserver.WithStatefulSet(true), testserver.WithReplicas(2), testserver.WithNamespace(namespace))).
 			Setup(k8sCluster)
 		Expect(err).ToNot(HaveOccurred())
 	})
 
 	E2EAfterEach(func() {
 		Expect(k8sCluster.DeleteKuma()).To(Succeed())
-		Expect(k8sCluster.DeleteNamespace(TestNamespace)).To(Succeed())
+		Expect(k8sCluster.DeleteNamespace(namespace)).To(Succeed())
 		Expect(k8sCluster.DismissCluster()).To(Succeed())
 	})
 
@@ -57,8 +58,8 @@ spec:
 
 		// Succeed with virtual-outbound
 		Eventually(func(g Gomega) {
-			res, err := client.CollectEchoResponse(k8sCluster, "demo-client", "test-server_kuma-test_svc_80.foo:8080",
-				client.FromKubernetesPod(TestNamespace, "demo-client"),
+			res, err := client.CollectEchoResponse(k8sCluster, "demo-client", "test-server_virtual-outbound_svc_80.foo:8080",
+				client.FromKubernetesPod(namespace, "demo-client"),
 			)
 			g.Expect(err).ToNot(HaveOccurred())
 			g.Expect(res.Instance).To(Or(Equal("test-server-1"), Equal("test-server-0")))
@@ -66,8 +67,8 @@ spec:
 
 		// Fails with built in vip (it's disabled in conf)
 		Consistently(func(g Gomega) {
-			_, err := client.CollectEchoResponse(k8sCluster, "demo-client", "test-server_kuma-test_svc_80.mesh:80",
-				client.FromKubernetesPod(TestNamespace, "demo-client"),
+			_, err := client.CollectEchoResponse(k8sCluster, "demo-client", "test-server_virtual-outbound_svc_80.mesh:80",
+				client.FromKubernetesPod(namespace, "demo-client"),
 			)
 			g.Expect(err).To(HaveOccurred())
 		}, "2s", "250ms").Should(Succeed())
