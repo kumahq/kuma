08542975e9ff1c71ad34148156b1ccb478245ae7
Bart Smykla <bartek@smykla.com>
2024-02-28 08:12:55 +0100

    test(e2e): add e2e test for MeshRetry with delegated gateway
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- test/e2e_env/kubernetes/gateway/delegated.go
+++ test/e2e_env/kubernetes/gateway/delegated.go
@@ -87,4 +87,5 @@ spec:
 	Context("MeshCircuitBreaker", delegated.CircuitBreaker(&config))
 	Context("MeshProxyPatch", delegated.MeshProxyPatch(&config))
 	Context("MeshHealthCheck", delegated.MeshHealthCheck(&config))
+	Context("MeshRetry", delegated.MeshRetry(&config))
 }
