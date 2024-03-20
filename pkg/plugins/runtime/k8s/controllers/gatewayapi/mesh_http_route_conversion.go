b0d7b95c3dbf25e94712c489182938113591ad64
Bart Smykla <bartek@smykla.com>
2024-03-20 07:31:30 +0100

    fix(GatewayAPI): make MeshHTTPRoute conversion port redirect conformant
    According to https://github.com/kubernetes-sigs/gateway-api/pull/1880
    
    // When empty, the redirect port MUST be derived using the following rules:
    //
    // * If redirect scheme is not-empty, the redirect port MUST be the well-known
    //   port associated with the redirect scheme. Specifically "http" to port 80
    //   and "https" to port 443. If the redirect scheme does not have a
    //   well-known port, the listener port of the Gateway SHOULD be used.
    // * If redirect scheme is empty, the redirect port MUST be the Gateway
    //   Listener port.
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/plugins/runtime/k8s/controllers/gatewayapi/mesh_http_route_conversion.go
+++ pkg/plugins/runtime/k8s/controllers/gatewayapi/mesh_http_route_conversion.go
@@ -298,13 +298,26 @@ func (r *HTTPRouteReconciler) gapiToKumaMeshFilter(
 			path = &meshPath
 		}
 
+		port := (*v1alpha1.PortNumber)(redirect.Port)
+		if redirect.Scheme != nil && redirect.Port == nil {
+			// See https://github.com/kubernetes-sigs/gateway-api/pull/1880
+			// this would have been a breaking change for MeshGateway, so handle
+			// it here.
+			switch *redirect.Scheme {
+			case "http":
+				port = (*v1alpha1.PortNumber)(pointer.To(int32(80)))
+			case "https":
+				port = (*v1alpha1.PortNumber)(pointer.To(int32(443)))
+			}
+		}
+
 		return v1alpha1.Filter{
 			Type: v1alpha1.RequestRedirectType,
 			RequestRedirect: &v1alpha1.RequestRedirect{
 				Scheme:     redirect.Scheme,
 				Hostname:   (*v1alpha1.PreciseHostname)(redirect.Hostname),
 				Path:       path,
-				Port:       (*v1alpha1.PortNumber)(redirect.Port),
+				Port:       port,
 				StatusCode: redirect.StatusCode,
 			},
 		}, nil, true
