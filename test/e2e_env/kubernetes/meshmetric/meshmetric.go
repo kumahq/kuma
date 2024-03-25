ecef48bb711e1869e624880dba40059b723965ae
slonka <slonka@users.noreply.github.com>
2024-03-25 16:26:10 +0100

    feat(meshmetric): make format work
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- test/e2e_env/kubernetes/meshmetric/meshmetric.go
+++ test/e2e_env/kubernetes/meshmetric/meshmetric.go
@@ -393,9 +393,9 @@ func MeshMetric() {
 			g.Expect(err).ToNot(HaveOccurred())
 			g.Expect(stdout).ToNot(BeNil())
 			// metric from envoy
-			g.Expect(stdout).To(ContainSubstring("envoy_cluster_upstream_cx_active")) // from basic
+			g.Expect(stdout).To(ContainSubstring("envoy_cluster_upstream_cx_active"))        // from basic
 			g.Expect(stdout).To(ContainSubstring("envoy_cluster_default_total_match_count")) // from include
-			g.Expect(stdout).To(Not(ContainSubstring("envoy_cluster_lb_healthy_panic"))) // from exclude
+			g.Expect(stdout).To(Not(ContainSubstring("envoy_cluster_lb_healthy_panic")))     // from exclude
 		}).Should(Succeed())
 	})
 
