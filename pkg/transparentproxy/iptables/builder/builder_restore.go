9b691268b61cc2763756a5ac16d816ceb6ebb267
Bart Smykla <bartek@smykla.com>
2024-04-02 15:59:55 +0200

    fix(transparent-proxy): make iptables mode detection more defensive (#9776)
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/transparentproxy/iptables/builder/builder_restore.go
+++ pkg/transparentproxy/iptables/builder/builder_restore.go
@@ -32,7 +32,7 @@ func buildRestoreParameters(cfg config.Config, rulesFile *os.File, restoreLegacy
 		Build(cfg.Verbose, rulesFile.Name())
 }
 
-func findExecutable(name string) executable {
+func findExecutable(name string) Executable {
 	paths := append(
 		[]string{name},
 		fallbackPaths...,
@@ -53,26 +53,26 @@ func findExecutable(name string) executable {
 		}
 	}
 
-	return executable{name: name}
+	return Executable{Name: name}
 }
 
-type executable struct {
-	name string
-	path string
+type Executable struct {
+	Name string
+	Path string
 }
 
-func newExecutable(name string, path string) executable {
-	return executable{
-		name: name,
-		path: path,
+func newExecutable(name string, path string) Executable {
+	return Executable{
+		Name: name,
+		Path: path,
 	}
 }
 
-func (e executable) exec(ctx context.Context, args ...string) (*bytes.Buffer, error) {
+func (e Executable) exec(ctx context.Context, args ...string) (*bytes.Buffer, error) {
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 	// #nosec G204
-	cmd := exec.CommandContext(ctx, e.path, args...)
+	cmd := exec.CommandContext(ctx, e.Path, args...)
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 
@@ -87,79 +87,115 @@ func (e executable) exec(ctx context.Context, args ...string) (*bytes.Buffer, er
 	return &stdout, nil
 }
 
-type executables struct {
-	save                   executable
-	restore                executable
+type Executables struct {
+	Iptables               Executable
+	Save                   Executable
+	Restore                Executable
+	fallback               *Executables
+	legacy                 bool
 	foundDockerOutputChain bool
 }
 
-func newExecutables(ipv6 bool, mode string) *executables {
+func newExecutables(ipv6 bool, mode string) *Executables {
 	prefix := iptables
 	if ipv6 {
 		prefix = ip6tables
 	}
 
-	save := fmt.Sprintf("%s-%s-%s", prefix, mode, "save")
-	restore := fmt.Sprintf("%s-%s-%s", prefix, mode, "restore")
+	iptables := fmt.Sprintf("%s-%s", prefix, mode)
+	iptablesSave := fmt.Sprintf("%s-%s-%s", prefix, mode, "save")
+	iptablesRestore := fmt.Sprintf("%s-%s-%s", prefix, mode, "restore")
 
-	return &executables{
-		save:    findExecutable(save),
-		restore: findExecutable(restore),
+	return &Executables{
+		Iptables: findExecutable(iptables),
+		Save:     findExecutable(iptablesSave),
+		Restore:  findExecutable(iptablesRestore),
+		legacy:   mode == "legacy",
 	}
 }
 
-func (e *executables) verify(ctx context.Context, cfg config.Config) error {
+var necessaryMatchExtensions = []string{
+	"owner",
+	"tcp",
+	"udp",
+}
+
+func (e *Executables) verify(ctx context.Context, cfg config.Config) (*Executables, error) {
 	var missing []string
 
-	if e.save.path == "" {
-		missing = append(missing, e.save.name)
+	if e.Save.Path == "" {
+		missing = append(missing, e.Save.Name)
 	}
 
-	if e.restore.path == "" {
-		missing = append(missing, e.restore.name)
+	if e.Restore.Path == "" {
+		missing = append(missing, e.Restore.Name)
 	}
 
 	if len(missing) > 0 {
-		return errors.Errorf("couldn't find executables: [%s]", strings.Join(missing, ","))
+		return nil, errors.Errorf("couldn't find executables: [%s]", strings.Join(missing, ","))
 	}
 
 	// We always need to have access to the "nat" table
-	if stdout, err := e.save.exec(ctx, "-t", "nat"); err != nil {
-		return errors.Wrap(err, "couldn't verify if table: 'nat' is available")
+	if stdout, err := e.Save.exec(ctx, "-t", "nat"); err != nil {
+		return nil, errors.Wrap(err, "couldn't verify if table: 'nat' is available")
 	} else if cfg.ShouldRedirectDNS() || cfg.ShouldCaptureAllDNS() {
 		e.foundDockerOutputChain = dockerOutputChainRegex.Match(stdout.Bytes())
 	}
 
-	if cfg.ShouldConntrackZoneSplit() {
-		if _, err := e.save.exec(ctx, "-t", "raw"); err != nil {
-			return errors.Wrap(err, "couldn't verify if table: 'raw' is available")
+	// It seems in some cases (GKE with ContainerOS), even if "iptables-nft" is available
+	// there are some kernel modules with iptables match extensions missing.
+	for _, matchExtension := range necessaryMatchExtensions {
+		if _, err := e.Iptables.exec(ctx, "-m", matchExtension, "--help"); err != nil {
+			return nil, errors.Wrapf(err, "verification if match: %q exist failed", matchExtension)
 		}
 	}
 
-	return nil
+	if cfg.ShouldConntrackZoneSplit(e.Iptables.Path) {
+		if _, err := e.Save.exec(ctx, "-t", "raw"); err != nil {
+			return nil, errors.Wrap(err, "couldn't verify if table: 'raw' is available")
+		}
+	}
+
+	return e, nil
 }
 
-func detectIptablesExecutables(ctx context.Context, cfg config.Config, ipv6 bool) (*executables, bool, error) {
-	nft := newExecutables(ipv6, "nft")
-	legacy := newExecutables(ipv6, "legacy")
+func (e *Executables) withFallback(fallback *Executables) *Executables {
+	if fallback != nil {
+		e.fallback = fallback
+	}
 
-	if err := nft.verify(ctx, cfg); err != nil {
-		return legacy, true, legacy.verify(ctx, cfg)
+	return e
+}
+
+func DetectIptablesExecutables(
+	ctx context.Context,
+	cfg config.Config,
+	ipv6 bool,
+) (*Executables, error) {
+	nft, nftVerifyErr := newExecutables(ipv6, "nft").verify(ctx, cfg)
+	legacy, legacyVerifyErr := newExecutables(ipv6, "legacy").verify(ctx, cfg)
+
+	if nftVerifyErr != nil && legacyVerifyErr != nil {
+		return nil, fmt.Errorf("no valid iptables executable found: %s, %s", nftVerifyErr, legacyVerifyErr)
+	}
+
+	if nftVerifyErr != nil {
+		return legacy, nil
 	}
 
 	// Found DOCKER_OUTPUT chain in iptables-nft
 	if nft.foundDockerOutputChain {
-		return nft, false, nil
+		return nft.withFallback(legacy), nil
 	}
 
-	if err := legacy.verify(ctx, cfg); err != nil {
-		return nft, false, nil
+	if legacyVerifyErr != nil {
+		return nft, nil
 	}
 
 	// Found DOCKER_OUTPUT chain in iptables-legacy
 	if legacy.foundDockerOutputChain {
-		return legacy, true, nil
+		return legacy.withFallback(nft), nil
 	}
 
-	return nft, false, nil
+	return nft.withFallback(legacy), nil
 }
