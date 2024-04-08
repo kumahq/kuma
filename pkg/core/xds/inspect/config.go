a4add5338d37ba198bbb32150fbc3669cd6f2ba9
Ilya Lobkov <ilya.lobkov@konghq.com>
2024-04-08 23:45:29 +0200

    don't use TestSecrets, create dummySecrets instead
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- pkg/core/xds/inspect/config.go
+++ pkg/core/xds/inspect/config.go
@@ -3,20 +3,24 @@ package inspect
 import (
 	"context"
 	"encoding/json"
+	"time"
 
 	"github.com/envoyproxy/go-control-plane/pkg/cache/types"
 	"github.com/envoyproxy/go-control-plane/pkg/cache/v3"
 	"github.com/josephburnett/jd/v2"
 
+	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
 	api_common "github.com/kumahq/kuma/api/openapi/types/common"
+	core_mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
 	"github.com/kumahq/kuma/pkg/core/resources/model"
-	"github.com/kumahq/kuma/pkg/test/xds"
+	core_xds "github.com/kumahq/kuma/pkg/core/xds"
 	util_proto "github.com/kumahq/kuma/pkg/util/proto"
 	"github.com/kumahq/kuma/pkg/xds/cache/cla"
 	xds_context "github.com/kumahq/kuma/pkg/xds/context"
 	"github.com/kumahq/kuma/pkg/xds/envoy"
 	"github.com/kumahq/kuma/pkg/xds/generator"
 	xds_hooks "github.com/kumahq/kuma/pkg/xds/hooks"
+	"github.com/kumahq/kuma/pkg/xds/secrets"
 	v3 "github.com/kumahq/kuma/pkg/xds/server/v3"
 	"github.com/kumahq/kuma/pkg/xds/sync"
 )
@@ -56,7 +60,7 @@ func (p *ProxyConfigInspector) Get(ctx context.Context, name string, shadow bool
 		Mesh: p.meshContext,
 		ControlPlane: &xds_context.ControlPlaneContext{
 			CLACache: &cla.Retriever{},
-			Secrets:  &xds.TestSecrets{},
+			Secrets:  &dummySecrets{},
 			Zone:     p.zone,
 		},
 	}
@@ -136,3 +140,52 @@ func marshalMap(m map[string]types.ResourceWithTTL) (json.RawMessage, error) {
 	}
 	return json.Marshal(resourcesByName)
 }
+
+// dummySecrets returns hardcoded plausible values
+type dummySecrets struct{}
+
+func (ds *dummySecrets) GetForDataPlane(_ context.Context, _ *core_mesh.DataplaneResource, mesh *core_mesh.MeshResource, meshes []*core_mesh.MeshResource) (*core_xds.IdentitySecret, map[string]*core_xds.CaSecret, error) {
+	return ds.identity(), ds.cas(append(meshes, mesh)...), nil
+}
+
+func (ds *dummySecrets) GetForZoneEgress(_ context.Context, _ *core_mesh.ZoneEgressResource, mesh *core_mesh.MeshResource) (*core_xds.IdentitySecret, *core_xds.CaSecret, error) {
+	return ds.identity(), ds.cas(mesh)[mesh.GetMeta().GetName()], nil
+}
+
+func (ds *dummySecrets) GetAllInOne(ctx context.Context, _ *core_mesh.MeshResource, _ *core_mesh.DataplaneResource, _ []*core_mesh.MeshResource) (*core_xds.IdentitySecret, *core_xds.CaSecret, error) {
+	return ds.identity(), &core_xds.CaSecret{PemCerts: [][]byte{[]byte("COMBINED")}}, nil
+}
+
+func (ds *dummySecrets) identity() *core_xds.IdentitySecret {
+	return &core_xds.IdentitySecret{PemCerts: [][]byte{[]byte("CERT")}, PemKey: []byte("KEY")}
+}
+
+func (ds *dummySecrets) cas(meshes ...*core_mesh.MeshResource) map[string]*core_xds.CaSecret {
+	cas := map[string]*core_xds.CaSecret{}
+	for _, mesh := range meshes {
+		cas[mesh.GetMeta().GetName()] = &core_xds.CaSecret{PemCerts: [][]byte{[]byte("CA")}}
+	}
+	return cas
+}
+
+func (ds *dummySecrets) Info(dpKey model.ResourceKey) *secrets.Info {
+	return &secrets.Info{
+		Expiration: time.Unix(2, 2),
+		Generation: time.Unix(1, 1),
+		Tags: map[string]map[string]bool{
+			"kuma.io/service": {
+				dpKey.Name: true,
+			},
+		},
+		OwnMesh: secrets.MeshInfo{
+			MTLS: &mesh_proto.Mesh_Mtls{
+				EnabledBackend: "ca-1",
+				Backends:       nil,
+			},
+		},
+		IssuedBackend:     "ca-1",
+		SupportedBackends: []string{"ca-1"},
+	}
+}
+
+func (ds *dummySecrets) Cleanup(model.ResourceKey) {}
