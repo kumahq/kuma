ac3c493ae74f52a535a5557bb42313a1702d2f95
Ilya Lobkov <ilya.lobkov@konghq.com>
2024-04-10 13:37:01 +0200

    feat(kumactl): support for new Inspect API endpoint `_config` (#9887)
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- app/kumactl/cmd/inspect/inspect_dataplane.go
+++ app/kumactl/cmd/inspect/inspect_dataplane.go
@@ -20,6 +20,7 @@ const (
 	InspectionTypeConfigDump = "config-dump"
 	InspectionTypeStats      = "stats"
 	InspectionTypeClusters   = "clusters"
+	InspectionConfig         = "config"
 )
 
 var dataplaneInspectTemplate = `{{ with IsSidecar . }}{{ range $num, $item := .Items }}{{ .AttachmentEntry | FormatAttachment }}:
@@ -61,6 +62,8 @@ func newInspectDataplaneCmd(pctx *cmd.RootContext) *cobra.Command {
 	}
 	var configDump bool
 	var inspectionType string
+	var shadow bool
+	var include []string
 	cmd := &cobra.Command{
 		Use:   "dataplane NAME",
 		Short: "Inspect Dataplane",
@@ -70,6 +73,13 @@ func newInspectDataplaneCmd(pctx *cmd.RootContext) *cobra.Command {
 			name := args[0]
 			if configDump {
 				inspectionType = InspectionTypeConfigDump
+			}
+
+			if shadow && inspectionType != InspectionConfig {
+				return errors.New("flag '--shadow' can be used only when '--type=config'")
+			}
+			if len(include) > 0 && inspectionType != InspectionConfig {
+				return errors.New("flag '--include' can be used only when '--type=config'")
 			}
 
 			client, err := pctx.CurrentInspectEnvoyProxyClient(mesh.DataplaneResourceTypeDescriptor)
@@ -84,6 +94,7 @@ func newInspectDataplaneCmd(pctx *cmd.RootContext) *cobra.Command {
 				if err != nil {
 					return errors.Wrap(err, "failed to create a dataplane inspect client")
 				}
+
 				entryList, err := client.InspectPolicies(context.Background(), pctx.CurrentMesh(), name)
 				if err != nil {
 					return err
@@ -110,14 +121,23 @@ func newInspectDataplaneCmd(pctx *cmd.RootContext) *cobra.Command {
 				}
 				_, err = fmt.Fprint(cmd.OutOrStdout(), string(bytes))
 				return err
+			case InspectionConfig:
+				bytes, err := client.Config(context.Background(), resourceKey, shadow, include)
+				if err != nil {
+					return err
+				}
+				_, err = fmt.Fprint(cmd.OutOrStdout(), string(bytes))
+				return err
 			default:
 				return errors.New("invalid inspection type")
 			}
 		},
 	}
-	cmd.PersistentFlags().StringVar(&inspectionType, "type", InspectionTypePolicies, kuma_cmd.UsageOptions("inspection type", InspectionTypePolicies, InspectionTypeConfigDump, InspectionTypeStats, InspectionTypeClusters))
+	cmd.PersistentFlags().StringVar(&inspectionType, "type", InspectionTypePolicies, kuma_cmd.UsageOptions("inspection type", InspectionTypePolicies, InspectionTypeConfigDump, InspectionTypeStats, InspectionTypeClusters, InspectionConfig))
 	cmd.PersistentFlags().BoolVar(&configDump, "config-dump", false, "if set then the command returns envoy config dump for provided dataplane")
 	_ = cmd.PersistentFlags().MarkDeprecated("config-dump", "use --type=config-dump")
 	cmd.PersistentFlags().StringVarP(&pctx.Args.Mesh, "mesh", "m", "default", "mesh to use")
+	cmd.PersistentFlags().BoolVar(&shadow, "shadow", false, "when computing XDS config the CP takes into account policies with 'kuma.io/effect: shadow' label")
+	cmd.PersistentFlags().StringSliceVar(&include, "include", []string{}, "an array of extra fields to include in the response. When `include=diff` the server computes a diff in JSONPatch format between the XDS config returned in 'xds' and the current proxy XDS config.")
 	return cmd
 }
