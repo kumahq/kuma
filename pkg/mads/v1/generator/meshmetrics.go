094b245d6b6e657645018221adcb40d09441b848
Marcin Skalski <skalskimarcin33@gmail.com>
2024-04-11 16:57:33 +0200

    fix(policy): add missing metrics labels in meshmetric
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- pkg/mads/v1/generator/meshmetrics.go
+++ pkg/mads/v1/generator/meshmetrics.go
@@ -41,7 +41,7 @@ func Generate(meshMetricToDataplane map[*v1alpha1.Conf]*core_mesh.DataplaneResou
 					Name:        dataplane.GetMeta().GetName(),
 					Address:     net.JoinHostPort(dataplane.GetIP(), strconv.FormatUint(uint64(prometheusEndpoint.Port), 10)),
 					MetricsPath: prometheusEndpoint.Path,
-					Labels:      mads.DataplaneLabels(dataplane, []*core_mesh.MeshGatewayResource{}), // what is this mesh gw and why we have labels there?
+					Labels:      map[string]string{},
 				}},
 			}
 
