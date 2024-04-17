97fb85c5913229f6c856d0f6685ee0b9a4ad4485
Bart Smykla <bartek@smykla.com>
2024-04-17 08:19:46 +0200

    chore(GAPI): move from deprecated behavior (#9965)
    Specifying `kuma.io/service` tag for `MeshGatewayInstance` is
    deprecated, so we shouldn't be use it when converting Gateway API
    resources to our internal ones.
    
    Specifying `kuma.io/mesh` as an annotation is also deprecated,
    so I changed it to label in conversion to `MeshGatewayInstance`
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/plugins/runtime/k8s/controllers/gatewayapi/common/common.go
+++ pkg/plugins/runtime/k8s/controllers/gatewayapi/common/common.go
@@ -2,27 +2,18 @@ package common
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/pkg/errors"
 	kube_apierrs "k8s.io/apimachinery/pkg/api/errors"
 	kube_types "k8s.io/apimachinery/pkg/types"
 	kube_client "sigs.k8s.io/controller-runtime/pkg/client"
 	gatewayapi "sigs.k8s.io/gateway-api/apis/v1beta1"
-
-	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
 )
 
 const (
 	ControllerName = gatewayapi.GatewayController("gateways.kuma.io/controller")
 	HTTPRouteKind  = gatewayapi.Kind("HTTPRoute")
 )
-
-func ServiceTagForGateway(name kube_types.NamespacedName) map[string]string {
-	return map[string]string{
-		mesh_proto.ServiceTag: fmt.Sprintf("%s_%s_gateway", name.Name, name.Namespace),
-	}
-}
 
 func GetGatewayClass(ctx context.Context, client kube_client.Client, name gatewayapi.ObjectName) (*gatewayapi.GatewayClass, error) {
 	class := &gatewayapi.GatewayClass{}
