aa63b60a400519662601f351d941d5732d42f861
Bart Smykla <bartek@smykla.com>
2024-04-24 16:15:28 +0200

    refactor(transparent-proxy): simplify tproxy configuration (#10058)
    Get rid of unnecessary intermediate `TransparentProxyConfig`
    struct. It's a leftover from the time when we had two tproxy
    engines. Now we can safely use `Config` struct.
    
    There is no V2 or V1 iptables engines. There is only one, so we
    don't have to have interface representing engine and can just use
    pure functions, so I could get rid of `TransparentProxyV2` struct
    and `TransparentProxy`
    
    We can use general `Config` struct, so I removed
    `transparentProxyArgs` struct for tproxy uninstall.
    
    Replace writes to stdout buffers with `fmt.Fprintln` in tproxy
    uninsatll, which is simpler and more elegant`.
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/transparentproxy/transparentproxy_suite_test.go
+++ pkg/transparentproxy/transparentproxy_suite_test.go
@@ -6,6 +6,6 @@ import (
 	"github.com/kumahq/kuma/pkg/test"
 )
 
-func TestTransparentProxyV2(t *testing.T) {
-	test.RunSpecs(t, "Transparent Proxy V2")
+func TestTransparentProxy(t *testing.T) {
+	test.RunSpecs(t, "Transparent Proxy")
 }
