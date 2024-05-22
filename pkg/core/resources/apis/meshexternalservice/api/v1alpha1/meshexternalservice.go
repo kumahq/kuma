8f20c13da27d694e8e34649ff0f6c7fecf00f1ce
slonka <slonka@users.noreply.github.com>
2024-05-22 11:10:18 +0200

    feat(MeshExternalService): remove validator for now
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- pkg/core/resources/apis/meshexternalservice/api/v1alpha1/meshexternalservice.go
+++ pkg/core/resources/apis/meshexternalservice/api/v1alpha1/meshexternalservice.go
@@ -2,22 +2,23 @@
 package v1alpha1
 
 import (
-    "github.com/kumahq/kuma/api/common/v1alpha1"
-    apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+
+	"github.com/kumahq/kuma/api/common/v1alpha1"
 )
 
 // MeshExternalService
 // +kuma:policy:is_policy=false
 // +kuma:policy:has_status=true
 type MeshExternalService struct {
-    // Match defines traffic that should be routed through the sidecar.
-    Match Match `json:"match"`
-    // Extension struct for a plugin configuration, in the presence of an extension `endpoints` and `tls` are not required anymore - it's up to the extension to validate them independently.
-    Extension Extension `json:"extension"`
-    // Endpoints defines a list of destinations to send traffic to.
-    Endpoints []Endpoint `json:"endpoints"`
-    // Tls provides a TLS configuration when proxy is resposible for a TLS origination
-    Tls Tls `json:"tls"`
+	// Match defines traffic that should be routed through the sidecar.
+	Match Match `json:"match"`
+	// Extension struct for a plugin configuration, in the presence of an extension `endpoints` and `tls` are not required anymore - it's up to the extension to validate them independently.
+	Extension Extension `json:"extension"`
+	// Endpoints defines a list of destinations to send traffic to.
+	Endpoints []Endpoint `json:"endpoints"`
+	// Tls provides a TLS configuration when proxy is resposible for a TLS origination
+	Tls Tls `json:"tls"`
 }
 
 // +kubebuilder:validation:Enum=HostnameGenerator
@@ -27,19 +28,19 @@ type MatchType string
 type ProtocolType string
 
 type Match struct {
-    // Type of the match, only `HostnameGenerator` is available at the moment.
-    Type MatchType `json:"type"`
-    // Port defines a port to which a user does request.
-    Port int `json:"port"`
-    // Protocol defines a protocol of the communication. Possible values: `tcp`, `grpc`, `http`, `http2`.
-    Protocol ProtocolType `json:"protocol"`
+	// Type of the match, only `HostnameGenerator` is available at the moment.
+	Type MatchType `json:"type"`
+	// Port defines a port to which a user does request.
+	Port int `json:"port"`
+	// Protocol defines a protocol of the communication. Possible values: `tcp`, `grpc`, `http`, `http2`.
+	Protocol ProtocolType `json:"protocol"`
 }
 
 type Extension struct {
-    // Type of the extension.
-    Type string `json:"type"`
-    // Config freeform configuration for the extension.
-    Config *apiextensionsv1.JSON `json:"config"`
+	// Type of the extension.
+	Type string `json:"type"`
+	// Config freeform configuration for the extension.
+	Config *apiextensionsv1.JSON `json:"config"`
 }
 
 // +kubebuilder:validation:Minimum=1
@@ -47,25 +48,25 @@ type Extension struct {
 type EndpointPort int
 
 type Endpoint struct {
-    // Address defines an address to which a user want to send a request. Is possible to provide `domain`, `ip` and `unix` sockets.
-    // +kubebuilder:example="127.0.0.1"
-    // +kubebuilder:example="example.com"
-    // +kubebuilder:example="unix:///tmp/example.sock"
-    // +kubebuilder:validation:MinLength=1
-    Address string `json:"address"`
-    // Port of the endpoint
-    Port EndpointPort `json:"port,omitempty"`
+	// Address defines an address to which a user want to send a request. Is possible to provide `domain`, `ip` and `unix` sockets.
+	// +kubebuilder:example="127.0.0.1"
+	// +kubebuilder:example="example.com"
+	// +kubebuilder:example="unix:///tmp/example.sock"
+	// +kubebuilder:validation:MinLength=1
+	Address string `json:"address"`
+	// Port of the endpoint
+	Port EndpointPort `json:"port,omitempty"`
 }
 
 type Tls struct {
-    // Enabled defines if proxy should originate TLS.
-    Enabled bool `json:"enabled,omitempty"`
-    // Version section for providing version specification.
-    Version TlsVersion `json:"version"`
-    // AllowRenegotiation defines if TLS sessions will allow renegotiation.
-    AllowRenegotiation bool `json:"allowRenegotiation"`
-    // Verification section for providing TLS verification details.
-    Verification Verification `json:"verification"`
+	// Enabled defines if proxy should originate TLS.
+	Enabled bool `json:"enabled,omitempty"`
+	// Version section for providing version specification.
+	Version TlsVersion `json:"version"`
+	// AllowRenegotiation defines if TLS sessions will allow renegotiation.
+	AllowRenegotiation bool `json:"allowRenegotiation"`
+	// Verification section for providing TLS verification details.
+	Verification Verification `json:"verification"`
 }
 
 // +kubebuilder:validation:Enum=TLSAuto;TLS10;TLS11;TLS12;TLS13
@@ -73,10 +74,10 @@ type Tls struct {
 type TlsMinMaxVersion string
 
 type TlsVersion struct {
-    // Min defines minimum supported version. One of `TLSAuto`, `TLS10`, `TLS11`, `TLS12`, `TLS13`.
-    Min TlsMinMaxVersion `json:"min"`
-    // Max defines maximum supported version. One of `TLSAuto`, `TLS10`, `TLS11`, `TLS12`, `TLS13`.
-    Max TlsMinMaxVersion `json:"max"`
+	// Min defines minimum supported version. One of `TLSAuto`, `TLS10`, `TLS11`, `TLS12`, `TLS13`.
+	Min TlsMinMaxVersion `json:"min"`
+	// Max defines maximum supported version. One of `TLSAuto`, `TLS10`, `TLS11`, `TLS12`, `TLS13`.
+	Max TlsMinMaxVersion `json:"max"`
 }
 
 // +kubebuilder:validation:Enum=SkipSAN;SkipCA;Secured;SkipALL
@@ -84,65 +85,65 @@ type TlsVersion struct {
 type VerificationMode string
 
 type Verification struct {
-    // Mode defines if proxy should skip verification, one of `SkipSAN`, `SkipCA`, `Secured`, `SkipALL`. Default `Secured`.
-    Mode VerificationMode `json:"mode"`
-    // SubjectAltNames list of names to verify in the certificate.
-    SubjectAltNames []SANMatch `json:"subjectAltNames"`
-    // CaCert defines a certificate of CA.
-    CaCert *v1alpha1.DataSource `json:"caCert"`
-    // ClientCert defines a certificate of a client.
-    ClientCert *v1alpha1.DataSource `json:"clientCert"`
-    // ClientKey defines a client private key.
-    ClientKey *v1alpha1.DataSource `json:"clientKey"`
+	// Mode defines if proxy should skip verification, one of `SkipSAN`, `SkipCA`, `Secured`, `SkipALL`. Default `Secured`.
+	Mode VerificationMode `json:"mode"`
+	// SubjectAltNames list of names to verify in the certificate.
+	SubjectAltNames []SANMatch `json:"subjectAltNames"`
+	// CaCert defines a certificate of CA.
+	CaCert *v1alpha1.DataSource `json:"caCert"`
+	// ClientCert defines a certificate of a client.
+	ClientCert *v1alpha1.DataSource `json:"clientCert"`
+	// ClientKey defines a client private key.
+	ClientKey *v1alpha1.DataSource `json:"clientKey"`
 }
 
 // +kubebuilder:validation:Enum=Exact;Prefix
 type SANMatchType string
 
 type SANMatch struct {
-    // Type specifies matching type, one of `Exact`, `Prefix`. Default: `Exact`
-    Type SANMatchType `json:"type"`
-    // Value to match.
-    Value string `json:"value"`
+	// Type specifies matching type, one of `Exact`, `Prefix`. Default: `Exact`
+	Type SANMatchType `json:"type"`
+	// Value to match.
+	Value string `json:"value"`
 }
 
 type MeshExternalServiceStatus struct {
-    // Vip section for allocated IP
-    Vip       VipStatus `json:"vip"`
-    // Addresses section for generated domains
-    Addresses []Address `json:"addresses"`
+	// Vip section for allocated IP
+	Vip VipStatus `json:"vip"`
+	// Addresses section for generated domains
+	Addresses []Address `json:"addresses"`
 }
 
 // +kubebuilder:validation:Enum=Kuma
 type StatusType string
 
 type VipStatus struct {
-    // Value allocated IP for a provided domain with `HostnameGenerator` type in a match section or provided IP.
-    Value string `json:"value"`
-    // Type provides information about the way IP was provided.
-    Type  StatusType `json:"type"`
+	// Value allocated IP for a provided domain with `HostnameGenerator` type in a match section or provided IP.
+	Value string `json:"value"`
+	// Type provides information about the way IP was provided.
+	Type StatusType `json:"type"`
 }
 
 // +kubebuilder:validation:Enum=Available;NotAvailable
 type AddressStatus string
 
 type Address struct {
-    // Hostname of the generated domain
-    Hostname string `json:"hostname"`
-    // Status indicates if an address is available
-    Status   AddressStatus `json:"status"`
-    // Origin provides information what generated the vip
-    Origin   AddressOrigin `json:"origin"`
-    // +kubebuilder:example="addresses are overlapping with my-mesh-external-service-2"
-    Reason string `json:"reason"`
+	// Hostname of the generated domain
+	Hostname string `json:"hostname"`
+	// Status indicates if an address is available
+	Status AddressStatus `json:"status"`
+	// Origin provides information what generated the vip
+	Origin AddressOrigin `json:"origin"`
+	// +kubebuilder:example="addresses are overlapping with my-mesh-external-service-2"
+	Reason string `json:"reason"`
 }
 
 // +kubebuilder:validation:Enum=HostnameGenerator
 type OriginKind string
 
 type AddressOrigin struct {
-    // Kind points to entity kind that generated the domain.
-    Kind OriginKind `json:"kind"`
-    // Name of the entity that generated the domain.
-    Name string `json:"name"`
+	// Kind points to entity kind that generated the domain.
+	Kind OriginKind `json:"kind"`
+	// Name of the entity that generated the domain.
+	Name string `json:"name"`
 }
--- pkg/core/resources/apis/meshexternalservice/api/v1alpha1/validator.go
+++ pkg/core/resources/apis/meshexternalservice/api/v1alpha1/validator.go
@@ -1,23 +1,10 @@
 package v1alpha1
 
 import (
-	common_api "github.com/kumahq/kuma/api/common/v1alpha1"
-	"github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
 	"github.com/kumahq/kuma/pkg/core/validators"
 )
 
 func (r *MeshExternalServiceResource) validate() error {
 	var verr validators.ValidationError
-	path := validators.RootedAt("spec")
-	verr.AddErrorAt(path.Field("targetRef"), validateTop(r.Spec.TargetRef))
 	return verr.OrNil()
-}
-
-func validateTop(targetRef common_api.TargetRef) validators.ValidationError {
-	targetRefErr := mesh.ValidateTargetRef(targetRef, &mesh.ValidateTargetRefOpts{
-		SupportedKinds: []common_api.TargetRefKind{
-			// TODO add supported TargetRef kinds for this policy
-		},
-	})
-	return targetRefErr
 }
