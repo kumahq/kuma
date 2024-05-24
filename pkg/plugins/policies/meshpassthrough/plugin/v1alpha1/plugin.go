4c3c0fbf6468e117206eef2e37d00db0e0f72a56
Lukasz Dziedziak <lukidzi@gmail.com>
2024-05-24 16:59:38 -0500

    init
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/plugins/policies/meshpassthrough/plugin/v1alpha1/plugin.go
+++ pkg/plugins/policies/meshpassthrough/plugin/v1alpha1/plugin.go
@@ -1,13 +1,21 @@
 package v1alpha1
 
 import (
+	"fmt"
+	"strings"
+
+	envoy_listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
 	"github.com/kumahq/kuma/pkg/core"
 	core_plugins "github.com/kumahq/kuma/pkg/core/plugins"
 	core_mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
 	core_xds "github.com/kumahq/kuma/pkg/core/xds"
 	"github.com/kumahq/kuma/pkg/plugins/policies/core/matchers"
+	core_rules "github.com/kumahq/kuma/pkg/plugins/policies/core/rules"
+	policies_xds "github.com/kumahq/kuma/pkg/plugins/policies/core/xds"
 	api "github.com/kumahq/kuma/pkg/plugins/policies/meshpassthrough/api/v1alpha1"
+	"github.com/kumahq/kuma/pkg/util/pointer"
 	xds_context "github.com/kumahq/kuma/pkg/xds/context"
+	v3 "github.com/kumahq/kuma/pkg/xds/envoy/listeners/v3"
 )
 
 var (
@@ -22,10 +30,143 @@ func NewPlugin() core_plugins.Plugin {
 }
 
 func (p plugin) MatchedPolicies(dataplane *core_mesh.DataplaneResource, resources xds_context.Resources, opts ...core_plugins.MatchedPoliciesOption) (core_xds.TypedMatchingPolicies, error) {
-	return matchers.MatchedPolicies(api.MeshPassthroughType, dataplane, resources)
+	return matchers.MatchedPolicies(api.MeshPassthroughType, dataplane, resources, opts...)
 }
 
 func (p plugin) Apply(rs *core_xds.ResourceSet, ctx xds_context.Context, proxy *core_xds.Proxy) error {
-	log.Info("apply is not implemented")
+	if proxy.Dataplane == nil {
+		return nil
+	}
+	policies, ok := proxy.Policies.Dynamic[api.MeshPassthroughType]
+	if !ok {
+		return nil
+	}
+	if proxy.Dataplane != nil && proxy.Dataplane.Spec.Networking.TransparentProxying == nil {
+		policies.Warnings = append(policies.Warnings, fmt.Sprintf("policy doesn't support proxy running without transparent-proxy"))
+		return nil
+	}
+	
+	listeners := policies_xds.GatherListeners(rs)
+	if err := applyToInbounds(ctx, policies.SingleItemRules, listeners.Ipv4Passthrough, proxy.Dataplane); err != nil {
+		return err
+	}
 	return nil
 }
+
+func applyToInbounds(
+	ctx xds_context.Context,
+	rules core_rules.SingleItemRules,
+	passthrough *envoy_listener.Listener,
+	dataplane *core_mesh.DataplaneResource,
+) error {
+	if len(rules.Rules) == 0 {
+		return nil
+	}
+	rawConf := rules.Rules[0].Conf
+	conf := rawConf.(api.Conf)
+
+	if ctx.Mesh.Resource.Spec.IsPassthrough() && !pointer.Deref[bool](conf.Enabled) && len(conf.AppendMatch) == 0 {
+		// remove cluster
+	} else if !ctx.Mesh.Resource.Spec.IsPassthrough() && pointer.Deref[bool](conf.Enabled) {
+		// add cluster
+	} else if ctx.Mesh.Resource.Spec.IsPassthrough() && !pointer.Deref[bool](conf.Enabled) && len(conf.AppendMatch) > 0 {
+		// remove default and add matchers
+		_ = orderRules(conf)
+		// generateMatchers := generateMatchers(ordered, passthrough)
+	} else if !ctx.Mesh.Resource.Spec.IsPassthrough() && !pointer.Deref[bool](conf.Enabled) && len(conf.AppendMatch) > 0{
+		// ordered := orderRules(conf)
+	}
+
+	return nil
+}
+
+func generateMatchers(orderedRules map[string]map[int]map[MatchInfo][]api.Match, passthrough *envoy_listener.Listener) error {
+	configurer := &v3.TLSInspectorConfigurer{}
+	err := configurer.Configure(passthrough)
+	if err != nil {
+		return err
+	}
+
+	// passthrough.FilterChainMatcher := &v32.Matcher{
+	// 	MatcherType: &v32.Matcher_MatcherTree_{
+	// 		MatcherTree: &v32.Matcher_MatcherTree{
+	// 			Input: &v3.TypedExtensionConfig{
+
+	// 			}},
+	// 	},
+	// }
+
+
+	return nil
+}
+
+func orderRules(conf api.Conf) map[string]map[int]map[MatchInfo][]api.Match {
+	ordered := map[string]map[int]map[MatchInfo][]api.Match{}
+	for _, match := range conf.AppendMatch{
+		matchInfo := MatchInfo{
+			Type: match.Type,
+			IsWildcard: strings.HasPrefix(match.Value, "*"),
+		}
+		switch match.Protocol {
+		case "tls":
+			if _, ok := ordered["tls"]; !ok {
+				ordered["tls"] = map[int]map[MatchInfo][]api.Match{}
+			}
+			port := 0
+			if match.Port != nil {
+				port = *match.Port
+			}
+			if _, ok := ordered["tls"][port]; !ok {
+				ordered["tls"][port] = map[MatchInfo][]api.Match{}
+			}
+			if _, ok := ordered["tls"][port][matchInfo]; !ok {
+				ordered["tls"][port][matchInfo] = []api.Match{}
+			}
+			ordered["tls"][port][matchInfo] = append(ordered["tls"][port][matchInfo], match)
+		default:
+			if _, ok := ordered["raw_buffer"]; !ok {
+				ordered["raw_buffer"] = map[int]map[MatchInfo][]api.Match{}
+			}
+			port := 0
+			if match.Port != nil {
+				port = *match.Port
+			}
+			if _, ok := ordered["raw_buffer"][port]; !ok {
+				ordered["raw_buffer"][port] = map[MatchInfo][]api.Match{}
+			}
+			if _, ok := ordered["raw_buffer"][port][matchInfo]; !ok {
+				ordered["raw_buffer"][port][matchInfo] = []api.Match{}
+			}
+			ordered["raw_buffer"][port][matchInfo] = append(ordered["raw_buffer"][port][matchInfo], match)
+		}
+	}
+
+	// for _, protocol := range ordered {
+	// 	for _, port := range ordered[protocol] {
+	// 		orderedList := []MatchInfo{}
+
+	// 		for orderedList
+			
+	// 	}
+	// }
+	return ordered
+}
+
+type MatchInfo struct {
+	Type api.MatchType
+	IsWildcard bool
+}
+
+// tls 
+// 		port
+//			server name
+//			ip address
+//		ip address
+// raw_buffer
+// 		port
+//			http listener/hostheader
+//			source ip
+//		source ip
+// 
+
+// map[protocol]map[port][] string
---
+++ pkg/plugins/policies/meshpassthrough/plugin/v1alpha1/plugin_test.go
@@ -0,0 +1,117 @@
+package v1alpha1_test
+
+import (
+	"path/filepath"
+
+	envoy_resource "github.com/envoyproxy/go-control-plane/pkg/resource/v3"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	core_plugins "github.com/kumahq/kuma/pkg/core/plugins"
+	core_xds "github.com/kumahq/kuma/pkg/core/xds"
+	core_rules "github.com/kumahq/kuma/pkg/plugins/policies/core/rules"
+	plugins_xds "github.com/kumahq/kuma/pkg/plugins/policies/core/xds"
+	api "github.com/kumahq/kuma/pkg/plugins/policies/meshpassthrough/api/v1alpha1"
+	plugin "github.com/kumahq/kuma/pkg/plugins/policies/meshpassthrough/plugin/v1alpha1"
+	test_matchers "github.com/kumahq/kuma/pkg/test/matchers"
+	"github.com/kumahq/kuma/pkg/test/resources/builders"
+	xds_builders "github.com/kumahq/kuma/pkg/test/xds/builders"
+	xds_samples "github.com/kumahq/kuma/pkg/test/xds/samples"
+	"github.com/kumahq/kuma/pkg/util/pointer"
+	util_proto "github.com/kumahq/kuma/pkg/util/proto"
+	envoy_common "github.com/kumahq/kuma/pkg/xds/envoy"
+	. "github.com/kumahq/kuma/pkg/xds/envoy/listeners"
+	"github.com/kumahq/kuma/pkg/xds/generator"
+)
+
+var _ = Describe("MeshPassthrough", func() {
+	type sidecarTestCase struct {
+		resources            []*core_xds.Resource
+    singleItemRules      core_rules.SingleItemRules  
+		expectedListeners    []string
+	}
+	DescribeTable("should generate proper Envoy config",
+		func(given sidecarTestCase) {
+			// given
+			resourceSet := core_xds.NewResourceSet()
+			resourceSet.Add(given.resources...)
+
+			context := xds_samples.SampleContext()
+			proxy := xds_builders.Proxy().
+				WithDataplane(
+					builders.Dataplane().
+						WithName("test").
+						WithMesh("default").
+						WithAddress("127.0.0.1").
+            WithTransparentProxying(15006, 15001, "ipv4").
+						AddInbound(
+							builders.Inbound().
+								WithAddress("127.0.0.1").
+								WithPort(17777).
+								WithService("backend"),
+						),
+				).
+				WithPolicies(
+					xds_builders.MatchedPolicies().WithSingleItemPolicy(api.MeshPassthroughType, given.singleItemRules),
+				).
+				Build()
+			plugin := plugin.NewPlugin().(core_plugins.PolicyPlugin)
+
+			// when
+			Expect(plugin.Apply(resourceSet, context, proxy)).To(Succeed())
+
+			// then
+			for i, expected := range given.expectedListeners {
+				Expect(util_proto.ToYAML(resourceSet.ListOf(envoy_resource.ListenerType)[i].Resource)).To(test_matchers.MatchGoldenYAML(filepath.Join("testdata", expected)))
+			}
+		},
+		Entry("basic listener", sidecarTestCase{
+			resources: []*core_xds.Resource{
+        {
+					Name:   "outbound:passthrough:ipv4",
+					Origin: generator.OriginOutbound,
+					Resource: NewOutboundListenerBuilder(envoy_common.APIV3, "0.0.0.0", 15001, core_xds.SocketAddressProtocolTCP).
+						Configure(FilterChain(NewFilterChainBuilder(envoy_common.APIV3, envoy_common.AnonymousResource).
+							Configure(TCPProxy("outbound_passthrough_ipv4", []envoy_common.Split{
+                plugins_xds.NewSplitBuilder().WithClusterName("outbound:passthrough:ipv4").WithWeight(100).Build(),
+              }...)),
+						)).MustBuild(),
+				},
+			},
+      singleItemRules: core_rules.SingleItemRules{
+				Rules: []*core_rules.Rule{
+					{
+						Subset: []core_rules.Tag{},
+						Conf: api.Conf{
+							AppendMatch: []api.Match{
+                {
+                  Type:  api.MatchType("Domain"),
+                  Value: "example.com",
+                  Port:  pointer.To[int](443),
+                  Protocol: api.ProtocolType("tls"),
+                },
+                {
+                  Type:  api.MatchType("Domain"),
+                  Value: "example.com",
+                  Port:  pointer.To[int](8080),
+                  Protocol: api.ProtocolType("http"),
+                },
+                {
+                  Type:  api.MatchType("IP"),
+                  Value: "192.168.0.1",
+                  Port:  pointer.To[int](9091),
+                  Protocol: api.ProtocolType("tcp"),
+                },
+                {
+                  Type:  api.MatchType("CIDR"),
+                  Value: "192.168.0.1/24",
+                  Protocol: api.ProtocolType("tcp"),
+                },
+              },
+						},
+					},
+				},
+			},
+			expectedListeners:    []string{"basic_listener.golden.yaml"},
+		}),
+  )})
