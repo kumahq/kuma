2192f97bf1d110087888c7a1e475496c7ce08394
slonka <slonka@users.noreply.github.com>
2024-05-28 15:46:11 +0200

    feat(MeshExternalService): start adding clusters
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- pkg/xds/generator/meshexternalservice/generator.go
+++ pkg/xds/generator/meshexternalservice/generator.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/kumahq/kuma/pkg/core/resources/apis/meshexternalservice/api/v1alpha1"
 	envoy_common "github.com/kumahq/kuma/pkg/xds/envoy"
+	envoy_clusters "github.com/kumahq/kuma/pkg/xds/envoy/clusters"
 	"github.com/kumahq/kuma/pkg/xds/envoy/listeners"
 	"github.com/kumahq/kuma/pkg/xds/envoy/names"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
@@ -71,6 +72,16 @@ func (g Generator) generateResources(mes *v1alpha1.MeshExternalServiceResource, 
 		}
 		resources.AddSet(res)
 	} else {
+		cluster, err := createCluster(mes, version)
+		if err != nil {
+			return nil, err
+		}
+		resources.Add(&core_xds.Resource{
+			Name:     cluster.GetName(),
+			Origin:   OriginMeshExternalService,
+			Resource: cluster,
+		})
+
 		listener, err := createListener(mes, version)
 		if err != nil {
 			return nil, err
@@ -83,6 +94,22 @@ func (g Generator) generateResources(mes *v1alpha1.MeshExternalServiceResource, 
 	}
 
 	return resources, nil
+}
+
+func createCluster(mes *v1alpha1.MeshExternalServiceResource, version core_xds.APIVersion) (envoy_common.NamedResource, error) {
+	name := mes.Meta.GetName()
+	clusterName := names.GetMeshExternalServiceClusterName(name)
+	builder := envoy_clusters.NewClusterBuilder(version, clusterName)
+
+	var endpoints []core_xds.Endpoint
+	for _, e := range mes.Spec.Endpoints {
+		endpoints = append(endpoints, core_xds.Endpoint{
+			Target: e.Address,
+		})
+	}
+	builder.Configure(envoy_clusters.ProvidedEndpointCluster(false))
+
+	return builder.Build()
 }
 
 func createListener(mes *v1alpha1.MeshExternalServiceResource, version core_xds.APIVersion) (envoy_common.NamedResource, error) {
