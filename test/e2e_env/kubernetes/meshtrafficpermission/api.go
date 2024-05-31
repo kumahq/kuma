8e97f0f1aac39794a3be152e7912a1fbc3e38e47
Ilya Lobkov <ilya.lobkov@konghq.com>
2024-05-31 15:57:58 +0200

    feat(kuma-cp): remove e2e tests that are no longer relevant
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- test/e2e_env/kubernetes/meshtrafficpermission/api.go
+++ test/e2e_env/kubernetes/meshtrafficpermission/api.go
@@ -3,7 +3,6 @@ package meshtrafficpermission
 import (
 	"fmt"
 
-	"github.com/gruntwork-io/terratest/modules/k8s"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
@@ -77,37 +76,5 @@ spec:
 		Expect(err).ToNot(HaveOccurred())
 		Expect(mtps).To(HaveLen(1))
 		Expect(mtps[0]).To(Equal(fmt.Sprintf("mtp1.%s", Config.KumaNamespace)))
-	})
-
-	It("should deny creating policy in the non-system namespace", func() {
-		// given no MeshTrafficPermissions
-		mtps, err := kubernetes.Cluster.GetKumactlOptions().KumactlList("meshtrafficpermissions", meshName)
-		Expect(err).ToNot(HaveOccurred())
-		Expect(mtps).To(BeEmpty())
-
-		// when
-		err = k8s.KubectlApplyFromStringE(
-			kubernetes.Cluster.GetTesting(),
-			kubernetes.Cluster.GetKubectlOptions(), `
-apiVersion: kuma.io/v1alpha1
-kind: MeshTrafficPermission
-metadata:
-  name: mtp1
-  namespace: default
-  labels:
-    kuma.io/mesh: meshtrafficpermission-api
-spec:
-  targetRef:
-    kind: MeshService
-    name: backend
-  from:
-    - targetRef:
-        kind: Mesh
-      default:
-        action: Allow
-`)
-
-		Expect(err).To(HaveOccurred())
-		Expect(err.Error()).To(ContainSubstring(fmt.Sprintf("policy can only be created in the system namespace:%s", Config.KumaNamespace)))
 	})
 }
