78ca6b4bc57b603f71c64a9c04e41a6d43dad011
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2024-06-13 17:24:23 +0800

    chore(make): support extra targets for docs
    Signed-off-by: Jay Chen <1180092+jijiechen@users.noreply.github.com>

--- mk/docs.mk
+++ mk/docs.mk
@@ -1,12 +1,13 @@
 DOCS_PROTOS ?= api/mesh/v1alpha1/*.proto
 DOCS_CP_CONFIG ?= pkg/config/app/kuma-cp/kuma-cp.defaults.yaml
+DOCS_EXTRA_TARGETS ?=
 
 .PHONY: clean/docs
 clean/docs:
 	rm -rf docs/generated
 
 .PHONY: docs
-docs: helm-docs docs/generated/raw docs/generated/openapi.yaml ## Dev: Generate local documentation
+docs: helm-docs docs/generated/raw docs/generated/openapi.yaml $(DOCS_EXTRA_TARGETS) ## Dev: Generate local documentation
 
 .PHONY: helm-docs
 helm-docs: ## Dev: Runs helm-docs generator
@@ -17,9 +18,6 @@ docs/generated/raw:
 	mkdir -p $@
 	cp $(DOCS_CP_CONFIG) $@/kuma-cp.yaml
 	cp $(HELM_VALUES_FILE) $@/helm-values.yaml
-
-	mkdir -p $@/helm-values-prod
-	for f in $$(find deployments/charts -name '*.yaml' | grep 'values.prod.'); do cp $$f $@/helm-values-prod/$$(basename $$f | sed 's/.prod//'); done
 
 	mkdir -p $@/crds
 	for f in $$(find deployments/charts -name '*.yaml' | grep '/crds/'); do cp $$f $@/crds/; done
