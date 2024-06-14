8e9f6e4125bb15f64b02e959c0775c7b74210088
Mike Beaumont <mjboamail@gmail.com>
2024-06-14 19:01:18 +0200

    fix(kuma-cp): consistently update ZoneIngress available services (#10426)
    * feat(kuma-cp): update ZoneIngress in separate component
    * test(e2e): add ability to have multiple ZoneIngresses
    * test(e2e): make killing and starting an app easier
    * test(e2e): add test for incorrect ZoneIngress available services behavior
    
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>
    Co-authored-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- app/kuma-cp/cmd/run.go
+++ app/kuma-cp/cmd/run.go
@@ -27,6 +27,7 @@ import (
 	"github.com/kumahq/kuma/pkg/util/os"
 	kuma_version "github.com/kumahq/kuma/pkg/version"
 	"github.com/kumahq/kuma/pkg/xds"
+	"github.com/kumahq/kuma/pkg/zone"
 )
 
 var runLog = controlPlaneLog.WithName("run")
@@ -145,6 +146,21 @@ func newRunCmdWithOpts(opts kuma_cmd.RunCmdOpts) *cobra.Command {
 				runLog.Error(err, "unable to set up Control Plane Intercommunication")
 				return err
 			}
+<<<<<<< HEAD
+=======
+			if err := ipam.Setup(rt); err != nil {
+				runLog.Error(err, "unable to set up IPAM")
+				return err
+			}
+			if err := zone.Setup(rt); err != nil {
+				runLog.Error(err, "unable to set up ZoneIngress available services")
+				return err
+			}
+			if err := dns.SetupHostnameGenerator(rt); err != nil {
+				runLog.Error(err, "unable to set up hostname generator")
+				return err
+			}
+>>>>>>> 08bb1373c (fix(kuma-cp): consistently update ZoneIngress available services (#10426))
 
 			runLog.Info("starting Control Plane", "version", kuma_version.Build.Version)
 			if err := rt.Start(gracefulCtx.Done()); err != nil {
--- pkg/config/loader_test.go
+++ pkg/config/loader_test.go
@@ -612,6 +612,7 @@ multizone:
       responseBackoff: 2s
       tlsSkipVerify: true
     disableOriginLabelValidation: true
+    ingressUpdateInterval: 2s
 dnsServer:
   domain: test-domain
   CIDR: 127.1.0.0/16
@@ -938,6 +939,7 @@ tracing:
 				"KUMA_MULTIZONE_ZONE_KDS_RESPONSE_BACKOFF":                                                 "2s",
 				"KUMA_MULTIZONE_ZONE_KDS_TLS_SKIP_VERIFY":                                                  "true",
 				"KUMA_MULTIZONE_ZONE_DISABLE_ORIGIN_LABEL_VALIDATION":                                      "true",
+				"KUMA_MULTIZONE_ZONE_INGRESS_UPDATE_INTERVAL":                                              "2s",
 				"KUMA_EXPERIMENTAL_KDS_DELTA_ENABLED":                                                      "true",
 				"KUMA_MULTIZONE_GLOBAL_KDS_ZONE_INSIGHT_FLUSH_INTERVAL":                                    "5s",
 				"KUMA_DEFAULTS_SKIP_MESH_CREATION":                                                         "true",
