011bab987f0c07f8136ab339d65da5de98cb9e6a
Mike Beaumont <mjboamail@gmail.com>
2024-06-17 17:31:25 +0200

    chore: fix conflicts
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- test/e2e_env/multizone/multizone_suite_test.go
+++ test/e2e_env/multizone/multizone_suite_test.go
@@ -76,9 +76,5 @@ var (
 	_ = Describe("Advanced LocalityAwareness with MeshLoadBalancingStrategy with Gateway", localityawarelb.LocalityAwareLBGateway, Ordered)
 	_ = Describe("Advanced LocalityAwareness with MeshLoadBalancingStrategy and Enabled Egress", localityawarelb.LocalityAwareLBEgress, Ordered)
 	_ = Describe("Defaults", defaults.Defaults, Ordered)
-<<<<<<< HEAD
-=======
-	_ = Describe("MeshService", Label("ipv6-not-supported"), meshservice.MeshService, Ordered)
 	_ = Describe("Available services", connectivity.AvailableServices, Ordered)
->>>>>>> 08bb1373c (fix(kuma-cp): consistently update ZoneIngress available services (#10426))
 )
