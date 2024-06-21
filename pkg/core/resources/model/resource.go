2896dd48d541c8b0dca1356ef9e85dbb47951299
Marcin Skalski <marcin.skalski@konghq.com>
2024-06-21 14:39:27 +0200

    fix(kuma-cp): don't add zone on global originated (#10592)
    fix(kuma-c): don't add zone label when resource originates from global
    
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- pkg/core/resources/model/resource.go
+++ pkg/core/resources/model/resource.go
@@ -456,7 +456,9 @@ func ComputeLabels(r Resource, mode config_core.CpMode, isK8s bool, systemNamesp
 
 	if mode == config_core.Zone {
 		setIfNotExist(mesh_proto.ResourceOriginLabel, string(mesh_proto.ZoneResourceOrigin))
-		setIfNotExist(mesh_proto.ZoneTag, localZone)
+		if labels[mesh_proto.ResourceOriginLabel] != string(mesh_proto.GlobalResourceOrigin) {
+			setIfNotExist(mesh_proto.ZoneTag, localZone)
+		}
 	}
 
 	if ns, ok := labels[mesh_proto.KubeNamespaceTag]; ok && r.Descriptor().IsPolicy && r.Descriptor().IsPluginOriginated {
