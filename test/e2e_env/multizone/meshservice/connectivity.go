7dd2a35d5630716a6e70538420ba2d7f3ff1d059
Mike Beaumont <mjboamail@gmail.com>
2024-06-25 22:38:09 +0200

    test(e2e): use MeshService managed-by label, same as when autogenerated
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- test/e2e_env/multizone/meshservice/connectivity.go
+++ test/e2e_env/multizone/meshservice/connectivity.go
@@ -28,7 +28,7 @@ spec:
     meshService:
       matchLabels:
         kuma.io/origin: global
-        kuma.io/env: kubernetes
+        kuma.io/managed-by: k8s-controller
 `)).
 			Install(YamlUniversal(`
 type: HostnameGenerator
@@ -53,7 +53,7 @@ metadata:
   labels:
     kuma.io/origin: zone
     kuma.io/mesh: msconnectivity
-    kuma.io/env: kubernetes
+    kuma.io/managed-by: k8s-controller
 spec:
   selector:
     dataplaneTags:
