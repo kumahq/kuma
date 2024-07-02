21d2448a09234048f69d3d88c7cc244eeb250420
Bart Smykla <bartek@smykla.com>
2024-07-02 10:02:56 +0200

    test(framework): init TProxyConfig only in tproxy tests (#10706)
    TProxyConfig is used only in tproxy tests, and when it was
    initialized in `init()` function, it was run even for e2e tests,
    which in case of legacy e2e tests was failing because of missing
    kumactl binary.
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- test/transparentproxy/transparentproxy_suite_test.go
+++ test/transparentproxy/transparentproxy_suite_test.go
@@ -6,10 +6,13 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 
 	"github.com/kumahq/kuma/pkg/test"
+	"github.com/kumahq/kuma/test/framework"
 	"github.com/kumahq/kuma/test/transparentproxy/install"
 )
 
 func TestTransparentProxy(t *testing.T) {
+	framework.InitTproxyConfig()
+
 	test.RunSpecs(t, "Transparent Proxy Suite")
 }
 
