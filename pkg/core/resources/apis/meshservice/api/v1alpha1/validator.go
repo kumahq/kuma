e4d0de26697dd4654068396ef87fe8b82ad66b4d
Mike Beaumont <mjboamail@gmail.com>
2024-07-02 13:24:26 +0200

    fix(MeshService): validate name that will become the display name
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- pkg/core/resources/apis/meshservice/api/v1alpha1/validator.go
+++ pkg/core/resources/apis/meshservice/api/v1alpha1/validator.go
@@ -1,13 +1,15 @@
 package v1alpha1
 
 import (
+	"github.com/kumahq/kuma/pkg/core/resources/model"
 	"github.com/kumahq/kuma/pkg/core/validators"
 )
 
 func (r *MeshServiceResource) validate() error {
 	var verr validators.ValidationError
 
-	verr.Add(validators.ValidateLength(validators.RootedAt("name"), 63, r.GetMeta().GetName()))
+	name := model.GetDisplayName(r.GetMeta())
+	verr.Add(validators.ValidateLength(validators.RootedAt("name"), maxNameLength, name))
 
 	return verr.OrNil()
 }
