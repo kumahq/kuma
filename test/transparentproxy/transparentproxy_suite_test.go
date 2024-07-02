2b23e00564cc6706264e0139692f7a1a4c9a02e9
Bart Smykla <bartek@smykla.com>
2024-07-02 10:19:04 +0200

    test(framework): move tproxy tests config from test framework
    This Config is and will be used only in transparent proxy tests,
    so we can safely move it there. As now there is no conflict in
    names, I renamev it from `TProxyConfig` to just `Config`.
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- test/transparentproxy/transparentproxy_suite_test.go
+++ test/transparentproxy/transparentproxy_suite_test.go
@@ -6,13 +6,10 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 
 	"github.com/kumahq/kuma/pkg/test"
-	"github.com/kumahq/kuma/test/framework"
 	"github.com/kumahq/kuma/test/transparentproxy/install"
 )
 
 func TestTransparentProxy(t *testing.T) {
-	framework.InitTproxyConfig()
-
 	test.RunSpecs(t, "Transparent Proxy Suite")
 }
 
