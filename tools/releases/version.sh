90b2732876d1735a611e38b4633215845055bb20
Mike Beaumont <mjboamail@gmail.com>
2024-07-18 18:19:57 +0200

    chore(deps): upgrade envoy with DNS fix (#10933)
    * chore(deps): upgrade envoy with DNS fix
    * build: make envoy version handling more flexible
    
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- tools/releases/version.sh
+++ tools/releases/version.sh
@@ -8,17 +8,12 @@ function envoy_version() {
   # - if ENVOY_TAG is a real git tag like 'v1.20.0' then the version is equal to '1.20.0' (without the first letter 'v').
   # - if ENVOY_TAG is a commit hash then the version will look like '1.20.1-dev-b16d390f'
 
-  ENVOY_TAG=${ENVOY_TAG:-"v1.29.7"}
-  ENVOY_VERSION=$(curl --silent --location "https://raw.githubusercontent.com/envoyproxy/envoy/${ENVOY_TAG}/VERSION.txt")
+  ENVOY_TAG=${ENVOY_TAG:-"v1.29.7-k.1"}
 
-  # for envoy versions older than v1.22.0 file 'VERSION.txt' used to be called 'VERSION'
-  if [[ "${ENVOY_VERSION}" == "404: Not Found" ]]; then
-    ENVOY_VERSION=$(curl --silent --location --fail "https://raw.githubusercontent.com/envoyproxy/envoy/${ENVOY_TAG}/VERSION")
-  fi
-
-  if [[ "${ENVOY_TAG}" =~ ^v[0-9]*\.[0-9]*\.[0-9]*$ ]]; then
-    echo "${ENVOY_VERSION}"
+  if [[ "${ENVOY_TAG}" =~ ^v[0-9]*\.[0-9]*\.[0-9]* ]]; then
+    echo "${ENVOY_TAG#v}"
   else
+    ENVOY_VERSION=$(curl --silent --location "https://raw.githubusercontent.com/envoyproxy/envoy/${ENVOY_TAG%%-*}/VERSION.txt")
     echo "${ENVOY_VERSION}-${ENVOY_TAG:0:8}"
   fi
   set +o errexit
