454ef3d984dc15edc9b03f6b761d98dd00905b03
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2024-07-22 15:38:35 +0800

    use docker credentials specified by CI secret
    Signed-off-by: Jay Chen <1180092+jijiechen@users.noreply.github.com>

--- tools/ci/prepare-pull-credentials.sh
+++ tools/ci/prepare-pull-credentials.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-# set -x
+set -e
 
-DOCKER_AUTH=$(jq -r '.auths | keys | .[]' $HOME/.docker/config.json | grep docker)
-CRED=$(jq -r ".auths[\"$DOCKER_AUTH\"].auth //empty" $HOME/.docker/config.json)
-if [[ "$CRED" == "" ]]; then
-    >&2 echo "No existing docker auth information found on the host"
+if [[ "$DOCKERHUB_PULL_CREDENTIAL" == "" ]]; then
+    >&2 echo "No docker pull credential information specified"
 else
-    DOCKER_USER=$(echo $CRED | base64 --decode | cut -d ':' -f 1)
-    DOCKER_PWD=$(echo $CRED | base64 --decode | cut -d ':' -f 2)
+    DOCKER_USER=$(echo $DOCKERHUB_PULL_CREDENTIAL | base64 --decode | cut -d ':' -f 1)
+    DOCKER_PWD=$(echo $DOCKERHUB_PULL_CREDENTIAL | base64 --decode | cut -d ':' -f 2)
+    echo -n $(DOCKER_PWD) | docker login -u $(DOCKER_USER) --password-stdin > /dev/null
     cat <<EOF > $HOME/.docker/k3d-registry.yaml
 configs:
   registry-1.docker.io:
@@ -20,10 +19,10 @@ EOF
     echo "{\"auths\":{\"https://index.docker.io/v1/\":{\"auth\":\"$CRED\"}}}" > $HOME/.docker/kind-config.json
     EXTRA_MOUNTS="[ {\"containerPath\": \"/var/lib/kubelet/config.json\", \"hostPath\": \"$HOME/.docker/kind-config.json\"}]"
     for FILE in $(ls test/kind/*.yaml); do
-        if [[ ! -z "$(yq '.nodes[] | select(.role == "control-plane") | length' $FILE)" ]]; then
-            yq -i ".nodes[] | select(.role == \"control-plane\") *= {\"extraMounts\":$EXTRA_MOUNTS} | parent | parent" $FILE
+        if [[ "$($CI_TOOLS_DIR/bin/yq '.nodes[] | select(.role == "control-plane") | length' $FILE)" != "" ]]; then
+            $CI_TOOLS_DIR/bin/yq -i ".nodes[] | select(.role == \"control-plane\") *= {\"extraMounts\":$EXTRA_MOUNTS} | parent | parent" $FILE
         else
-            yq -i ".nodes = [ { \"role\": \"control-plane\", \"extraMounts\": $EXTRA_MOUNTS }]" $FILE
+            $CI_TOOLS_DIR/bin/yq -i ".nodes = [ { \"role\": \"control-plane\", \"extraMounts\": $EXTRA_MOUNTS }]" $FILE
         fi
     done
 fi
