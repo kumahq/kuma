6bf7baa324ce04791b5a49e90f0f1efda19243cf
Marcin Skalski <skalskimarcin33@gmail.com>
2024-08-26 17:25:31 +0200

    feat(policy): add e2e test
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- test/e2e_env/multizone/meshservice/policies.go
+++ test/e2e_env/multizone/meshservice/policies.go
@@ -8,6 +8,7 @@ import (
 
 	core_mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
 	"github.com/kumahq/kuma/pkg/plugins/policies/meshhttproute/api/v1alpha1"
+	meshtcproute_api "github.com/kumahq/kuma/pkg/plugins/policies/meshtcproute/api/v1alpha1"
 	. "github.com/kumahq/kuma/test/framework"
 	"github.com/kumahq/kuma/test/framework/client"
 	"github.com/kumahq/kuma/test/framework/deployments/testserver"
@@ -40,6 +41,11 @@ func MeshServiceTargeting() {
 				testserver.WithMesh(meshName),
 				testserver.WithNamespace(namespace),
 			)).
+			Install(testserver.Install(
+				testserver.WithName("second-test-server"),
+				testserver.WithMesh(meshName),
+				testserver.WithNamespace(namespace),
+			)).
 			Install(YamlK8s(fmt.Sprintf(`
 apiVersion: kuma.io/v1alpha1
 kind: HostnameGenerator
@@ -67,6 +73,7 @@ spec:
 
 	E2EAfterEach(func() {
 		Expect(DeleteMeshResources(multizone.KubeZone1, meshName, v1alpha1.MeshHTTPRouteResourceTypeDescriptor)).To(Succeed())
+		Expect(DeleteMeshResources(multizone.KubeZone1, meshName, meshtcproute_api.MeshTCPRouteResourceTypeDescriptor)).To(Succeed())
 		Expect(DeleteMeshResources(multizone.KubeZone1, meshName, core_mesh.ExternalServiceResourceTypeDescriptor)).To(Succeed())
 	})
 
@@ -109,9 +116,68 @@ spec:
 `, namespace, meshName))(multizone.KubeZone1)).To(Succeed())
 		// then receive redirect response
 		Eventually(func(g Gomega) {
-			resp, err := client.CollectEchoResponse(multizone.KubeZone1, "test-client", fmt.Sprintf("%s/prefix/world", addressToMeshService("test-server")), client.FromKubernetesPod(namespace, "test-client"))
+			resp, err := client.CollectEchoResponse(
+				multizone.KubeZone1,
+				"test-client",
+				fmt.Sprintf("%s/prefix/world", addressToMeshService("test-server")),
+				client.FromKubernetesPod(namespace, "test-client"),
+			)
 			g.Expect(err).ToNot(HaveOccurred())
 			g.Expect(resp.Received.Path).To(Equal("/hello/world"))
+		}, "30s", "1s").Should(Succeed())
+	})
+
+	It("should route to second test server", func() {
+		// given
+		// should route to original MeshService
+		Eventually(func(g Gomega) {
+			response, err := client.CollectEchoResponse(
+				multizone.KubeZone1,
+				"test-client",
+				addressToMeshService("test-server"),
+				client.FromKubernetesPod(namespace, "test-client"),
+			)
+			g.Expect(err).ToNot(HaveOccurred())
+			g.Expect(response.Instance).To(HavePrefix("test-server"))
+		}, "30s", "1s").Should(Succeed())
+
+		// when
+		Expect(YamlK8s(fmt.Sprintf(`
+apiVersion: kuma.io/v1alpha1
+kind: MeshTCPRoute
+metadata:
+  name: tcp-overwrite-to-real-meshservice
+  namespace: %s
+  labels:
+    kuma.io/mesh: %s
+    kuma.io/origin: zone
+spec:
+  targetRef:
+    kind: Mesh
+  to:
+  - targetRef:
+      kind: MeshService
+      name: test-server
+      sectionName: main
+    rules: 
+    - default:
+        backendRefs:
+        - kind: MeshService
+          name: second-test-server.%s
+          port: 80
+`, namespace, meshName, namespace))(multizone.KubeZone1)).To(Succeed())
+
+		// then
+		// should route to second MeshService
+		Eventually(func(g Gomega) {
+			response, err := client.CollectEchoResponse(
+				multizone.KubeZone1,
+				"test-client",
+				addressToMeshService("test-server"),
+				client.FromKubernetesPod(namespace, "test-client"),
+			)
+			g.Expect(err).ToNot(HaveOccurred())
+			g.Expect(response.Instance).To(HavePrefix("second-test-server"))
 		}, "30s", "1s").Should(Succeed())
 	})
 }
