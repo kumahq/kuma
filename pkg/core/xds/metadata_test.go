28b312abad484739e8d2a8dd6c8815070461641e
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2024-08-28 15:44:34 +0800

    fix conflicts
    Signed-off-by: Jay Chen <1180092+jijiechen@users.noreply.github.com>

--- pkg/core/xds/metadata_test.go
+++ pkg/core/xds/metadata_test.go
@@ -54,18 +54,14 @@ var _ = Describe("DataplaneMetadataFromXdsMetadata", func() {
 							StringValue: "8000",
 						},
 					},
-<<<<<<< HEAD
 					"dataplane.dns.empty.port": {
-=======
+						Kind: &structpb.Value_StringValue{
+							StringValue: "8001",
+						},
+					},
 					"dataplane.readinessReporter.port": {
 						Kind: &structpb.Value_StringValue{
 							StringValue: "9300",
-						},
-					},
-					"systemCaPath": {
->>>>>>> 20208eb60 (feat(kuma-dp): add a separate component to handle kuma-sidecar readiness probes (#11107))
-						Kind: &structpb.Value_StringValue{
-							StringValue: "8001",
 						},
 					},
 					"accessLogSocketPath": {
@@ -81,18 +77,12 @@ var _ = Describe("DataplaneMetadataFromXdsMetadata", func() {
 				},
 			},
 			expected: xds.DataplaneMetadata{
-<<<<<<< HEAD
 				AdminPort:           1234,
 				DNSPort:             8000,
 				EmptyDNSPort:        8001,
 				AccessLogSocketPath: "/tmp/logs",
 				MetricsSocketPath:   "/tmp/metrics",
-=======
-				AdminPort:     1234,
-				DNSPort:       8000,
-				SystemCaPath:  "/etc/certs/cert.pem",
-				ReadinessPort: 9300,
->>>>>>> 20208eb60 (feat(kuma-dp): add a separate component to handle kuma-sidecar readiness probes (#11107))
+				ReadinessPort:       9300,
 			},
 		}),
 		Entry("should ignore dependencies version provided through metadata if version is not set at all", testCase{
