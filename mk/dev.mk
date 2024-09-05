87154fad3f8205ab52d21edc5ab4b9d6f7890f1d
Marcin Skalski <skalskimarcin33@gmail.com>
2024-09-05 16:28:54 +0200

    feat(kuma-cp): fix check
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- mk/dev.mk
+++ mk/dev.mk
@@ -24,7 +24,7 @@ CI_TOOLS_BIN_DIR=$(CI_TOOLS_DIR)/bin
 K8S_MIN_VERSION = v1.23.17-k3s1
 K8S_MAX_VERSION = v1.30.0-k3s1
 export GO_VERSION=$(shell go mod edit -json | jq -r .Go)
-export GOLANGCI_LINT_VERSION=v1.59.1
+export GOLANGCI_LINT_VERSION=v1.59.0
 GOOS := $(shell go env GOOS)
 GOARCH := $(shell go env GOARCH)
 
