6184d8ac15d7c5b92d3f8f71484f8578647cce01
Lukasz Dziedziak <lukidzi@gmail.com>
2024-09-18 06:58:27 -0500

    test(meshtls): added an E2E test for delegated gateway (#11440)
    * fix(policy-gen): check lower case name for a suffix
    
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>
    
    * test(e2e): added E2E test for meshtls
    
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>
    
    ---------
    
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- test/e2e_env/kubernetes/gateway/delegated.go
+++ test/e2e_env/kubernetes/gateway/delegated.go
@@ -161,4 +161,5 @@ spec:
 	Context("MeshAccessLog", delegated.MeshAccessLog(&config))
 	XContext("MeshTCPRoute", delegated.MeshTCPRoute(&config))
 	Context("MeshPassthrough", delegated.MeshPassthrough(&config))
+	Context("MeshTLS", delegated.MeshTLS(&config))
 }
