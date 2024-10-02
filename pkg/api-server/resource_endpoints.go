ccdbb368f5d31054f6c4a206e88dc60c504b78b9
Ilya Lobkov <ilya.lobkov@konghq.com>
2024-10-02 12:07:12 +0200

    fix(inspect-api): don't set 'toRules' when the meshService mode is 'Exclusive'.
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- pkg/api-server/resource_endpoints.go
+++ pkg/api-server/resource_endpoints.go
@@ -898,12 +898,14 @@ func (r *resourceEndpoints) rulesForResource() restful.RouteFunction {
 				continue
 			}
 			toRules := []api_common.Rule{}
-			for _, ruleItem := range res.ToRules.Rules {
-				toRules = append(toRules, api_common.Rule{
-					Conf:     ruleItem.Conf,
-					Matchers: oapi_helpers.SubsetToRuleMatcher(ruleItem.Subset),
-					Origin:   oapi_helpers.ResourceMetaListToMetaList(res.Type, ruleItem.Origin),
-				})
+			if baseMeshContext.Mesh.Spec.MeshServicesMode() != mesh_proto.Mesh_MeshServices_Exclusive {
+				for _, ruleItem := range res.ToRules.Rules {
+					toRules = append(toRules, api_common.Rule{
+						Conf:     ruleItem.Conf,
+						Matchers: oapi_helpers.SubsetToRuleMatcher(ruleItem.Subset),
+						Origin:   oapi_helpers.ResourceMetaListToMetaList(res.Type, ruleItem.Origin),
+					})
+				}
 			}
 			var proxyRule *api_common.ProxyRule
 			if len(res.SingleItemRules.Rules) > 0 {
