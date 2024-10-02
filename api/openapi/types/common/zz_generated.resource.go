9892588f2e1c89f82d300818ce397be323443768
Ilya Lobkov <ilya.lobkov@konghq.com>
2024-10-02 15:03:43 +0200

    fix(inspect-api): don't set 'toRules' when `meshServices.mode: Exclusive` (#11623)
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- api/openapi/types/common/zz_generated.resource.go
+++ api/openapi/types/common/zz_generated.resource.go
@@ -33,10 +33,10 @@ type InspectRule struct {
 	FromRules *[]FromRule `json:"fromRules,omitempty"`
 	ProxyRule *ProxyRule  `json:"proxyRule,omitempty"`
 
-	// ToResourceRules a set of rules for the resources targeted by this proxy
+	// ToResourceRules a set of rules for the outbounds produced by real resources (i.e MeshService, MeshExternalService, MeshMultiZoneService).
 	ToResourceRules *[]ResourceRule `json:"toResourceRules,omitempty"`
 
-	// ToRules a set of rules for the outbounds of this proxy
+	// ToRules a set of rules for the outbounds of this proxy. The field is not set when 'meshService.mode' on Mesh is set to 'Exclusive'.
 	ToRules *[]Rule `json:"toRules,omitempty"`
 
 	// Type the type of the policy
