7c46fdf77c8f4cbc2a91ec31e74df9b1b4980d1c
Mike Beaumont <mjboamail@gmail.com>
2024-10-25 06:26:39 +0200

    ci(k8s): download calico manifests as needed (#11851)
    This way we don't have Calico CRDs show up here
    https://doc.crds.dev/github.com/kumahq/kuma@2.9.0
    
    Use make to download the manifests as needed per K3D version.
    
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- mk/dependencies/k3d.sh
+++ mk/dependencies/k3d.sh
@@ -3,7 +3,19 @@
 set -e
 
 OUTPUT_DIR=$1/bin
+<<<<<<< HEAD
 VERSION="5.6.0"
 # see https://raw.githubusercontent.com/rancher/k3d/main/install.sh
 curl --fail --location -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | \
           PATH=${OUTPUT_DIR}:${PATH} TAG=v${VERSION} USE_SUDO="false" K3D_INSTALL_DIR="${OUTPUT_DIR}" bash
+=======
+VERSION="5.7.4"
+
+if [[ $2 == "get-version" ]]; then
+  echo ${VERSION}
+else
+  # see https://raw.githubusercontent.com/rancher/k3d/main/install.sh
+  curl --fail --location -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | \
+            PATH=${OUTPUT_DIR}:${PATH} TAG=v${VERSION} USE_SUDO="false" K3D_INSTALL_DIR="${OUTPUT_DIR}" bash
+fi
+>>>>>>> 529694bad (ci(k8s): download calico manifests as needed (#11851))
