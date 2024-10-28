a6cb79fd7b3f68d99c643cf68391db940e4bdd62
Charly Molter <charly.molter@konghq.com>
2024-10-28 18:16:57 +0100

    improve docs
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- pkg/mads/v1/service/mads.go
+++ pkg/mads/v1/service/mads.go
@@ -2,8 +2,6 @@ package service
 
 import (
 	"context"
-	"errors"
-
 	envoy_sd "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 	envoy_cache "github.com/envoyproxy/go-control-plane/pkg/cache/v3"
 	"github.com/envoyproxy/go-control-plane/pkg/server/stream/v3"
@@ -36,7 +34,11 @@ func (s *server) FetchMonitoringAssignments(ctx context.Context, req *envoy_sd.D
 	}
 	resChan := make(chan envoy_cache.Response, 1)
 	streamState := stream.NewStreamState(false, nil)
-	// Because we want to do long polling we use the watch system if there's a deadline on the context
+	// When the context has a deadline we want to do long polling
+	// We therefore create a watch that will have 2 possible outcome:
+	// 1. context reaches deadline, in which nothing changed we're at the end of the long polling time
+	// 2. watch resolves, the cache changed, let's return the new info
+	// Note that for simplicity we don't use the value returned by the watch and simply fetch from the cache
 	if _, hasDeadline := ctx.Deadline(); hasDeadline {
 		cancelWatch := s.cache.CreateWatch(req, streamState, resChan)
 		if cancelWatch != nil {
@@ -47,14 +49,15 @@ func (s *server) FetchMonitoringAssignments(ctx context.Context, req *envoy_sd.D
 			}
 		}
 	}
+	// Don't include the deadline here as it is used for long polling.
 	resp, err := s.cache.Fetch(context.WithoutCancel(ctx), req)
 	if err != nil {
 		return nil, err
 	}
-	if resp == nil {
-		return nil, errors.New("missing response")
-	}
 	out, err := resp.GetDiscoveryResponse()
+	if err != nil {
+		return nil, err
+	}
 	if s.callbacks != nil {
 		s.callbacks.OnFetchResponse(req, out)
 	}
