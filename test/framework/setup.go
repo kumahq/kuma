b955f20c0b3c4d7975b42a20c4985eea7e36cc4e
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2024-10-29 10:16:38 +0100

    test(e2e): speed up reachable services (#11893)
    ## Motivation
    
    Let's try to speed up some non-shared e2e tests.
    They take as much resources as all e2e_env combined.
    
    Before
    ```
    ❯❯❯ python test-time.py
    Test: [It] > Auto Reachable Services on Kubernetes > should not connect to non auto reachable service, Duration: 74.248 seconds
    Test: [It] > Auto Reachable Mesh Services on Kubernetes > should not connect to non auto reachable service, Duration: 61.293 seconds
    Test: [It] > Auto Reachable Services on Kubernetes > should connect to ExternalService when MeshTrafficPermission defined, Duration: 60.537 seconds
    ```
    196s
    
    ```
    Test: [It] > Auto Reachable Mesh Services on Kubernetes > should not connect to non auto reachable service, Duration: 24.698 seconds
    Test: [It] > Auto Reachable Services on Kubernetes > should connect to ExternalService when MeshTrafficPermission defined, Duration: 23.711 seconds
    Test: [It] > Auto Reachable Services on Kubernetes > should not connect to non auto reachable service, Duration: 19.414 seconds
    Test: [BeforeSuite] > , Duration: 17.187 seconds
    Test: [AfterSuite] > , Duration: 15.619 seconds
    ```
    100s
    
    ## Implementation information
    
    1) Reuse created Kuma deployment for reachable services and reachable
    backends
    2) Adjust Consistently to more reasonable times
    3) Setup deployments in parallel (gives 38s boost)
    
    This strategy is useful, because
    1) we can merge it with for example CNI to save cluster setup / cleanup.
    2) potentially we can run this in parallel just like `e2e_env`
    3) potentially we can later move this as a separate zone that we spawn
    in `e2e_env`
    
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>

--- test/framework/setup.go
+++ test/framework/setup.go
@@ -12,6 +12,7 @@ import (
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/retry"
 	"github.com/pkg/errors"
+	"golang.org/x/sync/errgroup"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -992,6 +993,17 @@ func (cs *ClusterSetup) Install(fn InstallFunc) *ClusterSetup {
 
 func (cs *ClusterSetup) Setup(cluster Cluster) error {
 	return Combine(cs.installFuncs...)(cluster)
+}
+
+func (cs *ClusterSetup) SetupInParallel(cluster Cluster) error {
+	errGroup := errgroup.Group{}
+	for _, f := range cs.installFuncs {
+		fn := f
+		errGroup.Go(func() error {
+			return fn(cluster)
+		})
+	}
+	return errGroup.Wait()
 }
 
 func (cs *ClusterSetup) SetupWithRetries(cluster Cluster, maxRetries int) error {
