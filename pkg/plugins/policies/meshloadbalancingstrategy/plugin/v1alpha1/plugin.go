799b52ac2187cd8abf584ae2dfbdc61f7c0c10ec
Mike Beaumont <mjboamail@gmail.com>
2024-11-05 20:51:18 +0100

    fix(MeshLoadBalancingStrategy): set all priorities equal is localityAwareness is disabled
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- pkg/plugins/policies/meshloadbalancingstrategy/plugin/v1alpha1/plugin.go
+++ pkg/plugins/policies/meshloadbalancingstrategy/plugin/v1alpha1/plugin.go
@@ -233,6 +233,15 @@ func configureEndpoints(
 			}
 		}
 	}
+	if conf.LocalityAwareness != nil && pointer.Deref(conf.LocalityAwareness.Disabled) {
+		for _, cla := range endpoints {
+			for _, localityLbEndpoints := range cla.Endpoints {
+				if localityLbEndpoints.Locality != nil && localityLbEndpoints.Locality.Zone != localZone {
+					localityLbEndpoints.Priority = 0
+				}
+			}
+		}
+	}
 	return nil
 }
 
