df9f344df0bc136cbabe2279fa9244ce0c2b002d
Lukasz Dziedziak <lukidzi@gmail.com>
2024-12-02 10:12:31 +0100

    small refactor
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/plugins/policies/meshpassthrough/plugin/xds/order.go
+++ pkg/plugins/policies/meshpassthrough/plugin/xds/order.go
@@ -118,12 +118,12 @@ func GetOrderedMatchers(conf api.Conf) ([]FilterChainMatch, error) {
 					MatchType: matcher.MatchType,
 					Value:     matcher.Value,
 				}
-				if existingRoutes, found := matcherWithRoutesAndAdditionalPorts[additionalMatcher]; found {
+				if _, found := matcherWithRoutesAndAdditionalPorts[additionalMatcher]; found {
 					for route := range routes {
 						matcherWithRoutesAndAdditionalPorts[additionalMatcher][route] = true
 					}
 				} else {
-					matcherWithRoutesAndAdditionalPorts[additionalMatcher] = existingRoutes
+					matcherWithRoutesAndAdditionalPorts[additionalMatcher] = map[Route]bool{}
 				}
 			}
 		}
