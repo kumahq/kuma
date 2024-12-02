fed27c10bfddbe7d47fdd46d3ddc1f142e8c3638
Lukasz Dziedziak <lukidzi@gmail.com>
2024-12-02 10:04:11 +0100

    Update pkg/plugins/policies/meshloadbalancingstrategy/api/v1alpha1/deprecated.go
    Co-authored-by: Bart Smykla <bartek@smykla.com>
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/plugins/policies/meshloadbalancingstrategy/api/v1alpha1/deprecated.go
+++ pkg/plugins/policies/meshloadbalancingstrategy/api/v1alpha1/deprecated.go
@@ -15,25 +15,27 @@ func (t *MeshLoadBalancingStrategyResource) Deprecations() []string {
 func validateHashPoliciesType(confs []To) []string {
 	deprecations := []string{}
 	for ruleIdx, conf := range confs {
-		if conf.Default.LoadBalancer != nil {
-			switch conf.Default.LoadBalancer.Type {
-			case RingHashType:
-				if conf.Default.LoadBalancer.RingHash == nil || conf.Default.LoadBalancer.RingHash.HashPolicies == nil {
-					continue
+		if conf.Default.LoadBalancer == nil {
+			continue
+		}
+	
+		switch conf.Default.LoadBalancer.Type {
+		case RingHashType:
+			if conf.Default.LoadBalancer.RingHash == nil || conf.Default.LoadBalancer.RingHash.HashPolicies == nil {
+				continue
+			}
+			for lbIdx, lbConf := range *conf.Default.LoadBalancer.RingHash.HashPolicies {
+				if lbConf.Type == SourceIPType {
+					deprecations = append(deprecations, fmt.Sprintf("%s type for 'spec.to[%d].default.loadBalancer.ringHash.hashPolicies[%d].type' is deprecated, use %s instead", SourceIPType, ruleIdx, lbIdx, ConnectionType))
 				}
-				for lbIdx, lbConf := range *conf.Default.LoadBalancer.RingHash.HashPolicies {
-					if lbConf.Type == SourceIPType {
-						deprecations = append(deprecations, fmt.Sprintf("%s type for 'spec.to[%d].default.loadBalancer.ringHash.hashPolicies[%d].type' is deprecated, use %s instead", SourceIPType, ruleIdx, lbIdx, ConnectionType))
-					}
-				}
-			case MaglevType:
-				if conf.Default.LoadBalancer.Maglev == nil || conf.Default.LoadBalancer.Maglev.HashPolicies == nil {
-					continue
-				}
-				for lbIdx, lbConf := range *conf.Default.LoadBalancer.Maglev.HashPolicies {
-					if lbConf.Type == SourceIPType {
-						deprecations = append(deprecations, fmt.Sprintf("%s type for 'spec.to[%d].default.loadBalancer.maglev.hashPolicies[%d].type' is deprecated, use %s instead", SourceIPType, ruleIdx, lbIdx, ConnectionType))
-					}
+			}
+		case MaglevType:
+			if conf.Default.LoadBalancer.Maglev == nil || conf.Default.LoadBalancer.Maglev.HashPolicies == nil {
+				continue
+			}
+			for lbIdx, lbConf := range *conf.Default.LoadBalancer.Maglev.HashPolicies {
+				if lbConf.Type == SourceIPType {
+					deprecations = append(deprecations, fmt.Sprintf("%s type for 'spec.to[%d].default.loadBalancer.maglev.hashPolicies[%d].type' is deprecated, use %s instead", SourceIPType, ruleIdx, lbIdx, ConnectionType))
 				}
 			}
 		}
