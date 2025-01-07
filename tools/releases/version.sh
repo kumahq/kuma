3ae1fbcdc53aa49c8db37423d4ce3a75a99cd4f1
Lukasz Dziedziak <lukidzi@gmail.com>
2025-01-07 13:57:32 +0100

    chore(deps): upgrade envoy from v1.29.11 to v1.29.12 (#12454)
    ## Motivation
    
    New patch release with CVE fix:
    https://github.com/envoyproxy/envoy/releases/tag/v1.29.12
    
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- tools/releases/version.sh
+++ tools/releases/version.sh
@@ -8,7 +8,7 @@ function envoy_version() {
   # - if ENVOY_TAG is a real git tag like 'v1.20.0' then the version is equal to '1.20.0' (without the first letter 'v').
   # - if ENVOY_TAG is a commit hash then the version will look like '1.20.1-dev-b16d390f'
 
-  ENVOY_TAG=${ENVOY_TAG:-"v1.29.11"}
+  ENVOY_TAG=${ENVOY_TAG:-"v1.29.12"}
 
   if [[ "${ENVOY_TAG}" =~ ^v[0-9]*\.[0-9]*\.[0-9]* ]]; then
     echo "${ENVOY_TAG#v}"
