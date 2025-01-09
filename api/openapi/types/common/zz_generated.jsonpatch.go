d78ec690ecf7dd423368e46193a610e480a26faf
slonka <slonka@users.noreply.github.com>
2025-01-09 15:56:55 +0100

    chore(oapi): fix any value
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- api/openapi/types/common/zz_generated.jsonpatch.go
+++ api/openapi/types/common/zz_generated.jsonpatch.go
@@ -16,11 +16,10 @@ type JsonPatchItem struct {
 	Op JsonPatchItemOp `json:"op"`
 
 	// Path A JSON Pointer path indicating the part of the document to operate on.
-	Path  string `json:"path"`
-	Value struct {
-		// AnyValue The value to be used within the operations.
-		AnyValue *interface{} `json:"AnyValue,omitempty"`
-	} `json:"value"`
+	Path string `json:"path"`
+
+	// Value The value to be used within the operations.
+	Value interface{} `json:"value"`
 }
 
 // JsonPatchItemOp Operation to be performed.
