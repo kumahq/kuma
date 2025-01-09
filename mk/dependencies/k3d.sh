5851c8295e03a09a4087090db2386666bf183bd9
Bart Smykla <bartek@smykla.com>
2025-01-09 14:27:59 +0100

    ci(security): resolve security alerts (backport #12466) (#12496)
    Scorecard has some security alerts that need to be addressed.
    
    - Pinned git commit hashes for scripts sourced from GitHub repositories
    in `mk/dependencies/*.sh` scripts.
    - Pinned hashes for some GitHub Actions.
    - Added comments to cron schedules for CodeQL and Scorecard GitHub
    Actions.
    - Removed unnecessary `contents: read` permission in the CodeQL GitHub
    Action.
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- mk/dependencies/k3d.sh
+++ mk/dependencies/k3d.sh
@@ -8,7 +8,6 @@ VERSION="5.4.7"
 if [[ $2 == "get-version" ]]; then
   echo ${VERSION}
 else
-  # see https://raw.githubusercontent.com/rancher/k3d/main/install.sh
-  curl --fail --location -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | \
+  curl --fail --location -s https://raw.githubusercontent.com/rancher/k3d/4709d6adb24b23721f471e667e7301fa673b5efc/install.sh | \
             PATH=${OUTPUT_DIR}:${PATH} TAG=v${VERSION} USE_SUDO="false" K3D_INSTALL_DIR="${OUTPUT_DIR}" bash
 fi
