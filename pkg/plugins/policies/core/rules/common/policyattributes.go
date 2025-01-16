4dfa80cd9656c1aac1f9ea621851353fdd39a365
Ilya Lobkov <ilya.lobkov@konghq.com>
2025-01-16 07:18:40 +0100

    add comments
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- pkg/plugins/policies/core/rules/common/policyattributes.go
+++ pkg/plugins/policies/core/rules/common/policyattributes.go
@@ -11,10 +11,23 @@ type PolicyAttributes interface {
 	GetRuleIndex() int
 }
 
+// Entry is a piece of configuration that is part of a policy. Outbound policies, for example, have a list of entries called 'to'.
+// Inbound policies at this moment have a list of entries called 'from'. Entries in 'from' and 'to' have the same type:
+//
+//	type PolicyItem interface {
+//	    GetTargetRef() common_api.TargetRef
+//	    GetDefault() interface{}
+//	}
+//
+// But 'from' list of entries is going to be replaced with 'rules' according to docs/madr/decisions/069-inbound-policies.md.
+// Entries in 'to' and entries in 'rules' won't have the same type anymore, they're going to have 'ToEntry' and 'RuleEntry'
+// types respectively. So, we need to make 'Entry' generic to be able to use it in shared packages like 'sort', 'merge' and 'common'.
 type Entry[T BaseEntry] interface {
 	GetEntry() T
 }
 
+// BaseEntry is a base interface for all entries in policies. Regardless of the type of the entry,
+// it should always contain a piece of configuration.
 type BaseEntry interface {
 	GetDefault() interface{}
 }
