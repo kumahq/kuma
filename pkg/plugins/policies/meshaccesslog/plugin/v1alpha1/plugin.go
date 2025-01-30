959d7062e9a6c15c3ddf1e15adf92df6ef7b3582
Marcin Skalski <skalskimarcin33@gmail.com>
2025-01-30 08:16:24 +0100

    feat(kuma-cp): fix check
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- pkg/plugins/policies/meshaccesslog/plugin/v1alpha1/plugin.go
+++ pkg/plugins/policies/meshaccesslog/plugin/v1alpha1/plugin.go
@@ -6,7 +6,6 @@ import (
 	"github.com/pkg/errors"
 
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
-	"github.com/kumahq/kuma/pkg/core"
 	core_plugins "github.com/kumahq/kuma/pkg/core/plugins"
 	core_mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
 	"github.com/kumahq/kuma/pkg/core/xds"
@@ -77,8 +76,6 @@ func (p plugin) Apply(rs *core_xds.ResourceSet, ctx xds_context.Context, proxy *
 
 	return nil
 }
-
-var log = core.Log.WithName("kuma-cp")
 
 func applyToInbounds(rules core_rules.FromRules, inboundListeners map[core_rules.InboundListener]*envoy_listener.Listener, dataplane *core_mesh.DataplaneResource, backends *plugin_xds.EndpointAccumulator, path string) error {
 	for _, inbound := range dataplane.Spec.GetNetworking().GetInbound() {
