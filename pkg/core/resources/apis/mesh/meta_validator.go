4e3c0ae592894da36cc4c602529c51660cd8bdff
Lukasz Dziedziak <lukidzi@gmail.com>
2025-02-04 08:05:16 -0500

    chore(cleanup): remove deprecated code (#12714)
    ## Motivation
    
    We deprecated using `_` in a resource name in 2.5.x and we were supposed
    to remove it in 2.7.x. but we forgot about it. This pr removes this
    function and adjust to the change
    
    fix: https://github.com/kumahq/kuma/issues/12670
    
    ---------
    
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>
    Co-authored-by: Charly Molter <charly@molter.io>

--- pkg/core/resources/apis/mesh/meta_validator.go
+++ pkg/core/resources/apis/mesh/meta_validator.go
@@ -25,31 +25,6 @@ func ValidateMeta(m core_model.ResourceMeta, scope core_model.ResourceScope) val
 	return err
 }
 
-// ValidateMetaBackwardsCompatible allows 'name' and 'mesh' to contain '_'. Should be called when updating existing
-// resources or on the clients (like 'kumactl' or 'kuma-dp') when it's not clear if it's create or update operation.
-//
-// We're going to remove this function in Kuma 2.7.x
-func ValidateMetaBackwardsCompatible(m core_model.ResourceMeta, scope core_model.ResourceScope) (validators.ValidationError, string) {
-	// trying to validate meta against the new (more strict) rules
-	if verr := ValidateMeta(m, scope); !verr.HasViolations() {
-		return validators.ValidationError{}, ""
-	}
-
-	var err validators.ValidationError
-	err.AddError("name", validateIdentifier(m.GetName(), backwardCompatRegexp, backwardCompatErrMsg))
-	err.Add(ValidateMesh(m.GetMesh(), scope))
-
-	if !err.HasViolations() {
-		// ResourceMeta doesn't pass new validation, but passes old validation. In that case we'd like to warn user about
-		// the new validation rules
-		return validators.ValidationError{}, "[WARNING] Kuma 2.5.x introduces more strict validation rules for resource name, it must consist of " +
-			"lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character. You can't " +
-			"CREATE resources with invalid names anymore, but it's possible to UPDATE such resources until Kuma 2.7.x is released."
-	}
-
-	return err, ""
-}
-
 // ValidateMesh checks that resource's mesh matches the old regex (with '_'). Even if user creates entirely new resource,
 // we can't check resource's mesh against the new regex, because Mesh resource itself can be old and contain '_' in its name.
 // All new Mesh resources will have their name validated against new regex.
