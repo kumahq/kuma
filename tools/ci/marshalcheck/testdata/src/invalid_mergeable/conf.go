6fe977a6534437ba0a488965eff44dd74093d91a
slonka <slonka@users.noreply.github.com>
2025-02-10 11:05:15 +0100

    ci(lint): tests working
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- tools/ci/marshalcheck/testdata/src/invalid_mergeable/conf.go
+++ tools/ci/marshalcheck/testdata/src/invalid_mergeable/conf.go
@@ -3,9 +3,9 @@ package invalid_mergeable
 type Conf struct {
     MissingOmitEmpty *string `json:"missing_omit"` // want "field Conf.MissingOmitEmpty mergeable field must have 'omitempty' in JSON tag"
 
-    InvalidList []string `json:"invalid_list"` // want "field Conf.InvalidList \\(mergeable list\\) must be a pointer to a slice \\(e.g., \\*\\[\\]T\\)"
+    InvalidList []string `json:"invalid_list,omitempty"` // want "field Conf.InvalidList \\(mergeable list\\) must be a pointer to a slice \\(e\\.g\\., \\*\\[\\]T\\)"
 
-    InvalidListNoOmitEmpty *[]string `json:"invalid_list_no_omit"` // want "field Conf.InvalidListNoOmitEmpty \\(mergeable list\\) must have 'omitempty' in JSON tag"
+    InvalidListNoOmitEmpty *[]string `json:"invalid_list_no_omit"` // want "field Conf.InvalidListNoOmitEmpty mergeable field must have 'omitempty' in JSON tag"
 
     ValidList *[]string `json:"valid_list,omitempty"` // OK
 }
