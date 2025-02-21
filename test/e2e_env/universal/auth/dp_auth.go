54860294de67fa339fa4bb148b9e00c1a69de3f9
Jay Jijie Chen <1180092+jijiechen@users.noreply.github.com>
2025-02-21 14:41:22 +0800

    fix(xds): only auth once per xds gRPC stream in kuma-cp (backport of #12788) (#12842)
    Manual cherry-pick of #12788 to branch `release-2.7`
    
    cherry-picked commit 0323e80f4bea540c5f72e10d2a050ecd3a281085
    
    ---------
    
    Signed-off-by: Jay Chen <1180092+jijiechen@users.noreply.github.com>

--- test/e2e_env/universal/auth/dp_auth.go
+++ test/e2e_env/universal/auth/dp_auth.go
@@ -1,11 +1,6 @@
 package auth
 
 import (
-	"encoding/base64"
-	"fmt"
-	"math/rand"
-
-	"github.com/golang-jwt/jwt/v4"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
@@ -64,66 +59,5 @@ func DpAuth() {
 		Eventually(func() (string, error) {
 			return universal.Cluster.GetKumactlOptions().RunKumactlAndGetOutput("get", "dataplanes", "-oyaml")
 		}, "30s", "1s").ShouldNot(ContainSubstring("192.168.0.2"))
-	})
-
-	It("should revoke token and kick out dataplane proxy out of the mesh", func() {
-		// given
-		serviceName := "test-server-to-be-revoked"
-		token, err := universal.Cluster.GetKuma().GenerateDpToken(meshName, serviceName)
-		Expect(err).ToNot(HaveOccurred())
-
-		err = universal.Cluster.Install(TestServerUniversal(serviceName, meshName, WithServiceName(serviceName), WithToken(token)))
-		Expect(err).ToNot(HaveOccurred())
-
-		Eventually(func(g Gomega) {
-			online, found, err := IsDataplaneOnline(universal.Cluster, meshName, serviceName)
-			g.Expect(err).ToNot(HaveOccurred())
-			g.Expect(found).To(BeTrue())
-			g.Expect(online).To(BeTrue())
-		}).Should(Succeed())
-
-		// when token ID is added to revocation list
-		claims := &jwt.RegisteredClaims{}
-		_, _, err = jwt.NewParser().ParseUnverified(token, claims)
-		Expect(err).ToNot(HaveOccurred())
-
-		yaml := fmt.Sprintf(`
-type: Secret
-mesh: dp-auth
-name: dataplane-token-revocations-dp-auth
-data: %s`, base64.StdEncoding.EncodeToString([]byte(claims.ID)))
-		Expect(universal.Cluster.Install(YamlUniversal(yaml))).To(Succeed())
-
-		// then DPP is disconnected
-		Eventually(func(g Gomega) {
-			// we need to trigger XDS config change for this DP to disconnect it
-			// this limitation may be lifted in the future
-			yaml = fmt.Sprintf(`
-type: MeshRetry
-name: retry-policy
-mesh: dp-auth
-spec:
-  targetRef:
-    kind: MeshService
-    name: test-server-to-be-revoked
-  to:
-    - targetRef:
-        kind: MeshService
-        name: test-server-to-be-revoked
-      default:
-        http:
-          numRetries: %d
-          backOff:
-            baseInterval: 15s
-            maxInterval: 20m
-          retryOn:
-            - "5xx"
-`, rand.Int()%100+1) // #nosec G404 -- this is for tests no need to use secure rand
-			g.Expect(universal.Cluster.Install(YamlUniversal(yaml))).To(Succeed())
-
-			online, _, err := IsDataplaneOnline(universal.Cluster, meshName, serviceName)
-			g.Expect(err).ToNot(HaveOccurred())
-			g.Expect(online).To(BeFalse()) // either online or not found
-		}).Should(Succeed())
 	})
 }
