9195b5070edf364eac35db0332fea9779c4a498a
Charly Molter <charly.molter@konghq.com>
2025-02-26 19:04:46 +0100

    fix zone
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- pkg/kds/v2/client/zone_sync_test.go
+++ pkg/kds/v2/client/zone_sync_test.go
@@ -260,6 +260,8 @@ var _ = Describe("Zone Delta Sync", func() {
 			system.ConfigType,
 			system.GlobalSecretType,
 			hostnamegenerator_api.HostnameGeneratorType,
+			system.ZoneType,
+			system.ZoneInsightType,
 		}
 
 		actualConsumedTypes = append(actualConsumedTypes, extraTypes...)
