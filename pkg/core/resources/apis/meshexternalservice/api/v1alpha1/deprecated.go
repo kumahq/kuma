0015c05c211ec1f1f52c4073a7718ad29dd17d6d
Lukasz Dziedziak <lukidzi@gmail.com>
2025-03-06 13:43:27 -0500

    fix(resource): use kuma.io/display-name instead of name
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/core/resources/apis/meshexternalservice/api/v1alpha1/deprecated.go
+++ pkg/core/resources/apis/meshexternalservice/api/v1alpha1/deprecated.go
@@ -5,15 +5,18 @@ import (
 	"strings"
 
 	apimachineryvalidation "k8s.io/apimachinery/pkg/api/validation"
+
+	"github.com/kumahq/kuma/pkg/core/resources/model"
 )
 
 func (t *MeshExternalServiceResource) Deprecations() []string {
 	var deprecations []string
 
-	allErrs := apimachineryvalidation.NameIsDNS1035Label(t.GetMeta().GetName(), false)
+	name := model.GetDisplayName(t.GetMeta())
+	allErrs := apimachineryvalidation.NameIsDNS1035Label(name, false)
 	if len(allErrs) != 0 {
-		nameDeprecationMsg := fmt.Sprintf("Name that doesn't conform DNS (RFC 1035) format is deprecated: %s",
-			strings.Join(allErrs, "; "))
+		nameDeprecationMsg := fmt.Sprintf("Invalid name: '%s'. It does not conform to the DNS format (RFC 1035). This is deprecated. Errors: %s",
+			name, strings.Join(allErrs, "; "))
 		deprecations = append(deprecations, nameDeprecationMsg)
 	}
 
