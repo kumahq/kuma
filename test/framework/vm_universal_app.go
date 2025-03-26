819eb084519d275fbb84b437036a466a673377c5
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2025-03-26 18:01:36 +0800

    improve the bootstrap script when start universal app on VM
    Signed-off-by: Jay Chen <1180092+jijiechen@users.noreply.github.com>

--- test/framework/vm_universal_app.go
+++ test/framework/vm_universal_app.go
@@ -73,6 +73,7 @@ func (s *VmUniversalApp) GetIP() string {
 
 func (s *VmUniversalApp) Stop() error {
 	Logf("Stopping app:%q", s.appName)
+	_ = s.KillMainApp()
 	_ = s.universalNetworking.Close()
 	return fmt.Errorf("timed out waiting for app:%q to stop", s.appName)
 }
@@ -125,13 +126,13 @@ func (s *VmUniversalApp) CreateDP(
 	workingDir := fmt.Sprintf("/tmp/kuma")
 	// create the token file on the app container
 	_, _ = cmd.WriteString("#!/bin/sh\n")
-	_, _ = fmt.Fprintf(cmd, "mkdir -p %s/bin", workingDir)
+	_, _ = fmt.Fprintf(cmd, "mkdir -p %s/bin\n", workingDir)
 	_, _ = fmt.Fprintf(cmd, "printf %q > %s/token-%s\n", token, workingDir, name)
 
 	_, _ = fmt.Fprintf(cmd, `
 curl -L --no-progress-bar --fail '%s' | VERSION=%s sh
 DOWNLOAD_DIR=$(find -maxdepth 1 -type d -name '*-%s')
-mv $DOWNLOAD_DIR/bin/* %s/bin/
+mv -f $DOWNLOAD_DIR/bin/* %s/bin/
 export PATH=$PATH:%s/bin
 		`, Config.KumaInstallerUrl, Config.KumaImageTag, Config.KumaImageTag, workingDir, workingDir)
 
@@ -139,18 +140,28 @@ export PATH=$PATH:%s/bin
 		_, _ = fmt.Fprintf(cmd, "export %s=%s\n", k, utils.ShellEscape(v))
 	}
 
+	// todo: remove sudo prefix to adapt more distros
+
 	// Install transparent proxy
 	if transparent {
 		extraArgs := []string{}
 		if builtindns {
 			extraArgs = append(extraArgs, "--redirect-dns")
 		}
-		_, _ = fmt.Fprintf(cmd, "kumactl install transparent-proxy --exclude-inbound-ports %s %s\n", sshPort, strings.Join(extraArgs, " "))
+		_, _ = fmt.Fprintf(cmd, "sudo %s/bin/kumactl install transparent-proxy --exclude-inbound-ports %s %s\n",
+			workingDir, sshPort, strings.Join(extraArgs, " "))
+		_, _ = fmt.Fprintf(cmd, `function uninstall_transparent_proxy(){
+    sudo %s/bin/kumactl install transparent-proxy 
+}
+trap uninstall_transparent_proxy EXIT INT QUIT TERM
+\n`,
+			workingDir)
 	}
 
+	_, _ = fmt.Fprintf(cmd, "sudo useradd --system --no-create-home --shell /sbin/nologin kuma-dp\n")
 	// run the DP as user `envoy` so iptables can distinguish its traffic if needed
 	args := []string{
-		"runuser", "-u", "kuma-dp", "--",
+		"sudo", "runuser", "-u", "kuma-dp", "--",
 		"kuma-dp", "run",
 		"--cp-address=" + cpAddress,
 		fmt.Sprintf("--dataplane-token-file=%s/kuma/token-%s", workingDir, name),
