fef4c6e0c020e7837078ab16eac8d81d0f65aa39
slonka <slonka@users.noreply.github.com>
2025-04-07 09:07:33 +0200

    chore(logs): add more info to kuma-validation failure logs
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- pkg/transparentproxy/validate/validator.go
+++ pkg/transparentproxy/validate/validator.go
@@ -161,7 +161,8 @@ func (v *Validator) RunClient(
 			return nil
 		},
 	); err != nil {
-		return errors.Wrap(err, "client failed to connect to the verification server after retries")
+		return errors.Wrap(err, "client failed to connect to the verification server after retries - " +
+			"most likely iptables rules are not applied correctly, please check CNI logs")
 	}
 
 	v.Logger.Info("validation successful, iptables rules applied correctly")
