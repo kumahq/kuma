76d9da887dc94394d1e24025da908772408619c5
Lukasz Dziedziak <lukidzi@gmail.com>
2025-05-09 08:09:43 -0500

    feat(xds): rework DeltaXds to use metadata instead of proto (#13467)
    ## Motivation
    
    We shouldn't change the proto schema but instead use metadata.
    
    ## Implementation information
    
    * Remove proto file and field from Dataplane/ZoneIngress/ZoneEgress. It
    should be safe since clients (mink) were not able to set this property
    and only global knew about it
    * Introduce a new variable for kuma-dp
    `KUMA_DATAPLANE_RUNTIME_ENVOY_XDS_TRANSPORT_PROTOCOL_VARIANT` which
    allow setting specific variant of the xds protocol
    * Keep `kuma.io/xds-transport-protocol-variant` in the k8s to simplify
    delta testing for the user
    
    ## Supporting documentation
    
    Fix #13461
    
    ---------
    
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/xds/bootstrap/parameters.go
+++ pkg/xds/bootstrap/parameters.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
+	xds_types "github.com/kumahq/kuma/pkg/core/xds/types"
 	tproxy_config "github.com/kumahq/kuma/pkg/transparentproxy/config/dataplane"
 	"github.com/kumahq/kuma/pkg/xds/bootstrap/types"
 )
@@ -26,32 +27,32 @@ type AggregateMetricsConfig struct {
 }
 
 type configParameters struct {
-	Id                          string
-	Service                     string
-	AdminAddress                string
-	AdminPort                   uint32
-	ReadinessPort               uint32
-	AppProbeProxyEnabled        bool
-	AdminAccessLogPath          string
-	XdsHost                     string
-	XdsPort                     uint32
-	XdsConnectTimeout           time.Duration
-	Workdir                     string
-	MetricsCertPath             string
-	MetricsKeyPath              string
-	DataplaneToken              string
-	DataplaneTokenPath          string
-	DataplaneResource           string
-	CertBytes                   []byte
-	Version                     *mesh_proto.Version
-	HdsEnabled                  bool
-	DynamicMetadata             map[string]string
-	DNSPort                     uint32
-	ProxyType                   string
-	Features                    []string
-	IsGatewayDataplane          bool
-	Resources                   types.ProxyResources
-	SystemCaPath                string
-	TransparentProxy            *tproxy_config.DataplaneConfig
-	XdsTransportProtocolVariant types.XdsTransportProtocolVariant
+	Id                   string
+	Service              string
+	AdminAddress         string
+	AdminPort            uint32
+	ReadinessPort        uint32
+	AppProbeProxyEnabled bool
+	AdminAccessLogPath   string
+	XdsHost              string
+	XdsPort              uint32
+	XdsConnectTimeout    time.Duration
+	Workdir              string
+	MetricsCertPath      string
+	MetricsKeyPath       string
+	DataplaneToken       string
+	DataplaneTokenPath   string
+	DataplaneResource    string
+	CertBytes            []byte
+	Version              *mesh_proto.Version
+	HdsEnabled           bool
+	DynamicMetadata      map[string]string
+	DNSPort              uint32
+	ProxyType            string
+	Features             xds_types.Features
+	IsGatewayDataplane   bool
+	Resources            types.ProxyResources
+	SystemCaPath         string
+	TransparentProxy     *tproxy_config.DataplaneConfig
+	UseDeltaXds          bool
 }
