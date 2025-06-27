2f3d0d55a6edc4789ae75f4a7d153afceeb43784
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-06-27 14:09:37 +0200

    ci(lint): enable nestingReduce rule from go-critic
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- pkg/core/resources/apis/mesh/mesh_defaulter.go
+++ pkg/core/resources/apis/mesh/mesh_defaulter.go
@@ -10,52 +10,53 @@ import (
 func (mesh *MeshResource) Default() error {
 	// default settings for Prometheus metrics
 	for idx, backend := range mesh.Spec.GetMetrics().GetBackends() {
-		if backend.GetType() == mesh_proto.MetricsPrometheusType {
-			cfg := mesh_proto.PrometheusMetricsBackendConfig{}
-			if err := proto.ToTyped(backend.GetConf(), &cfg); err != nil {
-				return errors.Wrap(err, "could not convert the backend")
-			}
-
-			if cfg.SkipMTLS == nil && cfg.Tls == nil {
-				cfg.Tls = &mesh_proto.PrometheusTlsConfig{
-					Mode: mesh_proto.PrometheusTlsConfig_activeMTLSBackend,
-				}
-			}
-			if cfg.Tls == nil && cfg.SkipMTLS != nil && cfg.SkipMTLS.Value {
-				cfg.Tls = &mesh_proto.PrometheusTlsConfig{
-					Mode: mesh_proto.PrometheusTlsConfig_disabled,
-				}
-			}
-			if cfg.Tls == nil && cfg.SkipMTLS != nil && !cfg.SkipMTLS.Value {
-				cfg.Tls = &mesh_proto.PrometheusTlsConfig{
-					Mode: mesh_proto.PrometheusTlsConfig_activeMTLSBackend,
-				}
-			}
-			if cfg.Tls != nil && cfg.SkipMTLS != nil && cfg.Tls.GetMode() == mesh_proto.PrometheusTlsConfig_providedTLS {
-				cfg.Tls = &mesh_proto.PrometheusTlsConfig{
-					Mode: mesh_proto.PrometheusTlsConfig_providedTLS,
-				}
-				cfg.SkipMTLS = nil
-			}
-
-			if cfg.Port == 0 {
-				cfg.Port = 5670
-			}
-			if cfg.Path == "" {
-				cfg.Path = "/metrics"
-			}
-			if len(cfg.Tags) == 0 {
-				cfg.Tags = map[string]string{
-					mesh_proto.ServiceTag: "dataplane-metrics",
-				}
-			}
-
-			str, err := proto.ToStruct(&cfg)
-			if err != nil {
-				return errors.Wrap(err, "could not convert the backend")
-			}
-			mesh.Spec.Metrics.Backends[idx].Conf = str
+		if backend.GetType() != mesh_proto.MetricsPrometheusType {
+			continue
 		}
+		cfg := mesh_proto.PrometheusMetricsBackendConfig{}
+		if err := proto.ToTyped(backend.GetConf(), &cfg); err != nil {
+			return errors.Wrap(err, "could not convert the backend")
+		}
+
+		if cfg.SkipMTLS == nil && cfg.Tls == nil {
+			cfg.Tls = &mesh_proto.PrometheusTlsConfig{
+				Mode: mesh_proto.PrometheusTlsConfig_activeMTLSBackend,
+			}
+		}
+		if cfg.Tls == nil && cfg.SkipMTLS != nil && cfg.SkipMTLS.Value {
+			cfg.Tls = &mesh_proto.PrometheusTlsConfig{
+				Mode: mesh_proto.PrometheusTlsConfig_disabled,
+			}
+		}
+		if cfg.Tls == nil && cfg.SkipMTLS != nil && !cfg.SkipMTLS.Value {
+			cfg.Tls = &mesh_proto.PrometheusTlsConfig{
+				Mode: mesh_proto.PrometheusTlsConfig_activeMTLSBackend,
+			}
+		}
+		if cfg.Tls != nil && cfg.SkipMTLS != nil && cfg.Tls.GetMode() == mesh_proto.PrometheusTlsConfig_providedTLS {
+			cfg.Tls = &mesh_proto.PrometheusTlsConfig{
+				Mode: mesh_proto.PrometheusTlsConfig_providedTLS,
+			}
+			cfg.SkipMTLS = nil
+		}
+
+		if cfg.Port == 0 {
+			cfg.Port = 5670
+		}
+		if cfg.Path == "" {
+			cfg.Path = "/metrics"
+		}
+		if len(cfg.Tags) == 0 {
+			cfg.Tags = map[string]string{
+				mesh_proto.ServiceTag: "dataplane-metrics",
+			}
+		}
+
+		str, err := proto.ToStruct(&cfg)
+		if err != nil {
+			return errors.Wrap(err, "could not convert the backend")
+		}
+		mesh.Spec.Metrics.Backends[idx].Conf = str
 	}
 	return nil
 }
