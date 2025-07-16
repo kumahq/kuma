91f7e09893d2d58e0b49ef5ba850694e68500849
Lukasz Dziedziak <lukidzi@gmail.com>
2025-07-16 04:57:07 -0500

    review
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/plugins/policies/meshratelimit/api/v1alpha1/deprecated.go
+++ pkg/plugins/policies/meshratelimit/api/v1alpha1/deprecated.go
@@ -12,21 +12,21 @@ func (t *MeshRateLimitResource) Deprecations() []string {
 	if len(pointer.Deref(t.Spec.From)) > 0 {
 		deprecations = append(deprecations, "'from' field is deprecated, use 'rules' instead")
 		for i, rule := range pointer.Deref(t.Spec.From) {
-			if rule.Default.Local != nil && isStatusInvalid(pointer.Deref(rule.Default.Local)) {
+			if rule.Default.Local != nil && isStatusInvalid(*rule.Default.Local) {
 				deprecations = append(deprecations, fmt.Sprintf("'spec.from[%d].default.local.http.requestRate.status' must be 400 or higher. Please update your configuration.", i))
 			}
 		}
 	}
 	if len(pointer.Deref(t.Spec.Rules)) > 0 {
 		for i, rule := range pointer.Deref(t.Spec.Rules) {
-			if rule.Default.Local != nil && isStatusInvalid(pointer.Deref(rule.Default.Local)) {
+			if rule.Default.Local != nil && isStatusInvalid(*rule.Default.Local) {
 				deprecations = append(deprecations, fmt.Sprintf("'spec.rules[%d].default.local.http.requestRate.status' must be 400 or higher. Please update your configuration.", i))
 			}
 		}
 	}
 	if len(pointer.Deref(t.Spec.To)) > 0 {
 		for i, rule := range pointer.Deref(t.Spec.To) {
-			if rule.Default.Local != nil && isStatusInvalid(pointer.Deref(rule.Default.Local)) {
+			if rule.Default.Local != nil && isStatusInvalid(*rule.Default.Local) {
 				deprecations = append(deprecations, fmt.Sprintf("'spec.to[%d].default.local.http.requestRate.status' must be 400 or higher. Please update your configuration.", i))
 			}
 		}
