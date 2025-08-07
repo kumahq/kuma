459643438d64a015c08258378fbe177db2a958f0
Lukasz Dziedziak <lukidzi@gmail.com>
2025-08-07 07:25:26 -0500

    feat(api): add method to read DataSource on control-plane (#14152)
    ## Motivation
    
    We added a DataSource but no implementation 
    
    ## Implementation information
    
    * added method which reads data on Control-Plane
    
    ## Supporting documentation
    
    Part of #13876
    
    ---------
    
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- api/common/v1alpha1/datasource/datasource.go
+++ api/common/v1alpha1/datasource/datasource.go
@@ -1,7 +1,17 @@
 // +kubebuilder:object:generate=true
 package datasource
 
-import "github.com/kumahq/kuma/pkg/core/validators"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/kumahq/kuma/pkg/core/resources/apis/system"
+	"github.com/kumahq/kuma/pkg/core/resources/manager"
+	core_store "github.com/kumahq/kuma/pkg/core/resources/store"
+	"github.com/kumahq/kuma/pkg/core/validators"
+)
 
 // +kubebuilder:validation:Enum=File;Secret;EnvVar;InsecureInline
 type SecureDataSourceType string
@@ -31,6 +41,66 @@ const (
 	DataSourceEnvVar DataSourceType = "EnvVar"
 	DataSourceInline DataSourceType = "Inline"
 )
+
+// DataSource is just a way to provide data. Not necessarily secrets,
+// can be any data, i.e. certs, configs, OPA policies written in rego, lua plugins etc.
+type DataSource struct {
+	// +kuma:discriminator
+	Type   DataSourceType `json:"type"`
+	File   *File          `json:"file,omitempty"`
+	EnvVar *EnvVar        `json:"envVar,omitempty"`
+	Inline *Inline        `json:"inline,omitempty"`
+}
+
+type File struct {
+	Path string `json:"path"`
+}
+
+type EnvVar struct {
+	Name string `json:"name"`
+}
+
+// +kubebuilder:validation:Enum=Secret
+type RefType string
+
+const (
+	SecretRefType RefType = "Secret"
+)
+
+type SecretRef struct {
+	Kind RefType `json:"kind"`
+	Name string  `json:"name"`
+}
+
+type Inline struct {
+	Value string `json:"value"`
+}
+
+func (sds *SecureDataSource) ReadByControlPlane(ctx context.Context, secretManager manager.ReadOnlyResourceManager, mesh string) ([]byte, error) {
+	switch sds.Type {
+	case SecureDataSourceFile:
+		return os.ReadFile(sds.File.Path)
+	case SecureDataSourceEnvVar:
+		value, found := os.LookupEnv(sds.EnvVar.Name)
+		if !found {
+			return nil, fmt.Errorf("environment variable: %s is not defined", sds.EnvVar.Name)
+		}
+		return []byte(value), nil
+	case SecureDataSourceInline:
+		return []byte(sds.InsecureInline.Value), nil
+	case SecureDataSourceSecretRef:
+		if secretManager == nil {
+			return nil, errors.New("resource manager is not defined")
+		}
+		resource := system.NewSecretResource()
+		if err := secretManager.Get(ctx, resource, core_store.GetByKey(sds.SecretRef.Name, mesh)); err != nil {
+			return nil, err
+		}
+		return resource.Spec.GetData().GetValue(), nil
+	default:
+		return nil, fmt.Errorf("datasource type: %s is not supported", sds.Type)
+	}
+}
 
 func (s *SecureDataSource) ValidateSecureDataSource(path validators.PathBuilder) validators.ValidationError {
 	var verr validators.ValidationError
@@ -72,38 +142,4 @@ func (s *SecureDataSource) ValidateSecureDataSource(path validators.PathBuilder)
 		verr.AddViolationAt(path.Field("type"), validators.MustBeOneOf(string(s.Type), string(SecureDataSourceEnvVar), string(SecureDataSourceInline), string(SecureDataSourceFile), string(SecureDataSourceSecretRef)))
 	}
 	return verr
-}
-
-// DataSource is just a way to provide data. Not necessarily secrets,
-// can be any data, i.e. certs, configs, OPA policies written in rego, lua plugins etc.
-type DataSource struct {
-	// +kuma:discriminator
-	Type   DataSourceType `json:"type"`
-	File   *File          `json:"file,omitempty"`
-	EnvVar *EnvVar        `json:"envVar,omitempty"`
-	Inline *Inline        `json:"inline,omitempty"`
-}
-
-type File struct {
-	Path string `json:"path"`
-}
-
-type EnvVar struct {
-	Name string `json:"name"`
-}
-
-// +kubebuilder:validation:Enum=Secret
-type RefType string
-
-const (
-	SecretRefType RefType = "Secret"
-)
-
-type SecretRef struct {
-	Kind RefType `json:"kind"`
-	Name string  `json:"name"`
-}
-
-type Inline struct {
-	Value string `json:"value"`
 }
