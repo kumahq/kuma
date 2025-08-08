a93618f25d531ac9eb2094f81b38fb775d072b21
Lukasz Dziedziak <lukidzi@gmail.com>
2025-08-08 08:52:53 -0500

    feat(meshtrust): create mesh trust based on identity and store status
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/core/resources/apis/meshidentity/status/updater.go
+++ pkg/core/resources/apis/meshidentity/status/updater.go
@@ -10,12 +10,15 @@ import (
 	kube_meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	common_api "github.com/kumahq/kuma/api/common/v1alpha1"
+	"github.com/kumahq/kuma/pkg/core/kri"
 	meshidentity_api "github.com/kumahq/kuma/pkg/core/resources/apis/meshidentity/api/v1alpha1"
 	"github.com/kumahq/kuma/pkg/core/resources/apis/meshidentity/providers"
+	meshtrust_api "github.com/kumahq/kuma/pkg/core/resources/apis/meshtrust/api/v1alpha1"
 	"github.com/kumahq/kuma/pkg/core/resources/manager"
 	"github.com/kumahq/kuma/pkg/core/resources/store"
 	"github.com/kumahq/kuma/pkg/core/runtime/component"
 	"github.com/kumahq/kuma/pkg/core/user"
+	"github.com/kumahq/kuma/pkg/util/pointer"
 )
 
 const (
@@ -148,8 +151,99 @@ func (i *IdentityProviderReconciler) initialize(ctx context.Context, mid *meshid
 			Message: "Provider successfully initialized",
 		})
 	}
+	if mid.Spec.Provider.Type == meshidentity_api.BundledType &&
+		pointer.DerefOr(mid.Spec.Provider.Bundled.MeshTrustCreation, meshidentity_api.MeshTrustCreationEnabled) == meshidentity_api.MeshTrustCreationEnabled {
+		if err := i.createOrUpdateMeshTrust(ctx, mid); err != nil {
+			conditions = append(conditions, common_api.Condition{
+				Type:    meshidentity_api.MeshTrustConditionType,
+				Status:  kube_meta.ConditionFalse,
+				Reason:  "MeshTrustCreationError",
+				Message: err.Error(),
+			})
+			return conditions
+		} else {
+			conditions = append(conditions, common_api.Condition{
+				Type:    meshidentity_api.MeshTrustConditionType,
+				Status:  kube_meta.ConditionTrue,
+				Reason:  "MeshTrustCreated",
+				Message: "MeshTrust has been successfully created",
+			})
+		}
+	}
 
 	return conditions
+}
+
+func (i *IdentityProviderReconciler) createOrUpdateMeshTrust(ctx context.Context, identity *meshidentity_api.MeshIdentityResource) error {
+	meshTrust := meshtrust_api.NewMeshTrustResource()
+	meshName := identity.Meta.GetMesh()
+	resourceName := identity.Meta.GetName()
+
+	trustDomain, err := identity.Spec.GetTrustDomain(identity.GetMeta(), i.zone)
+	if err != nil {
+		return err
+	}
+	// Check if the MeshTrust resource already exists
+	update := true
+	if err := i.roResManager.Get(ctx, meshTrust, store.GetByKey(resourceName, meshName)); err != nil {
+		if store.IsNotFound(err) {
+			update = false
+		} else {
+			return err
+		}
+	}
+	ca, err := i.loadCA(ctx, identity)
+	if err != nil {
+		return err
+	}
+
+	origin := kri.From(identity).String()
+	caPEM := string(ca)
+
+	if update {
+		// Check if the CA PEM is already present in the MeshTrust resource
+		for _, bundle := range meshTrust.Spec.CABundles {
+			if bundle.PEM.Value == caPEM {
+				// Already exists; no need to update
+				return nil
+			}
+		}
+
+		// Append the new CA PEM
+		meshTrust.Spec.CABundles = append(meshTrust.Spec.CABundles, meshtrust_api.CABundle{
+			Type: meshtrust_api.PemCABundleType,
+			PEM: &meshtrust_api.PEM{
+				Value: caPEM,
+			},
+		})
+
+		return i.resManager.Update(ctx, meshTrust)
+	}
+
+	// Resource doesn't exist, create a new one
+	meshTrust.Spec = &meshtrust_api.MeshTrust{
+		Origin: &meshtrust_api.Origin{
+			KRI: pointer.To(origin),
+		},
+		TrustDomain: trustDomain,
+		CABundles: []meshtrust_api.CABundle{
+			{
+				Type: meshtrust_api.PemCABundleType,
+				PEM: &meshtrust_api.PEM{
+					Value: caPEM,
+				},
+			},
+		},
+	}
+	return i.resManager.Create(ctx, meshTrust, store.CreateByKey(resourceName, meshName))
+}
+
+func (i *IdentityProviderReconciler) loadCA(ctx context.Context, identity *meshidentity_api.MeshIdentityResource) ([]byte, error) {
+	provider, found := i.providers[string(identity.Spec.Provider.Type)]
+	if !found {
+		return nil, fmt.Errorf("provider: %s not found", identity.Spec.Provider.Type)
+	}
+	return provider.GetRootCA(ctx, identity)
 }
 
 func (i *IdentityProviderReconciler) NeedLeaderElection() bool {
---
+++ pkg/test/resources/builders/meshtrust_builder.go
@@ -0,0 +1,59 @@
+package builders
+
+import (
+	"context"
+
+	meshtrust_api "github.com/kumahq/kuma/pkg/core/resources/apis/meshtrust/api/v1alpha1"
+	"github.com/kumahq/kuma/pkg/core/resources/model"
+	"github.com/kumahq/kuma/pkg/core/resources/store"
+	test_model "github.com/kumahq/kuma/pkg/test/resources/model"
+)
+
+type MeshTrustBuilder struct {
+	res *meshtrust_api.MeshTrustResource
+}
+
+func MeshTrust() *MeshTrustBuilder {
+	return &MeshTrustBuilder{
+		res: &meshtrust_api.MeshTrustResource{
+			Meta: &test_model.ResourceMeta{
+				Mesh: "default",
+				Name: "trust-1",
+			},
+			Spec: &meshtrust_api.MeshTrust{
+				TrustDomain: "default.east.mesh.local",
+				CABundles: []meshtrust_api.CABundle{
+					{
+						Type: meshtrust_api.PemCABundleType,
+						PEM: &meshtrust_api.PEM{
+							// nolint:gosec
+							Value: `-----BEGIN CERTIFICATE-----
+MIIB9jCCAZ2gAwIBAgIUJ1gLZ/fvZhGq51qWrJzL6z2XWoQwCgYIKoZIzj0EAwIw
+EjEQMA4GA1UEChMHVGVzdCBDQTAeFw0yNTA3MzAxMjAwMDBaFw0zNTA3MjcxMjAw
+MDBaMBIxEDAOBgNVBAoTB1Rlc3QgQ0EwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNC
+AAQaXDFzOPslZ4e8n2KjsNkG+Wxi37L2zRWdMczDi7VqLrO03lczkB98/vzrdMKF
+JgYxycULx10EYgMGQkgLrf1po2QwYjAdBgNVHQ4EFgQUUQBd5VjEO3N4XcgrxgMK
+NU9xIQswHwYDVR0jBBgwFoAUUQBd5VjEO3N4XcgrxgMKNU9xIQswDwYDVR0TAQH/
+BAUwAwEB/zAKBggqhkjOPQQDAgNHADBEAiA3dhhIQCzNkeGSjj6jK+jGE8fEKVmp
+c9Vh+kJkmPUJZQIgQBr2GkV8uSfq/5ZKHD6jz6MJvKsg06dMBdvZBIA2ujg=
+-----END CERTIFICATE-----`,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func (mi *MeshTrustBuilder) Build() *meshtrust_api.MeshTrustResource {
+	return mi.res
+}
+
+func (mi *MeshTrustBuilder) Create(s store.ResourceStore) error {
+	return s.Create(context.Background(), mi.Build(), store.CreateBy(model.MetaToResourceKey(mi.res.GetMeta())))
+}
+
+func (mi *MeshTrustBuilder) WithName(name string) *MeshTrustBuilder {
+	mi.res.Meta.(*test_model.ResourceMeta).Name = name
+	return mi
+}
