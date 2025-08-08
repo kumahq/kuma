cc0de463fe349dcd8dff17d43c9a343ceb33e24e
Lukasz Dziedziak <lukidzi@gmail.com>
2025-08-08 08:16:13 -0500

    feat(meshidentity): add spire provider
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/core/resources/apis/meshidentity/providers.go
+++ pkg/core/resources/apis/meshidentity/providers.go
@@ -3,8 +3,10 @@ package meshidentity
 import (
 	"github.com/kumahq/kuma/pkg/core/plugins"
 	"github.com/kumahq/kuma/pkg/core/resources/apis/meshidentity/providers/bundled"
+	"github.com/kumahq/kuma/pkg/core/resources/apis/meshidentity/providers/spire"
 )
 
 var NameToModule = map[string]*plugins.PluginInitializer{
 	"bundled": {InitFn: bundled.InitProvider, Initialized: false},
+	"spire":   {InitFn: spire.InitProvider, Initialized: false},
 }
