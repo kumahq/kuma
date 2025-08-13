196b35fb154299d4fa861e0d30c30558cdcc824e
Krzysztof Słonka <slonka@users.noreply.github.com>
2025-08-13 11:46:46 +0200

    chore(deps): bump openapi-tool to v1.1.6 (#14226)
    https://github.com/kumahq/ci-tools/pull/89
    
    now it takes 7 seconds:
    https://github.com/kumahq/kuma/actions/runs/16933076161/job/47984263913?pr=14226#step:10:407
    
    bump dep
    
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- mk/docs.mk
+++ mk/docs.mk
@@ -39,7 +39,13 @@ docs/generated/openapi.yaml:
 	for i in $$( find $(POLICIES_DIR) -name '*.yaml' | grep '/api/'); do DIR=$(OAPI_TMP_DIR)/policies/$$(echo $${i} | awk -F/ '{print $$(NF-3)}'); mkdir -p $${DIR}; cp $${i} $${DIR}/$$(echo $${i} | awk -F/ '{print $$(NF)}'); done
 
 ifdef BASE_API
+<<<<<<< HEAD
 	docker run --rm -v $$PWD/$(dir $(BASE_API)):/base -v $(OAPI_TMP_DIR):/specs ghcr.io/kumahq/openapi-tool:v0.10.2 generate /base/$(notdir $(BASE_API)) '/specs/**/*.yaml'  '!/specs/kuma/**' > $@
 else
 	docker run --rm -v $(OAPI_TMP_DIR):/specs ghcr.io/kumahq/openapi-tool:v0.10.2 generate '/specs/**/*.yaml' > $@
+=======
+	docker run --rm -v $$PWD/$(dir $(BASE_API)):/base -v $(OAPI_TMP_DIR):/specs ghcr.io/kumahq/openapi-tool:v1.1.6 generate /base/$(notdir $(BASE_API)) '/specs/**/*.yaml'  '!/specs/kuma/**' > $@
+else
+	docker run --rm -v $(OAPI_TMP_DIR):/specs ghcr.io/kumahq/openapi-tool:v1.1.6 generate '/specs/**/*.yaml' > $@
+>>>>>>> afae05d06 (chore(deps): bump openapi-tool to v1.1.6 (#14226))
 endif
