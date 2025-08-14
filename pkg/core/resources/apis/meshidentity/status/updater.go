8e0f277ffa3fd2d74f325e51b924cb56f1230027
Lukasz Dziedziak <lukidzi@gmail.com>
2025-08-14 08:58:18 -0500

    feat(meshidentity): create mesh trust based on identity (#14217)
    ## Motivation
    
    We want to simplify some logic and introduce a component which creates
    MeshTrust based on MeshIdentity
    
    ## Implementation information
    
    * component check every 5seconds for meshidentity policy and reconcile
    them
    * creates MeshTrust or appends if one already exists
    
    ## Supporting documentation
    
    Fix #13883
    
    ---------
    
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/core/resources/apis/meshidentity/status/updater.go
+++ pkg/core/resources/apis/meshidentity/status/updater.go
@@ -1,7 +1,10 @@
 package status
 
 import (
+	"bytes"
 	"context"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"reflect"
 	"time"
@@ -10,18 +13,15 @@ import (
 	kube_meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	common_api "github.com/kumahq/kuma/api/common/v1alpha1"
+	"github.com/kumahq/kuma/pkg/core/kri"
 	meshidentity_api "github.com/kumahq/kuma/pkg/core/resources/apis/meshidentity/api/v1alpha1"
 	"github.com/kumahq/kuma/pkg/core/resources/apis/meshidentity/providers"
+	meshtrust_api "github.com/kumahq/kuma/pkg/core/resources/apis/meshtrust/api/v1alpha1"
 	"github.com/kumahq/kuma/pkg/core/resources/manager"
 	"github.com/kumahq/kuma/pkg/core/resources/store"
 	"github.com/kumahq/kuma/pkg/core/runtime/component"
 	"github.com/kumahq/kuma/pkg/core/user"
-)
-
-const (
-	ReadyType     string = "Ready"
-	ProviderType  string = "Provider"
-	MeshTrustType string = "MeshTrustCreated"
+	"github.com/kumahq/kuma/pkg/util/pointer"
 )
 
 type IdentityProviderReconciler struct {
@@ -121,16 +121,14 @@ func (i *IdentityProviderReconciler) initialize(ctx context.Context, mid *meshid
 		})
 		return conditions
 	}
-	if !mid.Status.IsInitialized() {
-		if err := provider.Initialize(ctx, mid); err != nil {
-			conditions = append(conditions, common_api.Condition{
-				Type:    meshidentity_api.ProviderConditionType,
-				Status:  kube_meta.ConditionFalse,
-				Reason:  "ProviderInitializationError",
-				Message: err.Error(),
-			})
-			return conditions
-		}
+	if err := provider.Initialize(ctx, mid); err != nil {
+		conditions = append(conditions, common_api.Condition{
+			Type:    meshidentity_api.ProviderConditionType,
+			Status:  kube_meta.ConditionFalse,
+			Reason:  "ProviderInitializationError",
+			Message: err.Error(),
+		})
+		return conditions
 	}
 	if err := provider.Validate(ctx, mid); err != nil {
 		conditions = append(conditions, common_api.Condition{
@@ -148,10 +146,115 @@ func (i *IdentityProviderReconciler) initialize(ctx context.Context, mid *meshid
 			Message: "Provider successfully initialized",
 		})
 	}
+	if mid.Spec.Provider.Type == meshidentity_api.BundledType &&
+		pointer.DerefOr(mid.Spec.Provider.Bundled.MeshTrustCreation, meshidentity_api.MeshTrustCreationEnabled) == meshidentity_api.MeshTrustCreationEnabled {
+		if err := i.createOrUpdateMeshTrust(ctx, mid); err != nil {
+			conditions = append(conditions, common_api.Condition{
+				Type:    meshidentity_api.MeshTrustConditionType,
+				Status:  kube_meta.ConditionFalse,
+				Reason:  "MeshTrustCreationError",
+				Message: err.Error(),
+			})
+			return conditions
+		} else {
+			conditions = append(conditions, common_api.Condition{
+				Type:    meshidentity_api.MeshTrustConditionType,
+				Status:  kube_meta.ConditionTrue,
+				Reason:  "MeshTrustCreated",
+				Message: "MeshTrust has been successfully created",
+			})
+		}
+	}
 
 	return conditions
 }
 
+func (i *IdentityProviderReconciler) createOrUpdateMeshTrust(ctx context.Context, identity *meshidentity_api.MeshIdentityResource) error {
+	meshTrust := meshtrust_api.NewMeshTrustResource()
+	meshName := identity.Meta.GetMesh()
+	resourceName := identity.Meta.GetName()
+
+	trustDomain, err := identity.Spec.GetTrustDomain(identity.GetMeta(), i.zone)
+	if err != nil {
+		return err
+	}
+	// Check if the MeshTrust resource already exists
+	update := true
+	if err := i.roResManager.Get(ctx, meshTrust, store.GetByKey(resourceName, meshName)); err != nil {
+		if store.IsNotFound(err) {
+			update = false
+		} else {
+			return err
+		}
+	}
+	ca, err := i.loadCA(ctx, identity)
+	if err != nil {
+		return err
+	}
+
+	origin := kri.From(identity).String()
+	caPEM := string(ca)
+
+	if update {
+		// Check if the CA PEM is already present in the MeshTrust resource
+		for _, bundle := range meshTrust.Spec.CABundles {
+			if pemEqual(bundle.PEM.Value, caPEM) {
+				// Already exists; no need to update
+				return nil
+			}
+		}
+
+		// Append the new CA PEM
+		meshTrust.Spec.CABundles = append(meshTrust.Spec.CABundles, meshtrust_api.CABundle{
+			Type: meshtrust_api.PemCABundleType,
+			PEM: &meshtrust_api.PEM{
+				Value: caPEM,
+			},
+		})
+
+		return i.resManager.Update(ctx, meshTrust)
+	}
+
+	// Resource doesn't exist, create a new one
+	meshTrust.Spec = &meshtrust_api.MeshTrust{
+		Origin: &meshtrust_api.Origin{
+			KRI: pointer.To(origin),
+		},
+		TrustDomain: trustDomain,
+		CABundles: []meshtrust_api.CABundle{
+			{
+				Type: meshtrust_api.PemCABundleType,
+				PEM: &meshtrust_api.PEM{
+					Value: caPEM,
+				},
+			},
+		},
+	}
+	return i.resManager.Create(ctx, meshTrust, store.CreateByKey(resourceName, meshName))
+}
+
+func (i *IdentityProviderReconciler) loadCA(ctx context.Context, identity *meshidentity_api.MeshIdentityResource) ([]byte, error) {
+	provider, found := i.providers[string(identity.Spec.Provider.Type)]
+	if !found {
+		return nil, fmt.Errorf("provider: %s not found", identity.Spec.Provider.Type)
+	}
+	return provider.GetRootCA(ctx, identity)
+}
+
 func (i *IdentityProviderReconciler) NeedLeaderElection() bool {
 	return true
+}
+
+func pemEqual(pem1, pem2 string) bool {
+	block1, _ := pem.Decode([]byte(pem1))
+	block2, _ := pem.Decode([]byte(pem2))
+	if block1 == nil || block2 == nil {
+		return false
+	}
+	cert1, err1 := x509.ParseCertificate(block1.Bytes)
+	cert2, err2 := x509.ParseCertificate(block2.Bytes)
+	if err1 != nil || err2 != nil {
+		return bytes.Equal(block1.Bytes, block2.Bytes)
+	}
+	return cert1.Equal(cert2)
 }
