ec14c24a5521e711c44300ebdf4fb526693e92b3
slonka <slonka@users.noreply.github.com>
2025-08-25 12:41:32 +0200

    feat(kuma-cp): make secrets naming depend on meshservice exclusive mode
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- pkg/xds/envoy/clusters/v3/client_side_mtls_configurer_test.go
+++ pkg/xds/envoy/clusters/v3/client_side_mtls_configurer_test.go
@@ -82,6 +82,7 @@ var _ = Describe("EdsClusterConfigurer", func() {
 							},
 						},
 					},
+					MeshServices: &mesh_proto.Mesh_MeshServices{Mode: mesh_proto.Mesh_MeshServices_Exclusive},
 				},
 			},
 			// no tags therefore SNI is empty
