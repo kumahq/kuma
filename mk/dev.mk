6874dfbfbab0355c1f8e649d6165a00d310c2bb3
Bart Smykla <bartek@smykla.com>
2025-10-17 09:28:16 +0200

    ci(renovate): annotate Envoy version in dev.mk for auto updates
    Add Renovate inline hint above `ENVOY_VERSION` so Renovate can track
    `kumahq/envoy-builds` GitHub tags and open PRs when new tags appear.
    
    - Added a Renovate comment right above `ENVOY_VERSION`:
    
      ```mk
      # renovate: datasource=github-tags depName=envoy packageName=kumahq/envoy-builds versioning=semver
      ```
    
    - Enable automatic PRs to bump Envoy builds, instead of manual updates
    - Use semver from GitHub tags for predictable update behavior
    - Improve dependency hygiene with small, regular updates
    
    - Change only guides Renovate. Build and tests are unaffected
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- mk/dev.mk
+++ mk/dev.mk
@@ -7,6 +7,7 @@ GIT_TAG = $(word 2, $(BUILD_INFO))
 GIT_COMMIT = $(word 3, $(BUILD_INFO))
 BUILD_DATE = $(word 4, $(BUILD_INFO))
 CI_TOOLS_VERSION = $(word 5, $(BUILD_INFO))
+# renovate: datasource=github-tags depName=envoy packageName=kumahq/envoy-builds versioning=semver
 ENVOY_VERSION ?= 1.34.7
 KUMA_CHARTS_URL ?= https://kumahq.github.io/charts
 CHART_REPO_NAME ?= kuma
