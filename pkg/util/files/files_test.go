0a8b37a34b34fcfff94757ffc914d33537989232
Bart Smykla <bartek@smykla.com>
2025-11-06 18:27:04 +0100

    test(files): fix RelativeToPkgMod for v2 module paths
    The `RelativeToPkgMod()` function hardcoded three `path.Dir()`
    calls which worked for v1 modules but incorrectly stripped
    `github.com/` from v2 module paths in `pkg/mod` due to the
    extra `/v2` directory.
    
    Fixed by detecting `/pkg/mod/` in paths and returning
    everything after it, correctly handling all module versions.
    
    Fixes downstream projects like Kong Mesh that use Kuma v2 as
    a dependency and run tests with `PostgresContainer`.
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/util/files/files_test.go
+++ pkg/util/files/files_test.go
@@ -22,3 +22,32 @@ var _ = DescribeTable("",
 	Entry(nil, "C?on_between_old_an/d_new_DPP_\"from_version:_2-7-12", "C-on_between_old_an-d_new_DPP_-from_version-_2-7-12"),
 	Entry(nil, "Compatibility_connection_between_old_and_new_DPP_from_version:_2-7-12", "Compatibility_connection_between_old_and_new_DPP_from_version-_2-7-12"),
 )
+
+var _ = DescribeTable("RelativeToPkgMod",
+	func(in string, out string) string {
+		return fmt.Sprintf(`RelativeToPkgMod(%q)=%q`, in, out)
+	},
+	func(in string, out string) {
+		Expect(files.RelativeToPkgMod(in)).To(Equal(out))
+	},
+	Entry("v1 module path",
+		"/home/ubuntu/go/pkg/mod/github.com/kumahq/kuma@v1.8.0-20231106140736-df9c4e43a672/pkg/test/store/postgres/test_container.go",
+		"/github.com/kumahq/kuma@v1.8.0-20231106140736-df9c4e43a672/pkg/test/store/postgres/test_container.go",
+	),
+	Entry("v2 module path",
+		"/home/ubuntu/go/pkg/mod/github.com/kumahq/kuma/v2@v2.0.0-20251106140736-df9c4e43a672/pkg/test/store/postgres/test_container.go",
+		"/github.com/kumahq/kuma/v2@v2.0.0-20251106140736-df9c4e43a672/pkg/test/store/postgres/test_container.go",
+	),
+	Entry("v2 module path with app directory",
+		"/home/ubuntu/go/pkg/mod/github.com/kumahq/kuma/v2@v2.0.0-20251106140736-df9c4e43a672/app/kumactl/cmd/root.go",
+		"/github.com/kumahq/kuma/v2@v2.0.0-20251106140736-df9c4e43a672/app/kumactl/cmd/root.go",
+	),
+	Entry("different org v2 module path",
+		"/home/ubuntu/go/pkg/mod/github.com/example/project/v2@v2.1.0/pkg/main.go",
+		"/github.com/example/project/v2@v2.1.0/pkg/main.go",
+	),
+	Entry("v3 module path",
+		"/home/ubuntu/go/pkg/mod/github.com/kumahq/kuma/v3@v3.0.0-20251106140736-df9c4e43a672/pkg/test/store/postgres/test_container.go",
+		"/github.com/kumahq/kuma/v3@v3.0.0-20251106140736-df9c4e43a672/pkg/test/store/postgres/test_container.go",
+	),
+)
