d49795fd68ef3162bb5fd611ffbdd8a7ba4d28d6
Marcin Skalski <skalskimarcin33@gmail.com>
2025-11-06 16:58:11 +0100

    feat(workload): add Workload resource
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

---
+++ pkg/core/resources/apis/workload/api/v1alpha1/validator.go
@@ -0,0 +1,16 @@
+package v1alpha1
+
+import (
+	"github.com/kumahq/kuma/v2/pkg/core/resources/model"
+	"github.com/kumahq/kuma/v2/pkg/core/validators"
+)
+
+const maxNameLength = 253
+
+func (r *WorkloadResource) validate() error {
+	var verr validators.ValidationError
+	name := model.GetDisplayName(r.GetMeta())
+	verr.Add(validators.ValidateLength(validators.RootedAt("name"), maxNameLength, name))
+
+	return verr.OrNil()
+}
---
+++ pkg/core/resources/apis/workload/api/v1alpha1/workload.go
@@ -0,0 +1,23 @@
+// +kubebuilder:object:generate=true
+package v1alpha1
+
+// Workload
+// +kuma:policy:is_policy=false
+// +kuma:policy:has_status=true
+// +kuma:policy:kds_flags=model.ZoneToGlobalFlag
+// +kuma:policy:short_name=wl
+type Workload struct {
+}
+type WorkloadStatus struct {
+	// DataplaneProxies defines statistics of data plane proxies that are part of this workload
+	DataplaneProxies DataplaneProxies `json:"dataplaneProxies,omitempty"`
+}
+
+type DataplaneProxies struct {
+	// Connected defines number of connected data plane proxies
+	Connected int32 `json:"connected"`
+	// Healthy defines number of healthy data plane proxies for this workload
+	Healthy int32 `json:"healthy"`
+	// Total defines total number of data plane proxies for this workload
+	Total int32 `json:"total"`
+}
