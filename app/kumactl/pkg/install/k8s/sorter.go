59cc116c85f98c4d85f5dd4da1a53311f91dc0b3
Bart Smykla <bartek@smykla.com>
2025-11-17 10:59:48 +0100

    fix(helm): add GatewayClass to custom InstallOrder
    In Helm v4, `MutatingWebhookConfiguration` and
    `ValidatingWebhookConfiguration` were added to the default
    `InstallOrder`, but `GatewayClass` was not included. This caused
    `GatewayClass` to be sorted alphabetically after webhook
    configurations, breaking Gateway functionality in E2E tests.
    
    `GatewayClass` must be installed before webhooks to ensure proper
    Gateway resource initialization and reconciliation.
    
    Changes:
    - Added `customInstallOrder` in
      `app/kumactl/pkg/install/k8s/sorter.go` that extends Helm v4's
      default order with `GatewayClass`
    - Updated golden file to reflect correct resource ordering
    
    Fixes Gateway E2E test: "connection limit is respected"
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- app/kumactl/pkg/install/k8s/sorter.go
+++ app/kumactl/pkg/install/k8s/sorter.go
@@ -7,6 +7,54 @@ import (
 	"github.com/kumahq/kuma/v2/pkg/util/data"
 )
 
+// customInstallOrder extends Helm v4's default InstallOrder with Gateway API resources.
+// In Helm v4, MutatingWebhookConfiguration and ValidatingWebhookConfiguration were added
+// to the InstallOrder, but GatewayClass was not included. This causes GatewayClass to be
+// sorted alphabetically after webhook configurations, breaking Gateway functionality.
+// GatewayClass must be installed before webhooks to ensure proper Gateway resource
+// initialization and reconciliation.
+var customInstallOrder = releaseutilv1.KindSortOrder{
+	"PriorityClass",
+	"Namespace",
+	"NetworkPolicy",
+	"ResourceQuota",
+	"LimitRange",
+	"PodSecurityPolicy",
+	"PodDisruptionBudget",
+	"ServiceAccount",
+	"Secret",
+	"SecretList",
+	"ConfigMap",
+	"StorageClass",
+	"PersistentVolume",
+	"PersistentVolumeClaim",
+	"CustomResourceDefinition",
+	"ClusterRole",
+	"ClusterRoleList",
+	"ClusterRoleBinding",
+	"ClusterRoleBindingList",
+	"Role",
+	"RoleList",
+	"RoleBinding",
+	"RoleBindingList",
+	"Service",
+	"DaemonSet",
+	"Pod",
+	"ReplicationController",
+	"ReplicaSet",
+	"Deployment",
+	"HorizontalPodAutoscaler",
+	"StatefulSet",
+	"Job",
+	"CronJob",
+	"IngressClass",
+	"Ingress",
+	"APIService",
+	"GatewayClass",
+	"MutatingWebhookConfiguration",
+	"ValidatingWebhookConfiguration",
+}
+
 func SortResourcesByKind(files []data.File, kindsToSkip ...string) ([]data.File, error) {
 	skippedKinds := map[string]struct{}{}
 	for _, k := range kindsToSkip {
@@ -15,7 +63,7 @@ func SortResourcesByKind(files []data.File, kindsToSkip ...string) ([]data.File,
 	singleFile := data.JoinYAML(files)
 	resources := releaseutilv1.SplitManifests(string(singleFile.Data))
 
-	hooks, manifests, err := releaseutilv1.SortManifests(resources, chartcommon.VersionSet{"v1", "v1beta1", "v1alpha1"}, releaseutilv1.InstallOrder)
+	hooks, manifests, err := releaseutilv1.SortManifests(resources, chartcommon.VersionSet{"v1", "v1beta1", "v1alpha1"}, customInstallOrder)
 	if err != nil {
 		return nil, err
 	}
