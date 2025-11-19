b1a8cdfca0872ce836d4a2a323d3019d9afe6a69
Marcin Skalski <skalskimarcin33@gmail.com>
2025-11-19 13:23:44 +0100

    fix e2e
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- test/e2e_env/kubernetes/workload/workload.go
+++ test/e2e_env/kubernetes/workload/workload.go
@@ -57,7 +57,7 @@ func Workload() {
 		Eventually(func(g Gomega) {
 			podName, err = PodNameOfApp(kubernetes.Cluster, appName, namespace)
 			g.Expect(err).ToNot(HaveOccurred())
-		}, "30s", "1s").Should(Succeed())
+		}, "30s").Should(Succeed())
 
 		// and verify dataplane has workload annotation set to service account
 		Eventually(func(g Gomega) {
@@ -77,7 +77,7 @@ func Workload() {
 			// (stored as annotation in k8s, accessible through labels API)
 			workloadLabel := dp.Meta.GetLabels()[metadata.KumaWorkload]
 			g.Expect(workloadLabel).To(Equal(serviceAccount), "workload should equal service account name")
-		}, "30s", "1s").Should(Succeed())
+		}, "30s").Should(Succeed())
 	})
 
 	It("should use pod label as workload when workload labels configured", func() {
@@ -103,7 +103,7 @@ func Workload() {
 		Eventually(func(g Gomega) {
 			podName, err = PodNameOfApp(kubernetes.Cluster, appName, namespace)
 			g.Expect(err).ToNot(HaveOccurred())
-		}, "30s", "1s").Should(Succeed())
+		}, "30s").Should(Succeed())
 
 		// and verify dataplane has workload annotation set to "test-server" (from app.kubernetes.io/name label)
 		Eventually(func(g Gomega) {
@@ -122,7 +122,7 @@ func Workload() {
 			// verify workload is set from app label, not service account
 			workloadLabel := dp.Meta.GetLabels()[metadata.KumaWorkload]
 			g.Expect(workloadLabel).To(Equal(appLabel), "workload should equal app label value")
-		}, "30s", "1s").Should(Succeed())
+		}, "30s").Should(Succeed())
 	})
 
 	It("should reject pod creation with manual kuma.io/workload label", func() {
@@ -180,7 +180,7 @@ spec:
 		Eventually(func(g Gomega) {
 			_, err = PodNameOfApp(kubernetes.Cluster, appName, namespace)
 			g.Expect(err).ToNot(HaveOccurred())
-		}, "30s", "1s").Should(Succeed())
+		}, "30s").Should(Succeed())
 
 		// and verify Workload resource is created
 		Eventually(func(g Gomega) {
@@ -206,6 +206,6 @@ spec:
 			_, err := GetWorkload(kubernetes.Cluster, workloadK8sName, mesh)
 			g.Expect(err).To(HaveOccurred(), "workload should be deleted")
 			g.Expect(err.Error()).To(ContainSubstring("No resources found in workloads mesh"))
-		}, "2m", "3s").Should(Succeed())
+		}, "2m").Should(Succeed())
 	})
 }
