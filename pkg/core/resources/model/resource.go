afa2df87960cc7aba9d62c69e772b26fa2de5898
Marcin Skalski <skalskimarcin33@gmail.com>
2025-11-21 13:30:59 +0100

    chore(cleanup): remove IsReferenceableInTo from descriptor
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- pkg/core/resources/model/resource.go
+++ pkg/core/resources/model/resource.go
@@ -216,8 +216,6 @@ type ResourceTypeDescriptor struct {
 	DumpForGlobal bool
 	// AllowedOnSystemNamespaceOnly whether this resource type can be created only in the system namespace
 	AllowedOnSystemNamespaceOnly bool
-	// IsReferenceableInTo whether this resource type can be used in spec.to[].targetRef
-	IsReferenceableInTo bool
 	// ShortName a name that is used in kubectl or in the envoy configuration
 	ShortName string
 	// IsFromAsRules if true, the entries in the spec.from field should be interpreted as rules.
--- pkg/plugins/policies/meshretry/api/v1alpha1/zz_generated.resource.go
+++ pkg/plugins/policies/meshretry/api/v1alpha1/zz_generated.resource.go
@@ -171,7 +171,6 @@ var MeshRetryResourceTypeDescriptor = model.ResourceTypeDescriptor{
 	HasRulesTargetRef:            false,
 	HasStatus:                    false,
 	AllowedOnSystemNamespaceOnly: false,
-	IsReferenceableInTo:          false,
 	ShortName:                    "mr",
 	IsFromAsRules:                false,
 }
