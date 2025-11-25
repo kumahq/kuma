9b269e2a26675d94f88d66273c7f64ab6e6fdbc5
Ilya Lobkov <ilya.lobkov@konghq.com>
2025-11-25 12:42:11 +0100

    resolve conflicts
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- pkg/test/kds/setup/client.go
+++ pkg/test/kds/setup/client.go
@@ -16,8 +16,7 @@ func StartDeltaClient(clientStreams []*grpc.MockDeltaClientStream, resourceTypes
 		clientID := fmt.Sprintf("client-%d", i)
 		runtimeInfo := core_runtime.NewRuntimeInfo(fmt.Sprintf("cp-%d", i), config_core.Zone)
 		item := clientStreams[i]
-		comp := kds_client_v2.NewKDSSyncClient(core.Log.WithName("kds").WithName(clientID), resourceTypes, kds_client_v2.NewDeltaKDSStream(item, clientID, runtimeInfo, ""), cb, 0)
-		comp := kds_client_v2.NewKDSSyncClient(core.Log.WithName("kds").WithName(clientID), resourceTypes, kds_client_v2.NewDeltaKDSStream(item, clientID, fmt.Sprintf("cp-%d", i), "", len(resourceTypes)), cb, 0)
+		comp := kds_client_v2.NewKDSSyncClient(core.Log.WithName("kds").WithName(clientID), resourceTypes, kds_client_v2.NewDeltaKDSStream(item, clientID, runtimeInfo, "", len(resourceTypes)), cb, 0)
 		go func() {
 			_ = comp.Receive()
 			_ = item.CloseSend()
