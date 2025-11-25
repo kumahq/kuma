89c6adbea13cb6e4cfb4f84eab4122a9119fc5e5
Ilya Lobkov <ilya.lobkov@konghq.com>
2025-11-25 10:06:33 +0100

    resolve conflicts
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- pkg/kds/v2/client/zone_sync_test.go
+++ pkg/kds/v2/client/zone_sync_test.go
@@ -97,8 +97,7 @@ var _ = Describe("Zone Delta Sync", func() {
 			policySync := client_v2.NewKDSSyncClient(
 				core.Log.WithName("kds-sink"),
 				kdsCtx.TypesSentByGlobal,
-				client_v2.NewDeltaKDSStream(clientStream, zoneName, runtimeInfo, ""),
-				client_v2.NewDeltaKDSStream(clientStream, zoneName, "zone-inst", "", len(kdsCtx.TypesSentByGlobal)),
+				client_v2.NewDeltaKDSStream(clientStream, zoneName, runtimeInfo, "", len(kdsCtx.TypesSentByGlobal)),
 				sync_store_v2.ZoneSyncCallback(context.Background(), zoneSyncer, false, nil, "kuma-system"), 0,
 			)
 			_ = policySync.Receive()
