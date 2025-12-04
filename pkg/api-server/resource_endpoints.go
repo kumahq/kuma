1ec6086835ad6b1b4e58de8ec6b042949467fd18
Marcin Skalski <skalskimarcin33@gmail.com>
2025-12-04 12:19:26 +0100

    test(api-server): add immutable label validation tests
    Add tests for validating immutable labels (origin, zone, display-name) cannot be changed on resource update. Fix test setup to not pre-load resources that tests intend to create.
    
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- pkg/api-server/resource_endpoints.go
+++ pkg/api-server/resource_endpoints.go
@@ -496,6 +496,24 @@ func (r *resourceEndpoints) updateResource(
 	}
 
 	_ = currentRes.SetSpec(newResRest.GetSpec())
+
+	// Compute labels for current state to get correct immutable values
+	currentLabels, err := core_model.ComputeLabels(
+		currentRes.Descriptor(),
+		currentRes.GetSpec(),
+		currentRes.GetMeta().GetLabels(),
+		meshName,
+		core_model.WithNamespace(core_model.GetNamespace(currentRes.GetMeta(), r.systemNamespace)),
+		core_model.WithMode(r.mode),
+		core_model.WithK8s(r.isK8s),
+		core_model.WithZone(r.zoneName),
+	)
+	if err != nil {
+		rest_errors.HandleError(ctx, response, err, "Could not compute current labels")
+		return
+	}
+
+	// Compute labels for new request
 	labels, err := core_model.ComputeLabels(
 		currentRes.Descriptor(),
 		currentRes.GetSpec(),
@@ -508,6 +526,12 @@ func (r *resourceEndpoints) updateResource(
 	)
 	if err != nil {
 		rest_errors.HandleError(ctx, response, err, "Could not compute labels for a resource")
+		return
+	}
+
+	// Validate immutable labels by comparing computed results
+	if validationErr := r.validateImmutableLabels(currentLabels, labels); validationErr.HasViolations() {
+		rest_errors.HandleError(ctx, response, &validationErr, "Could not update a resource")
 		return
 	}
 
@@ -639,6 +663,35 @@ func (r *resourceEndpoints) validateLabels(resource rest.Resource) validators.Va
 			err.AddViolationAt(validators.Root().Key(k), msg)
 		}
 	}
+	return err
+}
+
+func (r *resourceEndpoints) validateImmutableLabels(currentComputedLabels, newComputedLabels map[string]string) validators.ValidationError {
+	var err validators.ValidationError
+	
+	immutableLabels := []string{
+		mesh_proto.ResourceOriginLabel,
+		mesh_proto.ZoneTag,
+		mesh_proto.DisplayName,
+	}
+
+	for _, label := range immutableLabels {
+		currentVal, currentExists := currentComputedLabels[label]
+		newVal, newExists := newComputedLabels[label]
+
+		if currentExists && !newExists {
+			err.AddViolationAt(
+				validators.Root().Key(label),
+				fmt.Sprintf("is immutable, cannot be removed (was %q)", currentVal),
+			)
+		} else if currentExists && currentVal != newVal {
+			err.AddViolationAt(
+				validators.Root().Key(label),
+				fmt.Sprintf("is immutable, cannot be changed from %q to %q", currentVal, newVal),
+			)
+		}
+	}
+
 	return err
 }
 
