4fa6057ae9954a24ee4d919edf164c9d5b0f3736
slonka <slonka@users.noreply.github.com>
2025-12-10 17:59:17 +0100

    feat(meshtrust): remove newlines
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- test/e2e_env/universal/meshtrust/meshtrust.go
+++ test/e2e_env/universal/meshtrust/meshtrust.go
@@ -25,49 +25,27 @@ func MeshTrust() {
 		Expect(err).ToNot(HaveOccurred())
 
 		yaml := `
-
 								type: MeshTrust
-
 								mesh: meshtrust-test
-
 								name: trust-1
-
 								spec:
-
 								  trustDomain: example.com
-
 								  caBundles:
-
 								  - type: Pem
-
 								    pem:
-
 								      value: |
-
 								        -----BEGIN CERTIFICATE-----
-
 								        MIIB9jCCAZ2gAwIBAgIUJ1gLZ/fvZhGq51qWrJzL6z2XWoQwCgYIKoZIzj0EAwIw
-
 								        EjEQMA4GA1UEChMHVGVzdCBDQTAeFw0yNTA3MzAxMjAwMDBaFw0zNTA3MjcxMjAw
-
 								        MDBaMBIxEDAOBgNVBAoTB1Rlc3QgQ0EwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNC
-
 								        AAQaXDFzOPslZ4e8n2KjsNkG+Wxi37L2zRWdMczDi7VqLrO03lczkB98/vzrdMKF
-
 								        JgYxycULx10EYgMGQkgLrf1po2QwYjAdBgNVHQ4EFgQUUQBd5VjEO3N4XcgrxgMK
-
 								        NU9xIQswHwYDVR0jBBgwFoAUUQBd5VjEO3N4XcgrxgMKNU9xIQswDwYDVR0TAQH/
-
 								        BAUwAwEB/zAKBggqhkjOPQQDAgNHADBEAiA3dhhIQCzNkeGSjj6jK+jGE8fEKVmp
-
 								        c9Vh+kJkmPUJZQIgQBr2GkV8uSfq/5ZKHD6jz6MJvKsg06dMBdvZBIA2ujg=
-
 								        -----END CERTIFICATE-----
-
 								  origin:
-
 								    kri: kri-test-value
-
 								`
 
 		err = cluster.GetKumactlOptions().KumactlApplyFromString(yaml)
