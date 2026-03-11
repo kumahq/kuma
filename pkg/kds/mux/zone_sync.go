ed592c4d5f20b539aa778e6be25ec27c9da64a3f
Ilya Lobkov <ilya.lobkov@konghq.com>
2026-03-11 22:42:06 +0100

    delete createZoneOnFirstConnect flag

--- pkg/kds/mux/zone_sync.go
+++ pkg/kds/mux/zone_sync.go
@@ -42,17 +42,16 @@ type KDSSyncServiceServer struct {
 	extensions context.Context
 	eventBus   events.EventBus
 	mesh_proto.UnimplementedKDSSyncServiceServer
-	context                  context.Context
-	resManager               core_manager.ResourceManager
-	upsertCfg                config_store.UpsertConfig
-	instanceID               string
-	createZoneOnFirstConnect bool
-	deltaServer              delta.Server
-	typesSentByZone          []core_model.ResourceType
-	resourceSyncer           kds_sync_store_v2.ResourceSyncer
-	k8sStore                 bool
-	systemNamespace          string
-	responseBackoff          time.Duration
+	context         context.Context
+	resManager      core_manager.ResourceManager
+	upsertCfg       config_store.UpsertConfig
+	instanceID      string
+	deltaServer     delta.Server
+	typesSentByZone []core_model.ResourceType
+	resourceSyncer  kds_sync_store_v2.ResourceSyncer
+	k8sStore        bool
+	systemNamespace string
+	responseBackoff time.Duration
 }
 
 func NewKDSSyncServiceServer(
@@ -61,26 +60,25 @@ func NewKDSSyncServiceServer(
 	resourceSyncer kds_sync_store_v2.ResourceSyncer,
 ) *KDSSyncServiceServer {
 	return &KDSSyncServiceServer{
-		context:                  rt.AppContext(),
-		filters:                  rt.KDSContext().GlobalServerFiltersV2,
-		extensions:               rt.Extensions(),
-		eventBus:                 rt.EventBus(),
-		resManager:               rt.ResourceManager(),
-		upsertCfg:                rt.Config().Store.Upsert,
-		instanceID:               rt.GetInstanceId(),
-		deltaServer:              deltaServer,
-		createZoneOnFirstConnect: rt.KDSContext().CreateZoneOnFirstConnect,
-		typesSentByZone:          rt.KDSContext().TypesSentByZone,
-		resourceSyncer:           resourceSyncer,
-		k8sStore:                 rt.Config().Store.Type == config_store.KubernetesStore,
-		systemNamespace:          rt.Config().Store.Kubernetes.SystemNamespace,
-		responseBackoff:          rt.Config().Multizone.Global.KDS.ResponseBackoff.Duration,
+		context:         rt.AppContext(),
+		filters:         rt.KDSContext().GlobalServerFiltersV2,
+		extensions:      rt.Extensions(),
+		eventBus:        rt.EventBus(),
+		resManager:      rt.ResourceManager(),
+		upsertCfg:       rt.Config().Store.Upsert,
+		instanceID:      rt.GetInstanceId(),
+		deltaServer:     deltaServer,
+		typesSentByZone: rt.KDSContext().TypesSentByZone,
+		resourceSyncer:  resourceSyncer,
+		k8sStore:        rt.Config().Store.Type == config_store.KubernetesStore,
+		systemNamespace: rt.Config().Store.Kubernetes.SystemNamespace,
+		responseBackoff: rt.Config().Multizone.Global.KDS.ResponseBackoff.Duration,
 	}
 }
 
 var _ mesh_proto.KDSSyncServiceServer = &KDSSyncServiceServer{}
 
-func createZoneIfAbsent(ctx context.Context, log logr.Logger, name string, resManager core_manager.ResourceManager, createZoneOnConnect bool) error {
+func createZoneIfAbsent(ctx context.Context, log logr.Logger, name string, resManager core_manager.ResourceManager) error {
 	zone := &system.ZoneResource{
 		Spec: &system_proto.Zone{
 			Enabled: util_proto.Bool(true),
@@ -117,7 +115,7 @@ func (g *KDSSyncServiceServer) GlobalToZoneSync(stream mesh_proto.KDSSyncService
 	processingErrorsCh := make(chan error, 1)
 	go func() {
 		logger.Info("Global To Zone new session created")
-		if err := createZoneIfAbsent(stream.Context(), logger, zone, g.resManager, g.createZoneOnFirstConnect); err != nil {
+		if err := createZoneIfAbsent(stream.Context(), logger, zone, g.resManager); err != nil {
 			if errors.Is(err, context.Canceled) {
 				processingErrorsCh <- nil
 				return
@@ -184,7 +182,7 @@ func (g *KDSSyncServiceServer) ZoneToGlobalSync(stream mesh_proto.KDSSyncService
 
 	processingErrorsCh := make(chan error, 1)
 	go func() {
-		if err := createZoneIfAbsent(stream.Context(), logger, zone, g.resManager, g.createZoneOnFirstConnect); err != nil {
+		if err := createZoneIfAbsent(stream.Context(), logger, zone, g.resManager); err != nil {
 			if errors.Is(err, context.Canceled) {
 				processingErrorsCh <- nil
 				return
