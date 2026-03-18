9ff9898d835f933fa5c641c7f5f5480a9fef4ce4
Bart Smykla <bartek@smykla.com>
2026-03-18 11:25:24 +0100

    chore(motb): drop OTel endpoint deprecations
    Deprecation warnings for inline openTelemetry.endpoint
    will be added in a separate PR closer to 2.14.
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/plugins/policies/meshtrace/api/v1alpha1/deprecated.go
+++ pkg/plugins/policies/meshtrace/api/v1alpha1/deprecated.go
@@ -2,16 +2,8 @@ package v1alpha1
 
 import (
 	"github.com/kumahq/kuma/v2/pkg/plugins/policies/core/jsonpatch/validators"
-	"github.com/kumahq/kuma/v2/pkg/util/pointer"
 )
 
 func (t *MeshTraceResource) Deprecations() []string {
-	var deprecations []string
-	for _, backend := range pointer.Deref(t.Spec.Default.Backends) {
-		if backend.OpenTelemetry != nil && backend.OpenTelemetry.Endpoint != "" && backend.OpenTelemetry.BackendRef == nil {
-			deprecations = append(deprecations, "openTelemetry.endpoint is deprecated, use openTelemetry.backendRef with a MeshOpenTelemetryBackend resource instead")
-			break
-		}
-	}
-	return append(deprecations, validators.TopLevelTargetRefDeprecations(t.Spec.TargetRef)...)
+	return validators.TopLevelTargetRefDeprecations(t.Spec.TargetRef)
 }
