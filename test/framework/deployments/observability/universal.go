e6acad4789a9d1ee15c40d0c2be402bd500274af
Matthieu MOREL <matthieu.morel35@gmail.com>
2026-03-29 12:59:49 +0200

    Replace fmt.Errorf with errors.New for container error
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- test/framework/deployments/observability/universal.go
+++ test/framework/deployments/observability/universal.go
@@ -1,6 +1,7 @@
 package observability
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"strconv"
@@ -94,7 +95,7 @@ func (u *universalDeployment) Delete(cluster framework.Cluster) error {
 		func() (string, error) {
 			_, err := u.dockerBackend.StopE(cluster.GetTesting(), []string{u.container}, &docker.StopOptions{Time: 1})
 			if err == nil {
-				return "Container still running", fmt.Errorf("Container still running")
+				return "Container still running", errors.New("Container still running")
 			}
 			return "Container stopped", nil
 		})
