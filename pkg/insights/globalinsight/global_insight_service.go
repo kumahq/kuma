174674c5cc83a2cac57b89c71ab326c2f2cab772
Bart Smykla <bartek@smykla.com>
2026-04-22 14:43:49 +0200

    refactor(oapi): remove GlobalInsight schema alias (#16345)
    The `GlobalInsight` schema alias wrapped `GlobalInsightBase` with
    `allOf`, adding no value. This caused name collisions
    (`GlobalInsight-2`) when bundling Kuma specs with downstream projects
    that define their own `GlobalInsight` extending `GlobalInsightBase`.
    
    - Remove `GlobalInsight` alias schema from `api.yaml`
    - Update `GlobalInsightResponse` to reference `GlobalInsightBase`
    directly
    - Update Go code to use `GlobalInsightBase` type
    
    Follow-up to #16327 which renamed `GlobalInsight` to
    `GlobalInsightBase`.
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/insights/globalinsight/global_insight_service.go
+++ pkg/insights/globalinsight/global_insight_service.go
@@ -13,7 +13,7 @@ import (
 )
 
 type GlobalInsightService interface {
-	GetGlobalInsight(ctx context.Context) (*api_types.GlobalInsight, error)
+	GetGlobalInsight(ctx context.Context) (*api_types.GlobalInsightBase, error)
 }
 
 type defaultGlobalInsightService struct {
@@ -26,8 +26,8 @@ func NewDefaultGlobalInsightService(resourceStore core_store.ResourceStore) Glob
 	return &defaultGlobalInsightService{resourceStore: resourceStore}
 }
 
-func (gis *defaultGlobalInsightService) GetGlobalInsight(ctx context.Context) (*api_types.GlobalInsight, error) {
-	globalInsights := &api_types.GlobalInsight{CreatedAt: core.Now()}
+func (gis *defaultGlobalInsightService) GetGlobalInsight(ctx context.Context) (*api_types.GlobalInsightBase, error) {
+	globalInsights := &api_types.GlobalInsightBase{CreatedAt: core.Now()}
 
 	meshInsights := &mesh.MeshInsightResourceList{}
 	if err := gis.resourceStore.List(ctx, meshInsights); err != nil {
@@ -60,7 +60,7 @@ func (gis *defaultGlobalInsightService) GetGlobalInsight(ctx context.Context) (*
 
 func (gis *defaultGlobalInsightService) aggregateDataplanes(
 	meshInsights *mesh.MeshInsightResourceList,
-	globalInsight *api_types.GlobalInsight,
+	globalInsight *api_types.GlobalInsightBase,
 ) {
 	for _, meshInsight := range meshInsights.GetItems() {
 		dataplanesByType := meshInsight.GetSpec().(*mesh_proto.MeshInsight).GetDataplanesByType()
@@ -87,7 +87,7 @@ func (gis *defaultGlobalInsightService) aggregateDataplanes(
 
 func (gis *defaultGlobalInsightService) aggregatePolicies(
 	meshInsights *mesh.MeshInsightResourceList,
-	globalInsight *api_types.GlobalInsight,
+	globalInsight *api_types.GlobalInsightBase,
 ) {
 	for _, meshInsight := range meshInsights.GetItems() {
 		policies := meshInsight.GetSpec().(*mesh_proto.MeshInsight).GetPolicies()
@@ -98,9 +98,29 @@ func (gis *defaultGlobalInsightService) aggregatePolicies(
 	}
 }
 
+<<<<<<< HEAD
+=======
+func (gis *defaultGlobalInsightService) aggregateResources(
+	meshInsights *mesh.MeshInsightResourceList,
+	globalInsight *api_types.GlobalInsightBase,
+) {
+	globalInsight.Resources = map[string]api_types.ResourceStats{}
+	for _, meshInsight := range meshInsights.GetItems() {
+		for resName, resStat := range meshInsight.GetSpec().(*mesh_proto.MeshInsight).Resources {
+			stats, ok := globalInsight.Resources[resName]
+			if !ok {
+				stats = api_types.ResourceStats{}
+			}
+			stats.Total += int(resStat.Total)
+			globalInsight.Resources[resName] = stats
+		}
+	}
+}
+
+>>>>>>> 84ada43c9b (refactor(oapi): remove GlobalInsight schema alias (#16345))
 func (gis *defaultGlobalInsightService) aggregateServices(
 	ctx context.Context,
-	globalInsight *api_types.GlobalInsight,
+	globalInsight *api_types.GlobalInsightBase,
 ) error {
 	serviceInsights := &mesh.ServiceInsightResourceList{}
 	if err := gis.resourceStore.List(ctx, serviceInsights); err != nil {
@@ -143,7 +163,7 @@ func updateServiceStatus(serviceStatus mesh_proto.ServiceInsight_Service_Status,
 
 func (gis *defaultGlobalInsightService) aggregateZoneControlPlanes(
 	ctx context.Context,
-	globalInsight *api_types.GlobalInsight,
+	globalInsight *api_types.GlobalInsightBase,
 ) error {
 	zoneInsights := &system.ZoneInsightResourceList{}
 	if err := gis.resourceStore.List(ctx, zoneInsights); err != nil {
@@ -162,7 +182,7 @@ func (gis *defaultGlobalInsightService) aggregateZoneControlPlanes(
 
 func (gis *defaultGlobalInsightService) aggregateZoneIngresses(
 	ctx context.Context,
-	globalInsight *api_types.GlobalInsight,
+	globalInsight *api_types.GlobalInsightBase,
 ) error {
 	zoneIngressInsights := &mesh.ZoneIngressInsightResourceList{}
 	if err := gis.resourceStore.List(ctx, zoneIngressInsights); err != nil {
@@ -181,7 +201,7 @@ func (gis *defaultGlobalInsightService) aggregateZoneIngresses(
 
 func (gis *defaultGlobalInsightService) aggregateZoneEgresses(
 	ctx context.Context,
-	globalInsight *api_types.GlobalInsight,
+	globalInsight *api_types.GlobalInsightBase,
 ) error {
 	zoneEgressInsights := &mesh.ZoneEgressInsightResourceList{}
 	if err := gis.resourceStore.List(ctx, zoneEgressInsights); err != nil {
