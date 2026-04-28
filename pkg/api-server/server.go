3586e6c91f19e2dbd102ffc3c6c44622a49e3a91
Bart Smykla <bartek@smykla.com>
2026-04-28 12:16:09 +0200

    fix(api-server): add Handler method, align auth_test.go with master
    Add Handler() http.Handler method to ApiServer for test access to mux.
    Replace auth_test.go with master version using httptest-based pattern
    instead of real network calls to externalIP, which fails because test
    server now binds only to 127.0.0.1.
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/api-server/server.go
+++ pkg/api-server/server.go
@@ -77,6 +77,10 @@ func (a *ApiServer) Config() api_server.ApiServerConfig {
 	return a.config
 }
 
+func (a *ApiServer) Handler() http.Handler {
+	return a.mux
+}
+
 func init() {
 	// turn off escape & character so the link in "next" fields for resources is user friendly
 	restful.NewEncoder = func(w io.Writer) *json.Encoder {
