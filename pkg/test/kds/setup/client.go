f41fa0577ba541863ee6ff83c1e81181ea5f7d87
Lukasz Dziedziak <lukidzi@gmail.com>
2026-04-28 07:36:56 -0500

    resolve conflicts
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/test/kds/setup/client.go
+++ pkg/test/kds/setup/client.go
@@ -16,12 +16,8 @@ func StartDeltaClient(clientStreams []*grpc.MockDeltaClientStream, resourceTypes
 		clientID := fmt.Sprintf("client-%d", i)
 		runtimeInfo := core_runtime.NewRuntimeInfo(fmt.Sprintf("cp-%d", i), config_core.Zone)
 		item := clientStreams[i]
-<<<<<<< HEAD
-		comp := kds_client_v2.NewKDSSyncClient(core.Log.WithName("kds").WithName(clientID), resourceTypes, kds_client_v2.NewDeltaKDSStream(item, clientID, runtimeInfo, "", len(resourceTypes)), cb, 0)
-=======
-		kdsStream := kds_client_v2.NewDeltaKDSStream(item, clientID, fmt.Sprintf("cp-%d", i), "", len(resourceTypes))
+		kdsStream := kds_client_v2.NewDeltaKDSStream(item, clientID, runtimeInfo, "", len(resourceTypes))
 		comp := kds_client_v2.NewKDSSyncClient(core.Log.WithName("kds").WithName(clientID), resourceTypes, kdsStream, cb, 0)
->>>>>>> 666d45dc0f (fix(kds): reconnect mux client when GlobalToZone stream is closed by … (#16326))
 		go func() {
 			_ = comp.Receive()
 			_ = kdsStream.CloseSend()
