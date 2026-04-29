e166e1d1615a637e2dad123449e643b3145a3c68
Ilya Lobkov <ilya.lobkov@konghq.com>
2026-04-30 00:39:42 +0200

    revert unnesessary changes in registry.go
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- pkg/core/resources/labels/registry.go
+++ pkg/core/resources/labels/registry.go
@@ -8,14 +8,13 @@ import (
 // if changed sync with:
 // https://github.com/Kong/shared-speakeasy/blob/b3ddd3ef1f31e42bfe71b96ea473493072f9742c/customtypes/kumalabels/kumalabels.go#L15
 var AllComputedLabels = map[string]struct{}{
-	metadata.KumaMeshLabel:         {}, // can be set by user, on Universal must be equal to 'meta.mesh'
-	mesh_proto.ResourceOriginLabel: {}, // must be set on zone if special flag is on, should be 'global' on 'global' and 'zone' on 'zone'
-	mesh_proto.ZoneTag:             {}, // only on zone, should be equal to zone
-	mesh_proto.EnvTag:              {}, // uni on uni and k8s on k8s
-	mesh_proto.KubeNamespaceTag:    {}, // only on k8s, equal to the namespace
-	mesh_proto.PolicyRoleLabel:     {}, // only on policies, defined by the 'spec'
-	mesh_proto.ProxyTypeLabel:      {}, // only on Dataplane
-	metadata.KumaServiceAccount:    {}, // only on Dataplane
-	metadata.KumaWorkload:          {}, // only on Dataplane
-	mesh_proto.DisplayName:         {}, // can be set but must be equal to 'meta.name', unless the user is priviledged
+	metadata.KumaMeshLabel:         {},
+	mesh_proto.ResourceOriginLabel: {},
+	mesh_proto.ZoneTag:             {},
+	mesh_proto.EnvTag:              {},
+	mesh_proto.KubeNamespaceTag:    {},
+	mesh_proto.PolicyRoleLabel:     {},
+	mesh_proto.ProxyTypeLabel:      {},
+	metadata.KumaServiceAccount:    {},
+	metadata.KumaWorkload:          {},
 }
