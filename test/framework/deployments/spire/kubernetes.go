0a3f8f1405b45b5995d330ead4eaabfcd28e330e
Marcin Skalski <skalskimarcin33@gmail.com>
2026-05-14 16:44:54 +0200

    test(framework): preload spire chart images
    MeshIdentity Spire [BeforeAll] is the top e2e flake on master (~14
    failures in 5 days). The pod always stalls on Init:ImagePullBackOff
    for ghcr.io/spiffe/spiffe-helper:0.11.0 - kubelet's pull is canceled
    by a transient ghcr.io timeout and the 4.5min wait budget expires
    before backoff resolves.
    
    Add K8sCluster.PreloadImages(images ...string) that docker-pulls
    images in parallel and imports them into the cluster's container
    runtime (k3d image import / kind load). Pods then start from
    locally-cached images, eliminating runtime registry calls entirely.
    
    Wire it into spire.Install: render the spire-crds and spire charts
    with helm template, extract image references, preload before helm
    install. This generalises to any test that helm-installs an
    externally-sourced chart.
    
    > Changelog: skip
    
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- test/framework/deployments/spire/kubernetes.go
+++ test/framework/deployments/spire/kubernetes.go
@@ -1,6 +1,8 @@
 package spire
 
 import (
+	"regexp"
+
 	"github.com/gruntwork-io/terratest/modules/helm"
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/pkg/errors"
@@ -8,6 +10,8 @@ import (
 
 	"github.com/kumahq/kuma/v2/test/framework"
 )
+
+const spireChartRepo = "https://spiffe.github.io/helm-charts-hardened/"
 
 type k8sDeployment struct {
 	namespace      string
@@ -27,7 +31,6 @@ func (t *k8sDeployment) Name() string {
 }
 
 func (t *k8sDeployment) Deploy(cluster framework.Cluster) error {
-	var err error
 	if t.namespace == "" {
 		t.namespace = "spire"
 	}
@@ -35,45 +38,101 @@ func (t *k8sDeployment) Deploy(cluster framework.Cluster) error {
 	opts := helm.Options{
 		KubectlOptions: cluster.GetKubectlOptions(t.namespace),
 	}
-	// install crds
-	_, err = helm.RunHelmCommandAndGetStdOutE(cluster.GetTesting(), &opts, "install", "spire-crds",
+
+	spireSetValues := []string{
+		"--set", "global.spire.trustDomain=" + t.trustDomain,
+		"--set", "global.spire.tools.kubectl.tag=" + t.kubectlVersion,
+	}
+
+	// Preload chart images into the cluster's container runtime so pod startup
+	// does not hit ghcr.io / docker.io at test time. Transient registry
+	// timeouts during runtime pulls are the dominant cause of ImagePullBackOff
+	// flakes on this BeforeAll.
+	if k8sCluster, ok := cluster.(*framework.K8sCluster); ok {
+		images, err := chartImages(cluster, "spire-crds", &opts, "spire-crds")
+		if err != nil {
+			return errors.Wrap(err, "render spire-crds chart")
+		}
+		more, err := chartImages(cluster, "spire", &opts, "spire", spireSetValues...)
+		if err != nil {
+			return errors.Wrap(err, "render spire chart")
+		}
+		if err := k8sCluster.PreloadImages(dedupe(append(images, more...))...); err != nil {
+			return errors.Wrap(err, "preload spire images")
+		}
+	}
+
+	if _, err := helm.RunHelmCommandAndGetStdOutE(cluster.GetTesting(), &opts, "install", "spire-crds",
 		"--namespace", t.namespace,
-		"--repo", "https://spiffe.github.io/helm-charts-hardened/",
+		"--repo", spireChartRepo,
 		"spire-crds",
-	)
-	if err != nil {
+	); err != nil {
 		return err
 	}
 
-	_, err = helm.RunHelmCommandAndGetStdOutE(cluster.GetTesting(), &opts, "install", "spire",
+	installArgs := []string{
 		"--namespace", t.namespace,
-		"--repo", "https://spiffe.github.io/helm-charts-hardened/",
-		"--set", "global.spire.trustDomain="+t.trustDomain,
-		"--set", "global.spire.tools.kubectl.tag="+t.kubectlVersion,
-		"spire",
-	)
-	if err != nil {
+		"--repo", spireChartRepo,
+	}
+	installArgs = append(installArgs, spireSetValues...)
+	installArgs = append(installArgs, "spire")
+	if _, err := helm.RunHelmCommandAndGetStdOutE(cluster.GetTesting(), &opts, "install", append([]string{"spire"}, installArgs...)...); err != nil {
 		return err
 	}
 
-	err = t.isPodReady(cluster, "app.kubernetes.io/name=agent")
-	if err != nil {
-		return err
+	for _, selector := range []string{
+		"app.kubernetes.io/name=agent",
+		"app.kubernetes.io/name=server",
+		"app.kubernetes.io/name=spiffe-csi-driver",
+		"app.kubernetes.io/name=spiffe-oidc-discovery-provider",
+	} {
+		if err := t.isPodReady(cluster, selector); err != nil {
+			return err
+		}
 	}
-	err = t.isPodReady(cluster, "app.kubernetes.io/name=server")
-	if err != nil {
-		return err
-	}
-	err = t.isPodReady(cluster, "app.kubernetes.io/name=spiffe-csi-driver")
-	if err != nil {
-		return err
-	}
-	err = t.isPodReady(cluster, "app.kubernetes.io/name=spiffe-oidc-discovery-provider")
-	if err != nil {
-		return err
-	}
-
 	return nil
+}
+
+// chartImages renders the named chart with `helm template` and returns the
+// distinct container images referenced in the resulting manifests.
+func chartImages(cluster framework.Cluster, releaseName string, opts *helm.Options, chart string, extraArgs ...string) ([]string, error) {
+	args := []string{releaseName, chart, "--repo", spireChartRepo}
+	args = append(args, extraArgs...)
+	out, err := helm.RunHelmCommandAndGetStdOutE(cluster.GetTesting(), opts, "template", args...)
+	if err != nil {
+		return nil, err
+	}
+	return parseImages(out), nil
+}
+
+var imageLineRE = regexp.MustCompile(`(?m)^\s*image:\s*["']?([^"'\s]+)["']?\s*$`)
+
+func parseImages(rendered string) []string {
+	matches := imageLineRE.FindAllStringSubmatch(rendered, -1)
+	seen := map[string]struct{}{}
+	var images []string
+	for _, m := range matches {
+		img := m[1]
+		if _, ok := seen[img]; ok {
+			continue
+		}
+		seen[img] = struct{}{}
+		images = append(images, img)
+	}
+	return images
+}
+
+func dedupe(images []string) []string {
+	seen := map[string]struct{}{}
+	out := images[:0]
+	for _, img := range images {
+		if _, ok := seen[img]; ok {
+			continue
+		}
+		seen[img] = struct{}{}
+		out = append(out, img)
+	}
+	return out
 }
 
 func (t *k8sDeployment) isPodReady(cluster framework.Cluster, selector string) error {
