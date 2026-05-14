55cb5202f7d0a29b4c790f95a83f013ca93db881
Lukasz Dziedziak <lukidzi@gmail.com>
2026-05-14 16:22:58 -0500

    fix test
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/core/resources/sni/sni_suite_test.go
+++ pkg/core/resources/sni/sni_suite_test.go
@@ -3,17 +3,9 @@ package sni_test
 import (
 	"testing"
 
-	. "github.com/onsi/ginkgo/v2"
-
-	meshexternalservice_api "github.com/kumahq/kuma/v2/pkg/core/resources/apis/meshexternalservice/api/v1alpha1"
-	meshmultizoneservice_api "github.com/kumahq/kuma/v2/pkg/core/resources/apis/meshmultizoneservice/api/v1alpha1"
-	meshservice_api "github.com/kumahq/kuma/v2/pkg/core/resources/apis/meshservice/api/v1alpha1"
-	"github.com/kumahq/kuma/v2/pkg/core/resources/registry"
+	"github.com/kumahq/kuma/v2/pkg/test"
 )
 
 func TestSNI(t *testing.T) {
-	registry.RegisterType(meshservice_api.MeshServiceResourceTypeDescriptor)
-	registry.RegisterType(meshexternalservice_api.MeshExternalServiceResourceTypeDescriptor)
-	registry.RegisterType(meshmultizoneservice_api.MeshMultiZoneServiceResourceTypeDescriptor)
-	RunSpecs(t, "SNI Compliance Warnings Suite")
+	test.RunSpecs(t, "SNI Compliance Warnings Suite")
 }
