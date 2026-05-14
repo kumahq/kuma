ddbcc17c005bc3ebc81648acad2c418f69481624
Bart Smykla <bartek@smykla.com>
2026-05-14 18:07:36 +0200

    feat(log): export log volume counter (#16617)
    ## Motivation
    
    On-call has no way to see which loggers dominate kuma-cp log volume
    during an incident, so demotion decisions for chatty loggers (next PRs
    in this series) come from guesswork. Without a meter the standard rots
    within a year.
    
    ## Implementation information
    
    Hook `componentAwareSink.Info` and `.Error` to increment a package-level
    `atomic.Pointer[prometheus.CounterVec]`. The pointer is nil until
    `SetupMetrics` is called from `pkg/metrics/components.Setup` during CP
    startup - no-op before then. Labels are `logger` (dot-separated name
    from `WithName` calls) and `level` (info/debug/error).
    
    ---------
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- pkg/metrics/components/cp_info.go
+++ pkg/metrics/components/cp_info.go
@@ -7,6 +7,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/collectors"
 
 	"github.com/kumahq/kuma/v2/pkg/core/runtime"
+	kuma_log "github.com/kumahq/kuma/v2/pkg/log"
 	metrics "github.com/kumahq/kuma/v2/pkg/metrics/store"
 	"github.com/kumahq/kuma/v2/pkg/version"
 )
@@ -47,6 +48,12 @@ func Setup(rt runtime.Runtime) error {
 	}
 	if err := rt.Metrics().Register(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{})); err != nil {
 		if !isDuplicatedError(err) { // can be already registered when K8S registerer is used
+			return err
+		}
+	}
+
+	if _, err := kuma_log.SetupMetrics(rt.Metrics()); err != nil {
+		if !isDuplicatedError(err) {
 			return err
 		}
 	}
