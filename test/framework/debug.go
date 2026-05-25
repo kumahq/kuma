fc9a88bb55f30b53d1e629b0ebe57fea45facad6
Lukasz Dziedziak <lukidzi@gmail.com>
2026-05-25 09:26:40 -0500

    fix(xds): test
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- test/framework/debug.go
+++ test/framework/debug.go
@@ -74,7 +74,7 @@ func assertNoXdsNacks(cluster Cluster) {
 			if !isNack {
 				continue
 			}
-			Expect(metric.GetCounter().GetValue()).To(BeNumerically("<", 3),
+			Expect(metric.GetCounter().GetValue()).To(BeZero(),
 				"CP %s reported %s NACK(s) for labels %s", cluster.Name(), metricName, formatLabels(metric.GetLabel()))
 		}
 	}
