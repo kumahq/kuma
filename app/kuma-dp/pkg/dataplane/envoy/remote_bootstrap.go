586ebba05afb4e6c78e348c3361eb1aee0721979
Ilya Lobkov <ilya.lobkov@konghq.com>
2026-05-28 14:56:23 +0200

    fix universal e2e by adding kuma-dp --skip-verify
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- app/kuma-dp/pkg/dataplane/envoy/remote_bootstrap.go
+++ app/kuma-dp/pkg/dataplane/envoy/remote_bootstrap.go
@@ -63,7 +63,11 @@ func (b *remoteBootstrapClient) Fetch(ctx context.Context, opts Opts, metadata m
 		client.Transport = &http.Transport{
 			TLSClientConfig: tlsConfig,
 		}
-		if opts.Config.ControlPlane.CaCert != "" {
+		switch {
+		case opts.Config.ControlPlane.TlsSkipVerify:
+			log.Info("[WARNING] --skip-verify is set, the Control Plane certificate will not be verified")
+			tlsConfig.InsecureSkipVerify = true // #nosec G402 -- opt-in via --skip-verify
+		case opts.Config.ControlPlane.CaCert != "":
 			certPool := x509.NewCertPool()
 			if ok := certPool.AppendCertsFromPEM([]byte(opts.Config.ControlPlane.CaCert)); !ok {
 				return nil, nil, errors.New("could not add certificate")
