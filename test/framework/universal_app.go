d3fdd90357bdf21e38543db59e194d82b11bf220
Ilya Lobkov <ilya.lobkov@konghq.com>
2026-05-29 19:35:41 +0200

    Remove '--skip-verify' argument from command
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- test/framework/universal_app.go
+++ test/framework/universal_app.go
@@ -392,7 +392,6 @@ cp %s/envoy /usr/bin/envoy
 		"runuser", "-u", "kuma-dp", "--",
 		"/usr/bin/kuma-dp", "run",
 		"--cp-address=" + cpAddress,
-		"--skip-verify",
 		"--dataplane-token-file=/kuma/token-" + name,
 		"--binary-path", "/usr/local/bin/envoy",
 	}
