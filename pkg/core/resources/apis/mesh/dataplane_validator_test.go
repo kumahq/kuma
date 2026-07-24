9074ba8b3f28719ce974ca542878aed6637c8fdc
Marcin Skalski <skalskimarcin33@gmail.com>
2026-07-24 14:22:33 +0200

    test(mesh): set explicit empty gateway tags
    Set tags: {} explicitly rather than omitting the field, matching the
    real K8s scenario (GatewayByServiceFor/GatewayByDeploymentFor
    populate an empty map, not a nil one) more closely.
    
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- pkg/core/resources/apis/mesh/dataplane_validator_test.go
+++ pkg/core/resources/apis/mesh/dataplane_validator_test.go
@@ -1730,6 +1730,7 @@ var _ = Describe("Dataplane", func() {
                   address: 192.168.0.1
                   gateway:
                     type: DELEGATED
+                    tags: {}
 `), dataplane.Spec)
 			Expect(err).ToNot(HaveOccurred())
 
