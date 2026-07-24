67514d2519ff32ce17fae8b675f6cce716e54bc4
Lukasz Dziedziak <lukidzi@gmail.com>
2026-07-24 13:07:48 -0500

    test(meshhealthcheck): outbound to backendRef
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/plugins/policies/meshhealthcheck/plugin/v1alpha1/plugin_test.go
+++ pkg/plugins/policies/meshhealthcheck/plugin/v1alpha1/plugin_test.go
@@ -1,4 +1,3 @@
-//nolint:staticcheck // SA1019 Test file: tests backward compatibility with deprecated core_rules.Rule
 package v1alpha1_test
 
 import (
@@ -17,10 +16,8 @@ import (
 	meshexternalservice_api "github.com/kumahq/kuma/v3/pkg/core/resources/apis/meshexternalservice/api/v1alpha1"
 	core_model "github.com/kumahq/kuma/v3/pkg/core/resources/model"
 	core_xds "github.com/kumahq/kuma/v3/pkg/core/xds"
-	xds_types "github.com/kumahq/kuma/v3/pkg/core/xds/types"
 	core_rules "github.com/kumahq/kuma/v3/pkg/plugins/policies/core/rules"
 	"github.com/kumahq/kuma/v3/pkg/plugins/policies/core/rules/outbound"
-	"github.com/kumahq/kuma/v3/pkg/plugins/policies/core/rules/subsetutils"
 	api "github.com/kumahq/kuma/v3/pkg/plugins/policies/meshhealthcheck/api/v1alpha1"
 	plugin "github.com/kumahq/kuma/v3/pkg/plugins/policies/meshhealthcheck/plugin/v1alpha1"
 	"github.com/kumahq/kuma/v3/pkg/test"
@@ -50,6 +47,21 @@ var _ = Describe("MeshHealthCheck", func() {
 		Name:         "backend",
 		SectionName:  "",
 	}
+	httpMeshServiceIdentifier := kri.Identifier{
+		ResourceType: "MeshService",
+		Mesh:         "default",
+		Name:         "echo-http",
+	}
+	tcpMeshServiceIdentifier := kri.Identifier{
+		ResourceType: "MeshService",
+		Mesh:         "default",
+		Name:         "echo-tcp",
+	}
+	grpcMeshServiceIdentifier := kri.Identifier{
+		ResourceType: "MeshService",
+		Mesh:         "default",
+		Name:         "echo-grpc",
+	}
 
 	type testCase struct {
 		resources        []core_xds.Resource
@@ -58,30 +70,48 @@ var _ = Describe("MeshHealthCheck", func() {
 	}
 	httpClusters := []core_xds.Resource{
 		{
-			Name:   "cluster-echo-http",
-			Origin: metadata.OriginOutbound,
+			Name:           "cluster-echo-http",
+			Origin:         metadata.OriginOutbound,
+			ResourceOrigin: httpMeshServiceIdentifier,
+			Protocol:       core_meta.ProtocolHTTP,
 			Resource: clusters.NewClusterBuilder(envoy_common.APIV3, httpServiceTag).
 				MustBuild(),
 		},
 		{
-			Name:   "cluster-echo-http-_0_",
-			Origin: metadata.OriginOutbound,
+			Name:           "cluster-echo-http-_0_",
+			Origin:         metadata.OriginOutbound,
+			ResourceOrigin: httpMeshServiceIdentifier,
+			Protocol:       core_meta.ProtocolHTTP,
 			Resource: clusters.NewClusterBuilder(envoy_common.APIV3, splitHttpServiceTag).
 				MustBuild(),
 		},
 	}
 	tcpCluster := []core_xds.Resource{
 		{
-			Name:   "cluster-echo-tcp",
-			Origin: metadata.OriginOutbound,
+			Name:           "cluster-echo-tcp",
+			Origin:         metadata.OriginOutbound,
+			ResourceOrigin: tcpMeshServiceIdentifier,
+			Protocol:       core_meta.ProtocolTCP,
+			Resource: clusters.NewClusterBuilder(envoy_common.APIV3, tcpServiceTag).
+				MustBuild(),
+		},
+	}
+	tcpClusterBackend := []core_xds.Resource{
+		{
+			Name:           "cluster-echo-tcp",
+			Origin:         metadata.OriginOutbound,
+			ResourceOrigin: backendMeshServiceIdentifier,
+			Protocol:       core_meta.ProtocolTCP,
 			Resource: clusters.NewClusterBuilder(envoy_common.APIV3, tcpServiceTag).
 				MustBuild(),
 		},
 	}
 	grpcCluster := []core_xds.Resource{
 		{
-			Name:   "cluster-echo-grpc",
-			Origin: metadata.OriginOutbound,
+			Name:           "cluster-echo-grpc",
+			Origin:         metadata.OriginOutbound,
+			ResourceOrigin: grpcMeshServiceIdentifier,
+			Protocol:       core_meta.ProtocolGRPC,
 			Resource: clusters.NewClusterBuilder(envoy_common.APIV3, grpcServiceTag).
 				MustBuild(),
 		},
@@ -104,28 +134,6 @@ var _ = Describe("MeshHealthCheck", func() {
 				Build()
 			proxy := xds_builders.Proxy().
 				WithDataplane(samples.DataplaneBackendBuilder()).
-				WithOutbounds(xds_types.Outbounds{
-					{LegacyOutbound: builders.Outbound().WithAddress("127.0.0.1").WithPort(27777).WithTags(map[string]string{
-						mesh_proto.ServiceTag:  httpServiceTag,
-						mesh_proto.ProtocolTag: "http",
-					}).Build()},
-					{LegacyOutbound: builders.Outbound().WithAddress("127.0.0.1").WithPort(27778).WithTags(map[string]string{
-						mesh_proto.ServiceTag:  tcpServiceTag,
-						mesh_proto.ProtocolTag: "tcp",
-					}).Build()},
-					{LegacyOutbound: builders.Outbound().WithAddress("127.0.0.1").WithPort(27779).WithTags(map[string]string{
-						mesh_proto.ServiceTag:  grpcServiceTag,
-						mesh_proto.ProtocolTag: "grpc",
-					}).Build()},
-					{LegacyOutbound: builders.Outbound().WithAddress("240.0.0.1").WithPort(27779).WithTags(map[string]string{
-						mesh_proto.ServiceTag:  grpcServiceTag,
-						mesh_proto.ProtocolTag: "grpc",
-					}).Build()},
-					{LegacyOutbound: builders.Outbound().WithAddress("127.0.0.1").WithPort(27780).WithTags(map[string]string{
-						mesh_proto.ServiceTag:  splitHttpServiceTag,
-						mesh_proto.ProtocolTag: "http",
-					}).Build()},
-				}).
 				WithPolicies(xds_builders.MatchedPolicies().WithToPolicy(api.MeshHealthCheckType, given.toRules)).
 				WithRouting(
 					xds_builders.Routing().
@@ -150,42 +158,43 @@ var _ = Describe("MeshHealthCheck", func() {
 		Entry("HTTP HealthCheck", testCase{
 			resources: httpClusters,
 			toRules: core_rules.ToRules{
-				Rules: []*core_rules.Rule{
-					{
-						Subset: subsetutils.Subset{},
-						Conf: api.Conf{
-							Interval:                     test.ParseDuration("10s"),
-							Timeout:                      test.ParseDuration("2s"),
-							UnhealthyThreshold:           pointer.To[int32](3),
-							HealthyThreshold:             pointer.To[int32](1),
-							InitialJitter:                test.ParseDuration("13s"),
-							IntervalJitter:               test.ParseDuration("15s"),
-							IntervalJitterPercent:        pointer.To[int32](10),
-							HealthyPanicThreshold:        pointer.To(intstr.FromString("62.9")),
-							FailTrafficOnPanic:           pointer.To(true),
-							EventLogPath:                 pointer.To("/tmp/log.txt"),
-							AlwaysLogHealthCheckFailures: pointer.To(false),
-							NoTrafficInterval:            test.ParseDuration("16s"),
-							Http: &api.HttpHealthCheck{
-								Disabled: pointer.To(false),
-								Path:     pointer.To("/health"),
-								RequestHeadersToAdd: &api.HeaderModifier{
-									Add: &[]api.HeaderKeyValue{
-										{
-											Name:  "x-some-header",
-											Value: "value",
+				ResourceRules: map[kri.Identifier]outbound.ResourceRule{
+					httpMeshServiceIdentifier: {
+						Conf: []any{
+							api.Conf{
+								Interval:                     test.ParseDuration("10s"),
+								Timeout:                      test.ParseDuration("2s"),
+								UnhealthyThreshold:           pointer.To[int32](3),
+								HealthyThreshold:             pointer.To[int32](1),
+								InitialJitter:                test.ParseDuration("13s"),
+								IntervalJitter:               test.ParseDuration("15s"),
+								IntervalJitterPercent:        pointer.To[int32](10),
+								HealthyPanicThreshold:        pointer.To(intstr.FromString("62.9")),
+								FailTrafficOnPanic:           pointer.To(true),
+								EventLogPath:                 pointer.To("/tmp/log.txt"),
+								AlwaysLogHealthCheckFailures: pointer.To(false),
+								NoTrafficInterval:            test.ParseDuration("16s"),
+								Http: &api.HttpHealthCheck{
+									Disabled: pointer.To(false),
+									Path:     pointer.To("/health"),
+									RequestHeadersToAdd: &api.HeaderModifier{
+										Add: &[]api.HeaderKeyValue{
+											{
+												Name:  "x-some-header",
+												Value: "value",
+											},
+										},
+										Set: &[]api.HeaderKeyValue{
+											{
+												Name:  "x-some-other-header",
+												Value: "value",
+											},
 										},
 									},
-									Set: &[]api.HeaderKeyValue{
-										{
-											Name:  "x-some-other-header",
-											Value: "value",
-										},
-									},
+									ExpectedStatuses: &[]int32{200, 201},
 								},
-								ExpectedStatuses: &[]int32{200, 201},
+								ReuseConnection: pointer.To(true),
 							},
-							ReuseConnection: pointer.To(true),
 						},
 					},
 				},
@@ -198,18 +207,19 @@ var _ = Describe("MeshHealthCheck", func() {
 		Entry("TCP HealthCheck", testCase{
 			resources: tcpCluster,
 			toRules: core_rules.ToRules{
-				Rules: []*core_rules.Rule{
-					{
-						Subset: subsetutils.Subset{},
-						Conf: api.Conf{
-							Interval:           test.ParseDuration("10s"),
-							Timeout:            test.ParseDuration("2s"),
-							UnhealthyThreshold: pointer.To[int32](3),
-							HealthyThreshold:   pointer.To[int32](1),
-							Tcp: &api.TcpHealthCheck{
-								Disabled: pointer.To(false),
-								Send:     pointer.To("cGluZwo="),
-								Receive:  &[]string{"cG9uZwo="},
+				ResourceRules: map[kri.Identifier]outbound.ResourceRule{
+					tcpMeshServiceIdentifier: {
+						Conf: []any{
+							api.Conf{
+								Interval:           test.ParseDuration("10s"),
+								Timeout:            test.ParseDuration("2s"),
+								UnhealthyThreshold: pointer.To[int32](3),
+								HealthyThreshold:   pointer.To[int32](1),
+								Tcp: &api.TcpHealthCheck{
+									Disabled: pointer.To(false),
+									Send:     pointer.To("cGluZwo="),
+									Receive:  &[]string{"cG9uZwo="},
+								},
 							},
 						},
 					},
@@ -221,17 +231,18 @@ var _ = Describe("MeshHealthCheck", func() {
 		Entry("gRPC HealthCheck", testCase{
 			resources: grpcCluster,
 			toRules: core_rules.ToRules{
-				Rules: []*core_rules.Rule{
-					{
-						Subset: subsetutils.Subset{},
-						Conf: api.Conf{
-							Interval:           test.ParseDuration("10s"),
-							Timeout:            test.ParseDuration("2s"),
-							UnhealthyThreshold: pointer.To[int32](3),
-							HealthyThreshold:   pointer.To[int32](1),
-							Grpc: &api.GrpcHealthCheck{
-								ServiceName: pointer.To("grpc-client"),
-								Authority:   pointer.To("grpc-client.default.svc.cluster.local"),
+				ResourceRules: map[kri.Identifier]outbound.ResourceRule{
+					grpcMeshServiceIdentifier: {
+						Conf: []any{
+							api.Conf{
+								Interval:           test.ParseDuration("10s"),
+								Timeout:            test.ParseDuration("2s"),
+								UnhealthyThreshold: pointer.To[int32](3),
+								HealthyThreshold:   pointer.To[int32](1),
+								Grpc: &api.GrpcHealthCheck{
+									ServiceName: pointer.To("grpc-client"),
+									Authority:   pointer.To("grpc-client.default.svc.cluster.local"),
+								},
 							},
 						},
 					},
@@ -240,7 +251,7 @@ var _ = Describe("MeshHealthCheck", func() {
 			expectedClusters: []string{"basic_grpc_health_check_cluster.golden.yaml"},
 		}),
 		Entry("TCP HealthCheck to real MeshService", testCase{
-			resources: tcpCluster,
+			resources: tcpClusterBackend,
 			toRules: core_rules.ToRules{
 				ResourceRules: map[kri.Identifier]outbound.ResourceRule{
 					backendMeshServiceIdentifier: {
