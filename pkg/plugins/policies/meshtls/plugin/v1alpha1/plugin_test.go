c89954a212b926df2256fac6fd08acbf81d424b5
Lukasz Dziedziak <lukidzi@gmail.com>
2026-08-04 08:21:10 -0500

    test(meshtls): strict inbound ports with identity
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/plugins/policies/meshtls/plugin/v1alpha1/plugin_test.go
+++ pkg/plugins/policies/meshtls/plugin/v1alpha1/plugin_test.go
@@ -276,6 +276,22 @@ var _ = Describe("MeshTLS", func() {
 				xds_types.FeatureStrictInboundPorts: true,
 			},
 		}),
+		Entry("strict inbound ports feature with workload identity = port filtering", testCase{
+			caseName:    "strict-with-workload-identity-strict-inbound-ports",
+			meshBuilder: samples.MeshMTLSBuilder(),
+			workloadIdentity: &core_xds.WorkloadIdentity{
+				KRI: kri.Identifier{ResourceType: meshidentity_api.MeshIdentityType, Mesh: "default", Zone: "default", Name: "my-identity"},
+				IdentitySourceConfigurer: func() bldrs_common.Configurer[envoy_tls.SdsSecretConfig] {
+					return bldrs_tls.SdsSecretConfigSource(
+						"my-secret-name",
+						bldrs_core.NewConfigSource().Configure(bldrs_core.Sds()),
+					)
+				},
+			},
+			features: xds_types.Features{
+				xds_types.FeatureStrictInboundPorts: true,
+			},
+		}),
 	)
 })
 
