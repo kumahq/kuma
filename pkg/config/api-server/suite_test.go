1072818db971bc3de55d3fdf8bbd1e44b42ce69c
Marcin Skalski <skalskimarcin33@gmail.com>
2026-09-04 19:40:24 +0200

    fix(api-server): retain config validation errors
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

---
+++ pkg/config/api-server/suite_test.go
@@ -0,0 +1,11 @@
+package api_server_test
+
+import (
+	"testing"
+
+	"github.com/kumahq/kuma/v3/pkg/test"
+)
+
+func TestApiServerConfig(t *testing.T) {
+	test.RunSpecs(t, "API Server Config Suite")
+}
