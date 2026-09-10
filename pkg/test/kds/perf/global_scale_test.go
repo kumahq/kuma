b0e2763c25c5419e333c99fd1f314483d744ba19
Marcin Skalski <skalskimarcin33@gmail.com>
2026-09-10 08:16:32 +0200

    test(kds): port perf harness to zone API structs
    Zone moved out of the system proto package into its own API package with
    plain Go structs, so the harness seeds it through zone_api.Zone and a
    bool pointer instead of the proto wrapper.
    
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- pkg/test/kds/perf/global_scale_test.go
+++ pkg/test/kds/perf/global_scale_test.go
@@ -17,16 +17,13 @@ import (
 	"testing"
 	"time"
 
-	"google.golang.org/protobuf/types/known/wrapperspb"
-
 	mesh_proto "github.com/kumahq/kuma/v3/api/mesh/v1alpha1"
-	system_proto "github.com/kumahq/kuma/v3/api/system/v1alpha1"
 	kuma_cp "github.com/kumahq/kuma/v3/pkg/config/app/kuma-cp"
 	config_core "github.com/kumahq/kuma/v3/pkg/config/core"
 	config_types "github.com/kumahq/kuma/v3/pkg/config/types"
 	"github.com/kumahq/kuma/v3/pkg/core"
 	core_mesh "github.com/kumahq/kuma/v3/pkg/core/resources/apis/mesh"
-	"github.com/kumahq/kuma/v3/pkg/core/resources/apis/system"
+	zone_api "github.com/kumahq/kuma/v3/pkg/core/resources/apis/zone/api/v1alpha1"
 	"github.com/kumahq/kuma/v3/pkg/core/resources/manager"
 	core_model "github.com/kumahq/kuma/v3/pkg/core/resources/model"
 	"github.com/kumahq/kuma/v3/pkg/core/resources/registry"
@@ -39,6 +36,7 @@ import (
 	"github.com/kumahq/kuma/v3/pkg/plugins/resources/memory"
 	test_grpc "github.com/kumahq/kuma/v3/pkg/test/grpc"
 	"github.com/kumahq/kuma/v3/pkg/test/kds/setup"
+	"github.com/kumahq/kuma/v3/pkg/util/pointer"
 )
 
 // countingStore counts store operations so a run can be expressed in store
@@ -117,8 +115,8 @@ func seed(t *testing.T, st store.ResourceStore, meshes, zones, policiesPerType i
 	t.Helper()
 	ctx := context.Background()
 	for z := range zones {
-		zone := system.NewZoneResource()
-		zone.Spec = &system_proto.Zone{Enabled: wrapperspb.Bool(true)}
+		zone := zone_api.NewZoneResource()
+		zone.Spec = &zone_api.Zone{Enabled: pointer.To(true)}
 		if err := st.Create(ctx, zone, store.CreateByKey(zoneName(z), core_model.NoMesh)); err != nil {
 			t.Fatalf("seed zone: %v", err)
 		}
@@ -133,7 +131,7 @@ func seed(t *testing.T, st store.ResourceStore, meshes, zones, policiesPerType i
 
 	created := 0
 	for _, typ := range types {
-		if typ == core_mesh.MeshType || typ == system.ZoneType {
+		if typ == core_mesh.MeshType || typ == zone_api.ZoneType {
 			continue
 		}
 		desc, err := registry.Global().DescriptorFor(typ)
