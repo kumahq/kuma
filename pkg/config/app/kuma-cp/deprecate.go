a6cc1057cd3b891cda1581d291c3ff61021c3ff2
Marcin Skalski <skalskimarcin33@gmail.com>
2026-09-15 13:49:45 +0200

    docs(upgrade): store cache disable removal
    Warn users that the resource store cache is always enabled in 3.0
    and that KUMA_STORE_CACHE_ENABLED is ignored, and print a runtime
    deprecation notice when the env var is still set.
    
    Refs 5584
    
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- pkg/config/app/kuma-cp/deprecate.go
+++ pkg/config/app/kuma-cp/deprecate.go
@@ -6,7 +6,12 @@ import (
 	"github.com/kumahq/kuma/v3/pkg/config"
 )
 
-var deprecations = []config.Deprecation{}
+var deprecations = []config.Deprecation{
+	{
+		Env:    "KUMA_STORE_CACHE_ENABLED",
+		EnvMsg: "the resource store cache can no longer be disabled, the setting is ignored and will be removed. Only the expiration time (KUMA_STORE_CACHE_EXPIRATION_TIME) is configurable.",
+	},
+}
 
 func PrintDeprecations(cfg *Config, out io.Writer) {
 	config.PrintDeprecations(deprecations, cfg, out)
