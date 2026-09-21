8eea276c0e58d0f52785c885fc00a56544964b16
Krzysztof Słonka <slonka@users.noreply.github.com>
2026-09-21 11:51:45 +0200

    fix(tokens): correct admin token extraction hint (#18828)
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- pkg/plugins/authn/api-server/tokens/admin_token_bootstrap.go
+++ pkg/plugins/authn/api-server/tokens/admin_token_bootstrap.go
@@ -49,7 +49,12 @@ func (a *adminTokenBootstrap) Start(stop <-chan struct{}) error {
 			if a.cpCfg.Environment == config_core.KubernetesEnvironment {
 				msg += fmt.Sprintf("To extract credentials execute 'kubectl get secret %s -n %s --template={{.data.value}} | base64 -d'. ", globalSecretKey.Name, a.cpCfg.Store.Kubernetes.SystemNamespace)
 			} else {
-				msg += fmt.Sprintf("To extract admin credentials execute 'curl http://localhost:%d/global-secrets/%s | jq -r .data | base64 -d'. ", a.cpCfg.ApiServer.HTTP.Port, globalSecretKey.Name)
+				prefix := "To extract admin credentials"
+				if !a.cpCfg.ApiServer.Authn.LocalhostIsAdmin {
+					// the request is rejected unless it comes from an admin
+					prefix += " temporarily set KUMA_API_SERVER_AUTHN_LOCALHOST_IS_ADMIN to true and"
+				}
+				msg += fmt.Sprintf("%s execute 'curl http://localhost:%d/global-secrets/%s | jq -r .data | base64 -d'. ", prefix, a.cpCfg.ApiServer.HTTP.Port, globalSecretKey.Name)
 			}
 			msg += "You configure kumactl with them 'kumactl config control-planes add --auth-type=tokens --auth-conf token=YOUR_TOKEN'." +
 				" To disable bootstrap of Admin User Token set KUMA_API_SERVER_AUTHN_TOKENS_BOOTSTRAP_ADMIN_TOKEN to false."
